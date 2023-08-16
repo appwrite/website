3366bf9d166d185fcb59279c56e2dcab67e3dd94
Elad Shechter <elad2412@gmail.com>
2023-08-16 14:57:36 -0500

    Homepage - Plans box - qa style fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -665,16 +665,16 @@
 		</svg>
 
 		<div class="aw-container u-position-relative">
-			<div class="grid-1-1 u-gap-32">
+			<div class="aw-grid-1-1 u-gap-32 aw-u-row-gap-80">
 				<section class="aw-hero u-flex aw-u-row-gap-32 u-main-center u-cross-center">
-					<h2 class="aw-display u-max-width-600">Start building with Appwrite today</h2>
+					<h2 class="aw-display u-max-width-600 aw-u-text-align-center">Start building with Appwrite today</h2>
 					<button class="aw-button is-transparent aw-u-cross-child-center">
 						<span class="text">Get Started</span>
 					</button>
 				</section>
-				<section class="aw-card is-transparent">
+				<section class="aw-card is-transparent aw-u-max-inline-width-584-mobile aw-u-margin-inline-auto-mobile aw-u-inline-width-100-percent-mobile">
 					<header class="aw-strip-plans-header">
-						<div class="aw-strip-plans-header-wrapper">
+						<div class="aw-strip-plans-header-wrapper aw-u-row-gap-24">
 							<h3 class="aw-title">Our Plans</h3>
 							<ul class="aw-secondary-tabs">
 								<li class="aw-secondary-tabs-item">
@@ -694,40 +694,40 @@
 						<li class="aw-strip-plans-item aw-strip-plans-container-query">
 							<div class="aw-strip-plans-item-wrapper">
 								<div class="aw-strip-plans-plan">
-									<h4 class="title">Free</h4>
-									<div class="price">$0</div>
-									<div class="info" />
+									<h4 class="title aw-description">Free</h4>
+									<div class="aw-title aw-u-color-text-primary">$0</div>
+									<div class="info aw-caption-500" />
 								</div>
-								<p class="aw-strip-plans-info">
-									For personal, passion projects and non-commercial use.
+								<p class="aw-strip-plans-info aw-caption-500">
+                                    For personal, passion projects and non-commercial use.
 								</p>
-								<button class="aw-button"><span class="text">Get Started</span></button>
+								<button class="aw-button is-full-width-mobile aw-u-cross-child-end"><span class="text">Get Started</span></button>
 							</div>
 						</li>
 						<li class="aw-strip-plans-item aw-strip-plans-container-query">
 							<div class="aw-strip-plans-item-wrapper">
 								<div class="aw-strip-plans-plan">
-									<h4 class="title">Starter</h4>
-									<div class="price">$10</div>
-									<div class="info">per user/month</div>
+									<h4 class="title aw-description">Pro</h4>
+									<div class="aw-title aw-u-color-text-primary">$15</div>
+									<div class="info aw-caption-500">per user/month</div>
 								</div>
-								<p class="aw-strip-plans-info">
-									For small projects and teams that need more flexibility.
+								<p class="aw-strip-plans-info aw-caption-500">
+                                    For pro developers and production projects that need the ability to scale.
 								</p>
-								<button class="aw-button is-secondary"><span class="text">Learn More</span></button>
+								<button class="aw-button is-full-width-mobile is-secondary aw-u-cross-child-end"><span class="text">Learn More</span></button>
 							</div>
 						</li>
 						<li class="aw-strip-plans-item aw-strip-plans-container-query">
 							<div class="aw-strip-plans-item-wrapper">
 								<div class="aw-strip-plans-plan">
-									<h4 class="title">Pro</h4>
-									<div class="price">$20</div>
-									<div class="info">per user/month</div>
+									<h4 class="title aw-description">Scale</h4>
+									<div class="aw-title aw-u-color-text-primary">$685</div>
+									<div class="info aw-caption-500">per org/month</div>
 								</div>
-								<p class="aw-strip-plans-info">
-									Pro teams and production projects that need the ability to scale.
+								<p class="aw-strip-plans-info aw-caption-500">
+                                    For pro developers and production projects that need the ability to scale.
 								</p>
-								<button class="aw-button is-secondary"><span class="text">Learn More</span></button>
+								<button class="aw-button is-full-width-mobile is-secondary aw-u-cross-child-end"><span class="text">Learn More</span></button>
 							</div>
 						</li>
 					</ul>
