9c8c4cb67fe51b196b221cedac09b4cd997ff4b7
Elad Shechter <elad2412@gmail.com>
2023-08-17 14:19:50 -0500

    Pricing page - work in progress

---
+++ src/routes/pricing/+page.svelte
@@ -0,0 +1,125 @@
+<script lang="ts">
+    import { Homepage } from '$lib/layouts';
+</script>
+
+<div class="u-position-absolute" style="margin-block-start:-36%;pointer-events:none;">
+    <svg
+            style="width:1466.685px; height:804.009px; transform:rotate(150.348deg); fill: var(--appwrite-pink, #FD366E); opacity: 0.6499999761581421; filter: blur(127.51104736328125px);"
+            xmlns="http://www.w3.org/2000/svg"
+            width="1727"
+            height="520"
+            viewBox="0 0 1727 520"
+            fill="none"
+    >
+        <g opacity="0.65" filter="url(#filter0_f_278_15509)">
+            <path
+                    d="M887.773 -394.667L-52.2142 167.684L842.622 381.131L1616.37 124.584L887.773 -394.667Z"
+                    fill="#FD366E"
+            />
+        </g>
+        <defs>
+            <filter
+                    id="filter0_f_278_15509"
+                    x="-307.237"
+                    y="-649.69"
+                    width="2178.63"
+                    height="1285.84"
+                    filterUnits="userSpaceOnUse"
+                    color-interpolation-filters="sRGB"
+            >
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                <feGaussianBlur stdDeviation="127.511" result="effect1_foregroundBlur_278_15509" />
+            </filter>
+        </defs>
+    </svg>
+</div>
+
+<Homepage>
+    <div class="theme-dark">
+        <section class="aw-container">
+
+            <div class="aw-hero aw-u-margin-block-120w-80s">
+                <h1 class="aw-display aw-u-max-width-700 aw-u-cross-child-center aw-u-opacity-90">Simple, transparent pricing with no surprises</h1>
+            </div>
+
+            <div class="aw-pricing-cards">
+                <ul class="aw-pricing-cards-list">
+                    <li>
+                        <article class="aw-card is-transparent">
+                            <div class="aw-pricing-cards-item">
+                                <header class="aw-pricing-cards-header">
+                                    <h2 class="aw-label">Free</h2>
+                                    <div class="aw-title aw-u-color-text-primary u-margin-block-start-8">$0</div>
+                                    <div class="u-margin-block-start-4">per user/month</div>
+                                    <p class="aw-main-body-500 u-margin-block-start-16">For personal passion projects and non-commercial use</p>
+                                    <button class="aw-button is-secondary is-full-width u-margin-block-start-32">
+                                        <span class="aw-sub-body-500">Start building</span>
+                                    </button>
+                                </header>
+                                <div class="aw-pricing-cards-content">
+                                    <p>Everything in Free plus:</p>
+                                    <ul class="aw-checked-list-circle">
+                                        <li class="">
+
+                                            <span class="">300GB bandwidth</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </article>
+                    </li>
+                    <li>
+                        <article class="aw-card is-transparent">
+                            <div class="aw-pricing-cards-item">
+                                <header class="aw-pricing-cards-header">
+                                    <h2 class="aw-label">Free</h2>
+                                    <div class="aw-title aw-u-color-text-primary u-margin-block-start-8">$0</div>
+                                    <div class="u-margin-block-start-4">per user/month</div>
+                                    <p class="aw-main-body-500 u-margin-block-start-16">For personal passion projects and non-commercial use</p>
+                                    <button class="aw-button is-secondary is-full-width u-margin-block-start-32">
+                                        <span class="aw-sub-body-500">Start building</span>
+                                    </button>
+                                </header>
+                                <div class="aw-pricing-cards-content">
+                                    <p>Everything in Free plus:</p>
+                                    <ul class="aw-checked-list-circle">
+                                        <li class="">
+
+                                            <span class="">300GB bandwidth</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </article>
+                    </li>
+                    <li>
+                        <article class="aw-card is-transparent">
+                            <div class="aw-pricing-cards-item">
+                                <header class="aw-pricing-cards-header">
+                                    <h2 class="aw-label">Free</h2>
+                                    <div class="aw-title aw-u-color-text-primary u-margin-block-start-8">$0</div>
+                                    <div class="u-margin-block-start-4">per user/month</div>
+                                    <p class="aw-main-body-500 u-margin-block-start-16">For personal passion projects and non-commercial use</p>
+                                    <button class="aw-button is-secondary is-full-width u-margin-block-start-32">
+                                        <span class="aw-sub-body-500">Start building</span>
+                                    </button>
+                                </header>
+                                <div class="aw-pricing-cards-content">
+                                    <p>Everything in Free plus:</p>
+                                    <ul class="aw-checked-list-circle">
+                                        <li class="">
+
+                                            <span class="">300GB bandwidth</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </article>
+                    </li>
+                </ul>
+            </div>
+
+        </section>
+    </div>
+</Homepage>
