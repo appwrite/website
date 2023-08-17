72621f00b4eacdc6638d0e8d57262a1a4648d924
Elad Shechter <elad2412@gmail.com>
2023-08-17 14:58:27 -0500

    Pricing page - work in progress

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -46,7 +46,7 @@
             <div class="aw-pricing-cards">
                 <ul class="aw-pricing-cards-list">
                     <li>
-                        <article class="aw-card is-transparent">
+                        <article class="aw-card is-transparent u-height-100-percent">
                             <div class="aw-pricing-cards-item">
                                 <header class="aw-pricing-cards-header">
                                     <h2 class="aw-label">Free</h2>
@@ -58,60 +58,69 @@
                                     </button>
                                 </header>
                                 <div class="aw-pricing-cards-content">
-                                    <p>Everything in Free plus:</p>
                                     <ul class="aw-checked-list-circle">
-                                        <li class="">
-
-                                            <span class="">300GB bandwidth</span>
-                                        </li>
+                                        <li class=""><span class="">100GB bandwidth</span></li>
+                                        <li class=""><span class="">Unlimited projects</span></li>
+                                        <li class=""><span class="">Never paused</span></li>
+                                        <li class=""><span class="">750K executions</span></li>
+                                        <li class=""><span class="">50MB uploads</span></li>
+                                        <li class=""><span class="">1 Database, 3 Buckets, 5 Functions</span></li>
+                                        <li class=""><span class="">Community support</span></li>
+                                        <li class=""><span class="">Non-removable Appwrite branding</span></li>
+                                        <li class=""><span class="">No Add-ons</span></li>
                                     </ul>
                                 </div>
                             </div>
                         </article>
                     </li>
                     <li>
-                        <article class="aw-card is-transparent">
+                        <article class="aw-card is-transparent is-transparent-pink u-height-100-percent">
                             <div class="aw-pricing-cards-item">
                                 <header class="aw-pricing-cards-header">
-                                    <h2 class="aw-label">Free</h2>
+                                    <h2 class="aw-label">Pro</h2>
                                     <div class="aw-title aw-u-color-text-primary u-margin-block-start-8">$0</div>
                                     <div class="u-margin-block-start-4">per user/month</div>
-                                    <p class="aw-main-body-500 u-margin-block-start-16">For personal passion projects and non-commercial use</p>
-                                    <button class="aw-button is-secondary is-full-width u-margin-block-start-32">
-                                        <span class="aw-sub-body-500">Start building</span>
+                                    <p class="aw-main-body-500 u-margin-block-start-16">For pro teams and production projects that need to scale</p>
+                                    <button class="aw-button is-full-width u-margin-block-start-32">
+                                        <span class="aw-sub-body-500">Get Pro</span>
                                     </button>
                                 </header>
                                 <div class="aw-pricing-cards-content">
                                     <p>Everything in Free plus:</p>
                                     <ul class="aw-checked-list-circle">
-                                        <li class="">
-
-                                            <span class="">300GB bandwidth</span>
-                                        </li>
+                                        <li class=""><span class="">300GB bandwidth</span></li>
+                                        <li class=""><span class="">Unlimited projects</span></li>
+                                        <li class=""><span class="">Never paused</span></li>
+                                        <li class=""><span class="">150K Monthly active users</span></li>
+                                        <li class=""><span class="">150GB Storage</span></li>
+                                        <li class=""><span class="">3M executions</span></li>
+                                        <li class=""><span class="">Unlimited Databases, Buckets and Functions</span></li>
+                                        <li class=""><span class="">Email support</span></li>
+                                        <li class=""><span class="">Removable Appwrite branding</span></li>
+                                        <li class=""><span class="">Add-ons</span></li>
                                     </ul>
                                 </div>
                             </div>
                         </article>
                     </li>
                     <li>
-                        <article class="aw-card is-transparent">
+                        <article class="aw-card is-transparent u-height-100-percent">
                             <div class="aw-pricing-cards-item">
                                 <header class="aw-pricing-cards-header">
-                                    <h2 class="aw-label">Free</h2>
-                                    <div class="aw-title aw-u-color-text-primary u-margin-block-start-8">$0</div>
-                                    <div class="u-margin-block-start-4">per user/month</div>
-                                    <p class="aw-main-body-500 u-margin-block-start-16">For personal passion projects and non-commercial use</p>
+                                    <h2 class="aw-label">Scale</h2>
+                                    <div class="aw-title aw-u-color-text-primary u-margin-block-start-8">$685</div>
+                                    <div class="u-margin-block-start-4">per organiztion/month</div>
+                                    <p class="aw-main-body-500 u-margin-block-start-16">For scaling teams and agencies that need dedicated support.</p>
                                     <button class="aw-button is-secondary is-full-width u-margin-block-start-32">
-                                        <span class="aw-sub-body-500">Start building</span>
+                                        <span class="aw-sub-body-500">SGet Scale</span>
                                     </button>
                                 </header>
                                 <div class="aw-pricing-cards-content">
-                                    <p>Everything in Free plus:</p>
+                                    <p>Everything in Free & Pro, plus:</p>
                                     <ul class="aw-checked-list-circle">
-                                        <li class="">
-
-                                            <span class="">300GB bandwidth</span>
-                                        </li>
+                                        <li class=""><span class="">Extra support log retention days</span></li>
+                                        <li class=""><span class="">Additional org member roles</span></li>
+                                        <li class=""><span class="">SSO</span></li>
                                     </ul>
                                 </div>
                             </div>
