37927002bf02a7af6b6dd07186046c3a979a8770
Elad Shechter <elad2412@gmail.com>
2023-08-24 11:39:51 -0500

    Input - finish first iteration of input & textarea, include light/dark mode
    - Demo page - /kitchensink/form-dark

--- src/routes/kitchensink/form-dark/+page.svelte
+++ src/routes/kitchensink/form-dark/+page.svelte
@@ -1,7 +1,9 @@
 <script type="module" src="/src/main.ts"></script>
 
 
-<div class="u-margin-inline-auto aw-u-max-width-700">
+
+
+<div class="u-margin-inline-auto aw-u-max-width-700 u-padding-block-16">
     <input type="text" class="aw-input-text" placeholder="placeholder">
     <br><br>
     <input type="text" class="aw-input-text" placeholder="placeholder" disabled>
@@ -18,3 +20,22 @@
 </div>
 
 
+
+
+<div class="theme-light u-padding-block-16" style="background-color:hsl(var(--aw-color-neutral-50));">
+    <div class="u-margin-inline-auto aw-u-max-width-700">
+        <input type="text" class="aw-input-text" placeholder="placeholder">
+        <br><br>
+        <input type="text" class="aw-input-text" placeholder="placeholder" disabled>
+        <br><br>
+        <input type="text" class="aw-input-text is-error" placeholder="placeholder">
+
+        <br><br><br><br>
+
+        <textarea class="aw-input-text" placeholder="placeholder"></textarea>
+        <br><br>
+        <textarea class="aw-input-text" placeholder="placeholder" disabled></textarea>
+        <br><br>
+        <textarea class="aw-input-text is-error" placeholder="placeholder"></textarea>
+    </div>
+</div>
