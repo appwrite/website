bb1d5e01d8af32b1f50747ce1721307cb23edd82
Elad Shechter <elad2412@gmail.com>
2023-08-24 08:42:21 -0500

    Input - work in progress

---
+++ src/routes/kitchensink/form-dark/+page.svelte
@@ -0,0 +1,20 @@
+<script type="module" src="/src/main.ts"></script>
+
+
+<div class="u-margin-inline-auto aw-u-max-width-700">
+    <input type="text" class="aw-input-text" placeholder="placeholder">
+    <br><br>
+    <input type="text" class="aw-input-text" placeholder="placeholder" disabled>
+    <br><br>
+    <input type="text" class="aw-input-text is-error" placeholder="placeholder">
+
+    <br><br><br><br>
+
+    <textarea class="aw-input-text" placeholder="placeholder"></textarea>
+    <br><br>
+    <textarea class="aw-input-text" placeholder="placeholder" disabled></textarea>
+    <br><br>
+    <textarea class="aw-input-text is-error" placeholder="placeholder"></textarea>
+</div>
+
+
