b8b46efcda23c57a6570ece1f13687a79f7c0891
Elad Shechter <elad2412@gmail.com>
2023-08-24 11:54:24 -0500

    Input - Add label for demos - fox color style

--- src/routes/kitchensink/form-dark/+page.svelte
+++ src/routes/kitchensink/form-dark/+page.svelte
@@ -4,7 +4,11 @@
 
 
 <div class="u-margin-inline-auto aw-u-max-width-700 u-padding-block-16">
-    <input type="text" class="aw-input-text" placeholder="placeholder">
+
+    <div class="u-flex-vertical u-gap-4">
+        <label class="aw-caption-400" for="input-1">Label</label>
+        <input type="text" class="aw-input-text" placeholder="placeholder" id="input-1">
+    </div>
     <br><br>
     <input type="text" class="aw-input-text" placeholder="placeholder" disabled>
     <br><br>
@@ -12,7 +16,10 @@
 
     <br><br><br><br>
 
-    <textarea class="aw-input-text" placeholder="placeholder"></textarea>
+    <div class="u-flex-vertical u-gap-4">
+        <label class="aw-caption-400" for="textarea-1">Label</label>
+        <textarea class="aw-input-text" placeholder="placeholder" id="textarea-1"></textarea>
+    </div>
     <br><br>
     <textarea class="aw-input-text" placeholder="placeholder" disabled></textarea>
     <br><br>
@@ -24,7 +31,11 @@
 
 <div class="theme-light u-padding-block-16" style="background-color:hsl(var(--aw-color-neutral-50));">
     <div class="u-margin-inline-auto aw-u-max-width-700">
-        <input type="text" class="aw-input-text" placeholder="placeholder">
+
+        <div class="u-flex-vertical u-gap-4">
+            <label class="aw-caption-400" for="input-11">Label</label>
+            <input type="text" class="aw-input-text" placeholder="placeholder" id="input-11">
+        </div>
         <br><br>
         <input type="text" class="aw-input-text" placeholder="placeholder" disabled>
         <br><br>
@@ -32,7 +43,10 @@
 
         <br><br><br><br>
 
-        <textarea class="aw-input-text" placeholder="placeholder"></textarea>
+        <div class="u-flex-vertical u-gap-4">
+            <label class="aw-caption-400" for="textarea-11">Label</label>
+            <textarea class="aw-input-text" placeholder="placeholder" id="textarea-11"></textarea>
+        </div>
         <br><br>
         <textarea class="aw-input-text" placeholder="placeholder" disabled></textarea>
         <br><br>
