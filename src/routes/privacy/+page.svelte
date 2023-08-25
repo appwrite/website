aa374b8b58e4886299282dca38bdfef524fc7117
Elad Shechter <elad2412@gmail.com>
2023-08-25 10:50:21 -0500

    Privacy - AStart creating basic skeleton

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -3,5 +3,22 @@
 </script>
 
 <Homepage>
-    Privacy
+    <div class="aw-container">
+        <div class="aw-grid-120-1fr">
+            <aside>
+                [SIDE-CONTENT]
+            </aside>
+            <main>
+                <h1 class="aw-title">Privacy Policy</h1>
+                <p>
+                    This privacy policy (“Privacy Policy”) governs how we, Appwrite Code Ltd. (together, “Appwrite” “we”, “our” or “us”) use, collect and store Personal Data we collect or receive from or about you (“you”) such as in the following use cases:
+                </p>
+                <ol class="aw-order-list-level-1">
+                    <li>
+                        When you browse or visit our website, <a href="/" class="aw-link">https://appwrite.io/</a> (“Website”)
+                    </li>
+                </ol>
+            </main>
+        </div>
+    </div>
 </Homepage>
