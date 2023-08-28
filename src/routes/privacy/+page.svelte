e0fdada61cc0653fb7e0fb98f1fa4c557f7d04aa
Elad Shechter <elad2412@gmail.com>
2023-08-28 18:06:24 -0500

    Privacy Page - Work in progress

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -12,15 +12,98 @@
                 [SIDE-CONTENT]
             </aside>
             <main class="aw-grid-120-1fr-main">
+                <div class="aw-content is-count-headers">
+                    <section>
+                        <div>
+                            <p>
+                                This privacy policy (“Privacy Policy”) governs how we, Appwrite Code Ltd. (together, “Appwrite” “we”, “our” or “us”) use, collect and store Personal Data we collect or receive from or about you (“you”) such as in the following use cases:
+                            </p>
+                        <ol class="aw-order-list-level-1">
+                            <li>
+                                When you browse or visit our website, <a href="/" class="aw-link">https://appwrite.io/</a> (“Website”)
+                            </li>
+                            <li>
+                                When you make use of, or interact with, our Website
+                                <ol class="aw-order-list-level-2">
+                                    <li>When you create an account and when you log in</li>
+                                    <li>When you purchase our products in our Swag Store</li>
+                                    <li>When you subscribe to our distribution list(s) / newsletter(s) / blog(s)</li>
+                                    <li>When you submit a job application</li>
+                                    <li>When you contact us (e.g. customer support, need help, submit a request, when you report a security vulnerability)</li>
+                                    <li>When you apply for our fund</li>
+                                </ol>
+                            </li>
+                            <li>
+                                When you make use of, or interact with, our  application, and any other application, that we license (each individually, and collectively, the “App”).  
+                                <ol class="aw-order-list-level-2">
+                                    <li>When you create an account and when you log in </li>
+                                </ol>
+                            </li>
+                            <li>When you attend a marketing event and/or we exchange business cards and you provide us with your Personal Data </li>
+                            <li>When we use the Personal Data of our customers (e.g. contact details) </li>
+                            <li>When you interact with us on our social media profiles (e.g., Facebook, GitHub, Twitter, LinkedIn).</li>
+                        </ol>
+                        </div>
 
-                <p>
-                    This privacy policy (“Privacy Policy”) governs how we, Appwrite Code Ltd. (together, “Appwrite” “we”, “our” or “us”) use, collect and store Personal Data we collect or receive from or about you (“you”) such as in the following use cases:
-                </p>
-                <ol class="aw-order-list-level-1">
-                    <li>
-                        When you browse or visit our website, <a href="/" class="aw-link">https://appwrite.io/</a> (“Website”)
-                    </li>
-                </ol>
+                        <p>Hereinafter the Website and the App will be referred to as the “Service”.
+                            Please read this Privacy Policy carefully, so you can understand our practices and your rights in relation to personal data. “Personal Data” or “Personal Information” means any information that can be used, alone or together with other data, to uniquely identify any living human being and any information deemed as Personally Identifiable Information by privacy laws. Please note that this is a master privacy policy and some of its provisions only apply to individuals in certain jurisdictions. For example, the legal basis in the table below is only relevant to GDPR-protected individuals. Important note: Nothing in this Privacy Policy is intended to limit in any way your statutory right, including your rights to a remedy or means of enforcement. </p>
+                        <p>This Privacy Policy can be updated from time to time and, therefore, we ask you to check back periodically for the latest version of this Privacy Policy.  If we implement significant changes to the use of your Personal Data in a manner different from that stated at the time of collection, we will notify you by posting a notice on our Website or by other means.</p>
+                    </section>
+                    <section>
+                        <h2 class="aw-title">What personal data we collect, why we collect it, and how it is used</h2>
+                        <p>When you browse or visit our website</p>
+                        <div class="u-overflow-x">
+                            <div class="aw-info-table-wrapper">
+                                <table class="aw-info-table">
+                                <tbody class="aw-info-table-tbody">
+                                    <tr class="aw-info-table-col-row">
+                                        <th class="aw-info-table-col-header">Specific personal data we collect</th>
+                                        <td class="aw-info-table-col">Cookies, analytic tools and log files</td>
+                                    </tr>
+                                    <tr class="aw-info-table-col-row">
+                                        <th class="aw-info-table-col-header">What is the personal data collected and for what purpose?</th>
+                                        <td class="aw-info-table-col">Marketing purposes, usage analytics and statistics, improving quality of service</td>
+                                    </tr>
+                                    <tr class="aw-info-table-col-row">
+                                        <th class="aw-info-table-col-header">Legal basis (GDPR only, if applicable)</th>
+                                        <td class="aw-info-table-col">
+                                            <ul>
+                                                <li>Consent</li>
+                                                <li>Legitimate interest (e.g. essential cookies)</li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    <tr class="aw-info-table-col-row">
+                                        <th class="aw-info-table-col-header">Third parties with whom we share your personal data</th>
+                                        <td class="aw-info-table-col">
+                                            For more information, please read our cookies policy <a href="https://appwrite.io/policy/cookies" target="_blank">https://appwrite.io/policy/cookies</a>
+
+                                        </td>
+                                    </tr>
+                                    <tr class="aw-info-table-col-row">
+                                        <th class="aw-info-table-col-header">Consequences of not providing the personal data</th>
+                                        <td class="aw-info-table-col">Certain non-essential Website features may not be available </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            </div>
+                        </div>
+
+
+
+                        <div class="u-overflow-x">
+                            <table class="aw-info-table">
+                                <tbody class="aw-info-table-tbody">
+                                <tr class="aw-info-table-col-row">
+                                    <th class="aw-info-table-col-header"></th>
+                                    <td class="aw-info-table-col"></td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </section>
+                </div>
+
             </main>
         </div>
     </div>
