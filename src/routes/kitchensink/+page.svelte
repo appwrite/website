f0fdd9a1542f91ad5f07b293c0439af5af7b3e79
Elad Shechter <elad2412@gmail.com>
2023-09-01 08:33:23 -0500

    Table - Added table partial + demo

--- src/routes/kitchensink/+page.svelte
+++ src/routes/kitchensink/+page.svelte
@@ -1,38 +1,62 @@
 <script type="module" src="/src/main.ts"></script>
 
-<div class="u-position-absolute" style="margin-block-start:-36%;">
-	<svg
-		style="width:1466.685px; height:804.009px; transform:rotate(150.348deg); fill: var(--appwrite-pink, #FD366E); opacity: 0.6499999761581421; filter: blur(127.51104736328125px);"
-		xmlns="http://www.w3.org/2000/svg"
-		width="1727"
-		height="520"
-		viewBox="0 0 1727 520"
-		fill="none"
-	>
-		<g opacity="0.65" filter="url(#filter0_f_278_15509)">
-			<path
-				d="M887.773 -394.667L-52.2142 167.684L842.622 381.131L1616.37 124.584L887.773 -394.667Z"
-				fill="#FD366E"
-			/>
-		</g>
-		<defs>
-			<filter
-				id="filter0_f_278_15509"
-				x="-307.237"
-				y="-649.69"
-				width="2178.63"
-				height="1285.84"
-				filterUnits="userSpaceOnUse"
-				color-interpolation-filters="sRGB"
-			>
-				<feFlood flood-opacity="0" result="BackgroundImageFix" />
-				<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-				<feGaussianBlur stdDeviation="127.511" result="effect1_foregroundBlur_278_15509" />
-			</filter>
-		</defs>
-	</svg>
-</div>
+
 <div id="app" class="u-position-relative">
+	<div class="aw-container">
+
+		<div class="aw-table-wrapper">
+			<br><br><br>
+
+			<div class="aw-table-scroll">
+				<table class="aw-table">
+					<thead class="aw-table-header">
+						<tr class="aw-table-row">
+							<th class="aw-table-head-col">
+								<span class="aw-eyebrow">HEADER</span>
+							</th>
+							<th class="aw-table-head-col">
+								<span class="aw-eyebrow">HEADER</span>
+							</th>
+							<th class="aw-table-head-col">
+								<span class="aw-eyebrow">HEADER</span>
+							</th>
+							<th class="aw-table-head-col">
+								<span class="aw-eyebrow">HEADER</span>
+							</th>
+						</tr>
+					</thead>
+					<tbody class="aw-table-body">
+						<tr class="aw-table-row">
+							<td class="aw-table-col">placeholder text</td>
+							<td class="aw-table-col">placeholder text</td>
+							<td class="aw-table-col">placeholder text</td>
+							<td class="aw-table-col">placeholder text</td>
+						</tr>
+						<tr class="aw-table-row">
+							<td class="aw-table-col">placeholder text</td>
+							<td class="aw-table-col">placeholder text</td>
+							<td class="aw-table-col">placeholder text</td>
+							<td class="aw-table-col">placeholder text</td>
+						</tr>
+						<tr class="aw-table-row">
+							<td class="aw-table-col">placeholder text</td>
+							<td class="aw-table-col">placeholder text</td>
+							<td class="aw-table-col">placeholder text</td>
+							<td class="aw-table-col">placeholder text</td>
+						</tr>
+						<tr class="aw-table-row">
+							<td class="aw-table-col">placeholder text</td>
+							<td class="aw-table-col">placeholder text</td>
+							<td class="aw-table-col">placeholder text</td>
+							<td class="aw-table-col">placeholder text</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+
+			<br><br><br>
+		</div>
+	</div>
 	<div class="theme-dark aw-container">
 		<div>
 			<span class="aw-inline-code">inline-code</span>
