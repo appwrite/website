17055b05634bf84d43b113ca79f985eb8603b2a5
Elad Shechter <elad2412@gmail.com>
2023-09-04 11:50:51 -0500

    Docs Main Footer - replace with partial

--- src/routes/docs/tutorials/example/+page.svelte
+++ src/routes/docs/tutorials/example/+page.svelte
@@ -361,60 +361,9 @@
 			</aside>
 		</article>
 	</main>
-	<footer class="aw-main-footer u-margin-block-start-48 u-small">
-		<div class="aw-main-footer-grid-1">
-			<ul class="aw-main-footer-grid-1-column-1 u-flex u-gap-8">
-				<li>
-					<button class="aw-icon-button" aria-label="Appwrite on Discord">
-						<span class="icon-discord" aria-hidden="true" />
-					</button>
-				</li>
-				<li>
-					<button class="aw-icon-button" aria-label="Appwrite GitHub">
-						<span class="icon-github" aria-hidden="true" />
-					</button>
-				</li>
-				<li>
-					<button class="aw-icon-button" aria-label="Appwrite on Twitter">
-						<span class="icon-twitter" aria-hidden="true" />
-					</button>
-				</li>
-				<li>
-					<button class="aw-icon-button" aria-label="Appwrite on LinkedIn">
-						<span class="aw-icon-linkedin" aria-hidden="true" />
-					</button>
-				</li>
-				<li>
-					<button class="aw-icon-button" aria-label="Appwrite YouTube">
-						<span class="icon-youtube" aria-hidden="true" />
-					</button>
-				</li>
-			</ul>
-			<div class="aw-main-footer-grid-1-column-2">
-				<div class="aw-select is-colored">
-					<button class="physical-select">
-						<span class="icon-moon" aria-hidden="true" />
-						<span>Dark</span>
-					</button>
-					<span class="icon-cheveron-down" aria-hidden="true" />
-				</div>
-			</div>
-			<ul class="aw-main-footer-grid-1-column-3 aw-main-footer-links">
-				<li>
-					<a href=".">Support</a>
-				</li>
-				<li>
-					<a href=".">Status</a>
-				</li>
-				<li>
-					<a href=".">Changelog</a>
-				</li>
-			</ul>
-			<div class="aw-main-footer-grid-1-column-4 aw-main-footer-copyright">
-				Copyright © 2023 Appwrite
-			</div>
-		</div>
-	</footer>
+
+	<MainFooter variant="docs"/>
+
 </Docs>
 
 <!-- <script type="module" src="/src/main.ts"></script>
