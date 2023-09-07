2b7c3d5c8ee189da6bcc860e6ed729bab04f33ca
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-09-07 17:36:14 +0200

    feat: add more components

--- src/markdoc/nodes/Paragraph.svelte
+++ src/markdoc/nodes/Paragraph.svelte
@@ -1,1 +1,8 @@
-<p class={`aw-paragraph-md${$$props.class ? ` ${$$props.class}` : ''}`}><slot /></p>
+<script>
+	import { getContext, hasContext } from 'svelte';
+
+	const noParagraph = hasContext('no-paragraph') ? getContext('no-paragraph') : true;
+	const classes = (!noParagraph ? 'aw-paragraph-md' : '') + ($$props.class ? ` ${$$props.class}` : '');
+</script>
+
+<p class={classes}><slot /></p>
