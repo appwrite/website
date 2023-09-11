4d02a452eefac9b6d034afca0557851072d4db3d
tglide <26071571+TGlide@users.noreply.github.com>
2023-09-11 19:07:10 +0100

    homepage animation WIP

---
+++ src/lib/actions/rect.ts
@@ -0,0 +1,29 @@
+import type { Action } from 'svelte/action';
+import type { Writable } from 'svelte/store';
+
+type Args = Writable<DOMRect | null>;
+
+export const rect: Action<HTMLElement, Args> = (node, store) => {
+	let observer: ResizeObserver | null = null;
+	const update = (store: Args) => {
+		console.log('rect update');
+		observer?.disconnect();
+
+		store.set(node.getBoundingClientRect());
+
+		// Observe resize
+		observer = new ResizeObserver(() => {
+			store.set(node.getBoundingClientRect());
+		});
+		observer.observe(node);
+	};
+
+	update(store);
+
+	return {
+		update,
+		destroy() {
+			// no-op
+		}
+	};
+};
---
+++ src/lib/utils/test.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type UnknownFn = (...args: any[]) => any;
+
+export type TestCases<Fn extends UnknownFn> = {
+	args: Parameters<Fn>;
+	expected: ReturnType<Fn>;
+}[];
