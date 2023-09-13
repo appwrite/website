32ec0b7281b816d5d8beb888aa15abf8431b7341
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-09-13 17:13:34 +0200

    fix: build

--- src/markdoc/nodes/Fence.svelte
+++ src/markdoc/nodes/Fence.svelte
@@ -30,16 +30,16 @@
 	<section class="theme-dark aw-code-snippet" aria-label="code-snippet panel">
 		<header class="aw-code-snippet-header">
 			<div class="aw-code-snippet-header-start">
-				<div class="u-flex u-gap-16">
+				<!-- <div class="u-flex u-gap-16">
 					<div class="aw-tag"><span class="text">Default</span></div>
-				</div>
+				</div> -->
 			</div>
 			<div class="aw-code-snippet-header-end">
 				<ul class="buttons-list u-flex u-gap-8">
 					<li class="buttons-list-item aw-u-padding-inline-start-20">
-						<button class="aw-icon-button" aria-label="copy code from code-snippet"
-							><span class="icon-duplicate" aria-hidden="true" /></button
-						>
+						<button class="aw-icon-button" aria-label="copy code from code-snippet">
+							<span class="icon-duplicate" aria-hidden="true" />
+						</button>
 					</li>
 				</ul>
 			</div>
