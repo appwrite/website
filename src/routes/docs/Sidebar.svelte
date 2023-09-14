5ac0fdac0e5ddf6fe2d20da8b48b310cc0bf16ee
Vincent (Wen Yu) Ge <wenyu1999@hotmail.com>
2023-09-14 20:16:00 +0000

    Start storage docs. Missing permissions and images

--- src/routes/docs/Sidebar.svelte
+++ src/routes/docs/Sidebar.svelte
@@ -220,13 +220,29 @@
 				},
 				{
 					label: 'Storage',
-					href: '#',
+					href: '/docs/products/storage',
 					icon: 'icon-folder',
 					items: [
 						{
-							label: 'Inner item',
-							href: '#'
-						}
+							label: 'Quick start',
+							href: '/docs/products/storage/quick-start'
+						},
+						{
+							label: 'Buckets',
+							href: '/docs/products/storage/buckets'
+						},
+						{
+							label: 'Upload and download',
+							href: '/docs/products/storage/upload-download'
+						},
+						{
+							label: 'Images',
+							href: '/docs/products/storage/images'
+						},
+						{
+							label: 'Permissions',
+							href: '/docs/products/storage/permissions'
+						},
 					]
 				}
 			]
