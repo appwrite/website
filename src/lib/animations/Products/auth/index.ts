8fb4187af40688864cc1a0cd01be3924f8af87e8
tglide <26071571+TGlide@users.noreply.github.com>
2023-09-15 20:36:31 +0100

    working transition between sections

--- src/lib/animations/Products/auth/index.ts
+++ src/lib/animations/Products/auth/index.ts
@@ -10,9 +10,10 @@ export const Auth = {
 	Controls
 };
 
-import { sleep, write } from '$lib/animations';
+import { safeAnimate, sleep, write } from '$lib/animations';
 import { createResettable } from '$lib/utils/resettable';
 import { animate } from 'motion';
+import { getElSelector } from '../Products.svelte';
 
 type State = {
 	email: string;
@@ -46,57 +47,46 @@ const state = createResettable<State>({
 const emailToSet = 'walterobrian@example.com';
 const passwordToSet = 'password';
 
-type ExecuteArgs = {
-	elements: {
-		phone: HTMLElement | undefined;
-		box: HTMLElement | undefined;
-		code: HTMLElement | undefined;
-		controls: HTMLElement | undefined;
-	};
-};
+const execute = async () => {
+	const phone = getElSelector('phone');
+	const box = getElSelector('box');
+	const code = getElSelector('code');
+	const controls = getElSelector('controls');
 
-const execute = async ({ elements }: ExecuteArgs) => {
-	try {
-		const { phone, box, code, controls } = elements;
-		if (!phone || !box || !code || !controls) {
-			return;
-		}
+	// Reset
+	const { update } = state.reset();
+	safeAnimate(box, { x: 260, y: 32, opacity: 0 }, { duration: 0.25 });
+	safeAnimate(code, { x: 200, y: 460, opacity: 0 }, { duration: 0.25 });
+	safeAnimate(phone, { x: 0, y: 0 }, { duration: 0.25 });
+	await safeAnimate(controls, { x: 420, y: 0, opacity: 0 }, { duration: 0.25 })?.finished;
 
-		// Reset
-		const { update } = state.reset();
-		await animate(phone, { x: 0, y: 0 }, { duration: 0 }).finished;
-		await animate(box, { x: 260, y: 32, opacity: 0 }, { duration: 0 }).finished;
-		await animate(code, { x: 200, y: 460, opacity: 0 }, { duration: 0 }).finished;
-		await animate(controls, { x: 420, y: 0, opacity: 0 }, { duration: 0 }).finished;
+	// Start
+	await safeAnimate(box, { x: [260, 260], y: [48, 32], opacity: 1 }, { duration: 0.25, delay: 1 })
+		?.finished;
 
-		// Start
-		await animate(box, { x: [260, 260], y: [48, 32], opacity: 1 }, { duration: 0.25, delay: 1 })
-			.finished;
+	await sleep(150);
 
-		await sleep(150);
+	await write(emailToSet, (v) => update((p) => ({ ...p, email: v })), 300);
+	await sleep(150);
 
-		await write(emailToSet, (v) => update((p) => ({ ...p, email: v })), 300);
-		await sleep(150);
+	await write(passwordToSet, (v) => update((p) => ({ ...p, password: v })), 300);
+	await sleep(500);
 
-		await write(passwordToSet, (v) => update((p) => ({ ...p, password: v })), 300);
-		await sleep(500);
+	console.log('animating code');
+	await safeAnimate(
+		code,
+		{ x: [200, 200], y: [460 + 16, 460], opacity: [0, 1] },
+		{ duration: 0.25 }
+	)?.finished;
 
-		console.log('animating code');
-		await animate(code, { x: [200, 200], y: [460 + 16, 460], opacity: [0, 1] }, { duration: 0.25 })
-			.finished;
+	await sleep(500);
 
-		await sleep(500);
+	update((p) => ({ ...p, submitted: true }));
 
-		update((p) => ({ ...p, submitted: true }));
+	await sleep(1000);
 
-		await sleep(1000);
-
-		update((p) => ({ ...p, showControls: true }));
-		animate(controls, { x: [420, 420], y: [16, 0], opacity: 1 }, { duration: 0.5 });
-	} catch {
-		// Do nothing. It's expected to error when trying to animate elements that no longer exist,
-		// e.g. when the component is unmounted.
-	}
+	update((p) => ({ ...p, showControls: true }));
+	safeAnimate(controls, { x: [420, 420], y: [16, 0], opacity: 1 }, { duration: 0.5 });
 };
 
 export const authController = {
