8777e0449f3dca2d451f8c9d189f7e0d5d694bfc
Eldad Fux <eldad.fux@gmail.com>
2023-09-16 02:26:54 +0300

    Updates links in header and footer

--- src/lib/components/FooterNav.svelte
+++ src/lib/components/FooterNav.svelte
@@ -2,9 +2,9 @@
 	<img class="aw-logo" src="/images/logos/appwrite.svg" alt="appwrite" width="130" />
 	<ul class="aw-footer-nav-main-list">
 		<li class="aw-footer-nav-main-item">
-			<h5 class="aw-footer-nav-main-title aw-is-not-mobile"><a href="">Product</a></h5>
+			<h5 class="aw-footer-nav-main-title aw-is-not-mobile">Quick starts</h5>
 			<button class="aw-footer-nav-button aw-is-only-mobile">
-				<span class="aw-caption-500">Product</span>
+				<span class="aw-caption-500">Products</span>
 				<svg
 					class="aw-footer-nav-button-arrow"
 					xmlns="http://www.w3.org/2000/svg"
@@ -22,17 +22,76 @@
 				</svg>
 			</button>
 			<ul class="aw-footer-nav-secondary-list">
-				<li class="aw-footer-nav-secondary-item"><a href="">Docs</a></li>
-				<li class="aw-footer-nav-secondary-item"><a href="">Auth</a></li>
-				<li class="aw-footer-nav-secondary-item"><a href="">Databases</a></li>
-				<li class="aw-footer-nav-secondary-item"><a href="">Functions</a></li>
-				<li class="aw-footer-nav-secondary-item"><a href="">Storage</a></li>
-				<li class="aw-footer-nav-secondary-item"><a href="">Realtime</a></li>
-				<li class="aw-footer-nav-secondary-item"><a href="">Localization</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/docs/quick-starts/next-js">Next.js</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/docs/quick-starts/flutter">Flutter</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/docs/quick-starts/vue-js">Vue.js</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/docs/quick-starts/sveltekit">SvelteKit</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/docs/quick-starts/apple">Apple</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/docs/quick-starts/android">Android</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/docs/quick-starts/nuxt">Nuxt</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/docs/quick-starts/angular">Angular</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/docs/quick-starts/qwik">Qwik</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/docs/quick-starts/astro">Astro</a></li>
 			</ul>
 		</li>
 		<li class="aw-footer-nav-main-item">
-			<h5 class="aw-footer-nav-main-title aw-is-not-mobile"><a href="">About</a></h5>
+			<h5 class="aw-footer-nav-main-title aw-is-not-mobile">Products</h5>
+			<button class="aw-footer-nav-button aw-is-only-mobile">
+				<span class="aw-caption-500">Products</span>
+				<svg
+					class="aw-footer-nav-button-arrow"
+					xmlns="http://www.w3.org/2000/svg"
+					width="20"
+					height="20"
+					viewBox="0 0 20 20"
+					fill="none"
+				>
+					<path
+						fill-rule="evenodd"
+						clip-rule="evenodd"
+						d="M10.4243 13.0243C10.19 13.2586 9.81007 13.2586 9.57576 13.0243L5.07576 8.52426C4.84145 8.28995 4.84145 7.91005 5.07576 7.67574C5.31007 7.44142 5.68997 7.44142 5.92429 7.67574L10 11.7515L14.0758 7.67574C14.3101 7.44142 14.69 7.44142 14.9243 7.67574C15.1586 7.91005 15.1586 8.28995 14.9243 8.52426L10.4243 13.0243Z"
+						fill="#ADADB0"
+					/>
+				</svg>
+			</button>
+			<ul class="aw-footer-nav-secondary-list">
+				<li class="aw-footer-nav-secondary-item"><a href="/docs/products/auth">Auth</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/docs/products/databases">Databases</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/docs/products/functions">Functions</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/docs/products/storage">Storage</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/docs/apis/realtime">Realtime</a></li>
+			</ul>
+		</li>
+		<li class="aw-footer-nav-main-item">
+			<h5 class="aw-footer-nav-main-title aw-is-not-mobile">Learn</h5>
+			<button class="aw-footer-nav-button is-open aw-is-only-mobile">
+				<span class="aw-caption-500">Learn</span>
+				<svg
+					class="aw-footer-nav-button-arrow"
+					xmlns="http://www.w3.org/2000/svg"
+					width="20"
+					height="20"
+					viewBox="0 0 20 20"
+					fill="none"
+				>
+					<path
+						fill-rule="evenodd"
+						clip-rule="evenodd"
+						d="M10.4243 13.0243C10.19 13.2586 9.81007 13.2586 9.57576 13.0243L5.07576 8.52426C4.84145 8.28995 4.84145 7.91005 5.07576 7.67574C5.31007 7.44142 5.68997 7.44142 5.92429 7.67574L10 11.7515L14.0758 7.67574C14.3101 7.44142 14.69 7.44142 14.9243 7.67574C15.1586 7.91005 15.1586 8.28995 14.9243 8.52426L10.4243 13.0243Z"
+						fill="#ADADB0"
+					/>
+				</svg>
+			</button>
+			<ul class="aw-footer-nav-secondary-list">
+				<li class="aw-footer-nav-secondary-item"><a href="/docs">Docs</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/community">Community</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/blog">Blog</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="TODO">Roadmap</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="TODO">Changelog</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="https://status.appwrite.io" target="_blank">Status</a></li>
+			</ul>
+		</li><li class="aw-footer-nav-main-item">
+			<h5 class="aw-footer-nav-main-title aw-is-not-mobile">About</h5>
 			<button class="aw-footer-nav-button is-open aw-is-only-mobile">
 				<span class="aw-caption-500">About</span>
 				<svg
@@ -52,17 +111,17 @@
 				</svg>
 			</button>
 			<ul class="aw-footer-nav-secondary-list">
-				<li class="aw-footer-nav-secondary-item"><a href="">Company</a></li>
-				<li class="aw-footer-nav-secondary-item"><a href="">Community</a></li>
-				<li class="aw-footer-nav-secondary-item"><a href="">Blog</a></li>
-				<li class="aw-footer-nav-secondary-item"><a href="">Careers</a></li>
-				<li class="aw-footer-nav-secondary-item"><a href="">Contact Us</a></li>
-				<li class="aw-footer-nav-secondary-item"><a href="">Roadmap</a></li>
-				<li class="aw-footer-nav-secondary-item"><a href="">Changelog</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/pricing">Pricing</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/company">Company</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="https://careers.appwrite.io">Careers</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/heroes">Heroes</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="https://store.appwrite.io" target="_blank">Store</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/brand">Brand</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/contact-us">Contact us</a></li>
 			</ul>
 		</li>
 		<li class="aw-footer-nav-main-item">
-			<h5 class="aw-footer-nav-main-title aw-is-not-mobile"><a href="">Legal</a></h5>
+			<h5 class="aw-footer-nav-main-title aw-is-not-mobile">Legal</h5>
 			<button class="aw-footer-nav-button aw-is-only-mobile">
 				<span class="aw-caption-500">Legal</span>
 				<svg
@@ -82,10 +141,10 @@
 				</svg>
 			</button>
 			<ul class="aw-footer-nav-secondary-list">
-				<li class="aw-footer-nav-secondary-item"><a href="">Status</a></li>
-				<li class="aw-footer-nav-secondary-item"><a href="">Terms</a></li>
-				<li class="aw-footer-nav-secondary-item"><a href="">Privacy</a></li>
-				<li class="aw-footer-nav-secondary-item"><a href="">Security</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/terms">Terms</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/privacy">Privacy</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/cookies">Cookies</a></li>
+				<li class="aw-footer-nav-secondary-item"><a href="/security">Security</a></li>
 			</ul>
 		</li>
 	</ul>
