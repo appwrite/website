0f4cf2b3d1aa8808d0002fb252dba249537cfe9b
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-09-15 15:51:48 +0200

    fix: add rel attribute to links

--- src/markdoc/nodes/Link.svelte
+++ src/markdoc/nodes/Link.svelte
@@ -2,7 +2,9 @@
 	export let href: string;
 	export let title: string;
 
-	const target = href.startsWith('http') ? '_blank' : undefined;
+	const isExternal = ['http://', 'https://'].some((prefix) => href.startsWith(prefix));
+	const target = isExternal ? '_blank' : undefined;
+	const rel = isExternal ? 'noopener nofollow' : undefined;
 </script>
 
-<a class="aw-link" {href} {title} {target}><slot /></a>
+<a class="aw-link" {href} {title} {target} {rel}><slot /></a>
