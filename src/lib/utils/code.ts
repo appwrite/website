dcbf7118a4224dbd77abe8904d41288d46e4b627
Vincent (Wen Yu) Ge <wenyu1999@hotmail.com>
2023-09-15 17:50:55 +0000

    Resolve merge conflict

--- src/lib/utils/code.ts
+++ src/lib/utils/code.ts
@@ -13,7 +13,6 @@ import python from 'highlight.js/lib/languages/python';
 import diff from 'highlight.js/lib/languages/diff';
 import ruby from 'highlight.js/lib/languages/ruby';
 import csharp from 'highlight.js/lib/languages/csharp';
-<<<<<<< HEAD
 import kotlin from 'highlight.js/lib/languages/kotlin';
 import java from 'highlight.js/lib/languages/java';
 import cpp from 'highlight.js/lib/languages/cpp';
@@ -23,8 +22,6 @@ import dos from 'highlight.js/lib/languages/dos';
 import yaml from 'highlight.js/lib/languages/yaml';
 import plaintext from 'highlight.js/lib/languages/plaintext';
 import graphql from 'highlight.js/lib/languages/graphql';
-=======
->>>>>>> feat-markdoc
 import http from 'highlight.js/lib/languages/http';
 
 const languages = {
