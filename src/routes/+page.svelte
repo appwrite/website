22b44abf792215ba8d31b7a6278e9dde77701b54
Eldad Fux <eldad.fux@gmail.com>
2023-09-16 13:15:01 +0300

    Commneted top promo change inline promotion text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,7 +52,7 @@
 						<span class="aw-icon-star" aria-hidden="true" />
 						<span class="aw-caption-500">New</span>
 						<div class="aw-hero-banner-button-sep" />
-						<span class="aw-caption-400">Pricing Plans announced</span>
+						<span class="aw-caption-400">Intorudcing a *brand* new Appwrite</span>
 						<span class="aw-icon-arrow-right" aria-hidden="true" />
 					</button>
 					<div class="aw-hero is-horizontal">
