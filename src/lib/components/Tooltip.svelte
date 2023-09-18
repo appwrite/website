955694d7148d15f92d85bc52b6823742314b2bb1
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-09-18 17:55:45 +0200

    feat: tooltips and line numbers

---
+++ src/lib/components/Tooltip.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { createTooltip, melt } from '@melt-ui/svelte';
+
+	const {
+		elements: { trigger, content, arrow },
+		states: { open }
+	} = createTooltip({
+		positioning: {
+			placement: 'top'
+		},
+		openDelay: 0,
+		closeOnPointerDown: false,
+		forceVisible: true
+	});
+</script>
+
+<span use:melt={$trigger}>
+	<slot />
+</span>
+
+{#if $open}
+	<div use:melt={$content} class="aw-tooltip aw-sub-body-400">
+		<div use:melt={$arrow} />
+		<slot name="tooltip" />
+	</div>
+{/if}
--- src/lib/utils/code.ts
+++ src/lib/utils/code.ts
@@ -1,4 +1,4 @@
-import type { LanguageFn } from 'highlight.js';
+import type { HLJSApi, LanguageFn } from 'highlight.js';
 import hljs from 'highlight.js/lib/core';
 import dart from 'highlight.js/lib/languages/dart';
 import javascript from 'highlight.js/lib/languages/javascript';
@@ -16,6 +16,9 @@ import csharp from 'highlight.js/lib/languages/csharp';
 import http from 'highlight.js/lib/languages/http';
 import kotlin from 'highlight.js/lib/languages/kotlin';
 import java from 'highlight.js/lib/languages/java';
+import css from 'highlight.js/lib/languages/css';
+import graphql from 'highlight.js/lib/languages/graphql';
+import { Platform } from './references';
 
 const languages = {
 	js: javascript,
@@ -35,22 +38,54 @@ const languages = {
 	http: http,
 	kotlin: kotlin,
 	java: java,
+	css: css,
+	graphql: graphql
 } as const satisfies Record<string, LanguageFn>;
+
+const platformAliases: Record<Platform, keyof typeof languages> = {
+	[Platform.ClientWeb]: 'js',
+	[Platform.ClientFlutter]: 'dart',
+	[Platform.ClientAndroidJava]: 'java',
+	[Platform.ClientAndroidKotlin]: 'kotlin',
+	[Platform.ClientApple]: 'swift',
+	[Platform.ServerDart]: 'dart',
+	[Platform.ServerDeno]: 'ts',
+	[Platform.ServerDotNet]: 'cs',
+	[Platform.ServerNodeJs]: 'js',
+	[Platform.ServerPhp]: 'php',
+	[Platform.ServerPython]: 'py',
+	[Platform.ServerRuby]: 'rb',
+	[Platform.ServerSwift]: 'swift'
+};
 
 Object.entries(languages).forEach(([key, value]) => {
 	hljs.registerLanguage(key, value);
 });
 
-export type Language = keyof typeof languages;
+Object.entries(platformAliases).forEach(([key, value]) => {
+	hljs.registerAliases(key, {
+		languageName: value
+	});
+});
+
+export type Language = keyof typeof languages | Platform;
 
 type Args = {
 	content: string;
 	language?: Language;
+	withLineNumbers?: boolean;
 };
 
 export const getCodeHtml = (args: Args) => {
-	const { content, language } = args;
+	const { content, language, withLineNumbers } = args;
 	const res = hljs.highlight(content, { language: language ?? 'sh' }).value;
+	const lines = res.split(/\n/g).slice(0, -1);
+	const final = lines.reduce((carry, line) => {
+		carry += `<span class="line">${line}</span>\n`;
+		return carry;
+	}, '');
 
-	return `<pre><code class="language-${language}">${res}</code></pre>`;
+	return `<pre><code class="language-${language} ${
+		withLineNumbers ? 'line-numbers' : ''
+	}">${final}</code></pre>`;
 };
