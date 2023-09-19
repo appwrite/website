a1c22a74a56a1bcc77c2650928ee92a5ce08b9f2
Arman <arman@appwrite.io>
2023-09-19 16:58:47 +0200

    feat: add footer

---
+++ src/lib/components/Newsletter.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { Tabs } from '$lib/UI';
+</script>
+
+<img src="/images/bgs/pre-footer.svg" alt="" class="pre-footer-bg" />
+
+<div class="aw-u-row-gap-80 u-position-relative">
+	<div class="aw-container">
+		<div class="aw-hero aw-u-max-width-800">
+			<h5 class="aw-display aw-u-color-text-primary">Subscribe to our newsletter</h5>
+			<p class="aw-description aw-u-color-text-primary" style="opacity:0.64">
+				Sign up to our company blog and get the latest insights from Appwrite. Learn more about
+				engineering, product design, building community, and tips & tricks for using Appwrite.
+			</p>
+			<div
+				class="aw-subscribe-input aw-input-text is-reset-input-inside u-width-full-line aw-u-max-width-380 u-margin-inline-auto u-margin-block-start-32"
+			>
+				<input type="email" placeholder="Enter your email" />
+				<button class="aw-button">Sign up</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+	.pre-footer-bg {
+		position: absolute;
+		top: clamp(300px, 50vw, 50%);
+		left: clamp(300px, 50vw, 50%);
+		transform: translate(-50%, -70%);
+		width: clamp(1200px, 200vw, 3000px);
+		height: auto;
+		max-inline-size: unset;
+		max-block-size: unset;
+	}
+</style>
--- src/markdoc/layouts/Post.svelte
+++ src/markdoc/layouts/Post.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { base } from '$app/paths';
-	import { MainFooter } from '$lib/components';
+	import { FooterNav, MainFooter, Newsletter, PreFooter } from '$lib/components';
 	import { Main } from '$lib/layouts';
 
 	export let title: string;
@@ -52,99 +52,110 @@
 	fetchAuthorData();
 </script>
 
-<div class="aw-big-padding-section">
+<Main>
 	<div class="aw-big-padding-section">
-		<div class="aw-big-padding-section-level-1">
-			<div class="aw-big-padding-section-level-2">
-				<div class="aw-container" style="--container-size:42.5rem">
-					<article class="aw-main-article">
-						<header class="aw-main-article-header">
-							<a class="aw-link aw-u-color-text-secondary" href="/blog">
-								<span class="aw-icon-chevron-left" aria-hidden="true" />
-								<span>Back to blog</span>
-							</a>
-							<ul class="aw-metadata aw-caption-400">
-								<li>
-									<time datetime={date}>{date}</time>
-								</li>
-								<li>{timeToRead} min</li>
-							</ul>
-							<h1 class="aw-title aw-u-color-text-primary">{title}</h1>
-							<p class="aw-description u-margin-block-start-8">
-								{description}
-							</p>
-							{#if authorData}
-								<div class="aw-author u-margin-block-start-16">
-									<a href={authorData.href} class="u-flex u-cross-center u-gap-8">
-										{#if authorData.avatar}
-										<img
-											class="aw-author-image"
-											src={authorData.avatar}
-											width="44"
-											height="44"
-											alt=""
-										/>
-										{/if}
-										<div class="u-flex-vertical">
-											<h4 class="aw-sub-body-400 aw-u-color-text-primary">{authorData.name}</h4>
-											<p class="aw-caption-400">{authorData.role}</p>
-										</div>
-									</a>
-									<ul class="u-flex u-gap-8 u-margin-inline-start-auto u-cross-child-center">
-										{#if authorData.twitter}
-											<li>
-												<a
-													href={authorData.twitter}
-													class="aw-icon-button"
-													aria-label="Author twitter"
-													target="_blank"
-													rel="noopener noreferrer"
-												>
-													<span class="aw-icon-twitter" aria-hidden="true" />
-												</a>
-											</li>
-										{/if}
-										{#if authorData.linkedin}
-											<li>
-												<a
-													href={authorData.linkedin}
-													class="aw-icon-button"
-													aria-label="Author LinkedIn"
-													target="_blank"
-													rel="noopener noreferrer"
-												>
-													<span class="aw-icon-linkedin" aria-hidden="true" />
-												</a>
-											</li>
-										{/if}
-										{#if authorData.github}
-											<li>
-												<a
-													href={authorData.github}
-													class="aw-icon-button"
-													aria-label="Author GitHub"
-													target="_blank"
-													rel="noopener noreferrer"
-												>
-													<span class="aw-icon-github" aria-hidden="true" />
-												</a>
-											</li>
-										{/if}
-									</ul>
-								</div>
-							{/if}
-						</header>
+		<div class="aw-big-padding-section">
+			<div class="aw-big-padding-section-level-1">
+				<div class="aw-big-padding-section-level-2">
+					<div class="aw-container" style="--container-size:42.5rem">
+						<article class="aw-main-article">
+							<header class="aw-main-article-header">
+								<a class="aw-link aw-u-color-text-secondary" href="/blog">
+									<span class="aw-icon-chevron-left" aria-hidden="true" />
+									<span>Back to blog</span>
+								</a>
+								<ul class="aw-metadata aw-caption-400">
+									<li>
+										<time datetime={date}>{date}</time>
+									</li>
+									<li>{timeToRead} min</li>
+								</ul>
+								<h1 class="aw-title aw-u-color-text-primary">{title}</h1>
+								<p class="aw-description u-margin-block-start-8">
+									{description}
+								</p>
+								{#if authorData}
+									<div class="aw-author u-margin-block-start-16">
+										<a href={authorData.href} class="u-flex u-cross-center u-gap-8">
+											{#if authorData.avatar}
+												<img
+													class="aw-author-image"
+													src={authorData.avatar}
+													width="44"
+													height="44"
+													alt=""
+												/>
+											{/if}
+											<div class="u-flex-vertical">
+												<h4 class="aw-sub-body-400 aw-u-color-text-primary">{authorData.name}</h4>
+												<p class="aw-caption-400">{authorData.role}</p>
+											</div>
+										</a>
+										<ul class="u-flex u-gap-8 u-margin-inline-start-auto u-cross-child-center">
+											{#if authorData.twitter}
+												<li>
+													<a
+														href={authorData.twitter}
+														class="aw-icon-button"
+														aria-label="Author twitter"
+														target="_blank"
+														rel="noopener noreferrer"
+													>
+														<span class="aw-icon-twitter" aria-hidden="true" />
+													</a>
+												</li>
+											{/if}
+											{#if authorData.linkedin}
+												<li>
+													<a
+														href={authorData.linkedin}
+														class="aw-icon-button"
+														aria-label="Author LinkedIn"
+														target="_blank"
+														rel="noopener noreferrer"
+													>
+														<span class="aw-icon-linkedin" aria-hidden="true" />
+													</a>
+												</li>
+											{/if}
+											{#if authorData.github}
+												<li>
+													<a
+														href={authorData.github}
+														class="aw-icon-button"
+														aria-label="Author GitHub"
+														target="_blank"
+														rel="noopener noreferrer"
+													>
+														<span class="aw-icon-github" aria-hidden="true" />
+													</a>
+												</li>
+											{/if}
+										</ul>
+									</div>
+								{/if}
+							</header>
 
-						<div class="aw-media-container">
-							<img class="u-block" src={cover} alt="console dashboard" />
-						</div>
+							<div class="aw-media-container">
+								<img class="u-block" src={cover} alt="console dashboard" />
+							</div>
 
-						<div class="aw-article-content u-margin-block-start-32">
-							<slot />
-						</div>
-					</article>
+							<div class="aw-article-content u-margin-block-start-32">
+								<slot />
+							</div>
+						</article>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
+	<div
+		class="aw-big-padding-section-level-2 is-margin-replace-padding u-position-relative u-overflow-hidden"
+	>
+		<div class="aw-container">
+			<Newsletter />
+			<FooterNav />
+			<MainFooter />
+		</div>
+	</div>
+</Main>
