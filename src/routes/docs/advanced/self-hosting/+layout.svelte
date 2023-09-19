bec080cbb8815cfb45872514c129644b922ad435
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-09-19 20:19:17 +0200

    fix: some routes

--- src/routes/docs/advanced/self-hosting/+layout.svelte
+++ src/routes/docs/advanced/self-hosting/+layout.svelte
@@ -3,16 +3,60 @@
 	import Sidebar, { type NavParent, type NavTree } from '$lib/layouts/Sidebar.svelte';
 
 	const parent: NavParent = {
-		backlink: '/docs',
-		href: '/docs/advanced/self-hosting',
-		label: 'Self-hosting',
-		icon: 'icon-server'
+		href: '/docs',
+		label: 'Self-hosting'
 	};
 
 	const navigation: NavTree = [
 		{
+			label: 'Getting started',
+			items: [
+				{
+					label: 'Overview',
+					href: '/docs/advanced/self-hosting'
+				},
+				{
+					label: 'Installation',
+					href: '/docs/advanced/self-hosting/installation'
+				}
+			]
+		},
+		{
 			label: 'Guides',
-			items: []
+			items: [
+				{
+					label: 'Update',
+					href: '/docs/advanced/self-hosting/update'
+				},
+				{
+					label: 'Debug',
+					href: '/docs/advanced/self-hosting/debug'
+				},
+				{
+					label: 'E-Mail',
+					href: '/docs/advanced/self-hosting/email'
+				},
+				{
+					label: 'Functions',
+					href: '/docs/advanced/self-hosting/functions'
+				},
+				{
+					label: 'Production',
+					href: '/docs/advanced/self-hosting/production'
+				},
+				{
+					label: 'SMS',
+					href: '/docs/advanced/self-hosting/sms'
+				},
+				{
+					label: 'Storage',
+					href: '/docs/advanced/self-hosting/storage'
+				},
+				{
+					label: 'TLS Certificates',
+					href: '/docs/advanced/self-hosting/tls-certificates'
+				}
+			]
 		}
 	];
 </script>
