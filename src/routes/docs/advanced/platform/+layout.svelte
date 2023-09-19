7fe5c07714a630e0a9918d778f1a4e0c20bd58ee
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-09-19 15:24:57 +0200

    feat: overall routes

---
+++ src/routes/docs/advanced/platform/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Docs from '$lib/layouts/Docs.svelte';
+	import Sidebar, { type NavParent, type NavTree } from '$lib/layouts/Sidebar.svelte';
+
+	const parent: NavParent = {
+		backlink: '/docs',
+		href: '/docs/advanced/platform',
+		label: 'Platform',
+		icon: 'icon-play'
+	};
+
+	const navigation: NavTree = [
+		{
+			label: 'Guides',
+			items: []
+		}
+	];
+</script>
+
+<Docs variant="two-side-navs">
+	<Sidebar {navigation} {parent} />
+	<slot />
+</Docs>
