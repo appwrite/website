538c4a598471fe5fd9374c69bea28d08b60d7789
Arman <arman@appwrite.io>
2023-09-19 15:16:30 +0200

    feat: author

--- src/markdoc/layouts/Post.svelte
+++ src/markdoc/layouts/Post.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import { MainFooter } from '$lib/components';
 	import { Main } from '$lib/layouts';
 
@@ -9,6 +10,46 @@
 	export let date: string;
 	export let timeToRead: string;
 	export let cover: string;
+
+	type AuthorData = {
+		name: string;
+		role: string;
+		avatar: string;
+		bio: string;
+		twitter: string;
+		linkedin: string;
+		github: string;
+		href: string;
+	};
+
+	let authorData: AuthorData | undefined;
+
+	function fetchAuthorData() {
+		const authors = import.meta.glob('$routes/blog/author/**/*.markdoc', {
+			eager: true
+		});
+		console.log(authors);
+		const data = Object.entries(authors).map(([_filepath, authors]) => {
+			const { frontmatter } = authors as {
+				frontmatter: AuthorData;
+			};
+
+			return {
+				name: frontmatter.name,
+				role: frontmatter.role,
+				avatar: frontmatter.avatar,
+				bio: frontmatter.bio,
+				twitter: frontmatter.twitter,
+				linkedin: frontmatter.linkedin,
+				github: frontmatter.github,
+				href: `${base}/blog/author/${frontmatter.name.toLowerCase()}`
+			};
+		});
+
+		authorData = data.find((a) => a.name.includes(author));
+		console.log(authorData);
+	}
+	fetchAuthorData();
 </script>
 
 <div class="aw-big-padding-section">
@@ -20,7 +61,7 @@
 						<header class="aw-main-article-header">
 							<a class="aw-link aw-u-color-text-secondary" href="/blog">
 								<span class="aw-icon-chevron-left" aria-hidden="true" />
-								<span class="">Back to blog</span>
+								<span>Back to blog</span>
 							</a>
 							<ul class="aw-metadata aw-caption-400">
 								<li>
@@ -32,43 +73,66 @@
 							<p class="aw-description u-margin-block-start-8">
 								{description}
 							</p>
-							<div class="aw-author u-margin-block-start-16">
-								<div class="u-flex u-cross-center u-gap-8">
-									<img
-										class="aw-author-image"
-										src="/images/blog/placeholder.png"
-										width="44"
-										height="44"
-										alt=""
-									/>
-									<div class="aw-author-info">
-										<h4 class="aw-sub-body-400 aw-u-color-text-primary">{author}</h4>
-										<p class="aw-caption-400 u-hide">Author's role or bio</p>
-									</div>
+							{#if authorData}
+								<div class="aw-author u-margin-block-start-16">
+									<a href={authorData.href} class="u-flex u-cross-center u-gap-8">
+										{#if authorData.avatar}
+										<img
+											class="aw-author-image"
+											src={authorData.avatar}
+											width="44"
+											height="44"
+											alt=""
+										/>
+										{/if}
+										<div class="u-flex-vertical">
+											<h4 class="aw-sub-body-400 aw-u-color-text-primary">{authorData.name}</h4>
+											<p class="aw-caption-400">{authorData.role}</p>
+										</div>
+									</a>
+									<ul class="u-flex u-gap-8 u-margin-inline-start-auto u-cross-child-center">
+										{#if authorData.twitter}
+											<li>
+												<a
+													href={authorData.twitter}
+													class="aw-icon-button"
+													aria-label="Author twitter"
+													target="_blank"
+													rel="noopener noreferrer"
+												>
+													<span class="aw-icon-twitter" aria-hidden="true" />
+												</a>
+											</li>
+										{/if}
+										{#if authorData.linkedin}
+											<li>
+												<a
+													href={authorData.linkedin}
+													class="aw-icon-button"
+													aria-label="Author LinkedIn"
+													target="_blank"
+													rel="noopener noreferrer"
+												>
+													<span class="aw-icon-linkedin" aria-hidden="true" />
+												</a>
+											</li>
+										{/if}
+										{#if authorData.github}
+											<li>
+												<a
+													href={authorData.github}
+													class="aw-icon-button"
+													aria-label="Author GitHub"
+													target="_blank"
+													rel="noopener noreferrer"
+												>
+													<span class="aw-icon-github" aria-hidden="true" />
+												</a>
+											</li>
+										{/if}
+									</ul>
 								</div>
-								<ul class="u-flex u-gap-8 u-margin-inline-start-auto u-cross-child-center">
-									<li>
-										<a
-											href="https://twitter.com/appwrite"
-											class="aw-icon-button"
-											aria-label="Author twitter"
-											target="_blank"
-										>
-											<span class="aw-icon-twitter" aria-hidden="true" />
-										</a>
-									</li>
-									<li>
-										<a
-											href="https://twitter.com/appwrite"
-											class="aw-icon-button"
-											aria-label="Author LinkedIn"
-											target="_blank"
-										>
-											<span class="aw-icon-linkedin" aria-hidden="true" />
-										</a>
-									</li>
-								</ul>
-							</div>
+							{/if}
 						</header>
 
 						<div class="aw-media-container">
