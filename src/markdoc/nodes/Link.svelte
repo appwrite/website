540242b19853b2c6140f5e5d135f9e5cc30b9876
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-09-20 14:00:25 +0200

    fix: icons and twitter logo

--- src/markdoc/nodes/Link.svelte
+++ src/markdoc/nodes/Link.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+	import { getContext, hasContext } from 'svelte';
+
 	export let href: string;
 	export let title: string;
 
+	const isHeading = hasContext('is-heading') ? getContext<boolean>('is-heading') : false;
 	const isExternal = ['http://', 'https://'].some((prefix) => href.startsWith(prefix));
 	const target = isExternal ? '_blank' : undefined;
 	const rel = isExternal ? 'noopener nofollow' : undefined;
 </script>
 
-<a class="aw-link" {href} {title} {target} {rel}><slot /></a>
+<a class="aw-link" {href} {title} {target} {rel}>
+	<slot />
+	{#if isExternal}
+		<span class="icon-cheveron-right" />
+	{/if}
+</a>
