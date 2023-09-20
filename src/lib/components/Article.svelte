017fb89a2937de18d7f88c0e10ccc3a64d6ad2a5
Arman <arman@appwrite.io>
2023-09-20 19:34:15 +0200

    feat: create article component, refactor blog pages, fix image size

---
+++ src/lib/components/Article.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	export let title: string;
+	export let cover: string;
+	export let href: string;
+	export let date: Date;
+	export let timeToRead: number;
+	export let author: string;
+	export let avatar: string;
+</script>
+
+<li>
+	<a class="aw-grid-articles-item" {href}>
+		<div class="aw-grid-articles-item-image">
+			<img src={cover} class="aw-u-media-ratio-16-9" alt={title} />
+		</div>
+		<div class="aw-grid-articles-item-content">
+			<h4 class="aw-label aw-u-color-text-primary">
+				{title}
+			</h4>
+			<div class="aw-author">
+				<div class="u-flex u-cross-center u-gap-8">
+					<img class="aw-author-image" src={avatar} width="24" height="24" alt={author} />
+					<div class="aw-author-info">
+						<h4 class="aw-sub-body-400 aw-u-color-text-primary">{author}</h4>
+						<ul class="aw-metadata aw-caption-400 aw-is-not-mobile">
+							<li>{date.toLocaleDateString()}</li>
+							<li>{timeToRead} min</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</a>
+</li>
