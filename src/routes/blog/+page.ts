3fbe2d07910b8d80c6b7c795cece2865ad75b297
Arman <arman@appwrite.io>
2023-09-20 12:13:20 +0200

    fix: blog page, small code fixes

--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -1,34 +1,57 @@
+import { base } from '$app/paths';
+import type { AuthorData } from '$markdoc/layouts/Author.svelte';
+import type { PostsData } from '$markdoc/layouts/Post.svelte';
+
 export function load() {
-	const modules = import.meta.glob('./post/**/*.markdoc', {
+	const postsGlob = import.meta.glob('./post/**/*.markdoc', {
+		eager: true
+	});
+	const authorsGlob = import.meta.glob('./author/**/*.markdoc', {
 		eager: true
 	});
 
-	const posts = Object.entries(modules)
-		.map(([filepath, module]) => {
-			const { frontmatter } = module as {
-				frontmatter: {
-					title: string;
-					description: string;
-					date: string;
-					cover: string;
-					timeToRead: number;
-				};
+	const posts = Object.entries(postsGlob)
+		.map(([filepath, postList]) => {
+			const { frontmatter } = postList as {
+				frontmatter: PostsData;
 			};
+			const slug = filepath.replace('./', '').replace('/+page.markdoc', '');
+			const postName = slug.slice(slug.lastIndexOf('/') + 1);
+
 			return {
-				filepath,
-				slug: filepath.replace('./post/', '').replace('.markdoc', ''),
 				title: frontmatter.title,
 				description: frontmatter.description,
 				date: new Date(frontmatter.date),
 				cover: frontmatter.cover,
-				timeToRead: frontmatter.timeToRead
+				timeToRead: frontmatter.timeToRead,
+				author: frontmatter.author,
+				category: frontmatter.category,
+				href: `${base}/blog/post/${postName}`
 			};
 		})
 		.sort((a, b) => {
 			return b.date.getTime() - a.date.getTime();
 		});
 
+	const authors = Object.entries(authorsGlob).map(([_filepath, authorList]) => {
+		const { frontmatter } = authorList as {
+			frontmatter: AuthorData;
+		};
+
+		return {
+			name: frontmatter.name,
+			role: frontmatter.role,
+			avatar: frontmatter.avatar,
+			bio: frontmatter.bio,
+			twitter: frontmatter.twitter,
+			linkedin: frontmatter.linkedin,
+			github: frontmatter.github,
+			href: `${base}/blog/author/${frontmatter.name.toLowerCase()}`
+		};
+	});
+
 	return {
-		posts
+		posts,
+		authors
 	};
 }
