8b2467a6816c8e7ed7006ce36475e5c8b25a00cd
Arman <arman@appwrite.io>
2023-09-21 13:17:02 +0200

    chore: add nav and sidebar to error page example

---
+++ src/routes/kitchensink/error/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { Docs } from '$lib/layouts';
+	import Sidebar from '$routes/docs/Sidebar.svelte';
+</script>
+
+<Docs variant="two-side-navs">
+	<Sidebar />
+
+	<!-- error page here -->
+</Docs>
