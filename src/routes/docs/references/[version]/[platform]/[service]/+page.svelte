9ae5c5dec831d7aed1617f2627ca20cd8527e335
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-09-21 18:37:26 +0200

    feat: models

--- src/routes/docs/references/[version]/[platform]/[service]/+page.svelte
+++ src/routes/docs/references/[version]/[platform]/[service]/+page.svelte
@@ -181,23 +181,30 @@
 										<div class="aw-card is-transparent u-padding-16 u-margin-block-start-16">
 											<ul>
 												{#each method.responses as response}
-													<li>
-														<article>
-															<header class="u-flex u-cross-baseline u-gap-8">
-																<h3 class="aw-eyebrow aw-u-color-text-primary">
-																	{response.code}
-																</h3>
-																<span class="aw-caption-400">{response.model?.name}</span>
-															</header>
-															<p class="aw-sub-body-400 u-margin-block-start-16">
-																<a
-																	href={`/docs/references/${$page.params.version}/models/${response.model?.id}`}
-																>
-																	Payload <span class="icon-cheveron-right" />
-																</a>
-															</p>
-														</article>
-													</li>
+													{#if response.models}
+														<li>
+															<article>
+																<header class="u-flex u-cross-baseline u-gap-8">
+																	<h3 class="aw-eyebrow aw-u-color-text-primary">
+																		{response.code}
+																	</h3>
+																	<span class="aw-caption-400">application/json</span>
+																</header>
+																<ul class="aw-sub-body-400 u-margin-block-start-16">
+																	{#each response.models as model}
+																		<li>
+																			<a
+																				class="aw-link"
+																				href={`/docs/references/${$page.params.version}/models/${model.id}`}
+																			>
+																				{model.name}
+																			</a>
+																		</li>
+																	{/each}
+																</ul>
+															</article>
+														</li>
+													{/if}
 												{/each}
 											</ul>
 										</div>
