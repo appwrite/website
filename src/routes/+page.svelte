393fe7a71ba256fce5defda4a211227446ddecf0
Arman <arman@appwrite.io>
2023-09-22 16:35:52 +0200

    Initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -318,12 +318,26 @@
 		</div>
 
 		<div class="aw-big-padding-section-level-1 u-position-relative u-overflow-hidden">
-			<div
-				class="u-position-absolute aw-is-not-mobile"
-				style="inline-size:600px; inset-block-start:0; inset-inline-start:calc(100% - 600px)"
-			>
-				<img src="/images/temp/bg-side-2.png" width="600" alt="" />
-			</div>
+			<Spline let:fallback let:viewer>
+				<div
+					class="u-position-absolute aw-is-not-mobile"
+					style="inline-size:600px; block-size: 600px;  inset-inline-start:calc(100% - 500px)"
+				>
+					<img
+						src="/images/animations/tech.png"
+						width="600"
+						height="600"
+						alt=""
+						style="position: absolute; display: block;"
+						use:fallback
+					/>
+
+					<spline-viewer
+						url="https://prod.spline.design/KvoQWZAi5foGUXxJ/scene.splinecode"
+						use:viewer
+					/>
+				</div>
+			</Spline>
 			<div class="aw-big-padding-section-level-2 is-margin-replace-padding">
 				<div class="aw-container u-position-relative">
 					<section class="aw-hero is-align-start">
