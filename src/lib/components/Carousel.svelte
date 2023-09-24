fef3041caba16110ede7bdf98317a1ed30f7a23c
Eldad Fux <eldad.fux@gmail.com>
2023-09-25 00:42:06 +0300

    Reverted Carousel changes

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,64 +1,74 @@
 <script lang="ts">
 	let carousel: HTMLElement;
 
-	const gap = 32;
+	export let gap = 32;
 	let scroll = 0;
+	let touchStart = 0;
+	let touchEnd = 0;
 
 	function calculateScrollAmount(prev = false) {
 		const direction = prev ? -1 : 1;
 		const carouselSize = carousel?.clientWidth;
-		const size = scroll || carouselSize;
-		if (!scroll) {
-			scroll = size;
-		}
 		const childSize = (carousel.childNodes[0] as HTMLUListElement)?.clientWidth + gap;
+
+		scroll = scroll || carouselSize;
+
 		const numberOfItems = Math.floor(carouselSize / childSize);
-		const overflow = size % childSize;
+		const overflow = scroll % childSize;
 		const amount = numberOfItems * childSize - overflow * direction;
 		scroll += amount * direction;
 		return amount * direction;
 	}
 
 	function next() {
-		const scrollAmount = calculateScrollAmount();
-		scroll += scrollAmount;
 		carousel.scrollBy({
-			left: scrollAmount,
+			left: calculateScrollAmount(),
 			behavior: 'smooth'
 		});
-		logMe();
 	}
 	function prev() {
-		const scrollAmount = calculateScrollAmount();
-		scroll -= scrollAmount;
 		carousel.scrollBy({
-			left: -scrollAmount,
+			left: calculateScrollAmount(true),
 			behavior: 'smooth'
 		});
-		logMe();
 	}
 
-	function logMe() {
-		console.log(carousel?.clientWidth, carousel?.scrollWidth, carousel?.offsetWidth);
+	function handleTouchStart(e: TouchEvent) {
+		touchStart = e.touches[0].clientX;
+	}
+	function handleTouchMove(e: TouchEvent) {
+		touchEnd = e.touches[0].clientX;
+	}
+
+	function handleTouchEnd() {
+		if (touchEnd > touchStart) {
+			prev();
+		} else {
+			next();
+		}
 	}
 </script>
 
 <div class="u-flex u-main-space-between u-flex-wrap">
 	<slot name="header" />
-	<div class="u-flex u-gap-12 u-cross-end">
+	<div class="u-flex u-gap-12 u-cross-end u-margin-block-start-8">
 		<button class="aw-icon-button" aria-label="Move carousel backward" on:click={() => prev()}>
-			<span class="aw-icon-arrow-left" aria-hidden="true" />
+			<span class="icon-arrow-left" aria-hidden="true" />
 		</button>
 		<button class="aw-icon-button" aria-label="Move carousel forward" on:click={() => next()}>
-			<span class="aw-icon-arrow-right" aria-hidden="true" />
+			<span class="icon-arrow-right" aria-hidden="true" />
 		</button>
 	</div>
 </div>
-<div class=" wrapper">
-	<ul class="aw-grid-articles aw-u-gap-32 u-margin-block-start-32 carousel" bind:this={carousel}>
-		<slot />
-	</ul>
-</div>
+<ul
+	class="aw-grid-articles aw-u-gap-32 u-margin-block-start-32 carousel"
+	bind:this={carousel}
+	on:touchstart={handleTouchStart}
+	on:touchmove={handleTouchMove}
+	on:touchend={handleTouchEnd}
+>
+	<slot />
+</ul>
 
 <style lang="scss">
 	.carousel {
