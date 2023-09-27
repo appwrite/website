e586948e2da65c2afdd0853181c29557ab598f4a
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-09-27 05:28:51 +0200

    fix: layout

--- src/routes/docs/references/[version]/[platform]/[service]/+layout.svelte
+++ src/routes/docs/references/[version]/[platform]/[service]/+layout.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
-	import Layout from '../../../Layout.svelte';
+	import { onMount } from 'svelte';
+	import { getReferencesContext } from '$routes/docs/references/+layout.svelte';
+
+	const expandable = getReferencesContext();
+
+	onMount(() => {
+		expandable.set(true);
+
+		return () => {
+			expandable.set(false);
+		};
+	});
 </script>
 
-<Layout expandable>
-	<slot />
-</Layout>
+<slot />
