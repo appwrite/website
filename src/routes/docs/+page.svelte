2c5e91565613752440416ab491b64076bfa8e543
Arman <arman@appwrite.io>
2023-09-28 13:27:35 +0200

    fix: word break

--- src/routes/docs/+page.svelte
+++ src/routes/docs/+page.svelte
@@ -266,7 +266,9 @@
 				Learn how to build like a team of hundreds
 			</h1>
 			<p class="aw-description u-max-width-600">
-				Appwrite is a backend development platform built by hundreds of open-source developers, where you can build any application at any scale using the coding languages and tools you want.
+				Appwrite is a backend development platform built by hundreds of open-source developers,
+				where you can build any application at any scale using the coding languages and tools you
+				want.
 			</p>
 		</section>
 		<section class="aw-hero is-align-start">
@@ -310,6 +312,7 @@
 								</header>
 								<p
 									class="aw-articles-item-paragraph aw-sub-body-500 u-trim-2 u-margin-block-start-4"
+									style="word-break: keep-all;"
 								>
 									Learn Appwrite Auth, Databases, and more with React.
 								</p>
@@ -336,9 +339,10 @@
 								</header>
 								<p
 									class="aw-articles-item-paragraph aw-sub-body-500 u-trim-2 u-margin-block-start-4"
+									style="word-break: keep-all;"
 								>
-								Learn Appwrite Auth, Databases, and more with SvelteKit.
-							</p>
+									Learn Appwrite Auth, Databases, and more with SvelteKit.
+								</p>
 							</article>
 						</a>
 					</li>
@@ -362,8 +366,9 @@
 								</header>
 								<p
 									class="aw-articles-item-paragraph aw-sub-body-500 u-trim-2 u-margin-block-start-4"
+									style="word-break: keep-all;"
 								>
-								Learn Appwrite Auth, Databases, and more with Vue.
+									Learn Appwrite Auth, Databases, and more with Vue.
 								</p>
 							</article>
 						</a>
@@ -388,9 +393,10 @@
 								</header>
 								<p
 									class="aw-articles-item-paragraph aw-sub-body-500 u-trim-2 u-margin-block-start-4"
+									style="word-break: keep-all;"
 								>
-								Coming soon...
-							</p>
+									Coming soon...
+								</p>
 							</article>
 						</a>
 					</li>
@@ -414,9 +420,10 @@
 								</header>
 								<p
 									class="aw-articles-item-paragraph aw-sub-body-500 u-trim-2 u-margin-block-start-4"
+									style="word-break: keep-all;"
 								>
-								Coming soon...
-							</p>
+									Coming soon...
+								</p>
 							</article>
 						</a>
 					</li>
