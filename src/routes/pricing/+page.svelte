01f669a546385d5455c6292cd692f1e58559fddf
Eldad Fux <eldad.fux@gmail.com>
2023-09-28 12:24:53 +0300

    Fixes

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -62,9 +62,9 @@
 										<header class="aw-pricing-cards-header">
 											<h2 class="aw-label">Starter</h2>
 											<div class="aw-title aw-u-color-text-primary u-margin-block-start-8">$0</div>
-											<div class="u-margin-block-start-4 u-opacity-0">per user/month</div>
+											<div class="u-margin-block-start-4">&nbsp;</div>
 											<p class="aw-main-body-500 u-margin-block-start-16">
-												For students and hobby projects.
+												For personal hobby projects and students.
 											</p>
 											<a
 												href="https://cloud.appwrite.io/register"
