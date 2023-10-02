7e5bdabbaffdf10e8f106e3ab518c43c3c2ec16d
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-10-02 14:46:32 +0200

    fix: precompress assets

--- src/lib/utils/markdown.ts
+++ src/lib/utils/markdown.ts
@@ -2,35 +2,38 @@ import MarkdownIt from 'markdown-it';
 
 const md = new MarkdownIt('commonmark');
 export function parse(content: string): string {
-	const tokens = md.parse(content, null);
+    const tokens = md.parse(content, null);
 
-	return md.renderer.render(
-		transform_tokens(tokens),
-		{
-			highlight: null
-		},
-		null
-	);
+    return md.renderer.render(
+        transform_tokens(tokens),
+        {
+            highlight: null
+        },
+        null
+    );
 }
 
 function transform_tokens(tokens: ReturnType<typeof md.parse>): ReturnType<typeof md.parse> {
-	return tokens.map((token) => {
-		if (token.children) {
-			token.children = transform_tokens(token.children);
-		}
-		switch (token.type) {
-			case 'paragraph_open':
-				token.attrPush(['class', 'aw-paragraph']);
-				break;
-			case 'link_open':
-				if (token.attrGet('href')?.startsWith('http')) {
-					token.attrPush(['target', '_blank']);
-				}
-				// disable links for now
-				token.attrSet('href', '#');
-				token.attrPush(['class', 'aw-link']);
-				break;
-		}
-		return token;
-	});
+    return tokens.map((token) => {
+        if (token.children) {
+            token.children = transform_tokens(token.children);
+        }
+        switch (token.type) {
+            case 'paragraph_open':
+                token.attrPush(['class', 'aw-paragraph']);
+                break;
+            case 'link_open': {
+                const href = token.attrGet('href');
+                if (href?.startsWith('http')) {
+                    if (!href.startsWith('https://appwrite.io')) {
+						token.attrPush(['rel', 'noopener noreferrer']);
+                        token.attrPush(['target', '_blank']);
+                    }
+                }
+                token.attrPush(['class', 'aw-link']);
+                break;
+            }
+        }
+        return token;
+    });
 }
