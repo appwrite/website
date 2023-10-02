d583aa3950472e02c177429c2ab6dc1a2ee08710
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-10-02 16:17:04 +0200

    fix: various lighthouse issues

--- src/markdoc/tags/Icon_Image.svelte
+++ src/markdoc/tags/Icon_Image.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
-	export let src: string;
-	export let alt: string;
-	export let size: string = 's';
+    export let src: string;
+    export let alt: string;
+    export let size: string = 's';
 
-	const sizes: Record<string, string> = {
-		s: '16px',
+    const sizes: Record<string, string> = {
+        s: '16px',
         m: '20px',
-		l: '32px',
-		xl: '40px'
-	};
+        l: '32px',
+        xl: '40px'
+    };
 </script>
 
-<img {src} {alt} loading="lazy" style:width={sizes[size]} style:vertical-align="middle" />
+<img
+    {src}
+    {alt}
+    loading="lazy"
+    width={sizes[size]}
+    height={sizes[size]}
+    style:width={sizes[size]}
+    style:vertical-align="middle"
+/>
