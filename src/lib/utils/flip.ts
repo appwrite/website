a0de8da6f0676efe7e205794e05dd7644901c0bb
tglide <26071571+TGlide@users.noreply.github.com>
2023-10-03 20:12:29 +0100

    scrollable nav

--- src/lib/utils/flip.ts
+++ src/lib/utils/flip.ts
@@ -3,11 +3,11 @@ import { cubicOut } from 'svelte/easing';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export function is_function(thing: unknown): thing is Function {
-	return typeof thing === 'function';
+    return typeof thing === 'function';
 }
 
 type Params = FlipParams & {
-	scale?: boolean;
+    scale?: boolean;
 };
 
 /**
@@ -17,43 +17,43 @@ type Params = FlipParams & {
  * https://svelte.dev/docs/svelte-animate#flip
  */
 export function flip(
-	node: HTMLElement,
-	{ from, to }: { from: DOMRect; to: DOMRect },
-	params: Params = {}
+    node: HTMLElement,
+    { from, to }: { from: DOMRect; to: DOMRect },
+    params: Params = {}
 ): AnimationConfig {
-	const style = getComputedStyle(node);
-	const transform = style.transform === 'none' ? '' : style.transform;
-	const [ox, oy] = style.transformOrigin.split(' ').map(parseFloat);
-	const dx = from.left + (from.width * ox) / to.width - (to.left + ox);
-	const dy = from.top + (from.height * oy) / to.height - (to.top + oy);
+    const style = getComputedStyle(node);
+    const transform = style.transform === 'none' ? '' : style.transform;
+    const [ox, oy] = style.transformOrigin.split(' ').map(parseFloat);
+    const dx = from.left + (from.width * ox) / to.width - (to.left + ox);
+    const dy = from.top + (from.height * oy) / to.height - (to.top + oy);
 
-	const {
-		delay = 0,
-		duration = (d) => Math.sqrt(d) * 120,
-		easing = cubicOut,
-		scale = true
-	} = params;
-	return {
-		delay,
-		duration: is_function(duration) ? duration(Math.sqrt(dx * dx + dy * dy)) : duration,
-		easing,
-		css: (t, u) => {
-			const x = u * dx;
-			const y = u * dy;
-			const sx = scale ? t + (u * from.width) / to.width : 1;
-			const sy = scale ? t + (u * from.height) / to.height : 1;
-			return `transform: ${transform} translate(${x}px, ${y}px) scale(${sx}, ${sy});
+    const {
+        delay = 0,
+        duration = (d) => Math.sqrt(d) * 120,
+        easing = cubicOut,
+        scale = true
+    } = params;
+    return {
+        delay,
+        duration: is_function(duration) ? duration(Math.sqrt(dx * dx + dy * dy)) : duration,
+        easing,
+        css: (t, u) => {
+            const x = u * dx;
+            const y = u * dy;
+            const sx = scale ? t + (u * from.width) / to.width : 1;
+            const sy = scale ? t + (u * from.height) / to.height : 1;
+            return `transform: ${transform} translate(${x}px, ${y}px) scale(${sx}, ${sy});
      
       `;
-		},
-		tick(t, u) {
-			const sx = scale ? t + (u * from.width) / to.width : 1;
-			const sy = scale ? t + (u * from.height) / to.height : 1;
-			const inverse_sx = scale ? 1 / sx : 1;
-			const inverse_sy = scale ? 1 / sy : 1;
+        },
+        tick(t, u) {
+            const sx = scale ? t + (u * from.width) / to.width : 1;
+            const sy = scale ? t + (u * from.height) / to.height : 1;
+            const inverse_sx = scale ? 1 / sx : 1;
+            const inverse_sy = scale ? 1 / sy : 1;
 
-			node.style.setProperty('--inverse-sx', inverse_sx.toString());
-			node.style.setProperty('--inverse-sy', inverse_sy.toString());
-		}
-	};
+            node.style.setProperty('--inverse-sx', inverse_sx.toString());
+            node.style.setProperty('--inverse-sy', inverse_sy.toString());
+        }
+    };
 }
