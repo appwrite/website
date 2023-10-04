8860cce55519ee358599cc70b385839bed93a102
tglide <26071571+TGlide@users.noreply.github.com>
2023-10-04 21:47:51 +0100

    downloadable assets first batch

---
+++ src/routes/api/image/+server.ts
@@ -0,0 +1,45 @@
+import { error } from '@sveltejs/kit';
+import sharp from 'sharp';
+
+const formats = ['webp', 'jpeg', 'png', 'tiff', 'avif'] as const;
+
+const filenameMap: Record<Format, string> = {
+    webp: 'webp',
+    jpeg: 'jpg',
+    png: 'png',
+    tiff: 'tiff',
+    avif: 'avif'
+};
+
+const getFilename = (imgUrl: string, format: Format) => {
+    const filename = imgUrl.split('/').pop() ?? '';
+    const ext = filename.split('.').pop() ?? '';
+    if (ext === filenameMap[format]) return filename;
+    return filename.replace(ext, filenameMap[format]);
+};
+
+type Format = (typeof formats)[number];
+
+const isFormat = (format: unknown): format is Format => {
+    return typeof format === 'string' && formats.includes(format as Format);
+};
+
+export const GET = async ({ fetch, url }) => {
+    const img = url.searchParams.get('img');
+    const format = url.searchParams.get('format') || 'webp';
+    if (!img) throw error(400, 'Missing image parameter');
+    if (!isFormat(format)) throw error(400, 'Invalid format parameter');
+
+    try {
+        const fetchedImg = await fetch(img);
+        const imgBuffer = await fetchedImg.arrayBuffer();
+        return new Response(await sharp(imgBuffer).toFormat(format).toBuffer(), {
+            headers: {
+                'Content-Type': `image/${format}`,
+                'Content-Disposition': `attachment; filename="${getFilename(img, format)}"`
+            }
+        });
+    } catch (e) {
+        throw error(400, 'Invalid image URL');
+    }
+};
