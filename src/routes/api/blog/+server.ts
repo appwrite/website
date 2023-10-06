990175d480e0d548d4d135f2a6843ffdc277de7b
tglide <26071571+TGlide@users.noreply.github.com>
2023-10-06 17:33:34 +0100

    improve blog api

--- src/routes/api/blog/+server.ts
+++ src/routes/api/blog/+server.ts
@@ -1,6 +1,19 @@
-const posts = import.meta.glob('../../blog/**/*.markdoc', {
-    eager: true
-});
+const parsePosts = (posts: Record<string, any>) => {
+    return Object.fromEntries(
+        Object.entries(posts).map(([key, value]) => {
+            return [
+                key.replace('../../blog/post/', '').replace('/+page.markdoc', ''),
+                value.frontmatter
+            ];
+        })
+    );
+};
+
+const posts = parsePosts(
+    import.meta.glob('../../blog/post/**/*.markdoc', {
+        eager: true
+    })
+);
 
 export const GET = async () => {
     return new Response(JSON.stringify({ posts, total: Object.keys(posts).length }), {
