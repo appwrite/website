61cea58fa20ac5974f448e25e94e5ad717e068bd
tglide <26071571+TGlide@users.noreply.github.com>
2023-10-11 16:41:12 +0100

    update cover

--- src/markdoc/tags/Video.svelte
+++ src/markdoc/tags/Video.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
     export let src: string;
     export let type = 'video/mp4';
+    export let autoplay = false;
 </script>
 
 <div class="aw-media">
     <!-- svelte-ignore a11y-media-has-caption -->
-    <video {src} class="aw-u-media-ratio-16-9 u-width-full-line" controls>
+    <video {src} class="aw-u-media-ratio-16-9 u-width-full-line" controls {autoplay}>
         <source {src} {type} />
     </video>
 </div>
