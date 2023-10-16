b4f4941a0644555325e060b4470400c1df92b982
tglide <26071571+TGlide@users.noreply.github.com>
2023-10-16 12:26:40 +0100

    fix icons

--- src/icons/scripts.js
+++ src/icons/scripts.js
@@ -4,12 +4,21 @@ import svgtofont from 'svgtofont';
 import { resolve } from 'path';
 
 const src = resolve(process.cwd(), 'src/icons/svg');
+const optimized = resolve(process.cwd(), 'src/icons/optimized');
 const dist = resolve(process.cwd(), 'src/icons/output');
+
+export const optimizeSVG = async () => {
+    const fixer = new SVGFixer(src, optimized, {
+        showProgressBar: true
+    });
+
+    await fixer.fix();
+};
 
 export const generateIcons = async () => {
     await svgtofont({
         classNamePrefix: 'aw-icon',
-        src: src,
+        src: optimized,
         dist: dist,
         fontName: 'aw-icon',
         styleTemplates: resolve(process.cwd(), 'src/icons/templates'),
@@ -28,12 +37,4 @@ export const generateIcons = async () => {
         emptyDist: true,
         generateInfoData: true
     });
-};
-
-export const optimizeSVG = async () => {
-    const fixer = new SVGFixer(src, src, {
-        showProgressBar: true
-    });
-
-    await fixer.fix();
 };
