43a43f3c4e84d615f6d0bec5bda840e643ab63b6
Prerak Mathur <mathur.prerak@gmail.com>
2023-10-22 00:41:24 +0530

    Tutorial for TS with React

--- src/routes/docs/tutorials/+page.svelte
+++ src/routes/docs/tutorials/+page.svelte
@@ -51,6 +51,17 @@
 						</a>
 					</li>
 					<li class="is-mobile-col-span-2">
+						<a href="/docs/tutorials/typescript" class="aw-card is-normal">
+							<header class="u-flex u-cross-baseline u-gap-4">
+								<span class="icon-typescript aw-u-font-size-24" aria-hidden="true" />
+								<h4 class="aw-sub-body-500 aw-u-color-text-primary">TypeScript</h4>
+							</header>
+							<p class="aw-sub-body-400 u-margin-block-start-4">
+								Learn Appwrite Auth, Databases, and more with TypeScript.
+							</p>
+						</a>
+					</li>
+					<li class="is-mobile-col-span-2">
 						<a href="/docs/tutorials/vue" class="aw-card is-normal">
 							<header class="u-flex u-cross-baseline u-gap-4">
 								<span class="aw-icon-vue aw-u-font-size-24" aria-hidden="true" />
