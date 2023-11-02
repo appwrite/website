5d7d4e1d109be01327a6a40873aeadaa0a7a2db3
tglide <26071571+TGlide@users.noreply.github.com>
2023-11-02 16:54:36 +0000

    fix deploy

--- src/routes/api/banner/+server.ts
+++ src/routes/api/banner/+server.ts
@@ -1,9 +1,5 @@
 import { BANNER_KEY } from './constants.js';
 
-const isVercel = process.env.VERCEL === '1';
-
-export const prerender = isVercel;
-
 export const POST = ({ cookies }) => {
     cookies.set(BANNER_KEY, 'true', {
         path: '/'
