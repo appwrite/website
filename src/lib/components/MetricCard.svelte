75e11e6e29aa01635577b2e995ac13b047139878
tglide <26071571+TGlide@users.noreply.github.com>
2023-11-06 18:06:40 +0000

    metric card

---
+++ src/lib/components/MetricCard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let metric: string;
+    export let description: string;
+</script>
+
+<div class="aw-card is-normal has-border-gradient">
+    <div class="aw-title aw-u-color-text-primary">{metric}</div>
+    <div class="aw-description">{description}</div>
+</div>
+
+<style lang="scss">
+    .aw-card {
+        padding: 1.25rem;
+    }
+</style>
