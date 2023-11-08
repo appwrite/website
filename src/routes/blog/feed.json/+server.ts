39e47e47a63f282ec594d08a684f8c90d3bc1a47
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-11-08 13:40:19 +0100

    fix: prerender endpoints

--- src/routes/blog/feed.json/+server.ts
+++ src/routes/blog/feed.json/+server.ts
@@ -1,11 +1,12 @@
 import type { RequestHandler } from './$types';
 import { posts } from '../content';
+import { json } from '@sveltejs/kit';
+
+export const prerender = true;
 
 export const GET: RequestHandler = () => {
-    return new Response(
-        JSON.stringify({
-            posts,
-            total: Object.keys(posts).length
-        })
-    );
+    return json({
+        posts,
+        total: Object.keys(posts).length
+    });
 };
