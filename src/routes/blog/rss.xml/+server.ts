f9131973e0806044a2c979eb68edacea26192a2d
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-11-08 13:40:19 +0100

    fix: prerender endpoints

--- src/routes/blog/rss.xml/+server.ts
+++ src/routes/blog/rss.xml/+server.ts
@@ -1,6 +1,8 @@
 import type { RequestHandler } from './$types';
 import { posts } from '../content';
 
+export const prerender = true;
+
 export const GET: RequestHandler = () => {
     const feed = `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
