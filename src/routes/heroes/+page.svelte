d1c307f86b358d89805f921eda323432ef03402a
Elad Shechter <elad2412@gmail.com>
2023-11-14 16:15:57 -0300

    Pixelate SCG's in Chrome - update SVG's to PNG to fix issue

--- src/routes/heroes/+page.svelte
+++ src/routes/heroes/+page.svelte
@@ -159,7 +159,7 @@
                         <div class="avatar-3">
                             <FloatingHead src="/images/heroes/avatars/emilia.png" size={64} />
                         </div>
-                        <img class="teal-blur" src="/images/heroes/teal-blur.svg" alt="" />
+                        <img class="teal-blur" src="/images/heroes/teal-blur.png" alt="" />
                     </div>
                 </section>
             </div>
