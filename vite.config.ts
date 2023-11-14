c1fede17be3ecfe520130a8b47aaa6f11ebb9b66
tglide <26071571+TGlide@users.noreply.github.com>
2023-11-14 19:36:08 +0000

    enhance some images

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import dynamicImport from 'vite-plugin-dynamic-import';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
 	plugins: [
+		enhancedImages(),
 		sveltekit(),
 		dynamicImport({
 			filter(id) {
