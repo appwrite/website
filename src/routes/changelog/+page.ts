0295d97ba733b5d5a41a0c9a66e0cc4767c65baa
tglide <26071571+TGlide@users.noreply.github.com>
2023-11-15 17:18:57 +0000

    vercel fix attempt

--- src/routes/changelog/+page.ts
+++ src/routes/changelog/+page.ts
@@ -2,10 +2,12 @@ import { CHANGELOG_DEPENDENCY, getAllChangelogEntries } from './utils';
 
 const PER_PAGE = 5;
 
+const isVercel = import.meta.env.VERCEL === '1';
+
 export const load = async ({ depends, url }) => {
   depends(CHANGELOG_DEPENDENCY);
 
-  const page = parseInt(url.searchParams.get('page') || '1', 10);
+  const page = isVercel ? 10 ** 6 : parseInt(url.searchParams.get('page') || '1', 10);
 
   const entries = await getAllChangelogEntries();
   return {
