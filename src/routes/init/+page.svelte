e9e9dd5c0c0ac58c6941f3ae1db31ab4d44f9bae
tglide <26071571+TGlide@users.noreply.github.com>
2023-11-16 17:48:58 +0000

    init hero

---
+++ src/routes/init/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import { Main } from '$lib/layouts';
+</script>
+
+<Main>
+    <div class="hero">
+        <img src="/images/logos/init.svg" alt="init" />
+        <p class="aw-description">
+            Appwrite is unveiling new features over a week of exciting announcements
+        </p>
+        <div class="buttons">
+            <div class="aw-button">Get your ticket</div>
+            <div class="aw-button is-secondary">Blog announcement</div>
+        </div>
+    </div>
+</Main>
+
+<style lang="scss">
+    .hero {
+        min-height: 500px;
+        height: 70dvh;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 1rem;
+
+        p {
+            max-width: 23.125rem;
+            text-align: center;
+        }
+
+        .buttons {
+            display: flex;
+            gap: 0.5rem;
+
+            padding-block-start: 1rem;
+        }
+    }
+</style>
