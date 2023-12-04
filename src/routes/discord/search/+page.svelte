eabade8c0b642d3aedc578cdc8481d44aaec574d
Elad Shechter <elad2412@gmail.com>
2023-12-04 15:02:46 +0100

    Discord - New pages (empty)

---
+++ src/routes/discord/search/+page.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+    import { DEFAULT_DESCRIPTION, DEFAULT_HOST } from '$lib/utils/metadata';
+    import { Main } from '$lib/layouts';
+    import { TITLE_SUFFIX } from '$routes/titles';
+    import FooterNav from '$lib/components/FooterNav.svelte';
+    import MainFooter from '$lib/components/MainFooter.svelte';
+
+    let email = '';
+    let firstName = '';
+    let subject = '';
+    let message = '';
+    let error: string | undefined;
+    let submitted = false;
+
+    async function handleSubmit() {
+        error = undefined;
+        const response = await fetch('https://growth.appwrite.io/v1/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                firstName,
+                subject,
+                message
+            })
+        });
+        if (response.status >= 400) {
+            error = response.status >= 500 ? 'Server Error.' : 'Error submitting form.';
+            return;
+        }
+
+        submitted = true;
+    }
+
+    const title = 'Contact us' + TITLE_SUFFIX;
+    const description = DEFAULT_DESCRIPTION;
+    const ogImage = DEFAULT_HOST + '/images/open-graph/website.png';
+</script>
+
+<svelte:head>
+    <!-- Titles -->
+    <title>{title}</title>
+    <meta property="og:title" content={title} />
+    <meta name="twitter:title" content={title} />
+    <!-- Desscription -->
+    <meta name="description" content={description} />
+    <meta property="og:description" content={description} />
+    <meta name="twitter:description" content={description} />
+    <!-- Image -->
+    <meta property="og:image" content={ogImage} />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta name="twitter:image" content={ogImage} />
+    <meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+
+<div class="u-position-absolute" style="pointer-events:none;">
+    <enhanced:img src="./bg.png" alt="" />
+</div>
+
+<Main>
+    <div class="aw-big-padding-section">
+        <div class="aw-big-padding-section-level-1 theme-dark">
+            <div class="aw-big-padding-section-level-2">
+                <div class="aw-container">
+                    <div>DISCORD SEARCH PAGE</div>
+                </div>
+            </div>
+            <div class="aw-big-padding-section-level-2">
+                <div class="aw-container">
+                    <FooterNav />
+                    <MainFooter />
+                </div>
+            </div>
+        </div>
+</Main>
