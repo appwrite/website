dd3e0cd21d19a6f00622fa6e6acb4dc569456b90
Elad Shechter <elad2412@gmail.com>
2023-12-05 14:46:35 +0100

    Support Threads - update main styles & layout

--- src/routes/support-threads/Thread.svelte
+++ src/routes/support-threads/Thread.svelte
@@ -21,16 +21,44 @@
             <time class="aw-caption-400 u-margin-inline-start-auto">12 Jan, 2023</time>
         </div>
 
-        <p>{thread.text}</p>
+        <p class="aw-main-body-500 u-margin-block-start-4">{thread.text}</p>
 
-        <h4 class="aw-eyebrow">Replies</h4>
-        <div class="replies">
-            {#each thread.replies as reply}
-                <div class="reply">
-                    <p class="aw-caption-400" use:highlight={highlightTerms}>{reply.text}</p>
-                </div>
-            {/each}
+        <div class="u-flex u-main-space-between u-gap-16 u-margin-block-start-16">
+            <ul class="u-flex u-gap-8">
+                  <li>
+                      <div class="aw-tag">Flutter</div>
+                  </li>
+                <li>
+                    <div class="aw-tag">Self hosted</div>
+                </li>
+            </ul>
+
+            <ul class="u-flex u-gap-8">
+                <li>
+                    <div class="aw-icon-button is-more-content" aria-label="up votes">
+                        <span class="aw-icon-arrow-up" aria-hidden="true"></span>
+                        <span class="aw-caption-400 aw-u-line-height-1-2">4</span>
+                    </div>
+                </li>
+                <li>
+                    <div class="aw-icon-button is-more-content" aria-label="responds">
+                        <span class="aw-icon-discord" aria-hidden="true" style="font-size:1rem"></span>
+                        <span class="aw-caption-400 aw-u-line-height-1-2">4</span>
+                    </div>
+                </li>
+            </ul>
         </div>
+
+        <!--
+            <h4 class="aw-eyebrow">Replies</h4>
+            <div class="replies">
+                {#each thread.replies as reply}
+                    <div class="reply">
+                        <p class="aw-caption-400" use:highlight={highlightTerms}>{reply.text}</p>
+                    </div>
+                {/each}
+            </div>
+        -->
     </a>
 {/key}
 
@@ -64,15 +92,5 @@
 
         border-inline-start: 1px solid hsl(var(--aw-color-greyscale-500));
         margin-inline-start: 0.5rem;
-    }
-
-    a {
-        position: absolute;
-        right: 1rem;
-        top: 1rem;
-
-        &:hover {
-            opacity: 0.5;
-        }
     }
 </style>
