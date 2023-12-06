5e0c75a85669f99ae333d4a0c032f8c8e30fa32e
tglide <26071571+TGlide@users.noreply.github.com>
2023-12-06 17:12:00 +0100

    detail page redesign

--- src/routes/support-threads/[id]/+page.svelte
+++ src/routes/support-threads/[id]/+page.svelte
@@ -7,6 +7,7 @@
     import MainFooter from '$lib/components/MainFooter.svelte';
     import { formatDate } from '$lib/utils/date.js';
     import MessageCard from './MessageCard.svelte';
+    import PreFooter from '../PreFooter.svelte';
 
     export let data;
 
@@ -35,73 +36,145 @@
 </svelte:head>
 
 <Main>
-    <div class="center-grid aw-container">
-        <a class="aw-link aw-u-color-text-secondary" href="/support-threads">
-            <span class="aw-icon-chevron-left" aria-hidden="true" />
-            <span>Back to support threads</span>
-        </a>
-        <div class="thread">
-            {#if timestamp}
-                <p class="aw-caption-400 timestamp">
-                    {formatDate(timestamp)}
-                </p>
-            {/if}
-            <h1 class="aw-title aw-u-color-text-primary">{data.name}</h1>
-            <hr />
-
-            <div class="messages">
-                {#each data.messages ?? [] as message}
-                    <MessageCard {message} />
-                {/each}
+    <div class="aw-container" style="padding-block-end: 0;">
+        <div class="header">
+            <div>
+                <a class="aw-link aw-u-color-text-secondary" href="/support-threads">
+                    <span class="aw-icon-chevron-left" aria-hidden="true" />
+                    <span>Back</span>
+                </a>
+                <h1 class="aw-title aw-u-color-text-primary">{data.name}</h1>
+                <ul class="tags">
+                    <li class="aw-tag">
+                        <span class="aw-icon-arrow-up" />
+                        <span class="text">4</span>
+                    </li>
+                    {#each data.tags ?? [] as tag}
+                        <li class="aw-tag">
+                            <span class="text">{tag}</span>
+                        </li>
+                    {/each}
+                </ul>
             </div>
-
             <a
                 class="aw-button"
                 href="https://discord.com/channels/1096473701832200302/{data.discord_id}"
             >
-                <span class="aw-icon-ext-link" />
-                <span class="text">View on Discord</span>
+                <span class="aw-icon-discord" />
+                <span class="text">View on discord</span>
             </a>
         </div>
+
+        <div class="thread-grid">
+            <div class="messages">
+                {#each data.messages ?? [] as message}
+                    <MessageCard {message} />
+                {/each}
+                <div class="aw-card is-normal has-border-gradient">
+                    <span class="aw-sub-body-500 aw-u-color-text-primary">Reply</span>
+                    <p class="aw-sub-body-500 u-margin-block-start-16">
+                        Reply to this thread by joining our Discord
+                    </p>
+                    <a
+                        class="aw-button u-margin-block-start-24"
+                        href="https://discord.com/channels/1096473701832200302/{data.discord_id}"
+                    >
+                        <span class="aw-icon-discord" />
+                        <span class="text">Reply on discord</span>
+                    </a>
+                </div>
+            </div>
+            <div class="related">
+                <h2 class="aw-eyebrow aw-u-color-text-primary">Recommended threads</h2>
+            </div>
+        </div>
     </div>
-    <div class="aw-container">
-        <FooterNav />
-        <MainFooter />
+    <div class="u-position-relative u-overflow-hidden">
+        <div class="aw-container">
+            <PreFooter />
+            <FooterNav />
+            <MainFooter />
+        </div>
     </div>
 </Main>
 
 <style lang="scss">
-    .center-grid {
+    .header {
         display: grid;
-        grid-template-columns: 1fr 3fr 1fr;
-        gap: 3.4rem;
+        grid-template-columns: 1fr auto;
+        gap: 4rem;
+        align-items: center;
 
-        margin-block-start: 3rem;
+        margin-block-start: 2.5rem;
     }
 
-    .timestamp {
-        margin-block-end: 0.5rem;
+    h1 {
+        margin-block-start: 1rem;
     }
 
-    hr {
-        border-block-end: 1px solid hsl(var(--aw-color-smooth));
-        margin-block: 2rem 3rem;
+    .tags {
+        display: flex;
+        gap: 0.5rem;
+        margin-block-start: 1rem;
+    }
+
+    .aw-tag {
+        display: flex;
+        align-items: center;
+        gap: 0.25rem;
+    }
+
+    .thread-grid {
+        display: grid;
+        grid-template-columns: 2fr 1fr;
+        gap: 3rem;
+
+        margin-block-start: 2rem;
+        border-block-start: 1px solid hsl(var(--aw-color-border));
+        padding-block-end: 5rem;
     }
 
     .messages {
         display: flex;
         flex-direction: column;
         gap: 1rem;
+
+        padding-block-start: 2rem;
+
+        :global(.aw-card) {
+            padding: 1.25rem;
+        }
     }
 
-    .aw-button {
-        margin-block-start: 5rem;
-        margin-inline: auto;
+    .related {
+        height: 100%;
+
+        position: relative;
+        padding-block-start: 2.25rem;
+
+        &::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: -20px;
+            width: 1px;
+            height: calc(100% + 5rem);
+            background-color: hsl(var(--aw-color-border));
+        }
     }
 
     @media #{$break1} {
-        .center-grid {
+        .header {
+            gap: 2rem;
             grid-template-columns: 1fr;
+        }
+
+        .thread-grid {
+            grid-template-columns: 1fr;
+        }
+
+        .related::before {
+            background-color: transparent;
         }
     }
 </style>
