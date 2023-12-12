12b64d3f7dbc3863b687f47acd6ad73450b1f28d
Matej Bačo <matejbaco2000@gmail.com>
2023-12-12 15:30:00 +0000

    Add pricing banner

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,7 @@
-export const BANNER_KEY = 'discord-banner-01'; // Change key to force banner to show again
+export const BANNER_KEY = 'pricing-banner-01'; // Change key to force banner to show again
+
+/**
+ * History:
+ * discord-banner-01
+ * pricing-banner-01
+ */
