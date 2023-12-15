c8d0396d3d6875c61eb1c7ed9d586c49f0c89b50
tglide <26071571+TGlide@users.noreply.github.com>
2023-12-15 15:23:14 +0000

    tickets preview

--- src/routes/init/(components)/CountdownCard.svelte
+++ src/routes/init/(components)/CountdownCard.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { createCountdown, pad } from '../helpers';
+    import { createCountdown } from '../helpers';
     import Counter from './Counter.svelte';
 
     export let date: Date;
@@ -9,7 +9,7 @@
 
 <div class="aw-card is-normal aw-u-color-text-primary">
     <img src="/images/icons/gradients/lock.svg" alt="Lock" />
-    {#if $days >= 1}
+    {#if $days > 1}
         <p class="aw-title">
             {$days}
             {$days > 1 ? 'days' : 'day'}<span class="aw-u-color-text-accent">_</span>
