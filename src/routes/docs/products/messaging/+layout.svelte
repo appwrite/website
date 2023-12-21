48e407e74ecd575abb17c8c67461f561df43e1f0
Vincent (Wen Yu) Ge <wenyu1999@hotmail.com>
2023-12-21 02:02:27 +0000

    Add messaging shell

---
+++ src/routes/docs/products/messaging/+layout.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	import Docs from '$lib/layouts/Docs.svelte';
+	import Sidebar, { type NavParent, type NavTree } from '$lib/layouts/Sidebar.svelte';
+
+	const parent: NavParent = {
+		href: '/docs',
+		label: 'Auth'
+	};
+
+	const navigation: NavTree = [
+		{
+			label: 'Getting started',
+			items: [
+				{
+					label: 'Overview',
+					href: '/docs/products/XXXXX'
+				},
+				{
+					label: 'Quick start',
+					href: '/docs/products/XXXXX/quick-start'
+				}
+			]
+		},
+		{
+			label: 'Concepts',
+			items: [
+				{
+					label: 'Topics',
+					href: '/docs/products/messaging/XXXXX'
+				},
+				{
+					label: 'Providers',
+					href: '/docs/products/messaging/XXXXX'
+				},
+				{
+					label: 'XXXXX',
+					href: '/docs/products/messaging/XXXXX'
+				},
+				
+			]
+		},
+		{
+			label: 'Journeys',
+			items: [
+				{
+					label: 'XXXXX',
+					href: '/docs/products/messaging/XXXXX'
+				},
+			]
+		},
+		{
+			label: 'References',
+			items: [
+				{
+					label: 'XXXXX',
+					href: '/docs/references/cloud/client-web/XXXXX'
+				},
+			]
+		},
+	];
+</script>
+
+<Docs variant="two-side-navs">
+	<Sidebar {navigation} {parent} />
+	<slot />
+</Docs>
