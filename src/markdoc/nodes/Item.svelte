bfc8f3dc53fb3feb0388cc2e9d15e874238c7610
tglide <26071571+TGlide@users.noreply.github.com>
2023-12-26 16:33:47 +0000

    fix policy list styles

--- src/markdoc/nodes/Item.svelte
+++ src/markdoc/nodes/Item.svelte
@@ -2,17 +2,15 @@
     import { isInDocs } from '$lib/layouts/Docs.svelte';
     import { isInChangelog } from '$markdoc/layouts/Changelog.svelte';
     import { isInPolicy } from '$markdoc/layouts/Policy.svelte';
-    import { isInTable } from './Table.svelte';
 
     const inDocs = isInDocs();
     const inChangelog = isInChangelog();
     const inPolicy = isInPolicy();
-    const inTable = isInTable();
 
     $: classes = (() => {
         if (inDocs) return '';
         if (inChangelog) return '';
-        if (inPolicy && inTable) return '';
+        if (inPolicy) return '';
         return 'aw-paragraph-lg';
     })();
 </script>
--- src/markdoc/nodes/Link.svelte
+++ src/markdoc/nodes/Link.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { isInChangelog } from '$markdoc/layouts/Changelog.svelte';
     import { hasContext } from 'svelte';
-    import { isInTable } from './Table.svelte';
 
     export let href: string;
     export let title: string;
@@ -12,22 +11,18 @@
 
     const isDocs = hasContext('isDocs');
     const inChangelog = isInChangelog();
-    const inTable = isInTable();
 
     $: classes = (() => {
-        if (isDocs) return 'aw-link is-inline aw-paragraph-md';
-        if (inChangelog) return 'aw-link is-inline aw-paragraph-lg';
-        if (inTable) return 'aw-link is-inline';
+        if (isDocs) return 'aw-paragraph-md';
+        if (inChangelog) return 'aw-paragraph-lg in-changelog';
         return '';
     })();
 </script>
 
-<a class={classes} data-in-changelog={inChangelog ? '' : undefined} {href} {title} {target} {rel}
-    ><slot /></a
->
+<a class="aw-link is-inline {classes}" {href} {title} {target} {rel}><slot /></a>
 
 <style lang="scss">
-    [data-in-changelog]:last-child {
+    .in-changelog:last-child {
         margin-block-start: 1rem;
     }
 </style>
