d5e3f0efaa5f5b529010c073eddac883a23adc61
tglide <26071571+TGlide@users.noreply.github.com>
2023-12-29 17:39:58 +0000

    basic ticket customization

--- src/routes/init_/ticket/customize/+page.svelte
+++ src/routes/init_/ticket/customize/+page.svelte
@@ -3,8 +3,32 @@
     import MainFooter from '$lib/components/MainFooter.svelte';
     import Main from '$lib/layouts/Main.svelte';
     import ShineSVG from '../../(assets)/shine.svg';
+    import Ticket from './ticket.svelte';
+    import TribeToggle from './tribe-toggle.svelte';
 
-    let name = '';
+    let name = 'Eldad Fux';
+
+    const tribes = [
+        null,
+        'Appwrite',
+        'Flutter',
+        'JavaScript',
+        'Svelte',
+        'React',
+        'Vue',
+        'Angular',
+        'Next',
+        'Apple',
+        'Android',
+        'Windows',
+        'Linux',
+        'Python',
+        'Dart',
+        'php',
+        'Ruby',
+        'NET'
+    ];
+    let tribe: string | null = null;
 </script>
 
 <svelte:head>
@@ -21,6 +45,7 @@
             <h1 class="aw-title aw-u-color-text-primary" style:margin-block-start="1.5rem">
                 Customize ticket<span class="aw-u-color-text-accent">_</span>
             </h1>
+
             <div class="u-flex u-flex-vertical u-gap-4 u-margin-block-start-32">
                 <label for="name">Your name</label>
                 <input
@@ -31,10 +56,42 @@
                     name="name"
                     required
                     bind:value={name}
+                    maxlength="128"
                 />
+            </div>
+
+            <hr />
+
+            <h2 class="aw-sub-body-500 aw-u-color-text-primary">Integrate your Appwrite account</h2>
+            <p class="aw-sub-body-500" style:margin-block-start="0.25rem">
+                Sign in with your Appwrite Account and see the magic happen in your ticket
+            </p>
+            <button class="aw-button is-full-width is-secondary u-margin-block-start-24">
+                <div class="aw-icon-appwrite aw-u-color-text-primary" />
+                <span class="text">Log in to Appwrite Account</span>
+            </button>
+
+            <hr />
+
+            <h2 class="aw-sub-body-500 aw-u-color-text-primary">Add your tribe</h2>
+            <p class="aw-sub-body-500" style:margin-block-start="0.25rem">
+                Add your favorite technology to your Init ticket and join your tribe of like-minded
+                developers
+            </p>
+            <div class="tribes">
+                {#each tribes as t}
+                    <TribeToggle
+                        tribe={t}
+                        checked={t === tribe}
+                        onClick={() => {
+                            tribe = t;
+                        }}
+                    />
+                {/each}
             </div>
         </div>
         <div class="ticket-preview">
+            <Ticket {name} user="eldadfux" id="abc1234" {tribe} />
             <img class="shine" src={ShineSVG} alt="" />
         </div>
     </div>
@@ -66,11 +123,26 @@
         width: 100%;
         aspect-ratio: 0.925 / 1;
 
+        display: grid;
+        place-items: center;
+
         .shine {
             position: absolute;
             inset-block-start: -100px;
             inset-inline-end: -100px;
             z-index: 20;
         }
+    }
+
+    hr {
+        margin-block: 2rem;
+        border-block-end: 1px solid hsl(var(--aw-color-offset));
+    }
+
+    .tribes {
+        margin-block-start: 1.5rem;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1rem;
     }
 </style>
---
+++ src/routes/init_/ticket/customize/ticket.svelte
@@ -0,0 +1,99 @@
+<script lang="ts" context="module">
+    export type TicketProps = {
+        name?: string;
+        user?: string;
+        id: string;
+        showGithub?: boolean;
+        tribe?: string | null;
+    };
+</script>
+
+<script lang="ts">
+    import BG from './(assets)/ticket-bg.svg';
+    import Logo from './(assets)/logo.svg';
+
+    type $$Props = TicketProps;
+    $: ({ name, user, id, showGithub, tribe } = $$props as $$Props);
+</script>
+
+<div class="ticket">
+    <img src={BG} alt="" class="bg" />
+    <p class="aw-title aw-u-color-text-primary">{name || 'Wade Wilson'}</p>
+    {#if user}
+        <p class="aw-label" style:margin-block-start="0.25rem">@{user}</p>
+    {/if}
+    <div class="bottom-left">
+        <img src={Logo} alt="init_" />
+        <span>JAN 15 - 20</span>
+    </div>
+
+    <span class="id">#{id}</span>
+    {#if tribe}
+        <img class="tribe" src={`/images/tribes/${tribe.toLowerCase()}.svg`} alt={tribe} />
+    {/if}
+</div>
+
+<style lang="scss">
+    .ticket {
+        width: 28.75rem;
+        aspect-ratio: 460 / 644;
+
+        padding: 2rem;
+        padding-block-start: 3.5rem;
+        position: relative;
+        overflow: hidden;
+    }
+
+    .bg {
+        position: absolute;
+        inset: 0;
+        z-index: -1;
+    }
+
+    .bottom-left {
+        position: absolute;
+        inset-block-end: 2rem;
+        inset-inline-start: 2rem;
+
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+
+        img {
+            width: 5.6875rem;
+        }
+
+        span {
+            font-family: var(--aw-font-family-aeonik-fono);
+            color: hsl(var(--aw-color-primary));
+            font-size: 0.875rem;
+            font-style: normal;
+            font-weight: 400;
+            line-height: 1rem; /* 114.286% */
+        }
+    }
+
+    .id {
+        position: absolute;
+        inset-block-end: 2rem;
+        inset-inline-end: 2rem;
+
+        color: rgba(255, 255, 255, 0.5);
+        font-family: var(--aw-font-family-aeonik-fono);
+        font-size: 0.875rem;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 1rem; /* 114.286% */
+    }
+
+    .tribe {
+        position: absolute;
+        inset-block-end: -6rem;
+        inset-inline-start: -8rem;
+
+        width: 28.75rem;
+        /* Make it white */
+        filter: brightness(0) invert(1);
+        opacity: 0.04;
+    }
+</style>
