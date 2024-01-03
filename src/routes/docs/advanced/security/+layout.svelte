a2694f3dc88496fa804a5e336883aebea7c96eb9
Vincent (Wen Yu) Ge <wenyu1999@hotmail.com>
2024-01-03 00:52:43 +0000

    Adds beginning of page for security page

---
+++ src/routes/docs/advanced/security/+layout.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import Docs from '$lib/layouts/Docs.svelte';
+	import Sidebar, { type NavParent, type NavTree } from '$lib/layouts/Sidebar.svelte';
+
+	const parent: NavParent = {
+		href: '/docs',
+		label: 'Security'
+	};
+
+	const navigation: NavTree = [
+		{
+			label: 'Getting started',
+			items: [
+				{
+					label: 'Overview',
+					href: '/docs/advanced/security'
+				}
+			]
+		},
+		{
+			label: 'Guides',
+			items: [
+				{
+					label: 'Compliance',
+					href: '/docs/advanced/security/compliance'
+				},
+				{
+					label: 'DPA',
+					href: '/docs/advanced/security/dpa'
+				},
+				{
+					label: 'Endpoint',
+					href: '/docs/advanced/security/endpoint'
+				},
+				{
+					label: 'Authentication',
+					href: '/docs/products/auth/security'
+				},
+			]
+		}
+	];
+</script>
+
+<Docs variant="two-side-navs">
+	<Sidebar {navigation} {parent} />
+	<slot />
+</Docs>
