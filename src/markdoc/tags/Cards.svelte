cef55f28f40b634e8cb7a0e8b36b7b8253907485
tglide <26071571+TGlide@users.noreply.github.com>
2024-01-04 00:19:44 +0000

    add margin

--- src/markdoc/tags/Cards.svelte
+++ src/markdoc/tags/Cards.svelte
@@ -1,5 +1,5 @@
-<div class="u-margin-block-start-24">
-	<ul class="aw-grid-row-2">
-		<slot />
-	</ul>
+<div style:margin-block="24px">
+    <ul class="aw-grid-row-2">
+        <slot />
+    </ul>
 </div>
