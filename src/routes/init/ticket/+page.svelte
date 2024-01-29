9aef0ad6512ad7b657059fcc0392f9dd7ac20834
tglide <26071571+TGlide@users.noreply.github.com>
2024-01-29 18:46:52 +0000

    mock

--- src/routes/init/ticket/+page.svelte
+++ src/routes/init/ticket/+page.svelte
@@ -22,7 +22,7 @@
                         class="aw-u-color-text-accent">_</span
                     >
                 </span>
-                launches on February 5th
+                launches on February 12th
             </h1>
 
             <div class="info">
@@ -60,8 +60,8 @@
                 </div>
                 <div class="ticket-2">
                     <Ticket
-                        name="Eldad"
-                        gh_user="eldadfux"
+                        name="Walter O'Brien"
+                        gh_user="walterobrien"
                         id={1}
                         contributions={getMockContributions()}
                     />
