7329fb39d1c2dae4b4046a32b2e708225a51c970
Laura Du Ry <123562732+LauraDuRy@users.noreply.github.com>
2024-02-01 08:46:15 +0100

    Update MobileNav.svelte

--- src/lib/components/MobileNav.svelte
+++ src/lib/components/MobileNav.svelte
@@ -43,7 +43,7 @@
             >
                 <span class="aw-icon-star" aria-hidden="true" />
                 <span class="text">Star on GitHub</span>
-                <span class="aw-inline-tag aw-sub-body-400">38.4K</span>
+                <span class="aw-inline-tag aw-sub-body-400">39.1K</span>
             </a>
         </div>
     </div>
