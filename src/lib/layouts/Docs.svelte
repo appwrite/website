a1ead854d43454355268fa485b52cad7c5952278
Laura Du Ry <123562732+LauraDuRy@users.noreply.github.com>
2024-02-01 08:46:44 +0100

    Update Docs.svelte

--- src/lib/layouts/Docs.svelte
+++ src/lib/layouts/Docs.svelte
@@ -169,7 +169,7 @@
                     >
                         <span class="aw-icon-star" aria-hidden="true" />
                         <span class="text">Star on GitHub</span>
-                        <span class="aw-inline-tag aw-sub-body-400">38.4K</span>
+                        <span class="aw-inline-tag aw-sub-body-400">39.1K</span>
                     </a>
                     <a href="https://cloud.appwrite.io/console" class="aw-button">
                         <span class="aw-sub-body-500">Go to console</span>
