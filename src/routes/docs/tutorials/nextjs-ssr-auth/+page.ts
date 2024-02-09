07b669543172b7a8ceaf1facd8cf70b7c15ddabf
loks0n <22452787+loks0n@users.noreply.github.com>
2024-02-09 17:33:50 +0000

    chore: fix redirects

--- src/routes/docs/tutorials/nextjs-ssr-auth/+page.ts
+++ src/routes/docs/tutorials/nextjs-ssr-auth/+page.ts
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async () => {
-	throw redirect(303, '/docs/tutorials/sveltekit-auth/step-1');
+    throw redirect(303, '/docs/tutorials/nextjs-ssr-auth/step-1');
 };
