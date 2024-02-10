0e8a3590eeda0cc39d70aa9ade41a7612b1050bb
loks0n <22452787+loks0n@users.noreply.github.com>
2024-02-10 21:52:35 +0000

    chore: autotype load

--- src/routes/docs/tutorials/sveltekit-csr-auth/+page.ts
+++ src/routes/docs/tutorials/sveltekit-csr-auth/+page.ts
@@ -1,6 +1,5 @@
 import { redirect } from '@sveltejs/kit';
-import type { PageLoad } from './$types';
 
-export const load: PageLoad = async () => {
+export function load() {
     throw redirect(303, '/docs/tutorials/sveltekit-csr-auth/step-1');
-};
+}
