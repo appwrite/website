3e81e7ee368a89ec7c4bbf5e59d03b6aefae3756
loks0n <22452787+loks0n@users.noreply.github.com>
2024-02-10 21:52:35 +0000

    chore: autotype load

--- src/routes/docs/tutorials/nextjs-ssr-auth/+page.ts
+++ src/routes/docs/tutorials/nextjs-ssr-auth/+page.ts
@@ -1,6 +1,5 @@
 import { redirect } from '@sveltejs/kit';
-import type { PageLoad } from './$types';
 
-export const load: PageLoad = async () => {
+export function load() {
     throw redirect(303, '/docs/tutorials/nextjs-ssr-auth/step-1');
-};
+}
--- src/routes/docs/tutorials/nuxt-ssr-auth/+page.ts
+++ src/routes/docs/tutorials/nuxt-ssr-auth/+page.ts
@@ -1,6 +1,5 @@
 import { redirect } from '@sveltejs/kit';
-import type { PageLoad } from './$types';
 
-export const load: PageLoad = async () => {
+export function load() {
     throw redirect(303, '/docs/tutorials/nuxt-ssr-auth/step-1');
-};
+}
