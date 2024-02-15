9c54b9d627e8eb9f48bc801fb2202c3ac1ef3465
tglide <26071571+TGlide@users.noreply.github.com>
2024-02-15 16:17:13 +0000

    draft preview

--- src/routes/products/messaging/(components)/Draft.svelte
+++ src/routes/products/messaging/(components)/Draft.svelte
@@ -3,8 +3,8 @@
     import Marker from '../(assets)/marker.svg';
     import MarkerActive from '../(assets)/marker-active.svg';
 
-    let title = 'Edit me';
-    let message = 'Edit me';
+    let title = 'Edit the title on the console';
+    let message = 'Message goes here';
 </script>
 
 <Step title="Step 1: Draft">
@@ -31,12 +31,40 @@
                     <hr />
 
                     <label class="aw-u-color-text-primary" for="title">Title</label>
-                    <input class="aw-input-text" type="text" id="title" name="title" />
+                    <input
+                        class="aw-input-text"
+                        type="text"
+                        id="title"
+                        name="title"
+                        bind:value={title}
+                        maxlength="48"
+                    />
                     <label class="aw-u-color-text-primary" for="message">Message</label>
-                    <textarea class="aw-input-text" id="message" name="message" />
+                    <textarea
+                        class="aw-input-text"
+                        id="message"
+                        name="message"
+                        bind:value={message}
+                    />
                 </div>
             </div>
         </div>
+        <div class="phone">
+            <div class="notch" />
+            <span class="date">Sunday, October 1</span>
+            <span class="time">9:41</span>
+        </div>
+
+        <div class="notification">
+            <div class="header">
+                <div class="icon-bell" />
+                <span>APPWRITE</span>
+                <span>now</span>
+            </div>
+            <span class="title aw-caption-500">{title}</span>
+            <span class="message">{message}</span>
+        </div>
+
         <div class="overlay" />
     </div>
     <div class="text">
@@ -60,9 +88,103 @@
             hsl(0, 0%, 100%, 0)
         );
 
-        aspect-ratio: 16 / 9;
-
+        aspect-ratio: 16 / 11;
         position: relative;
+        overflow: hidden;
+
+        .inner {
+            border: 8px solid #2d2d2f;
+            border-radius: 18px;
+            overflow: hidden;
+            height: 100%;
+
+            .header {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                padding-inline: 2.5rem;
+                padding-block: 20px;
+                background: hsl(240, 6%, 10%);
+                border-block-end: 1px solid hsl(var(--aw-color-offset));
+                color: white;
+            }
+
+            .griddy {
+                display: grid;
+                grid-template-columns: 260px 1fr;
+                gap: 0;
+                height: 100%;
+
+                .sidebar {
+                    background-color: #1c1c20;
+                    padding: 2.5rem;
+
+                    ul {
+                        display: flex;
+                        flex-direction: column;
+                        gap: 3rem;
+
+                        li {
+                            position: relative;
+                            display: flex;
+                            align-items: center;
+                            gap: 0.75rem;
+
+                            &::before {
+                                content: '';
+                                background: var(--marker);
+                                background-repeat: no-repeat;
+                                background-position: center center;
+                                background-size: cover;
+                                display: block;
+
+                                width: 1rem;
+                                height: 1rem;
+                            }
+
+                            &:not(:last-child)::after {
+                                content: '';
+                                position: absolute;
+                                inset-inline-start: 0.45rem;
+                                inset-block-end: -40px;
+                                height: 32px;
+                                width: 1px;
+                                border-inline-end: 1px solid hsl(var(--aw-color-offset));
+                            }
+
+                            &:first-child {
+                                color: white;
+
+                                &::before {
+                                    background: var(--marker-active);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                .content {
+                    background-color: #242427;
+                    padding: 2.5rem 2rem;
+
+                    hr {
+                        border-block-end: 1px solid hsl(var(--aw-color-offset));
+                        margin-block: 2rem;
+                    }
+
+                    label {
+                        display: block;
+                        margin-block-start: 2rem;
+                    }
+
+                    input,
+                    textarea {
+                        --p-input-bg-color: 0 0 0 / 0.16;
+                        margin-block-start: 0.25rem;
+                    }
+                }
+            }
+        }
 
         .overlay {
             position: absolute;
@@ -72,97 +194,107 @@
             pointer-events: none;
         }
 
-        .inner {
-            border: 8px solid #2d2d2f;
-            border-radius: 18px;
-            overflow: hidden;
-            height: 100%;
+        .phone {
+            @include border-gradient;
+            --m-border-radius: 60px 60px 0 0;
+            --m-border-gradient-before: linear-gradient(
+                to bottom,
+                hsl(0, 0%, 100%, 0.12),
+                hsl(0, 0%, 100%, 0)
+            );
+            width: 364px;
+            height: 700px;
+
+            position: absolute;
+            inset-inline-end: 10%;
+            inset-block-end: 0;
+
+            background-color: hsl(0, 0%, 100%, 0.02);
+            backdrop-filter: blur(42px);
+
+            .notch {
+                position: absolute;
+                inset-inline-start: 50%;
+                inset-block-start: 1.5rem;
+                transform: translateX(-50%);
+                block-size: 1.5rem;
+                inline-size: 7.5rem;
+                background-color: hsl(0, 0%, 0%, 0.25);
+                border-radius: 1000px;
+            }
+
+            .date {
+                position: absolute;
+                inset-inline-start: 50%;
+                inset-block-start: 5rem;
+                transform: translateX(-50%);
+                color: hsl(0, 0%, 100%, 0.24);
+                font-size: 0.75rem;
+            }
+
+            .time {
+                position: absolute;
+                inset-inline-start: 50%;
+                inset-block-start: 5rem;
+                transform: translateX(-50%);
+                color: hsl(0, 0%, 100%, 0.24);
+                font-size: 5.25rem;
+                font-weight: 600;
+            }
         }
 
-        .header {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding-inline: 2.5rem;
-            padding-block: 20px;
-            background: hsl(240, 6%, 10%);
-            border-block-end: 1px solid hsl(var(--aw-color-offset));
-            color: white;
-        }
+        .notification {
+            @include border-gradient;
+            --m-border-radius: 20px;
+            --m-border-gradient-before: linear-gradient(
+                to bottom,
+                hsl(0, 0%, 100%, 0.12),
+                hsl(0, 0%, 100%, 0)
+            );
+            background-color: hsl(0, 0%, 100%, 0.06);
+            backdrop-filter: blur(42px);
 
-        .griddy {
-            display: grid;
-            grid-template-columns: 260px 1fr;
-            gap: 0;
-            height: 100%;
+            padding: 1.125rem;
 
-            .sidebar {
-                background-color: #1c1c20;
-                padding: 2.5rem;
+            position: absolute;
+            inset-inline-end: 2.5rem;
+            inset-block-start: 20rem;
 
-                ul {
-                    display: flex;
-                    flex-direction: column;
-                    gap: 3rem;
+            width: 23.75rem;
+            min-height: 8.75rem;
 
-                    li {
-                        position: relative;
-                        display: flex;
-                        align-items: center;
-                        gap: 0.75rem;
+            .header {
+                display: flex;
+                align-items: center;
+                gap: 0.375rem;
 
-                        &::before {
-                            content: '';
-                            background: var(--marker);
-                            background-repeat: no-repeat;
-                            background-position: center center;
-                            background-size: cover;
-                            display: block;
+                [class*='icon'] {
+                    color: hsl(var(--aw-color-accent));
+                }
 
-                            width: 1rem;
-                            height: 1rem;
-                        }
+                span:not(:last-child) {
+                    font-size: 0.75rem;
+                }
 
-                        &:not(:last-child)::after {
-                            content: '';
-                            position: absolute;
-                            inset-inline-start: 0.45rem;
-                            inset-block-end: -40px;
-                            height: 32px;
-                            width: 1px;
-                            border-inline-end: 1px solid hsl(var(--aw-color-offset));
-                        }
-
-                        &:first-child {
-                            color: white;
-
-                            &::before {
-                                background: var(--marker-active);
-                            }
-                        }
-                    }
+                span:last-child {
+                    margin-inline-start: auto;
+                    font-size: 0.75rem;
                 }
             }
 
-            .content {
-                background-color: #242427;
-                padding: 2.5rem 2rem;
+            .title {
+                display: block;
+                margin-block-start: 0.5rem;
+                color: hsl(var(--aw-color-primary));
+                overflow-wrap: break-word;
+            }
 
-                hr {
-                    border-block-end: 1px solid hsl(var(--aw-color-offset));
-                    margin-block: 2rem;
-                }
-
-                label {
-                    display: block;
-                    margin-block-start: 2rem;
-                }
-
-                input,
-                textarea {
-                    --p-input-bg-color: 0 0 0 / 0.16;
-                    margin-block-start: 0.25rem;
-                }
+            .message {
+                display: block;
+                margin-block-start: 0.25rem;
+                color: hsl(var(--aw-color-text-secondary));
+                font-size: 0.875rem;
+                overflow-wrap: break-word;
             }
         }
     }
