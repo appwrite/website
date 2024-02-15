b3f16775c4e3cd61ed38e2e71bd524545777517b
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-02-15 20:23:21 +0000

    fix: cta cloud flicker

---
+++ src/lib/components/IsLoggedIn.svelte
@@ -0,0 +1,21 @@
+<div class="logged-in">
+    <slot name="isLoggedIn" />
+</div>
+<div class="not-logged-in">
+    <slot name="isNotLoggedIn" />
+</div>
+
+<style lang="scss">
+    :global(body[data-logged-in]) {
+        .logged-in {
+            display: none;
+            display: block;
+        }
+    }
+    :global(body[data-logged-in]) {
+        .not-logged-in {
+            display: block;
+            display: none;
+        }
+    }
+</style>
