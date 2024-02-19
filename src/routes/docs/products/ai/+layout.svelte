4bc720a8c2b9cb37cb5d2c7a1c0f9a80f18af69f
Bradley Schofield <ionicisere@gmail.com>
2024-02-19 10:17:41 +0000

    Switch main page to use svelte

--- src/routes/docs/products/ai/+layout.svelte
+++ src/routes/docs/products/ai/+layout.svelte
@@ -1,51 +1,60 @@
 <script lang="ts">
-	import Docs from '$lib/layouts/Docs.svelte';
-	import Sidebar, { type NavParent, type NavTree } from '$lib/layouts/Sidebar.svelte';
+    import Docs from '$lib/layouts/Docs.svelte';
+    import Sidebar, { type NavParent, type NavTree } from '$lib/layouts/Sidebar.svelte';
 
-	const parent: NavParent = {
-		href: '/docs',
-		label: 'AI'
-	};
+    const parent: NavParent = {
+        href: '/docs',
+        label: 'AI'
+    };
 
-	const navigation: NavTree = [
-		{
-			label: 'Getting started',
-			items: [
-				{
-					label: 'Overview',
-					href: '/docs/products/ai'
-				},
-				{
-					label: 'Quick start',
-					href: '/docs/products/ai/quick-start'
-				},
-			]
-		},
-		{
-			label: 'Concepts',
-			items: [
-				{
-					label: 'Computer Vision',
-					href: '/docs/products/ai/computer-vision'
-				},
-				{
-					label: 'Natural Language Processing',
-					href: '/docs/products/ai/natural-language-processing'
-				},
-				{
-					label: 'Audio Processing',
-					href: '/docs/products/ai/audio-processing'
-				},
-				{
-					label: 'Video Processing',
-					href: '/docs/products/ai/video-processing'
-				}
-			]
-		}
-	];
+    const navigation: NavTree = [
+        {
+            label: 'Getting started',
+            items: [
+                {
+                    label: 'Overview',
+                    href: '/docs/products/ai'
+                }
+            ]
+        },
+        {
+            label: 'Concepts',
+            items: [
+                {
+                    label: 'Computer Vision',
+                    href: '/docs/products/ai/computer-vision'
+                },
+                {
+                    label: 'Natural Language Processing',
+                    href: '/docs/products/ai/natural-language'
+                },
+                {
+                    label: 'Audio Processing',
+                    href: '/docs/products/ai/audio-processing'
+                },
+                {
+                    label: 'Video Processing',
+                    href: '/docs/products/ai/video-processing'
+                }
+            ]
+        },
+        {
+            label: 'Tutorials',
+            items: [
+                {
+                    label: 'Image Recognition',
+                    href: '/docs/products/ai/tutorials/image-recognition'
+                },
+                {
+                    label: 'Object Detection',
+                    href: '/docs/products/ai/tutorials/object-detection'
+                }
+            ]
+        }
+    ];
 </script>
 
 <Docs variant="two-side-navs">
-	<Sidebar {navigation} {parent} />
-	<slot />
+    <Sidebar {navigation} {parent} />
+    <slot />
 </Docs>
---
+++ src/routes/docs/products/ai/+page.svelte
@@ -0,0 +1,281 @@
+<script lang="ts">
+    import { Carousel, Spline } from '$lib/components';
+    import Technologies from '$lib/components/Technologies.svelte';
+    import { DEFAULT_DESCRIPTION, DEFAULT_HOST } from '$lib/utils/metadata';
+    import { TITLE_SUFFIX } from '$routes/titles';
+    import MainFooter from '$lib/components/MainFooter.svelte';
+    import CodeCard, { type CodeCardProps } from '../../CodeCard.svelte';
+
+    const title = 'Docs' + TITLE_SUFFIX;
+    const description = DEFAULT_DESCRIPTION;
+    const ogImage = DEFAULT_HOST + '/images/open-graph/docs.png';
+
+    const tutorials: CodeCardProps[] = [
+        {
+            href: '/docs/products/ai/image-recognition/tutorials',
+            cover: '/images/tutorials/react.png',
+            title: 'Image Recognition Tutorial',
+            description: 'Label entire images with ease using Hugging Face'
+        },
+        {
+            href: '/docs/products/ai/natural-language/tutorials',
+            cover: '/images/tutorials/svelte.png',
+            title: 'Object Detection Tutorial',
+            description: 'Annotate and label objects within an Image'
+        }
+    ];
+</script>
+
+<svelte:head>
+    <!-- Titles -->
+    <title>{title}</title>
+    <meta property="og:title" content={title} />
+    <meta name="twitter:title" content={title} />
+    <!-- Desscription -->
+    <meta name="description" content={description} />
+    <meta property="og:description" content={description} />
+    <meta name="twitter:description" content={description} />
+    <!-- Image -->
+    <meta property="og:image" content={ogImage} />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta name="twitter:image" content={ogImage} />
+    <meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+
+<div>
+    <div class="u-position-absolute u-inset-inline-end-0 aw-u-opacity-40-mobile" style="">
+        <enhanced:img src="./blur-1.png" alt="" />
+    </div>
+
+    <main
+        class="aw-main-section u-position-relative aw-u-overflow-hidden-break1-to-break3"
+        id="main"
+    >
+        <div class="u-position-absolute aw-u-opacity-40-mobile bg-blur">
+            <img src="/images/bgs/docs-blur-1.svg" alt="" />
+        </div>
+
+        <div
+            class="u-position-absolute u-inset-inline-start-0 u-inset-block-start-16 aw-u-opacity-40-mobile"
+            style="    margin-inline-start: -30px;"
+        >
+            <enhanced:img src="./blur-2.png" alt="" />
+        </div>
+
+        <section class="aw-hero is-align-start u-position-relative e-hero-docs">
+            <h1 class="aw-display aw-u-color-text-primary u-max-width-600">
+                Learn how to build AI like a team of hundreds<span class="aw-u-color-text-accent"
+                    >_
+                </span>
+            </h1>
+            <p class="aw-description u-max-width-600">
+                Appwrite allows you to build powerful AI powered applications with ease, Leverage
+                Appwrite's powerful functions architecture and start building the future.
+            </p>
+        </section>
+        <section>
+            <h2 class="aw-title aw-u-color-text-primary u-max-width-600">Show me some code</h2>
+            <p class="aw-description u-max-width-600 u-margin-block-start-16">
+                If you learn best from code examples, follow one of our tutorials.
+            </p>
+            <Carousel size="medium">
+                {#each tutorials as tutorial}
+                    <li style:scroll-snap-align="start">
+                        <CodeCard {...tutorial} />
+                    </li>
+                {/each}
+            </Carousel>
+        </section>
+        <section class="aw-hero is-align-start is-no-max-width">
+            <h2 class="aw-title aw-u-color-text-primary u-max-width-600">Explore capabilities</h2>
+            <p class="aw-description u-max-width-600">
+                Detailed explanations and deep dives into how you can implement different machine
+                learning techiques
+            </p>
+            <div class="u-margin-block-start-24">
+                <ul class="aw-grid-row-4 aw-grid-row-4-m-1">
+                    <li>
+                        <a href="/docs/ai/computer-vision" class="aw-card is-normal">
+                            <img
+                                src="/images/icons/illustrated/dark/auth.png"
+                                alt=""
+                                class="u-only-dark"
+                                width="48"
+                                height="48"
+                            />
+                            <img
+                                src="/images/icons/illustrated/light/auth.png"
+                                alt=""
+                                class="u-only-light"
+                                width="48"
+                                height="48"
+                            />
+                            <h4
+                                class="aw-sub-body-500 aw-u-color-text-primary u-margin-block-start-8"
+                            >
+                                Computer Vision
+                            </h4>
+                            <p class="aw-sub-body-400 u-margin-block-start-4">
+                                Understand and label the contents of images
+                            </p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/docs/ai/natural-language" class="aw-card is-normal">
+                            <img
+                                src="/images/icons/illustrated/dark/databases.png"
+                                alt=""
+                                class="u-only-dark"
+                                width="48"
+                                height="48"
+                            />
+                            <img
+                                src="/images/icons/illustrated/light/databases.png"
+                                alt=""
+                                class="u-only-light"
+                                width="48"
+                                height="48"
+                            />
+                            <h4
+                                class="aw-sub-body-500 aw-u-color-text-primary u-margin-block-start-8"
+                            >
+                                Natural Language Processing
+                            </h4>
+                            <p class="aw-sub-body-400 u-margin-block-start-4">
+                                Store your application and user data.
+                            </p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/docs/ai/audio-processing" class="aw-card is-normal">
+                            <img
+                                src="/images/icons/illustrated/dark/functions.png"
+                                alt=""
+                                class="u-only-dark"
+                                width="48"
+                                height="48"
+                            />
+                            <img
+                                src="/images/icons/illustrated/light/functions.png"
+                                alt=""
+                                class="u-only-light"
+                                width="48"
+                                height="48"
+                            />
+                            <h4
+                                class="aw-sub-body-500 aw-u-color-text-primary u-margin-block-start-8"
+                            >
+                                Audio Processing
+                            </h4>
+                            <p class="aw-sub-body-400 u-margin-block-start-4">
+                                Extend and customize your server's functionality.
+                            </p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/docs/ai/video-processing" class="aw-card is-normal">
+                            <img
+                                src="/images/icons/illustrated/dark/storage.png"
+                                alt=""
+                                class="u-only-dark"
+                                width="48"
+                                height="48"
+                            />
+                            <img
+                                src="/images/icons/illustrated/light/storage.png"
+                                alt=""
+                                class="u-only-light"
+                                width="48"
+                                height="48"
+                            />
+                            <h4
+                                class="aw-sub-body-500 aw-u-color-text-primary u-margin-block-start-8"
+                            >
+                                Video Processing
+                            </h4>
+                            <p class="aw-sub-body-400 u-margin-block-start-4">
+                                Store images, videos, documents, and files.
+                            </p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <MainFooter variant="docs" />
+    </main>
+</div>
+
+<style lang="scss">
+    .e-hero-docs {
+        @media (min-width: 1280px) {
+            margin-block-start: 5rem;
+        }
+    }
+    .aw-main-section {
+        max-inline-size: unset;
+        margin-inline: unset;
+        @media (min-width: 1280px) {
+            padding-inline-start: 3rem; // 48px
+        }
+    }
+
+    .bg-blur {
+        inset-inline-end: -300px;
+        inset-block-start: -100px;
+    }
+
+    .tech-hero {
+        @include border-block-gradient;
+
+        --m-border-size: 1px;
+        --m-border-gradient-before: linear-gradient(
+            to right,
+            hsl(var(--aw-color-smooth)) 0%,
+            hsl(var(--aw-color-smooth)) 90%,
+            transparent
+        );
+
+        max-inline-size: unset;
+        margin-block-start: 2rem;
+        margin-inline-start: -3rem;
+        padding-block: 5rem;
+        padding-inline-start: 3rem;
+
+        position: relative;
+        z-index: 10;
+
+        .spline-wrapper {
+            --p-size: 700px;
+            inline-size: var(--p-size);
+            block-size: var(--p-size);
+            inset-block-start: -3rem;
+            z-index: -1;
+            left: calc(50% - 384px + 600px);
+        }
+
+        .bg-overlay {
+            position: absolute;
+
+            background: linear-gradient(to right, #ffffff00 0%, #ffffff00 10%, #ffffff);
+
+            top: 0;
+            right: 10rem;
+            translate: 100%;
+            width: 25rem;
+            height: 100%;
+            content: '';
+            z-index: 9999;
+        }
+    }
+
+    :global(.theme-dark) .tech-hero {
+        background: linear-gradient(to right, #19191c80, #19191c00);
+        backdrop-filter: blur(1rem);
+        -webkit-backdrop-filter: blur(1rem);
+    }
+
+    :global(.theme-dark) .bg-overlay {
+        background: linear-gradient(to right, #19191c00 0%, #19191c00 10%, #19191c);
+    }
+</style>
