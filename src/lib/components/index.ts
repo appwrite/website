7cdf3d280e0e00a29e4c87dbc0eb520c4d6419a8
Arman <arman@appwrite.io>
2024-02-21 16:23:15 +0100

    chore: remove spline

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -5,7 +5,6 @@ export { default as MobileNav } from './MobileNav.svelte';
 export { default as Switch } from './Switch.svelte';
 export { default as Newsletter } from './Newsletter.svelte';
 export { default as Tooltip } from './Tooltip.svelte';
-export { default as Spline } from './Spline.svelte';
 export { default as Article } from './Article.svelte';
 export { default as Carousel } from './Carousel.svelte';
 export { default as FloatingHeads } from './FloatingHeads.svelte';
