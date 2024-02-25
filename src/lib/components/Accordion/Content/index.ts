9dc62c4d9652c90e325f2ad0c55842010b33a4ff
Christy Jacob <christyjacob4@gmail.com>
2024-02-25 20:42:35 +0000

    chore: review comments

--- src/lib/components/Accordion/Content/index.ts
+++
@@ -1,9 +0,0 @@
-import Request from "./Request.svelte"
-import Response from "./Response.svelte"
-import Table from "./Table.svelte"
-
-export {
-    Request as ContentRequest,
-    Response as ContentResponse,
-    Table as ContentTable,
-}
