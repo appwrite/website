30846f3f57f103d3c854fa2943a9f52ccef1449f
Christy Jacob <christyjacob4@gmail.com>
2024-02-25 20:24:56 +0000

    chore: address review comments

--- src/lib/components/Accordion/Item.svelte
+++ src/lib/components/Accordion/Item.svelte
@@ -1,16 +1,26 @@
 <script lang="ts">
     export let open: boolean = false;
+    export let title: string = '';
 </script>
 
 <li class="collapsible-item">
     <details class="collapsible-wrapper" {open}>
-        <slot name="trigger"/>
-        <slot name="content"/>
+        <summary class="collapsible-button">
+            <span class="text">{title}</span>
+            <div class="icon aw-u-color-text-primary">
+                <span class="icon-cheveron-down" aria-hidden="true" />
+            </div>
+        </summary>
+        <slot/>
     </details>
 </li>
 
 <style>
     .collapsible-item {
         border-block-end: 0.0625rem solid hsl(var(--aw-color-offset));
+    }
+
+    .collapsible-button {
+        padding-block: 1rem;
     }
 </style>
