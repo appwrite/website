e08cec1a6ded311c39b0c43f4a163dd8d718bcb6
Christy Jacob <christyjacob4@gmail.com>
2024-02-25 21:13:50 +0000

    chore: update accordion

--- src/lib/components/Accordion/Root.svelte
+++ src/lib/components/Accordion/Root.svelte
@@ -1,9 +1,3 @@
-<!-- AccordionRoot.svelte -->
-<script>
-    import { getContext, setContext } from 'svelte';
-    import { writable } from 'svelte/store';
-</script>
-
 <ul class="collapsible u-width-full-line" style="--p-toggle-border-color: var(--aw-color-border);">
     <slot />
 </ul>
