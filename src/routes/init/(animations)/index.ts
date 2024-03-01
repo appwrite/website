0b961c00fdfa1407e9f0e82a4946b27472675443
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-03-01 11:58:41 +0100

    feat: day 5

--- src/routes/init/(animations)/index.ts
+++ src/routes/init/(animations)/index.ts
@@ -3,11 +3,13 @@ import Messaging from './Messaging.svelte';
 import SSR from './SSR.svelte';
 import Enum from './Enum.svelte';
 import Operators from './Operators.svelte';
+import Integrations from './Integrations.svelte';
 
 export const Animations = {
     Logo,
     Messaging,
     SSR,
     Enum,
-    Operators
+    Operators,
+    Integrations
 };
