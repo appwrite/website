f3f0dfb34f2704f4c22e3dbbe6bcd47b651a9212
Arman <arman@appwrite.io>
2024-03-06 14:25:50 +0100

    vincent -.-

--- src/routes/docs/references/[version]/[platform]/[service]/+page.svelte
+++ src/routes/docs/references/[version]/[platform]/[service]/+page.svelte
@@ -205,7 +205,7 @@
                             <AccordionItem title="Response">
                                 <Response {method} />
                             </AccordionItem>
-                            {#if method?.['rate-limit'] > 0 && method?.['rate-key']?.length > 0}
+                            {#if method?.['rate-limit'] > 0 && method?.['rate-key']?.length > 0 && platformType === 'CLIENT'}
                                 <AccordionItem title="Rate limits">
                                     <RateLimits {method} />
                                 </AccordionItem>
