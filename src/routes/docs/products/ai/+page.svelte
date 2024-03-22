f1c392865bdd3cfba8dbfbca2d1024c2dced67e3
Bradley Schofield <ionicisere@gmail.com>
2024-03-22 10:38:19 +0000

    Conitnue work

--- src/routes/docs/products/ai/+page.svelte
+++ src/routes/docs/products/ai/+page.svelte
@@ -2,8 +2,8 @@
     import { Carousel } from '$lib/components';
     import { DEFAULT_DESCRIPTION, DEFAULT_HOST } from '$lib/utils/metadata';
     import { TITLE_SUFFIX } from '$routes/titles';
-    import MainFooter from '$lib/components/MainFooter.svelte';
     import CodeCard, { type CodeCardProps } from '../../CodeCard.svelte';
+    import Article from '$markdoc/layouts/Article.svelte';
     const title = 'Docs' + TITLE_SUFFIX;
     const description = DEFAULT_DESCRIPTION;
     const ogImage = DEFAULT_HOST + '/images/open-graph/docs.png';
@@ -11,7 +11,7 @@
     enum Field {
         ComputerVision = 'Computer Vision',
         NaturalLanguage = 'Natural Language',
-        AudioProcessing = 'Audio Processing',
+        AudioProcessing = 'Audio Processing'
     }
 
     type Tutorial = {
@@ -68,14 +68,14 @@
             }
         },
         {
-          title: 'Facial Recognition',
-          description: 'Recognize and identify faces in images',
-          href: '/docs/products/ai/tutorials/facial-recognition',
-          field: Field.ComputerVision,
-          covers: {
-              dark: '/images/icons/illustrated/dark/auth.png',
-              light: '/images/icons/illustrated/light/auth.png'
-          }
+            title: 'Image Segmentation',
+            description: 'Split up objects in images into individual layers',
+            href: '/docs/products/ai/tutorials/image-segmentation',
+            field: Field.ComputerVision,
+            covers: {
+                dark: '/images/icons/illustrated/dark/auth.png',
+                light: '/images/icons/illustrated/light/auth.png'
+            }
         },
         {
             title: 'Text Generation',
@@ -88,9 +88,9 @@
             }
         },
         {
-            title: 'Sentiment Analysis',
-            description: 'Understand the sentiment of text',
-            href: '/docs/products/ai/tutorials/sentiment-analysis',
+            title: 'Language Translation',
+            description: 'Translate text between languages',
+            href: '/docs/products/ai/tutorials/language-translation',
             field: Field.NaturalLanguage,
             covers: {
                 dark: '/images/icons/illustrated/dark/auth.png',
@@ -98,34 +98,34 @@
             }
         },
         {
-          title: 'Speech To Text',
-          description: 'Speech Recognition',
-          href: '/docs/products/ai/tutorials/speech-recognition',
-          field: Field.AudioProcessing,
-          covers: {
-              dark: '/images/icons/illustrated/dark/auth.png',
-              light: '/images/icons/illustrated/light/auth.png'
-          }
+            title: 'Speech To Text',
+            description: 'Speech Recognition',
+            href: '/docs/products/ai/tutorials/speech-recognition',
+            field: Field.AudioProcessing,
+            covers: {
+                dark: '/images/icons/illustrated/dark/auth.png',
+                light: '/images/icons/illustrated/light/auth.png'
+            }
         },
         {
-          title: 'Text To Speech',
-          description: 'Convert text to speech',
-          href: '/docs/products/ai/tutorials/text-to-speech',
-          field: Field.AudioProcessing,
-          covers: {
-              dark: '/images/icons/illustrated/dark/auth.png',
-              light: '/images/icons/illustrated/light/auth.png'
-          }
+            title: 'Text To Speech',
+            description: 'Convert text to speech',
+            href: '/docs/products/ai/tutorials/text-to-speech',
+            field: Field.AudioProcessing,
+            covers: {
+                dark: '/images/icons/illustrated/dark/auth.png',
+                light: '/images/icons/illustrated/light/auth.png'
+            }
         },
         {
-          title: 'Music Generation',
-          description: 'Generate music from text',
-          href: '/docs/products/ai/tutorials/music-generation',
-          field: Field.AudioProcessing,
-          covers: {
-              dark: '/images/icons/illustrated/dark/auth.png',
-              light: '/images/icons/illustrated/light/auth.png'
-          }
+            title: 'Music Generation',
+            description: 'Generate music from text',
+            href: '/docs/products/ai/tutorials/music-generation',
+            field: Field.AudioProcessing,
+            covers: {
+                dark: '/images/icons/illustrated/dark/auth.png',
+                light: '/images/icons/illustrated/light/auth.png'
+            }
         }
     ];
 </script>
@@ -147,97 +147,68 @@
     <meta name="twitter:card" content="summary_large_image" />
 </svelte:head>
 
-<div>
-    <div class="u-position-absolute u-inset-inline-end-0 aw-u-opacity-40-mobile" style="">
-        <enhanced:img src="./../../blur-1.png" alt="" />
-    </div>
-
-    <main
-        class="aw-main-section u-position-relative aw-u-overflow-hidden-break1-to-break3"
-        id="main"
-    >
-        <div class="u-position-absolute aw-u-opacity-40-mobile bg-blur">
-            <img src="/images/bgs/docs-blur-1.svg" alt="" />
+<Article
+    title="Artifical Inteligence"
+    description="Learn how to implement machine learning models in your applications"
+>
+    <section class="aw-hero is-align-start">
+        <h2 class="aw-title aw-u-color-text-primary u-max-width-600">Explore capabilities</h2>
+        <p class="aw-description u-max-width-600">
+            Detailed explanations and deep dives into how you can implement different machine
+            learning techiques
+        </p>
+        <Carousel size="medium">
+            {#each fields as field}
+                <li style:scroll-snap-align="start">
+                    <CodeCard {...field} />
+                </li>
+            {/each}
+        </Carousel>
+    </section>
+    <section class="aw-hero is-align-start">
+        <h2 class="aw-title aw-u-color-text-primary u-max-width-600">Show me some code</h2>
+        <p class="aw-description u-max-width-600 u-margin-block-start-16">
+            If you learn best from code examples, follow one of our tutorials.
+        </p>
+        <div class="u-margin-block-start-24">
+            {#each Object.values(Field) as field}
+                <h3 class="aw-label aw-u-color-text-primary web-snap-location">{field}</h3>
+                <ul class="aw-grid-row-4 aw-grid-row-4-m-1">
+                    {#each tutorials as tutorial}
+                        {#if tutorial.field === field}
+                            <li>
+                                <a href={tutorial.href} class="aw-card is-normal">
+                                    <img
+                                        src={tutorial.covers.dark}
+                                        alt=""
+                                        class="u-only-dark"
+                                        width="48"
+                                        height="48"
+                                    />
+                                    <img
+                                        src={tutorial.covers.light}
+                                        alt=""
+                                        class="u-only-light"
+                                        width="48"
+                                        height="48"
+                                    />
+                                    <h4
+                                        class="aw-sub-body-500 aw-u-color-text-primary u-margin-block-start-8"
+                                    >
+                                        {tutorial.title}
+                                    </h4>
+                                    <p class="aw-sub-body-400 u-margin-block-start-4">
+                                        {tutorial.description}
+                                    </p>
+                                </a>
+                            </li>
+                        {/if}
+                    {/each}
+                </ul>
+            {/each}
         </div>
-
-        <div
-            class="u-position-absolute u-inset-inline-start-0 u-inset-block-start-16 aw-u-opacity-40-mobile"
-            style="    margin-inline-start: -30px;"
-        >
-            <enhanced:img src="./../../blur-2.png" alt="" />
-        </div>
-
-        <section class="aw-hero is-align-start u-position-relative e-hero-docs">
-            <h1 class="aw-display aw-u-color-text-primary u-max-width-600">
-                Learn how to build AI like a team of hundreds<span class="aw-u-color-text-accent"
-                    >_
-                </span>
-            </h1>
-            <p class="aw-description u-max-width-600">
-                Appwrite allows you to build powerful AI powered applications with ease, Leverage
-                Appwrite's powerful functions architecture and start building the future.
-            </p>
-        </section>
-        <section class="aw-hero is-align-start">
-            <h2 class="aw-title aw-u-color-text-primary u-max-width-600">Explore capabilities</h2>
-            <p class="aw-description u-max-width-600">
-                Detailed explanations and deep dives into how you can implement different machine
-                learning techiques
-            </p>
-            <Carousel size="medium">
-                {#each fields as field}
-                    <li style:scroll-snap-align="start">
-                        <CodeCard {...field} />
-                    </li>
-                {/each}
-            </Carousel>
-        </section>
-        <section class="aw-hero is-align-start">
-            <h2 class="aw-title aw-u-color-text-primary u-max-width-600">Show me some code</h2>
-            <p class="aw-description u-max-width-600 u-margin-block-start-16">
-                If you learn best from code examples, follow one of our tutorials.
-            </p>
-            <div class="u-margin-block-start-24">
-                {#each Object.values(Field) as field}
-                    <h3 class="aw-label aw-u-color-text-primary web-snap-location">{field}</h3>
-                    <ul class="aw-grid-row-4 aw-grid-row-4-m-1">
-                        {#each tutorials as tutorial}
-                            {#if tutorial.field === field}
-                                <li>
-                                    <a href={tutorial.href} class="aw-card is-normal">
-                                        <img
-                                            src={tutorial.covers.dark}
-                                            alt=""
-                                            class="u-only-dark"
-                                            width="48"
-                                            height="48"
-                                        />
-                                        <img
-                                            src={tutorial.covers.light}
-                                            alt=""
-                                            class="u-only-light"
-                                            width="48"
-                                            height="48"
-                                        />
-                                        <h4
-                                            class="aw-sub-body-500 aw-u-color-text-primary u-margin-block-start-8"
-                                        >
-                                            {tutorial.title}
-                                        </h4>
-                                        <p class="aw-sub-body-400 u-margin-block-start-4">
-                                            {tutorial.description}
-                                        </p>
-                                    </a>
-                                </li>
-                            {/if}
-                        {/each}
-                    </ul>
-                {/each}
-            </div>
-        </section>
-        <MainFooter variant="docs" />
-    </main>
-</div>
+    </section>
+</Article>
 
 <style lang="scss">
     .e-hero-docs {
