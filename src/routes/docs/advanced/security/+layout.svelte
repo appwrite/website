bed3467b8afc9c3da20eb52f2d875ea0944ade0f
Vincent (Wen Yu) Ge <vincent@appwrite.io>
2024-04-04 00:05:03 -0400

    Update MFA docs

--- src/routes/docs/advanced/security/+layout.svelte
+++ src/routes/docs/advanced/security/+layout.svelte
@@ -50,8 +50,8 @@
 					href: '/docs/advanced/security/encryption'
 				},
 				{
-					label: 'Two-Factor authentication',
-					href: '/docs/advanced/security/2fa'
+					label: 'Multi-Factor authentication',
+					href: '/docs/advanced/security/mfa'
 				},
 				{
 					label: 'HTTPS',
