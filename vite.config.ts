143a45f7f0eda3662162da8673b991a85eb298d5
Steven Nguyen <stnguyen90@users.noreply.github.com>
2024-05-03 13:51:51 -0700

    fix: remove envFixer plugin to remove zero-width spaces
    The plugin was created because `process.env` in our code samples was
    being replaced so the code samples looked incorrect. However, Vite 5
    has fixed this so we don't need this plugin anymore.
    
    In addition, the zero-width space was causing issues when people
    would copy and paste the code into their editor to run.

--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { enhancedImages } from '@sveltejs/enhanced-img';
-import type { Plugin } from 'vite';
-
-const envFixer: Plugin = {
-    name: 'env-fixer',
-    enforce: 'pre',
-    transform(code, id) {
-        if (!id.includes('.markdoc')) {
-            return { code };
-        }
-
-        // The replacement uses a zero-width space to avoid being detected by vite
-        const transformed = code.replaceAll(/process\.env/g, 'process​.env');
-        return {
-            code: transformed
-        };
-    }
-};
 
 export default defineConfig({
     plugins: [
-        envFixer,
         enhancedImages(),
         sveltekit(),
         dynamicImport({
