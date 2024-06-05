32f7942f773ec04732f430e86fce532cbd60e784
Bradley Schofield <ionicisere@gmail.com>
2024-06-05 11:53:06 +0000

    Change pricing to be per unit

--- src/routes/pricing/compare-plans.svelte
+++ src/routes/pricing/compare-plans.svelte
@@ -34,8 +34,8 @@
                 {
                     title: 'Additional bandwidth',
                     free: '-',
-                    pro: '$40 per 100GB',
-                    scale: '$40 per 100GB'
+                    pro: '0.40$ per GB',
+                    scale: '0.40% per GB'
                 },
                 {
                     title: 'Storage',
@@ -46,8 +46,8 @@
                 {
                     title: 'Additional storage',
                     free: '-',
-                    pro: '$3 per 100GB',
-                    scale: '$3 per 100GB'
+                    pro: '0.30$ per GB',
+                    scale: '0.30$ per GB'
                 },
                 {
                     title: 'Compute',
@@ -146,8 +146,8 @@
                 {
                     title: 'Additional users',
                     free: '-',
-                    pro: '$3 per 1,000 users',
-                    scale: '$3 per 1,000 users'
+                    pro: '0.03$ per user',
+                    scale: '0.03$ per user'
                 },
                 {
                     title: 'Teams',
@@ -251,8 +251,8 @@
                 {
                     title: 'Additional concurrent connections',
                     free: '-',
-                    pro: '$5 per 1,000',
-                    scale: '$5 per 1,000'
+                    pro: '0.05$ per 1,000',
+                    scale: '0.05$ per 1,000'
                 },
                 {
                     title: 'Messages',
