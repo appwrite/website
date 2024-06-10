510f456a5d60e0f1db20f2ceb5d31017f2cf240f
Elad Shechter <elad2412@gmail.com>
2024-06-10 16:06:35 +0300

    Work in progress

--- src/routes/integration/article/+page.svelte
+++ src/routes/integration/article/+page.svelte
@@ -169,7 +169,126 @@
 		</div>
 	</div>
 
+
+
 	<div class="web-big-padding-section-level-1 u-overflow-hidden">
+		<div class="web-big-padding-section-level-2">
+			<div class="web-container">
+				<h3 class="web-label web-u-color-text-primary u-text-center">Keep exploring similar integrations</h3>
+				<ul class="u-margin-block-start-32 l-grid-1">
+					<li>
+						<a href="/docs/products/auth" class="web-card is-transparent u-block u-height-100-percent" style="--card-padding:1.5rem; --card-padding-mobile:1.5rem;">
+							<div class="u-flex u-cross-center u-gap-8">
+								<img
+										src="/images/icons/illustrated/dark/auth.png"
+										alt=""
+										class=""
+										width="32"
+										height="32"
+								/>
+								<h4
+										class="web-main-body-400 web-u-color-text-primary"
+								>
+									Auth
+								</h4>
+								<span class="icon-arrow-right u-margin-inline-start-auto" aria-hidden="true"></span>
+							</div>
+							<p class="web-sub-body-400 u-margin-block-start-4">
+								Build secure authentication and manage your users.
+							</p>
+						</a>
+					</li>
+					<li>
+						<a href="/docs/products/functions" class="web-card is-transparent u-block u-height-100-percent" style="--card-padding:1.5rem; --card-padding-mobile:1.5rem;">
+							<div class="u-flex u-cross-center u-gap-8">
+								<img
+										src="/images/icons/illustrated/dark/functions.png"
+										alt=""
+										class=""
+										width="32"
+										height="32"
+								/>
+								<h4
+										class="web-main-body-400 web-u-color-text-primary"
+								>
+									Functions
+								</h4>
+								<span class="icon-arrow-right u-margin-inline-start-auto" aria-hidden="true"></span>
+							</div>
+							<p class="web-sub-body-400 u-margin-block-start-4">
+								Scale big and unlock limitless potential with Appwrite functions.
+							</p>
+						</a>
+					</li>
+					<li>
+						<a href="/docs/products/databases" class="web-card is-transparent u-block u-height-100-percent" style="--card-padding:1.5rem; --card-padding-mobile:1.5rem;">
+							<div class="u-flex u-cross-center u-gap-8">
+								<img
+										src="/images/icons/illustrated/dark/databases.png"
+										alt=""
+										class=""
+										width="32"
+										height="32"
+								/>
+								<h4
+										class="web-main-body-400 web-u-color-text-primary"
+								>
+									Databases
+								</h4>
+								<span class="icon-arrow-right u-margin-inline-start-auto" aria-hidden="true"></span>
+							</div>
+							<p class="web-sub-body-400 u-margin-block-start-4">
+								Store and query structured data, ensuring scalable storage.
+							</p>
+						</a>
+					</li>
+					<li>
+						<a href="/docs/products/messaging" class="web-card is-transparent u-block u-height-100-percent" style="--card-padding:1.5rem;--card-padding-mobile:1.5rem;">
+							<div class="u-flex u-cross-center u-gap-8">
+								<img
+										src="/images/icons/illustrated/dark/messaging.png"
+										alt=""
+										class=""
+										width="32"
+										height="32"
+								/>
+								<h4
+										class="web-main-body-400 web-u-color-text-primary"
+								>
+									Messaging
+								</h4>
+								<span class="icon-arrow-right u-margin-inline-start-auto" aria-hidden="true"></span>
+							</div>
+							<p class="web-sub-body-400 u-margin-block-start-4">
+								Manage your files’ project, using convenient APIs and utilities.
+							</p>
+						</a>
+					</li>
+					<li>
+						<a href="/docs/apis/realtime" class="web-card is-transparent u-block u-height-100-percent" style="--card-padding:1.5rem; --card-padding-mobile:1.5rem;">
+							<div class="u-flex u-cross-center u-gap-8">
+								<img
+										src="/images/icons/illustrated/dark/realtime.png"
+										alt=""
+										class=""
+										width="32"
+										height="32"
+								/>
+								<h4
+										class="web-main-body-400 web-u-color-text-primary"
+								>
+									Realtime
+								</h4>
+								<span class="icon-arrow-right u-margin-inline-start-auto" aria-hidden="true"></span>
+							</div>
+							<p class="web-sub-body-400 u-margin-block-start-4">
+								Utilize realtime information from all Appwrite services.
+							</p>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
 		<div class="web-big-padding-section-level-2 u-position-relative">
 			<img src="/images/bgs/pre-footer.png" alt="" class="web-pre-footer-bg" style="z-index:-1" />
 
@@ -260,5 +379,13 @@
 			.l-grid-area-content { grid-area:content; }
 			.l-grid-area-side    { grid-area:side;    }
 		}
+	}
+
+	.l-grid-1 {
+		display:grid;
+		grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+		gap:2rem;
+		@media (max-width:600px) { gap:1.25rem; }
+
 	}
 </style>
