42aa55a32145e619abc7505f2440046bce0b0e9d
Jesse Winton <jesse@appwrite.io>
2024-06-13 14:11:39 -0400

    more structure for animation

---
+++ src/lib/animations/Products/messaging/code.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Code from '$lib/animations/CodeWindow/Code.svelte';
+
+	let content = `
+await messaging.createPush(  
+ 	ID.unique(),
+	'New task assigned to you',
+	'You were assigned a new task in your board. Tap to check it out.',
+);`.trim();
+</script>
+
+<Code {content} />
