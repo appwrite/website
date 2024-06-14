712516ab2a9214844f2618142d8e8490af6ea8fe
ItzNotABug <itznotabug@gmail.com>
2024-06-14 14:23:17 +0530

    fix: broken xml due to title and urls.

--- src/routes/blog/rss.xml/+server.ts
+++ src/routes/blog/rss.xml/+server.ts
@@ -3,23 +3,40 @@ import { posts } from '../content';
 
 export const prerender = true;
 
+function encodeTitle(str: string): string {
+    return str.replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;');
+}
+
+function encodeUrl(href: string): string {
+    return encodeURI(`https://appwrite.io${href}`)
+}
+
 export const GET: RequestHandler = () => {
     const feed = `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
     <title>Appwrite</title>
     <link>https://appwrite.io</link>
+    <atom:link href="https://appwrite.io/blog/rss.xml" rel="self" type="application/rss+xml" />
     <description>Appwrite is an open-source platform for building applications at any scale, using your preferred programming languages and tools.</description>
     ${posts.map((post) => `<item>
-        <title>${post.title}</title>
+        <title>${encodeTitle(post.title)}</title>
         <pubDate>${post.date.toUTCString()}</pubDate>
-        <link>https://appwrite.io${post.href}</link>
-        <guid>https://appwrite.io${post.href}</guid>
+        <link>${encodeUrl(post.href)}</link>
+        <guid>${encodeUrl(post.href)}</guid>
         <description>${post.description}</description>
     </item>
     `).join('')}
   </channel>
 </rss>`;
 
-    return new Response(feed);
+    return new Response(feed, {
+        headers: {
+            'Content-Type': 'application/rss+xml'
+        }
+    });
 };
