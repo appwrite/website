5bf25c1dfabd1e7ad0aa8fce6f779ad8335cf22d
choir27 <richardchoi54@gmail.com>
2024-06-18 09:07:18 -0400

    docs: remove docs/tooling/the-appwriter

--- src/routes/docs/tooling/the-appwriter/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import Docs from '$lib/layouts/Docs.svelte';
-	import Sidebar from '$lib/layouts/Sidebar.svelte';
-</script>
-
-<Docs variant="two-side-navs">
-	<Sidebar />
-	<slot />
-</Docs>
