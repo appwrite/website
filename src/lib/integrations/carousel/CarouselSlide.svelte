c27df16e5b9e71f634db9875633d363824b6f231
Jesse Winton <jesse@appwrite.io>
2024-06-25 19:17:29 -0400

    working carousel

---
+++ src/lib/integrations/carousel/CarouselSlide.svelte
@@ -0,0 +1,18 @@
+<div class="embla__slide web-carousel-item">
+    <slot />
+</div>
+
+<style lang="scss">
+    @use '$scss/abstract' as *;
+
+    .embla__slide {
+        @media #{$break1} {
+            flex: 0 0 100%;
+        }
+
+        flex: 0 0 33%;
+        min-width: 0;
+        margin-right: pxToRem(32);
+        transition: opacity 200ms ease-out;
+    }
+</style>
