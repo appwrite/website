af9dd952cc908fb1fcc249fe008ac010b82e4243
Jesse Winton <jesse@appwrite.io>
2024-06-26 11:25:09 -0700

    a rough start

---
+++ src/routes/init-2/Hero.svelte
@@ -0,0 +1,61 @@
+<script>
+    const lines = Array.from({ length: 100 });
+</script>
+
+<div class="hero">
+    <div class="lines">
+        {#each lines as _, i}
+            <div class="line" style:top={`${i * 10}px`} />
+        {/each}
+    </div>
+</div>
+
+<style lang="scss">
+    @keyframes -global-line {
+        0% {
+            left: -50%;
+        }
+        100% {
+            left: 110%;
+        }
+    }
+
+    .hero {
+        position: relative;
+        height: 80vh;
+        mask-image: url('/images/logos/init.svg');
+        mask-repeat: no-repeat;
+        mask-size: contain;
+        overflow: hidden;
+        display: flex;
+        margin: 0 auto;
+
+        .line {
+            position: absolute;
+            height: 2px;
+            width: 100%;
+            background: rgba(255, 255, 255, 0.1);
+            overflow: hidden;
+
+            &::after {
+                content: '';
+                display: block;
+                position: absolute;
+                height: 2px;
+                width: 5vw;
+                left: -50%;
+                top: 0;
+                will-change: left;
+                background: linear-gradient(
+                    to right,
+                    rgba(255, 255, 255, 0) 0%,
+                    #fff 75%,
+                    #fff 100%
+                );
+                animation: line 12s 0s infinite;
+                animation-fill-mode: forwards;
+                animation-timing-function: cubic-bezier(0.4, 0.26, 0, 0.97);
+            }
+        }
+    }
+</style>
