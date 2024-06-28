f64620b6e04f609a7875d5d33a5d0c28463706a1
choir27 <richardchoi54@gmail.com>
2024-06-28 14:28:56 -0400

    move appwrite.json after pull, move init project appwrite.json to install, add init project to installation docs

--- src/routes/docs/tooling/command-line/+layout.svelte
+++ src/routes/docs/tooling/command-line/+layout.svelte
@@ -29,6 +29,10 @@
 			label: 'Deployments',
 			items: [
 				{
+					label: 'Projects',
+					href: '/docs/tooling/command-line/projects'
+				},
+				{
 					label: 'Collections',
 					href: '/docs/tooling/command-line/collections'
 				},
