21e0a5ace1d19f5dcbdac04dbea04d836a012a19
Jesse Winton <jesse@appwrite.io>
2024-07-02 13:54:51 -0400

    experiment

--- src/routes/init-2/Hero.svelte
+++ src/routes/init-2/Hero.svelte
@@ -1,60 +1,67 @@
-<script>
-    const lines = Array.from({ length: 100 });
+<script lang="ts">
+    const lines = Array.from({ length: 50 });
+    const randomDelay = (i: number) => Math.floor(Math.random() * i) * 1000;
 </script>
 
-<div class="lockup">
-    {#each lines as _, i}
-        <div class="line" style={`--animation-delay: ${i * 2}ms;top: ${i * 10}px`} />
-    {/each}
+<div class="hero">
+    <div class="lockup">
+        {#each lines as _, i}
+            <div class="line" style={`--animation-delay: ${randomDelay(i)}ms;top: ${i * 100}px`} />
+        {/each}
+    </div>
 </div>
 
 <style lang="scss">
+    :root {
+        --line-width: 5vw;
+        --line-height: 2px;
+        --animation-duration: 2s;
+    }
     @keyframes -global-line {
         0% {
-            left: -50%;
+            left: -10%;
         }
         100% {
             left: 110%;
         }
     }
 
-    .lockup {
-        position: relative;
-        height: 100vh;
-        mask-image: url('/images/logos/init.svg');
-        mask-repeat: no-repeat;
-        mask-size: contain;
-        overflow: hidden;
-        display: flex;
-        margin: 0 auto;
-        align-items: center;
+    .hero {
+        .lockup {
+            position: relative;
+            height: 80vh;
+            flex-grow: 1;
 
-        .line {
-            position: absolute;
-            height: 2px;
-            width: 100%;
-            background: rgba(255, 255, 255, 0.1);
             overflow: hidden;
+            margin: 0 auto;
 
-            &::after {
-                content: '';
-                display: block;
+            .line {
                 position: absolute;
-                height: 2px;
-                width: 5vw;
-                left: -50%;
-                top: 0;
-                will-change: left;
-                background: linear-gradient(
-                    to right,
-                    rgba(255, 255, 255, 0) 0%,
-                    #fff 75%,
-                    #fff 100%
-                );
-                animation: line 12s infinite;
-                animation-fill-mode: forwards;
-                animation-delay: var(--animation-delay);
-                animation-timing-function: cubic-bezier(0.4, 0.26, 0, 0.97);
+                height: var(--line-height);
+                width: 100%;
+                background: transparent;
+                overflow: hidden;
+
+                &::after {
+                    content: '';
+                    display: block;
+                    position: absolute;
+                    width: var(--line-width);
+                    height: var(--line-height);
+                    left: -50%;
+                    top: 0;
+                    will-change: left;
+                    background: linear-gradient(
+                        to left,
+                        rgba(254, 83, 109, 0.75) 0%,
+                        rgba(253, 54, 110, 0.2) 100%
+                    );
+
+                    animation: line var(--animation-duration) infinite;
+                    animation-fill-mode: forwards;
+                    animation-delay: var(--animation-delay);
+                    animation-timing-function: cubic-bezier(0.1, -0.6, 0.2, 0);
+                }
             }
         }
     }
