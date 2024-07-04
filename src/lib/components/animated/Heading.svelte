b3ab77617c33a9b955bc2cba749ada4b6cd63a9e
Jesse Winton <jesse@appwrite.io>
2024-07-03 20:43:39 -0400

    new v

--- src/lib/components/animated/Heading.svelte
+++ src/lib/components/animated/Heading.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
     export let text: string;
     export let stagger: number = 100;
+    const splitText = text.split('');
 </script>
 
-<h1 class="sr-only" {...$$restProps}>{text}</h1>
+<h1 class="sr-only" {...$$restProps}><slot {text} /></h1>
 <h1 class="heading" {...$$restProps}>
-    {#each text.split('') as letter, i}
+    {#each splitText as letter, i}
         <span class="letter" style:animation-delay={`${i * stagger}ms`}>{letter}</span>
     {/each}
 </h1>
@@ -13,12 +14,14 @@
 <style lang="scss">
     @keyframes -global-enter {
         0% {
-            filter: blur(16px);
-            transform: translateY(100px);
+            opacity: 0;
+            filter: blur(15px);
+            transform: scale(1.3);
         }
         100% {
+            opacity: 1;
             filter: blur(0);
-            transform: translateY(0);
+            transform: scale(1);
         }
     }
 
@@ -27,15 +30,18 @@
         z-index: 100;
         color: white;
         pointer-events: none;
-        font-size: 50vw;
+        font-size: 20vw;
         position: relative;
-    }
 
-    .letter {
-        display: inline-block;
-        position: relative;
-        will-change: transform;
-        animation: enter 1200ms ease-in-out forwards;
+        .letter {
+            display: inline-block;
+            position: relative;
+            will-change: transform;
+            opacity: 0;
+            filter: blur(16px);
+            transform: translateY(20px);
+            animation: enter 1200ms ease-in-out forwards;
+        }
     }
 
     .sr-only {
