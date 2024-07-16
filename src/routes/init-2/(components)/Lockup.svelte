676be6dd875ed661cf78a92ab59992838a58fce8
Jesse Winton <jesse@appwrite.io>
2024-07-16 10:12:00 -0400

    Update Lockup.svelte

--- src/routes/init-2/(components)/Lockup.svelte
+++ src/routes/init-2/(components)/Lockup.svelte
@@ -86,7 +86,7 @@
         --stroke-color: #333;
         --stroke-width: 2;
         --fill: hsl(240 5.7% 10.4%);
-        --duration: 8s;
+        --duration: 5s;
         fill: none;
         animation: fade 1s ease-out;
 
