66a9a3d9de06b3b8f4485b9d1722811cf275933d
Jesse Winton <jesse@appwrite.io>
2024-07-16 16:40:39 -0400

    2 tickets start

---
+++ src/routes/init-2/tickets/+page.ts
@@ -0,0 +1,8 @@
+import { redirect } from '@sveltejs/kit';
+import { isLoggedIn } from '../helpers.js';
+
+export const load = async () => {
+    if (await isLoggedIn()) {
+        redirect(307, '/init/tickets/customize');
+    }
+};
