c04e6c6b8c6ff9b5e923119dd4801855415aa7f5
Jesse Winton <jesse@appwrite.io>
2024-07-16 17:17:07 -0400

    updates

--- src/lib/components/embla/index.ts
+++
@@ -1,7 +0,0 @@
-import CarouselRoot from './Carousel.svelte';
-import CarouselSlide from './CarouselSlide.svelte';
-
-export const Carousel = Object.assign({
-    Root: CarouselRoot,
-    Slide: CarouselSlide
-});
