9b52fc09f3dcaed13a3d7afcf0e5c8a6c47e05f4
Jesse Winton <jesse@appwrite.io>
2024-07-25 11:29:42 -0400

    fixes

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { classNames } from "$lib/utils/classnames";
+  import type { SvelteComponent } from "svelte";
+  import type { HTMLButtonAttributes } from "svelte/elements";
+
+  type ButtonProps = {
+    icon: SvelteComponent;
+  } & HTMLButtonAttributes;
+
+  type $$Props = ButtonProps;
+
+  //export let icon: $$Props["icon"];
+</script>
+
+<button
+  {...$$restProps}
+  class={classNames(
+    "flex items-center justify-center rounded-lg bg-pink-200 select-none",
+  )}
+>
+  <slot />
+</button>
