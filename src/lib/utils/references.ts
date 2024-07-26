5aa1f25f6d2092b10b834e236186cac599072b05
Steven Nguyen <1477010+stnguyen90@users.noreply.github.com>
2024-07-26 23:21:47 +0000

    docs: update API references to include 1.6.x

--- src/lib/utils/references.ts
+++ src/lib/utils/references.ts
@@ -2,7 +2,17 @@ import { writable } from 'svelte/store';
 import type { Language } from './code';
 import { browser } from '$app/environment';
 
-const allVersions = ['1.5.x', '1.4.x', '1.3.x', '1.2.x', '1.1.x', '1.0.x', '0.15.x', 'cloud'] as const;
+const allVersions = [
+    '1.6.x',
+    '1.5.x',
+    '1.4.x',
+    '1.3.x',
+    '1.2.x',
+    '1.1.x',
+    '1.0.x',
+    '0.15.x',
+    'cloud'
+] as const;
 
 export type Version = (typeof allVersions)[number];
 
