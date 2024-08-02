f984cab552b5bca45aa18adeb4f5113e92246c3a
Jesse Winton <jesse@appwrite.io>
2024-08-02 13:07:49 -0700

    clean up lines

--- src/routes/init/(components)/Lines.svelte
+++ src/routes/init/(components)/Lines.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    export let lines: Array<number> = [2, 4, 2, 3];
+    export let lines: Array<number> = [2, 1, 2, 4, 3];
+    const getRandomHeight = () => Math.floor(Math.random() * 100) + 75;
 </script>
 
 <div class="container">
@@ -7,8 +8,11 @@
         {#each lines as rows, index}
             <div style:position="relative">
                 {#each Array.from({ length: rows }) as _, i}
-                    {@const delay = i + 1 * index * 250}
-                    <div class="line" style={`--delay:${delay}ms`} />
+                    {@const delay = i + 1 * index * 150}
+                    <div
+                        class="line"
+                        style={`--delay:${delay}ms;--height:${getRandomHeight()}px;left:${i * 40}px`}
+                    />
                 {/each}
             </div>
         {/each}
@@ -16,21 +20,6 @@
 </div>
 
 <style lang="scss">
-    :root {
-        --background: hsl(240 5.7% 10.4%);
-        --gradient: linear-gradient(
-            to bottom,
-            rgba(255, 255, 255, 0.2) 0,
-            rgba(255, 255, 255, 0.2) 44%,
-            rgba(255, 255, 255, 0) 100%
-        );
-        --width: 2px;
-        --height: 20vh;
-        --starting-position: -60vh;
-        --duration: 6s;
-        --initial-delay: 0.5s;
-    }
-
     .container {
         position: absolute;
         overflow-y: hidden;
@@ -48,6 +37,18 @@
             justify-content: space-between;
             gap: 20px;
             padding: 0 24px;
+            opacity: 0.7;
+
+            --gradient: linear-gradient(
+                to bottom,
+                rgba(255, 255, 255, 1) 0%,
+                rgba(255, 255, 255, 1) 50%,
+                rgba(255, 255, 255, 0) 100%
+            );
+            --width: 2px;
+            --starting-position: -60vh;
+            --duration: 4s;
+            --initial-delay: 0.5s;
 
             .line {
                 position: relative;
@@ -71,6 +72,8 @@
                     width: var(--width);
                     height: var(--height);
                     background: var(--gradient);
+                    border-radius: 8px;
+                    filter: drop-shadow(0 0 2px rgba(255, 255, 255, 0.8));
                     animation: line var(--duration) calc(var(--initial-delay) + var(--delay))
                         infinite forwards cubic-bezier(0.1, -0.6, 0.2, 0);
                 }
