57ed0a7f36b4a84540a38250fae3ddb1ee4e9439
ItzNotABug <itznotabug@gmail.com>
2024-08-08 14:15:21 +0530

    feat: initial - automated `readtime`.

--- src/lib/utils/tutorials.ts
+++ src/lib/utils/tutorials.ts
@@ -1,11 +1,19 @@
 import { base } from '$app/paths';
+import readingTime from 'reading-time';
 import type { Tutorial } from '$markdoc/layouts/Tutorial.svelte';
 
-export function globToTutorial(data: { tutorials: Record<string, unknown>; pathname: string }) {
+export function globToTutorial(data: {
+	pathname: string;
+	tutorials: {
+		raw: Record<string, unknown>,
+		default: Record<string, unknown>
+	}
+}) {
 	let isFound = false;
-	let difficulty, readtime;
+	let difficulty: string;
+	const readtime = readingStats(data)
 
-	return Object.entries(data.tutorials)
+	return Object.entries(data.tutorials.default)
 		.map(([filepath, tutorial]) => {
 			const { frontmatter } = tutorial as {
 				frontmatter: Tutorial;
@@ -15,7 +23,6 @@ export function globToTutorial(data: { tutorials: Record<string, unknown>; pathn
 
 			if (!isFound && 'difficulty' in frontmatter && 'readtime' in frontmatter) {
 				isFound = true;
-				readtime = frontmatter.readtime;
 				difficulty = frontmatter.difficulty;
 			}
 
@@ -23,10 +30,21 @@ export function globToTutorial(data: { tutorials: Record<string, unknown>; pathn
 				readtime,
 				difficulty,
 				...frontmatter,
-				href: `${base}${tutorialName}/${slug}`
+				href: `${base}${tutorialName}/${slug}`,
 			};
 		})
 		.sort((a, b) => {
 			return a.step - b.step;
 		});
 }
+
+const readingStats = (data: object) => {
+	const content = Object.entries(data.tutorials.raw).map(([, rawContent]) => {
+		const frontmatterEnd = (rawContent as string).lastIndexOf('---') + 3;
+		return (rawContent as string).slice(frontmatterEnd).trimStart();
+	}).join('\n\n');
+
+	return readingTime(content, {
+		wordsPerMinute: 120
+	}).minutes;
+};
