8b4c64679d51c508a85a384f431e45e8eda69031
Jesse Winton <jesse@appwrite.io>
2024-08-08 21:09:35 -0400

    more work on a grid

---
+++ src/routes/init/(components)/grid/Cell.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let column: number | 'auto' = 'auto';
+    export let row: number | 'auto' = 'auto';
+</script>
+
+<div class="cell" style="--column:{column};--row:{row};">
+    <slot />
+</div>
+
+<style lang="scss">
+    @use '$scss/abstract' as *;
+
+    .cell {
+        grid-column: span var(--column) / span var(--column);
+        grid-row: span var(--row) / span var(--row);
+        display: flex;
+        flex-direction: column;
+        gap: pxToRem(32);
+    }
+</style>
