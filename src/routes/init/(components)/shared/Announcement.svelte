ee41bb6a8974a58ec7e65b3532e87d22217c0d1c
Jesse Winton <jesse@appwrite.io>
2024-08-12 12:47:51 -0400

    updates to days

--- src/routes/init/(components)/shared/Announcement.svelte
+++ src/routes/init/(components)/shared/Announcement.svelte
@@ -1,40 +1,71 @@
 <script lang="ts">
-    type $$Props = {
-        illustration?: string;
-        title: string;
-        description: string;
-    };
+  type $$Props = {
+    illustration?: string;
+    title: string;
+    description: string;
+  };
 
-    export let illustration: $$Props['illustration'] = undefined;
-    export let title: $$Props['title'];
-    export let description: $$Props['description'];
+  export let illustration: $$Props["illustration"] = undefined;
+  export let title: $$Props["title"];
+  export let description: $$Props["description"];
 </script>
 
 <a
-    href="/blog/post/announcing-appwrite-messaging"
-    class="web-card is-normal has-border-gradient"
-    style:padding="0"
-    style:overflow="hidden"
+  href="/blog/post/announcing-appwrite-messaging"
+  class="web-card is-normal has-border-gradient"
+  style:padding="0"
+  style:overflow="hidden"
 >
-    <div style:padding="2rem" style:padding-block-end="0">
-        <h3 class="web-label web-u-color-text-primary">{title}</h3>
-        <p style:margin-block-start="0.625rem">
-            {description}
-        </p>
-        <div class="web-card-link u-flex u-cross-center" style:margin-block-start="1.25rem">
-            <span class="text">Announcement</span>
-            <span class="web-icon-arrow-right" />
-        </div>
+  <div style:padding="2rem" style:padding-block-end="0">
+    <h3 class="web-label web-u-color-text-primary">{title}</h3>
+    <p style:margin-block-start="0.625rem">
+      {description}
+    </p>
+    <div
+      class="web-card-link u-flex u-cross-center"
+      style:margin-block-start="1.25rem"
+    >
+      <span class="text">Announcement</span>
+      <span class="web-icon-arrow-right" />
     </div>
+  </div>
+  <div class="illustration">
     <img src={illustration} alt="" />
+  </div>
 </a>
 
 <style lang="scss">
-    .web-card {
-        background-color: hsl(var(--web-color-subtle));
-    }
+  .web-card {
+    display: flex;
+    flex-direction: column;
+    background-color: hsl(var(--web-color-subtle));
+    background-image: linear-gradient(
+      150deg,
+      hsl(var(--web-color-subtle)) 0%,
+      hsl(var(--web-color-subtle)) 30%,
+      rgba(255, 255, 255, 0.025) 50%,
+      hsl(var(--web-color-subtle)) 70%,
+      hsl(var(--web-color-subtle)) 100%
+    );
+    backdrop-filter: blur(2px);
+  }
 
-    p {
-        text-wrap: pretty;
+  .illustration {
+    position: relative;
+    z-index: 0;
+    margin-left: auto;
+    margin-right: 0;
+    height: 350px;
+    width: 100%;
+
+    img {
+      width: 100%;
+      height: 100%;
+      margin-right: 0;
     }
+  }
+
+  p {
+    text-wrap: pretty;
+  }
 </style>
--- src/routes/init/(components)/shared/Article.svelte
+++ src/routes/init/(components)/shared/Article.svelte
@@ -1,38 +1,41 @@
 <script lang="ts">
-    type $$Props = {
-        url: string;
-        illustration?: string;
-        title: string;
-    };
+  type $$Props = {
+    url: string;
+    illustration?: string;
+    title: string;
+    label?: string;
+  };
 
-    export let url: $$Props['url'];
-    export let illustration: $$Props['illustration'] = undefined;
-    export let title: $$Props['title'];
+  export let url: $$Props["url"];
+  export let illustration: $$Props["illustration"] = undefined;
+  export let title: $$Props["title"];
+  export let label: $$Props["label"] = "Read article";
 </script>
 
 <a
-    href={url}
-    class="web-card is-normal has-border-gradient u-overflow-hidden"
-    style="padding: 1.25rem; --p-aspect-ratio: 0;"
+  href={url}
+  class="web-card is-normal has-border-gradient u-overflow-hidden"
+  style="padding: 1.25rem; --p-aspect-ratio: 0;"
 >
-    <h3 class="web-label web-u-color-text-primary" style="max-width: 18.75rem">
-        {title}
-    </h3>
-    <div class="web-card-link u-flex u-cross-center">
-        <span class="text">Article</span>
-        <span class="web-icon-arrow-right" />
-    </div>
-    {#if illustration}
-        <img
-            src={illustration}
-            alt="{title} illustration"
-            style="position: absolute; inset-block-start: 0; inset-inline-end: -2rem;"
-        />
-    {/if}
+  <h3 class="web-label web-u-color-text-primary" style="max-width: 18.75rem">
+    {title}
+  </h3>
+  <div class="web-card-link u-flex u-cross-center">
+    <span class="text">{label}</span>
+    <span class="web-icon-arrow-right" />
+  </div>
+  {#if illustration}
+    <img
+      src={illustration}
+      alt="{title} illustration"
+      style="position: absolute; inset-block-start: 0; inset-inline-end: 0;"
+    />
+  {/if}
 </a>
 
 <style lang="scss">
-    .web-card {
-        background-color: hsl(var(--web-color-subtle));
-    }
+  .web-card {
+    height: 100%;
+    background-color: hsl(var(--web-color-subtle));
+  }
 </style>
