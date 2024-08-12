de0d74f972c5e1d90a69a57ec9b7e94009a42e56
Jesse Winton <jesse@appwrite.io>
2024-08-12 09:54:34 -0400

    Revert "rm button"
    This reverts commit 281bcdfc4a1b465b34f91a7597dada66bcda4613.

--- src/lib/components/IsLoggedIn.svelte
+++ src/lib/components/IsLoggedIn.svelte
@@ -1,29 +1,28 @@
 <script lang="ts">
-  import { PUBLIC_APPWRITE_DASHBOARD } from "$env/static/public";
+    import Button from './Button.svelte';
+    import { PUBLIC_APPWRITE_DASHBOARD } from '$env/static/public';
 
-  export let classes = "";
+    export let classes = '';
 </script>
 
-<a href={PUBLIC_APPWRITE_DASHBOARD} class={`web-button ${classes}`}>
-  <span class="logged-in"><slot name="isLoggedIn">Go to Console</slot></span>
-  <span class="not-logged-in"
-    ><slot name="isNotLoggedIn">Get started</slot></span
-  >
-</a>
+<Button class={classes} href={PUBLIC_APPWRITE_DASHBOARD} intent="secondary">
+    <span class="[data-logged-in]:block hidden"><slot name="isLoggedIn">Go to Console</slot></span>
+    <span class="btton [data-logged-in]:hidden"><slot name="isNotLoggedIn">Get started</slot></span>
+</Button>
 
 <style lang="scss">
-  :global(body[data-logged-in]) {
-    .logged-in {
-      display: block;
+    :global(body[data-logged-in]) {
+        .logged-in {
+            display: block;
+        }
+        .not-logged-in {
+            display: none;
+        }
     }
     .not-logged-in {
-      display: none;
+        display: block;
     }
-  }
-  .not-logged-in {
-    display: block;
-  }
-  .logged-in {
-    display: none;
-  }
+    .logged-in {
+        display: none;
+    }
 </style>
