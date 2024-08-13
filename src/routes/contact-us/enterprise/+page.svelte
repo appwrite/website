5927f888236bee702ec52e2f786b4346257058e6
ItzNotABug <itznotabug@gmail.com>
2024-08-13 14:20:30 +0530

    new: contact us page for enterprise.

---
+++ src/routes/contact-us/enterprise/+page.svelte
@@ -0,0 +1,288 @@
+<script lang="ts">
+    import { DEFAULT_DESCRIPTION, DEFAULT_HOST } from '$lib/utils/metadata';
+    import { Main } from '$lib/layouts';
+    import { TITLE_SUFFIX } from '$routes/titles';
+    import FooterNav from '../../../lib/components/FooterNav.svelte';
+    import MainFooter from '../../../lib/components/MainFooter.svelte';
+    import { socials } from '$lib/constants';
+    //import BlobPink from "$routes/startups/(assets)/blob-pink.svg";
+    // import BlobPinkMobile from "$routes/startups/(assets)/blob-pink-mobile.svg";
+    import Pink from '../bg.png';
+    import { loggedIn, user } from '$lib/utils/console';
+
+    let email = '';
+    let name = '';
+    let companyName = '';
+    let companySize: string | null = null;
+    let companyWebsite = '';
+    let useCase = '';
+    let error: string | undefined;
+    let submitted = false;
+
+    async function handleSubmit() {
+        error = undefined;
+        let message = `Name of representative: ${name}\n\nWork Email: ${email}\n\nCompany Name: ${companyName}\n\nCompany Size: ${companySize}\n\nCompany Website: ${companyWebsite}\n\nUse-case: ${useCase}`;
+        const subject = `Enterprise Plan Inquiry: ${companyName}`;
+
+        if (loggedIn && $user?.email) {
+            message = message.replace(
+                `\n\nWork Email: ${email}\n\n`,
+                `\n\nWork Email: ${email}\n\nCloud Email: ${$user.email}\n\n`
+            );
+        }
+
+        const response = await fetch('https://growth.appwrite.io/v1/enterprise', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                firstName: name,
+                subject,
+                message
+            })
+        });
+
+        if (response.status >= 400) {
+            error = response.status >= 500 ? 'Server Error.' : 'Error submitting form.';
+            return;
+        }
+
+        submitted = true;
+    }
+
+    const title = 'Enterprise' + TITLE_SUFFIX;
+    const description = DEFAULT_DESCRIPTION;
+    const ogImage = DEFAULT_HOST + '/images/open-graph/website.png';
+</script>
+
+<svelte:head>
+    <!-- Titles -->
+    <title>{title}</title>
+    <meta property="og:title" content={title} />
+    <meta name="twitter:title" content={title} />
+    <!-- Desscription -->
+    <meta name="description" content={description} />
+    <meta property="og:description" content={description} />
+    <meta name="twitter:description" content={description} />
+    <!-- Image -->
+    <meta property="og:image" content={ogImage} />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta name="twitter:image" content={ogImage} />
+    <meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+
+<div class="u-position-absolute" style="pointer-events:none;">
+    <img src={Pink} alt="" />
+</div>
+
+<Main>
+    <div class="web-big-padding-section">
+        <div id="form" class="web-big-padding-section-level-1 u-padding-0 u-overflow-hidden">
+            <div
+                class="web-big-padding-section-level-2 is-margin-replace-padding u-position-relative"
+            >
+                <div class="u-position-relative">
+                    <div class="web-container u-position-relative">
+                        <!-- before submit -->
+                        <div
+                            class="u-position-relative u-z-index-1 web-grid-1-1-opt-2 u-gap-32 e-u-row-gap-0"
+                        >
+                            <div>
+                                <div
+                                    class="web-u-max-inline-size-none-mobile"
+                                    class:web-u-max-width-380={!submitted}
+                                >
+                                    {#if submitted}
+                                        <section class="u-flex-vertical web-u-gap-20">
+                                            <h1 class="web-display web-u-color-text-primary">
+                                                Thank you for your submission
+                                            </h1>
+                                            <p class="web-description web-u-padding-block-end-32">
+                                                Your details for the enterprise plan have been sent successfully. Our
+                                                team will back to you as soon as possible.
+                                            </p>
+                                            <a
+                                                href="/pricing"
+                                                class="web-button is-secondary web-u-margin-block-end-32"
+                                            >
+                                                <span>Back to pricing</span>
+                                            </a>
+                                        </section>
+                                    {:else}
+                                        <section class="u-flex-vertical web-u-gap-20">
+                                            <h4 class="web-display web-u-color-text-primary">
+                                                Enterprise Plan
+                                            </h4>
+                                            <p class="web-description">
+                                                Apply to our Enterprise Plan by filling
+                                                out this form. Our team will reach out to you soon.
+                                            </p>
+                                        </section>
+                                    {/if}
+                                    <section
+                                        class="u-flex-vertical u-gap-12 u-padding-block-start-40 u-margin-block-start-40 web-u-sep-block-start"
+                                    >
+                                        <h2 class="web-label web-u-color-text-primary">
+                                            Follow us
+                                        </h2>
+                                        <ul class="u-flex u-gap-8">
+                                            {#each socials as social}
+                                                <li>
+                                                    <a
+                                                        href={social.link}
+                                                        class="web-icon-button"
+                                                        aria-label={social.label}
+                                                        target="_blank"
+                                                        rel="noopener noreferrer"
+                                                    >
+                                                        <span
+                                                            class={social.icon}
+                                                            aria-hidden="true"
+                                                        />
+                                                    </a>
+                                                </li>
+                                            {/each}
+                                        </ul>
+                                    </section>
+                                    <div
+                                        class="web-is-only-mobile web-u-margin-block-start-40 web-u-padding-block-start-40 web-u-sep-block-start"
+                                    />
+                                </div>
+                            </div>
+                            {#if !submitted}
+                                <form
+                                    method="post"
+                                    on:submit|preventDefault={handleSubmit}
+                                    class="u-flex-vertical u-gap-16"
+                                >
+                                    <div class="u-flex u-main-end">
+                                        <ul
+                                            class="web-form-list is-two-columns u-gap-16 u-width-full-line web-u-max-width-580 web-u-max-inline-size-none-mobile"
+                                        >
+                                            <li class="web-form-item u-flex-vertical u-gap-4">
+                                                <label class="u-block" for="name"
+                                                >Name of representative</label
+                                                >
+                                                <input
+                                                    required
+                                                    class="web-input-text"
+                                                    type="text"
+                                                    placeholder="Walter O'Brien"
+                                                    id="name"
+                                                    bind:value={name}
+                                                />
+                                            </li>
+                                            <li class="web-form-item u-flex-vertical u-gap-4">
+                                                <label class="u-block" for="workEmail"
+                                                >Work email address</label
+                                                >
+                                                <input
+                                                    required
+                                                    class="web-input-text"
+                                                    type="email"
+                                                    placeholder="walter@appwrite.io"
+                                                    id="workEmail"
+                                                    bind:value={email}
+                                                />
+                                            </li>
+                                            <li class="web-form-item u-flex-vertical u-gap-4">
+                                                <label class="u-block" for="companyName"
+                                                >Company name</label
+                                                >
+                                                <input
+                                                    required
+                                                    class="web-input-text"
+                                                    type="text"
+                                                    placeholder="Acme Corp"
+                                                    id="companyName"
+                                                    bind:value={companyName}
+                                                />
+                                            </li>
+                                            <li class="web-form-item u-flex-vertical u-gap-4">
+                                                <label class="u-block" for="companySize"
+                                                >Company size</label
+                                                >
+
+                                                <div class="u-position-relative">
+                                                    <select
+                                                        class="web-input-text"
+                                                        id="companySize"
+                                                        bind:value={companySize}
+                                                    >
+                                                        <option value={null}>Select size</option>
+                                                        <option>1-10 employees</option>
+                                                        <option>11-50 employees</option>
+                                                        <option>51-200 employees</option>
+                                                        <option>201-500 employees</option>
+                                                        <option>501-1000 employees</option>
+                                                        <option>1001-5000 employees</option>
+                                                        <option>5000+ employees</option>
+                                                    </select>
+                                                    <span
+                                                        class="icon-cheveron-down u-position-absolute u-inset-inline-end-8 u-inset-block-start-8 web-u-pointer-events-none"
+                                                        aria-hidden="true"
+                                                    ></span>
+                                                </div>
+                                            </li>
+                                            <li
+                                                class="web-form-item is-column-span-2 u-flex-vertical u-gap-4"
+                                            >
+                                                <label class="u-block" for="companyWebsite"
+                                                >Company website</label
+                                                >
+                                                <input
+                                                    required
+                                                    class="web-input-text"
+                                                    type="url"
+                                                    placeholder="https://appwrite.io"
+                                                    id="companyWebsite"
+                                                    bind:value={companyWebsite}
+                                                />
+                                            </li>
+                                            <li
+                                                class="web-form-item is-column-span-2 u-flex-vertical u-gap-4"
+                                            >
+                                                <label class="u-block" for="use-case"
+                                                >Please share more information about your use case</label
+                                                >
+                                                <textarea
+                                                    required
+                                                    class="web-input-text"
+                                                    id="use-case"
+                                                    placeholder="Describe your use case and how our Enterprise Plan can support it"
+                                                    bind:value={useCase}
+                                                />
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div
+                                        class="u-flex u-gap-16 u-main-space-between web-u-flex-vertical-reverse-mobile"
+                                    >
+                                        <p class="web-caption-400 web-u-max-width-380">
+                                            {#if error}
+                                                {error}
+                                            {/if}
+                                        </p>
+                                        <button
+                                            type="submit"
+                                            class="web-button u-cross-child-center web-u-inline-width-100-percent-mobile-break1"
+                                        >
+                                            <span>Submit</span>
+                                        </button>
+                                    </div>
+                                </form>
+                            {/if}
+                        </div>
+                    </div>
+                    <div class="web-container">
+                        <FooterNav />
+                        <MainFooter />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</Main>
