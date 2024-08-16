b37a90ce1de53ac29ef5fb941d01ee14256adeec
Jesse Winton <jesse@appwrite.io>
2024-08-16 13:20:07 -0400

    Update +page.svelte

--- src/routes/init/+page.svelte
+++ src/routes/init/+page.svelte
@@ -23,6 +23,10 @@
 
     let base = new Date('2024-08-19T13:00:00.000Z');
 
+    const ff = () => {
+        base = addDays(base, -1);
+    };
+
     $: days = [
         {
             title: 'Local development',
@@ -78,6 +82,8 @@
     <div style:background="#171719">
         <div class="hero">
             <Hero />
+
+            <button on:click={ff}>Fast forward</button>
         </div>
 
         <div class="web-container">
