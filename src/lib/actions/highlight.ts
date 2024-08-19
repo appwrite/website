2a064e7f825ab4358fbe047cfefb74546ef7643d
ItzNotABug <itznotabug@gmail.com>
2024-08-19 13:48:11 +0530

    fix: encoding on content.

--- src/lib/actions/highlight.ts
+++ src/lib/actions/highlight.ts
@@ -1,6 +1,12 @@
 export const highlight = (node: HTMLElement, text: string[]) => {
     text.forEach((word) => {
         const regex = new RegExp(`(${word})`, 'gi');
-        node.innerHTML = node.innerHTML.replace(regex, '<mark>$1</mark>');
+        node.childNodes.forEach((child) => {
+            if (child.nodeType === Node.TEXT_NODE) {
+                const span = document.createElement('span');
+                span.innerHTML = child.textContent?.replace(regex, '<mark>$1</mark>') || '';
+                child.replaceWith(...Array.from(span.childNodes));
+            }
+        });
     });
 };
