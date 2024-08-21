27b1c2d5bf403cd05e4a149179571b3167069d3d
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-08-21 15:25:14 +0200

    fix: assets optimization

--- scripts/optimize-assets.js
+++ scripts/optimize-assets.js
@@ -5,11 +5,20 @@ import { fileURLToPath } from 'url';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 const root_dir = join(__dirname, '../static');
+const exceptions = ['assets/'];
 
+/**
+ * @type {{
+ *     jpeg: sharp.JpegOptions,
+ *     webp: sharp.WebpOptions,
+ *     png: sharp.PngOptions,
+ *     gif: sharp.GifOptions
+ * }}
+ */
 const config = {
     jpeg: { quality: 80 },
     webp: { quality: 80 },
-    png: { compressionLevel: 9 },
+    png: { compressionLevel: 9, quality: 80 },
     gif: { quality: 80 }
 };
 
@@ -28,25 +37,34 @@ function* walk_directory(dir) {
 }
 
 function is_image(file) {
-  const image_extensions = ["jpg", "jpeg", "png", "gif", "webp"];
-  const extension = file.split(".").pop().toLowerCase();
+    const image_extensions = ['jpg', 'jpeg', 'png', 'gif', 'webp'];
+    const extension = file.split('.').pop().toLowerCase();
 
-  return image_extensions.includes(extension);
+    return image_extensions.includes(extension);
 }
 
 function get_relative_path(file) {
-    return relative(root_dir, file)
+    return relative(root_dir, file);
 }
 
 async function main() {
     for (const file of walk_directory(join(__dirname, '../static'))) {
+        const relative_path = get_relative_path(file);
         if (!is_image(file)) continue;
-        console.log(get_relative_path(file));
+        if (exceptions.some((exception) => relative_path.startsWith(exception))) continue;
+
+        console.log(get_relative_path(relative_path));
 
         const image = sharp(file);
         const size_before = (await image.toBuffer()).length;
         const meta = await image.metadata();
-        const buffer = await image[meta.format](config[meta.format]).toBuffer();
+        const buffer = await image[meta.format](config[meta.format])
+            .resize({
+                width: 1280,
+                height: 1280,
+                fit: sharp.fit.inside
+            })
+            .toBuffer();
         const size_after = buffer.length;
         if (size_after >= size_before) continue;
 
