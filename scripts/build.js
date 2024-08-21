5ccac0be9bafa0cfeebc4cd119b66a6b4574af65
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-08-21 15:07:51 +0200

    chore: optimize assets

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,5 @@
-import { fileURLToPath } from 'url';
 import { build } from 'vite';
 import { downloadContributors } from './download-contributor-data.js';
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 async function main() {
     await downloadContributors();
