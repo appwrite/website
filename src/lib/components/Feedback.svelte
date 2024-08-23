428cbb0c14d98b20bcc0a820f23702b201ef994f
Jesse Winton <jesse@appwrite.io>
2024-08-23 12:20:13 -0400

    Revert "update components"
    This reverts commit 4ab7bb2f8e4b1450da17de6da44c5806044010ed.

--- src/lib/components/Feedback.svelte
+++ src/lib/components/Feedback.svelte
@@ -1,152 +1,166 @@
 <script lang="ts">
-    import { page } from '$app/stores';
+  import { page } from "$app/stores";
 
-    export let date: string | undefined = undefined;
-    let showFeedback = false;
-    let feedbackType = '';
-    let email = '';
-    let comment = '';
-    let error: string | undefined;
-    let submitted = false;
-    let submitting = false;
+  export let date: string | undefined = undefined;
+  let showFeedback = false;
+  let feedbackType = "";
+  let email = "";
+  let comment = "";
+  let error: string | undefined;
+  let submitted = false;
+  let submitting = false;
 
-    async function handleSubmit() {
-        submitting = true;
-        error = undefined;
-        const response = await fetch('https://growth.appwrite.io/v1/feedback/docs', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email,
-                type: feedbackType,
-                route: $page.route.id,
-                comment
-            })
-        });
-        submitting = false;
-        if (response.status >= 400) {
-            error = response.status >= 500 ? 'Server Error.' : 'Error submitting form.';
-            return;
-        }
-        comment = email = '';
-        submitted = true;
+  async function handleSubmit() {
+    submitting = true;
+    error = undefined;
+    const response = await fetch(
+      "https://growth.appwrite.io/v1/feedback/docs",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          type: feedbackType,
+          route: $page.route.id,
+          comment,
+        }),
+      },
+    );
+    submitting = false;
+    if (response.status >= 400) {
+      error =
+        response.status >= 500 ? "Server Error." : "Error submitting form.";
+      return;
     }
+    comment = email = "";
+    submitted = true;
+  }
 
-    function reset() {
-        comment = email = '';
-        feedbackType = '';
-        submitted = false;
-        error = undefined;
-    }
+  function reset() {
+    comment = email = "";
+    feedbackType = "";
+    submitted = false;
+    error = undefined;
+  }
 
-    $: if (!showFeedback) {
-        reset();
-    }
+  $: if (!showFeedback) {
+    reset();
+  }
 </script>
 
 <section class="web-content-footer">
-    <header class="web-content-footer-header u-width-full-line">
-        <div
-            class="u-flex u-gap-32 u-main-space-between u-cross-center u-width-full-line"
-            style="flex-wrap: wrap-reverse;"
-        >
-            <div class="u-flex u-gap-16 u-cross-center">
-                <h5 class="web-main-body-600 web-u-color-text-primary">Was this page helpful?</h5>
-                <div class="u-flex u-gap-8">
-                    <button
-                        class="web-radio-button"
-                        aria-label="helpful"
-                        on:click={() => {
-                            showFeedback = feedbackType === 'positive' ? false : true;
-                            feedbackType = 'positive';
-                        }}
-                    >
-                        <span class="icon-thumb-up" />
-                    </button>
-                    <button
-                        class="web-radio-button"
-                        aria-label="unhelpful"
-                        on:click={() => {
-                            showFeedback = feedbackType === 'negative' ? false : true;
-                            feedbackType = 'negative';
-                        }}
-                    >
-                        <!-- TODO: fix the icon name on pink -->
-                        <span class="icon-thumb-dowm" />
-                    </button>
-                </div>
-            </div>
-            <div class="web-content-footer-header-end">
-                <ul class="web-metadata web-caption-400">
-                    {#if date}
-                        <li>Last updated on {new Date(date)?.toLocaleDateString()}</li>
-                    {/if}
-                    <li>
-                        <a
-                            href="https://github.com/appwrite/website"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            class="web-link u-flex u-gap-4 u-cross-baseline"
-                        >
-                            <span class="icon-pencil-alt u-contents" aria-hidden="true" />
-                            <span>Update on GitHub</span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
+  <header class="web-content-footer-header w-full">
+    <div
+      class="flex w-full items-center justify-between gap-8"
+      style="flex-wrap: wrap-reverse;"
+    >
+      <div class="flex items-center gap-4">
+        <h5 class="web-main-body-600 web-u-color-text-primary">
+          Was this page helpful?
+        </h5>
+        <div class="flex gap-2">
+          <button
+            class="web-radio-button"
+            aria-label="helpful"
+            on:click={() => {
+              showFeedback = feedbackType === "positive" ? false : true;
+              feedbackType = "positive";
+            }}
+          >
+            <span class="icon-thumb-up" />
+          </button>
+          <button
+            class="web-radio-button"
+            aria-label="unhelpful"
+            on:click={() => {
+              showFeedback = feedbackType === "negative" ? false : true;
+              feedbackType = "negative";
+            }}
+          >
+            <!-- TODO: fix the icon name on pink -->
+            <span class="icon-thumb-dowm" />
+          </button>
         </div>
-    </header>
-    {#if showFeedback}
-        <form
-            on:submit|preventDefault={handleSubmit}
-            class="web-card is-normal"
-            style="--card-padding:1rem"
-        >
-            <div class="u-flex-vertical u-gap-8">
-                <label for="message">
-                    <span class="web-u-color-text-primary">
-                        What did you {feedbackType === 'negative' ? 'dislike' : 'like'}? (optional)
-                    </span>
-                </label>
-                <textarea
-                    class="web-input-text"
-                    id="message"
-                    placeholder="Write your message"
-                    bind:value={comment}
-                />
-                <label for="message" class="u-margin-block-start-8">
-                    <span class="web-u-color-text-primary">Email</span>
-                </label>
-                <input
-                    class="web-input-text"
-                    placeholder="Enter your email"
-                    type="email"
-                    name="email"
-                    required
-                    bind:value={email}
-                />
-            </div>
-            {#if submitted}
-                <p class="web-u-color-text-primary u-margin-block-start-16">
-                    Your message has been sent successfully. We appreciate your feedback.
-                </p>
-            {/if}
-            {#if error}
-                <p class="web-u-color-text-primary u-margin-block-start-16">
-                    There was an error submitting your feedback. Please try again later.
-                </p>
-            {/if}
+      </div>
+      <div class="web-content-footer-header-end">
+        <ul class="web-metadata web-caption-400">
+          {#if date}
+            <li>Last updated on {new Date(date)?.toLocaleDateString()}</li>
+          {/if}
+          <li>
+            <a
+              href="https://github.com/appwrite/website"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="web-link flex items-baseline gap-1"
+            >
+              <span class="icon-pencil-alt contents" aria-hidden="true" />
+              <span>Update on GitHub</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+  {#if showFeedback}
+    <form
+      on:submit|preventDefault={handleSubmit}
+      class="web-card is-normal"
+      style="--card-padding:1rem"
+    >
+      <div class="flex flex-col gap-2">
+        <label for="message">
+          <span class="web-u-color-text-primary">
+            What did you {feedbackType === "negative" ? "dislike" : "like"}?
+            (optional)
+          </span>
+        </label>
+        <textarea
+          class="web-input-text"
+          id="message"
+          placeholder="Write your message"
+          bind:value={comment}
+        />
+        <label for="message" class="mt-2">
+          <span class="web-u-color-text-primary">Email</span>
+        </label>
+        <input
+          class="web-input-text"
+          placeholder="Enter your email"
+          type="email"
+          name="email"
+          required
+          bind:value={email}
+        />
+      </div>
+      {#if submitted}
+        <p class="web-u-color-text-primary mt-4">
+          Your message has been sent successfully. We appreciate your feedback.
+        </p>
+      {/if}
+      {#if error}
+        <p class="web-u-color-text-primary mt-4">
+          There was an error submitting your feedback. Please try again later.
+        </p>
+      {/if}
 
-            <div class="u-flex u-main-end u-margin-block-start-16 u-gap-8">
-                <button class="web-button is-text" on:click={() => (showFeedback = false)}>
-                    <span>Cancel</span>
-                </button>
-                <button type="submit" class="web-button" disabled={submitting || !email}>
-                    <span>Submit</span>
-                </button>
-            </div>
-        </form>
-    {/if}
+      <div class="mt-4 flex justify-end gap-2">
+        <button
+          class="web-button is-text"
+          on:click={() => (showFeedback = false)}
+        >
+          <span>Cancel</span>
+        </button>
+        <button
+          type="submit"
+          class="web-button"
+          disabled={submitting || !email}
+        >
+          <span>Submit</span>
+        </button>
+      </div>
+    </form>
+  {/if}
 </section>
--- src/lib/components/PreFooter.svelte
+++ src/lib/components/PreFooter.svelte
@@ -1,101 +1,107 @@
 <script lang="ts">
-    import { PUBLIC_APPWRITE_DASHBOARD } from '$env/static/public';
+  import { PUBLIC_APPWRITE_DASHBOARD } from "$env/static/public";
 </script>
 
-<img src="/images/bgs/pre-footer.png" alt="" class="web-pre-footer-bg" style="z-index:-1" />
+<img
+  src="/images/bgs/pre-footer.png"
+  alt=""
+  class="web-pre-footer-bg"
+  style="z-index:-1"
+/>
 
-<div class="web-grid-1-1 u-gap-32 web-u-row-gap-80 u-position-relative">
-    <section class="web-hero u-flex web-u-row-gap-32 u-main-center u-cross-center">
-        <h2 class="web-display u-max-width-500 web-u-text-align-center web-u-color-text-primary">
-            Start building today
-        </h2>
-        <a
-            href={PUBLIC_APPWRITE_DASHBOARD}
-            class="web-button is-transparent web-u-cross-child-center"
-        >
-            <span class="text">Get started</span>
-        </a>
-    </section>
-    <section
-        class="web-card is-transparent has-border-gradient web-u-max-inline-width-584-mobile web-u-margin-inline-auto-mobile web-u-inline-width-100-percent-mobile"
+<div class="web-u-row-gap-80 relative grid grid-cols-2 gap-8">
+  <section class="web-hero web-u-row-gap-32 flex items-center justify-center">
+    <h2 class="web-display web-u-color-text-primary max-w-[500px] text-center">
+      Start building today
+    </h2>
+    <a
+      href={PUBLIC_APPWRITE_DASHBOARD}
+      class="web-button is-transparent web-self-center"
     >
-        <header class="web-strip-plans-header">
-            <div class="web-strip-plans-header-wrapper web-u-row-gap-24">
-                <h3 class="web-title web-u-color-text-primary">Our plans</h3>
-            </div>
-        </header>
+      <span class="text">Get started</span>
+    </a>
+  </section>
+  <section
+    class="web-card is-transparent has-border-gradient web-u-max-inline-width-584-mobile web-mx-auto-mobile web-u-inline-width-100-percent-mobile"
+  >
+    <header class="web-strip-plans-header">
+      <div class="web-strip-plans-header-wrapper web-u-row-gap-24">
+        <h3 class="web-title web-u-color-text-primary">Our plans</h3>
+      </div>
+    </header>
 
-        <ul class="web-strip-plans">
-            <li class="web-strip-plans-item web-strip-plans-container-query">
-                <div class="web-strip-plans-item-wrapper">
-                    <div class="web-strip-plans-plan">
-                        <h4 class="title web-description">Free</h4>
-                        <div class="web-title web-u-color-text-primary">$0</div>
-                        <div class="info web-caption-500" />
-                    </div>
-                    <p class="web-strip-plans-info web-caption-500">
-                        For personal hobby projects and students.
-                    </p>
-                    <a
-                        href={`${PUBLIC_APPWRITE_DASHBOARD}/register`}
-                        class="web-button is-secondary is-full-width-mobile web-u-cross-child-end"
-                    >
-                        <span class="text">Get started</span>
-                    </a>
-                </div>
-            </li>
-            <li class="web-strip-plans-item web-strip-plans-container-query">
-                <div class="web-strip-plans-item-wrapper">
-                    <div class="web-strip-plans-plan">
-                        <h4 class="title web-description">Pro</h4>
-                        <div class="web-title web-u-color-text-primary">$15</div>
-                        <div class="info web-caption-500">per member/month</div>
-                    </div>
-                    <p class="web-strip-plans-info web-caption-500">
-                        For pro developers and teams that need to scale their products.
-                    </p>
-                    <a
-                        href={`${PUBLIC_APPWRITE_DASHBOARD}/console?type=createPro`}
-                        class="web-button is-full-width-mobile web-u-cross-child-end"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <!-- <span class="text">Start trial</span> -->
-                        <span class="text">Start building</span>
-                    </a>
-                </div>
-            </li>
-            <li class="web-strip-plans-item web-strip-plans-container-query">
-                <div class="web-strip-plans-item-wrapper">
-                    <div class="web-strip-plans-plan">
-                        <h4 class="title web-description">Scale</h4>
-                        <div class="web-title web-u-color-text-primary">$599</div>
-                        <div class="info web-caption-500">per org/month</div>
-                    </div>
-                    <p class="web-strip-plans-info web-caption-500">
-                        For pro developers and production projects that need the ability to scale.
-                    </p>
-                    <button
-                        class="web-button is-full-width-mobile is-secondary web-u-cross-child-end"
-                        disabled
-                    >
-                        <span class="text">Coming soon</span>
-                    </button>
-                </div>
-            </li>
-        </ul>
-    </section>
+    <ul class="web-strip-plans">
+      <li class="web-strip-plans-item web-strip-plans-container-query">
+        <div class="web-strip-plans-item-wrapper">
+          <div class="web-strip-plans-plan">
+            <h4 class="title web-description">Free</h4>
+            <div class="web-title web-u-color-text-primary">$0</div>
+            <div class="info web-caption-500" />
+          </div>
+          <p class="web-strip-plans-info web-caption-500">
+            For personal hobby projects and students.
+          </p>
+          <a
+            href={`${PUBLIC_APPWRITE_DASHBOARD}/register`}
+            class="web-button is-secondary is-full-width-mobile web-u-cross-child-end"
+          >
+            <span class="text">Get started</span>
+          </a>
+        </div>
+      </li>
+      <li class="web-strip-plans-item web-strip-plans-container-query">
+        <div class="web-strip-plans-item-wrapper">
+          <div class="web-strip-plans-plan">
+            <h4 class="title web-description">Pro</h4>
+            <div class="web-title web-u-color-text-primary">$15</div>
+            <div class="info web-caption-500">per member/month</div>
+          </div>
+          <p class="web-strip-plans-info web-caption-500">
+            For pro developers and teams that need to scale their products.
+          </p>
+          <a
+            href={`${PUBLIC_APPWRITE_DASHBOARD}/console?type=createPro`}
+            class="web-button is-full-width-mobile web-u-cross-child-end"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <!-- <span class="text">Start trial</span> -->
+            <span class="text">Start building</span>
+          </a>
+        </div>
+      </li>
+      <li class="web-strip-plans-item web-strip-plans-container-query">
+        <div class="web-strip-plans-item-wrapper">
+          <div class="web-strip-plans-plan">
+            <h4 class="title web-description">Scale</h4>
+            <div class="web-title web-u-color-text-primary">$599</div>
+            <div class="info web-caption-500">per org/month</div>
+          </div>
+          <p class="web-strip-plans-info web-caption-500">
+            For pro developers and production projects that need the ability to
+            scale.
+          </p>
+          <button
+            class="web-button is-full-width-mobile is-secondary web-u-cross-child-end"
+            disabled
+          >
+            <span class="text">Coming soon</span>
+          </button>
+        </div>
+      </li>
+    </ul>
+  </section>
 </div>
 
 <style lang="scss">
-    .web-pre-footer-bg {
-        position: absolute;
-        top: clamp(300px, 50vw, 50%);
-        left: clamp(300px, 50vw, 50%);
-        transform: translate(-58%, -72%);
-        width: clamp(1200px, 200vw, 3000px);
-        height: auto;
-        max-inline-size: unset;
-        max-block-size: unset;
-    }
+  .web-pre-footer-bg {
+    position: absolute;
+    top: clamp(300px, 50vw, 50%);
+    left: clamp(300px, 50vw, 50%);
+    transform: translate(-58%, -72%);
+    width: clamp(1200px, 200vw, 3000px);
+    height: auto;
+    max-inline-size: unset;
+    max-block-size: unset;
+  }
 </style>
