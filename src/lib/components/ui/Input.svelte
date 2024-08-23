9f8f7a7ed556b7a5aedbfcb378965ae8fc5458db
Jesse Winton <jesse@appwrite.io>
2024-08-23 17:06:20 -0400

    big updates!

--- src/lib/components/ui/Input.svelte
+++ src/lib/components/ui/Input.svelte
@@ -1,15 +1,50 @@
 <script lang="ts">
+  import { classNames } from "$lib/utils/classnames";
   import type { HTMLInputAttributes } from "svelte/elements";
 
   type $$Props = {
-    label?: string;
+    label: string;
   } & HTMLInputAttributes;
 
   export let label: $$Props["label"] = "";
   export let value: $$Props["value"] = "";
+  const { class: classes, name, ...props } = $$restProps;
 </script>
 
-<label>
-  {label}
-  <input {value} {...$$restProps} />
-</label>
+{#if $$slots.icon}
+  <label
+    class={classNames(
+      "focus:border-greyscale-100 bg-greyscale-800 border-greyscale-700 flex items-center gap-1 rounded-lg border py-2 px-3 text-sm font-light transition-colors focus-within:border-white active:shadow-sm active:shadow-black/30",
+      classes,
+    )}
+  >
+    <slot name="icon" />
+    {label}
+    {name}
+    <input
+      on:input
+      on:change
+      on:focus
+      on:blur
+      bind:value
+      class="w-full border-0 ring-0 outline-none"
+      {...props}
+    />
+  </label>
+{:else}
+  <label for={name}>
+    {label}
+  </label>
+  <input
+    on:input
+    on:change
+    on:focus
+    on:blur
+    bind:value
+    class={classNames(
+      "focus:border-greyscale-100 bg-greyscale-800 border-greyscale-700 flex items-center gap-1 rounded-lg border py-2 px-3 text-sm font-light transition-colors focus-within:border-white active:shadow-sm active:shadow-black/30",
+      classes,
+    )}
+    {...props}
+  />
+{/if}
