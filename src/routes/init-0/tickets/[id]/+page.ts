9a7479111514ecc94dd398d86a3f4ba3797bbe23
Jesse Winton <jesse@appwrite.io>
2024-08-27 15:04:57 -0400

    correct prettier

--- src/routes/init-0/tickets/[id]/+page.ts
+++ src/routes/init-0/tickets/[id]/+page.ts
@@ -1,20 +1,23 @@
-import { getTicketById, getTicketContributions } from '$routes/init-0/helpers.js';
-import { error } from '@sveltejs/kit';
+import {
+  getTicketById,
+  getTicketContributions,
+} from "$routes/init-0/helpers.js";
+import { error } from "@sveltejs/kit";
 
 export const ssr = true;
 
 export const load = async ({ params, fetch }) => {
-    const { id } = params;
-    try {
-        const ticket = await getTicketById(id, fetch);
+  const { id } = params;
+  try {
+    const ticket = await getTicketById(id, fetch);
 
-        return {
-            ticket,
-            streamed: {
-                contributions: getTicketContributions(ticket.$id, fetch)
-            }
-        };
-    } catch (e) {
-        error(404, 'Ticket not found');
-    }
+    return {
+      ticket,
+      streamed: {
+        contributions: getTicketContributions(ticket.$id, fetch),
+      },
+    };
+  } catch (e) {
+    error(404, "Ticket not found");
+  }
 };
