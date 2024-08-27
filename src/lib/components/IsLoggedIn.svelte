644f65a50890f34bf1f876b2ed63f9b535ca2e98
Jesse Winton <jesse@appwrite.io>
2024-08-27 15:39:16 -0400

    updated isLoggedIn

--- src/lib/components/IsLoggedIn.svelte
+++ src/lib/components/IsLoggedIn.svelte
@@ -6,23 +6,6 @@
 </script>
 
 <Button class={classes} href={PUBLIC_APPWRITE_DASHBOARD}>
-    <span class="hidden [data-logged-in]:block"><slot name="isLoggedIn">Go to Console</slot></span>
-    <span class="btton [data-logged-in]:hidden"><slot name="isNotLoggedIn">Get started</slot></span>
+    <span class="hidden group-[&[data-logged-in]]/body:block">Go to Console</span>
+    <span class="block group-[&[data-logged-in]]/body:hidden">Get started</span>
 </Button>
-
-<style lang="scss">
-    :global(body[data-logged-in]) {
-        .logged-in {
-            display: block;
-        }
-        .not-logged-in {
-            display: none;
-        }
-    }
-    .not-logged-in {
-        display: block;
-    }
-    .logged-in {
-        display: none;
-    }
-</style>
