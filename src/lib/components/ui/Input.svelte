6042edb024d28635e8ec8ef7e288d7ba24605e0c
Jesse Winton <jesse@appwrite.io>
2024-08-30 10:20:46 -0400

    some tweaks

--- src/lib/components/ui/Input.svelte
+++ src/lib/components/ui/Input.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import { classNames } from '$lib/utils/classnames';
-    import { createEventDispatcher } from 'svelte';
     import type { HTMLInputAttributes } from 'svelte/elements';
 
     type $$Props = HTMLInputAttributes & {
@@ -12,22 +11,6 @@
     export let value: $$Props['value'] = '';
     export let classes: $$Props['class'] = '';
     export let name: $$Props['name'] = '';
-
-    const dispatch = createEventDispatcher<{
-        input: { value: string };
-        change: { value: string };
-    }>();
-
-    const handleInput = (event: Event) => {
-        const target = event.target as HTMLInputElement;
-        value = target.value;
-        dispatch('input', { value: target.value });
-    };
-
-    const handleChange = (event: Event) => {
-        const target = event.target as HTMLInputElement;
-        dispatch('change', { value: target.value });
-    };
 </script>
 
 {#if $$slots.icon}
@@ -42,8 +25,6 @@
             {name}
             {...{ type }}
             bind:value
-            on:input={handleInput}
-            on:change={handleChange}
             class="w-full border-0 ring-0 outline-none"
             {...$$restProps}
         />
@@ -53,9 +34,9 @@
         {label}
     </label>
     <input
+        {name}
+        {...{ type }}
         bind:value
-        on:input={handleInput}
-        on:change={handleChange}
         class={classNames(
             'focus:border-greyscale-100 bg-greyscale-800 border-greyscale-700 mt-2 flex w-full items-center gap-1 rounded-lg border py-2 px-3 text-sm font-light transition-colors focus-within:border-white active:shadow-sm active:shadow-black/30',
             classes
