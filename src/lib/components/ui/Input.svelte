e369f6d61e3dc485552eb1ec22fb4a1993f5005c
Jesse Winton <jesse@appwrite.io>
2024-08-30 11:15:36 -0400

    update

--- src/lib/components/ui/Input.svelte
+++ src/lib/components/ui/Input.svelte
@@ -25,6 +25,8 @@
             {name}
             {...{ type }}
             bind:value
+            on:input
+            on:change
             class="w-full border-0 ring-0 outline-none"
             {...$$restProps}
         />
@@ -37,6 +39,8 @@
         {name}
         {...{ type }}
         bind:value
+        on:input
+        on:change
         class={classNames(
             'focus:border-greyscale-100 bg-greyscale-800 border-greyscale-700 mt-2 flex w-full items-center gap-1 rounded-lg border py-2 px-3 text-sm font-light transition-colors focus-within:border-white active:shadow-sm active:shadow-black/30',
             classes
