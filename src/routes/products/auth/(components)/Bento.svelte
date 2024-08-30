cbf3c41428c85a03056cecbd5602b4b1e7fc3727
Jesse Winton <jesse@appwrite.io>
2024-08-30 13:04:09 -0400

    update

---
+++ src/routes/products/auth/(components)/Bento.svelte
@@ -0,0 +1,14 @@
+<div class="py-40">
+    <div class="container">
+        <div class="flex flex-col gap-4 text-center max-w-[325px] mx-auto">
+            <h2 class="text-4xl font-aeonik-pro text-primary text-pretty">
+                Built-in<br /> advanced security
+            </h2>
+            <p>
+                Provide top security and protection with built-in security and compliance features.
+            </p>
+        </div>
+
+        <div class="grid grid-cols-3 grid-row"></div>
+    </div>
+</div>
---
+++ src/routes/products/auth/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { FooterNav, MainFooter, PreFooter } from '$lib/components';
+    import Main from '$lib/layouts/Main.svelte';
+    import { DEFAULT_DESCRIPTION, DEFAULT_HOST } from '$lib/utils/metadata';
+    import { TITLE_SUFFIX } from '$routes/titles';
+
+    import { Platform } from '$lib/utils/references';
+    import { PUBLIC_APPWRITE_DASHBOARD } from '$env/static/public';
+    import Bento from './(components)/Bento.svelte';
+
+    const title = 'Auth' + TITLE_SUFFIX;
+    const description = DEFAULT_DESCRIPTION;
+    const ogImage = DEFAULT_HOST + '/images/open-graph/website.png';
+</script>
+
+<svelte:head>
+    <!-- Titles -->
+    <title>{title}</title>
+    <meta property="og:title" content={title} />
+    <meta name="twitter:title" content={title} />
+    <!-- Description -->
+    <meta name="description" content={description} />
+    <meta property="og:description" content={description} />
+    <meta name="twitter:description" content={description} />
+    <!-- Image -->
+    <meta property="og:image" content={ogImage} />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta name="twitter:image" content={ogImage} />
+    <meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+
+<Main>
+    <Bento />
+</Main>
