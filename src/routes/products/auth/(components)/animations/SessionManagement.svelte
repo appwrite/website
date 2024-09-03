d59dfe7b428d8a4d2ec2534b6b6e25e7bd87d9b9
Jesse Winton <jesse@appwrite.io>
2024-09-03 15:39:31 -0400

    Update SessionManagement.svelte

--- src/routes/products/auth/(components)/animations/SessionManagement.svelte
+++ src/routes/products/auth/(components)/animations/SessionManagement.svelte
@@ -8,9 +8,30 @@
             Manage user sessions with timeouts and auto-renewal to prevent logouts.
         </p>
     </div>
-    <div class="rounded-lg flex-1 flex flex-col justify-center pt-6 pl-6">
+    <div class="rounded-lg flex-1 flex flex-col justify-center pt-6 pl-6 overflow-hidden">
         <div
-            class="bg-gradient-to-br opacity-[0.24] border-l border-t border-white/[0.16] from-white/[0.4] via-[61%] via-white/[0.24] to-white/[0.4] to-transparent rounded-tl-2xl w-[90%] h-[90%] mb-0 mt-auto mr-0 ml-auto"
-        />
+            class="bg-gradient-to-br border-l border-t border-white/[0.16] from-white/[0.1] via-[61%] via-white/[0.04] to-white/[0.1] to-transparent rounded-tl-2xl w-[90%] h-[90%] mb-0 mt-auto mr-0 ml-auto"
+        >
+            <div class="ml-2 mt-2">
+                <header
+                    class="p-4 rounded-tl-2xl bg-greyscale-850/[0.6] flex gap-2 flex-col border-b border-white/20"
+                >
+                    <div class="flex gap-3 items-center">
+                        <span class="text-[18px] font-light font-aeonik-pro text-white"
+                            >Walter O'Brien</span
+                        >
+                        <span class="font-mono text-xs">637a40ba7</span>
+                    </div>
+                    <nav class="flex mt-4 -mb-4 gap-4 items-center text-xs">
+                        <button class="border-b border-transparent pb-2">Overview</button>
+                        <button class="border-b border-transparent pb-2">Memberships</button>
+                        <button class="border-b border-transparent border-white/40 pb-2"
+                            >Sessions</button
+                        >
+                        <button class="border-b border-transparent pb-2">Activity</button>
+                    </nav>
+                </header>
+            </div>
+        </div>
     </div>
 </div>
