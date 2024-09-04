eb0a0a8b38c7a85bed14672856086c2dcfe0fc77
Jesse Winton <jesse@appwrite.io>
2024-09-04 14:01:12 -0400

    add dictionary

--- src/routes/products/auth/(components)/animations/PasswordDictionary.svelte
+++ src/routes/products/auth/(components)/animations/PasswordDictionary.svelte
@@ -1,3 +1,90 @@
+<script lang="ts">
+    import Input from '$lib/components/ui/Input.svelte';
+    import { classNames } from '$lib/utils/classnames';
+
+    const options = [
+        '123456',
+        'password',
+        '12345678',
+        'qwerty',
+        '123456789',
+        '12345',
+        '1234',
+        '111111',
+        '1234567',
+        'dragon',
+        '123123',
+        'baseball',
+        'abc123',
+        'football',
+        'monkey',
+        'letmein',
+        '696969',
+        'shadow',
+        'master',
+        '666666',
+        'qwertyuiop',
+        '123321',
+        'mustang',
+        '1234567890',
+        'michael',
+        '654321',
+        'superman',
+        '1qaz2wsx',
+        '7777777',
+        '121212',
+        '000000',
+        'qazwsx',
+        '1111',
+        'zxcvbn',
+        '555555',
+        '11111111',
+        '131313',
+        'freedom',
+        '777777',
+        'pass',
+        'maggie',
+        '159753',
+        'aaaaaa',
+        'ginger',
+        'princess',
+        'joshua',
+        'cheese',
+        'amanda',
+        'summer',
+        'love',
+        'ashley',
+        '6969',
+        'nicole',
+        'chelsea',
+        'biteme',
+        'matthew',
+        'access',
+        'yankees',
+        '987654321',
+        'dallas',
+        'austin',
+        'thunder',
+        'taylor',
+        'matrix',
+        'love',
+        'ashley',
+        '6969',
+        'nicole',
+        'chelsea',
+        'biteme',
+        'matthew',
+        'access',
+        'yankees',
+        '987654321',
+        'dallas',
+        'austin',
+        'thunder',
+        'taylor',
+        'matrix'
+    ];
+</script>
+
 <div class="row-span-5 flex flex-col gap-4 rounded-2xl p-2 bg-greyscale-850/90">
     <div class="p-4">
         <span class="text-primary">Password dictionary</span>
@@ -5,5 +92,38 @@
             Check if the user's password matches any of the top 10K commonly used passwords.
         </p>
     </div>
-    <div class="bg-white/[0.02] rounded-lg flex-1"></div>
+    <div
+        class="bg-white/[0.02] rounded-lg flex-1 overflow-hidden relative flex flex-col justify-center items-center"
+    >
+        <div class="flex gap-x-2 text-sm flex-wrap w-[105%] mx-auto absolute">
+            {#each options as option}
+                {#if option === '654321'}
+                    <span
+                        class={classNames(
+                            'text-white relative bg-gradient-to-r font-mono from-greyscale-800 to-greyscale-900 flex items-center gap-2 px-1 shadow-lg shadow-black/30'
+                        )}
+                    >
+                        <span>6543</span>
+                        <span
+                            class="w-px h-[150%] bg-gradient-to-b from-transparent via-white to-transparent"
+                        ></span>
+                        <span class="text-white/50">21</span>
+                    </span>
+                {:else}
+                    <span class={classNames('font-mono text-white/20')}>{option}</span>{/if}
+            {/each}
+            <div class="mask inset-0 absolute z-0 backdrop-blur-md" />
+        </div>
+
+        <div class="relative z-10 shadow-xl shadow-black/30 w-3/4">
+            <Input type="password" label="Password" value="magicpassword" />
+        </div>
+    </div>
 </div>
+
+<style>
+    .mask {
+        mask-composite: intersect;
+        mask-image: radial-gradient(ellipse, transparent, rgba(0, 0, 0, 1) 70%);
+    }
+</style>
