94cb806c1577c9233794f29033b7c61f53636541
Jesse Winton <jesse@appwrite.io>
2024-09-09 15:17:16 +0200

    update text

--- src/routes/startups/faq.svelte
+++ src/routes/startups/faq.svelte
@@ -93,7 +93,7 @@
                             class="flex w-full items-center justify-between gap-2.5 py-6 text-left"
                             use:melt={$trigger(`${index}`)}
                         >
-                            <span class="text-label text-primary">
+                            <span class="text-label font-aeonik-pro text-primary">
                                 {faqItem.question}
                             </span>
                             <div
