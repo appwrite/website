992d5a4f42d326a1df69ab3b6986210c6c312c54
Jesse Winton <jesse@appwrite.io>
2024-09-30 12:03:58 -0400

    correct positioning and types

--- src/routes/globe/(components)/Map.svelte
+++ src/routes/globe/(components)/Map.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
     import DottedMap, { type DottedMapLib } from 'dotted-map';
     import { mousePosition } from '../mouse-position';
-    import { onMount } from 'svelte';
+    import { fly } from 'svelte/transition';
 
     let mapContainer: SVGSVGElement;
-
-    let x: number;
-    let y: number;
 
     let tooltip: {
         isActive: boolean;
@@ -21,11 +18,6 @@
     };
 
     let showTooltip: boolean = false;
-
-    mousePosition.subscribe(({ x: newX, y: newY }) => {
-        x = newX;
-        y = newY;
-    });
 
     const handleTooltip = ({
         activeCity,
@@ -47,121 +39,106 @@
         grid: 'diagonal'
     });
 
-    const pinsData = [
+    const pinsData: DottedMapLib.Pin[] = [
         {
             lat: 50.1109,
             lng: 8.6821,
-            city: 'Frankfurt',
-            country: 'Germany',
-            available: true,
-            color: 'red'
+            data: {
+                city: 'Frankfurt',
+                country: 'Germany',
+                available: true
+            }
         },
         {
             lat: 48.8575,
             lng: 2.3514,
-            city: 'Paris',
-            country: 'France',
-            available: false,
-            color: 'red'
+            data: {
+                city: 'Paris',
+                country: 'France',
+                available: false
+            }
         },
         {
             lat: 12.971599,
             lng: 77.5946,
-            city: 'Bangalore',
-            country: 'India',
-            available: false,
-            color: 'red'
+            data: {
+                city: 'Bangalore',
+                country: 'India',
+                available: false
+            }
         },
         {
             lat: 1.3521,
             lng: 103.8198,
-            city: 'Singapore',
-            country: 'Singapore',
-            available: false,
-            color: 'red'
+            data: {
+                city: 'Singapore',
+                country: 'Singapore',
+                available: false
+            }
         },
         {
             lat: 33.8688,
             lng: 151.2093,
-            city: 'Sydney',
-            country: 'Australia',
-            available: false,
-            color: 'red'
+            data: {
+                city: 'Sydney',
+                country: 'Australia',
+                available: false
+            }
         },
         {
             lat: 40.7128,
             lng: -73.935242,
-            city: 'New York, NY',
-            country: 'USA',
-            available: false,
-            color: 'blue'
+            data: {
+                city: 'New York, NY',
+                country: 'USA',
+                available: false
+            }
         },
         {
             lat: 37.7749,
             lng: -122.4194,
-            city: 'San Francisco, CA',
-            country: 'USA',
-            available: false,
-            color: 'blue'
+            data: {
+                city: 'San Francisco, CA',
+                country: 'USA',
+                available: false
+            }
         }
     ];
 
-    pinsData.forEach((pin, index) => {
+    pinsData.forEach((pin) => {
         map.addPin({
             lat: pin.lat,
             lng: pin.lng,
             data: {
-                city: pin.city,
-                country: pin.country,
-                available: pin.available
-            },
-            svgOptions: {
-                color: pin.color
+                city: pin.data.city,
+                country: pin.data.country,
+                available: pin.data.available
             }
         });
     });
 
     const points = map.getPoints();
     const pins: DottedMapLib.Point[] = points.filter((point) => point.data);
-
-    const positionMarkers = () => {
-        if (!mapContainer) return;
-
-        const svgRect = mapContainer.getBoundingClientRect();
-        const viewBox = mapContainer.viewBox.baseVal;
-
-        // Calculate scaling factors
-        const scaleX = svgRect.width / viewBox.width;
-        const scaleY = svgRect.height / viewBox.height;
-
-        pins.forEach((pin, index) => {
-            const marker = document.querySelector(`#marker-${index}`) as HTMLElement;
-            if (!marker) return;
-
-            const x = pin.x * scaleX + svgRect.left;
-            const y = pin.y * scaleY + svgRect.top;
-
-            marker.style.left = `${x}px`;
-            marker.style.top = `${y}px`;
-        });
-    };
-
-    onMount(() => {
-        positionMarkers();
-        window.addEventListener('resize', positionMarkers);
-        return () => window.removeEventListener('resize', positionMarkers);
-    });
 </script>
 
 <div class="bg-white">
     <div
         class="container relative mx-auto flex items-center justify-center perspective-distant transform-3d"
     >
-        <div class="absolute top-0 z-10 size-full">
-            {#each pins as pin, i}
-                <div
-                    id="marker-{i}"
-                    class="absolute z-50 h-4 w-4 -translate-1/2 rounded-full bg-black"
+        <svg
+            viewBox="0 0 105 50"
+            class="relative z-10 h-full w-full rotate-x-45"
+            bind:this={mapContainer}
+        >
+            {#each points as point}
+                <circle cx={point.x} cy={point.y} r="0.25" class="fill-black/[.08]" />
+            {/each}
+            {#each pins as pin}
+                <circle
+                    cx={pin.x}
+                    cy={pin.y}
+                    r="0.35"
+                    class="fill-black"
                     role="presentation"
                     on:mouseenter={() => {
                         handleTooltip({
@@ -173,22 +150,18 @@
                     on:mouseleave={() => (showTooltip = false)}
                 />
             {/each}
-        </div>
-        <svg viewBox="0 0 105 50" class="h-full w-full rotate-x-45" bind:this={mapContainer}>
-            {#each points as point}
-                <circle cx={point.x} cy={point.y} r="0.25" class="fill-black/[.08]" />
-            {/each}
-            {#each pins as pin}
-                <circle cx={pin.x} cy={pin.y} r="0.25" class="origin-center fill-black" />
-            {/each}
         </svg>
 
         {#if showTooltip}
             <div
-                class="pointer-events-none absolute top-0 left-0 z-10 rounded-2xl bg-white p-3"
-                style:transform={`translate(${x}px, ${y}px)`}
+                in:fly={{ y: 10, duration: 200 }}
+                out:fly={{ y: -10, duration: 200 }}
+                class="pointer-events-none absolute z-100 -translate-1/2 rounded-2xl bg-black p-3 text-white"
+                style:left="{$mousePosition.x}px"
+                style:top="{$mousePosition.y}px"
             >
-                {tooltip.city}, {tooltip.country}
+                {tooltip.city}
+                {tooltip.country}
             </div>
         {/if}
     </div>
--- src/routes/globe/mouse-position.ts
+++ src/routes/globe/mouse-position.ts
@@ -1,13 +1,16 @@
 import { readable } from 'svelte/store';
+import { browser } from '$app/environment';
 
-export const mousePosition = readable({ x: 0, y: 0 }, (set) => {
-    const updateMousePosition = (event: MouseEvent) => {
-        set({ x: event.clientX, y: event.clientY });
-    };
+export const mousePosition = browser
+    ? readable({ x: 0, y: 0 }, (set) => {
+          const updateMousePosition = (event: MouseEvent) => {
+              set({ x: event.clientX, y: event.clientY });
+          };
 
-    window.addEventListener('mousemove', updateMousePosition);
+          window.addEventListener('mousemove', updateMousePosition);
 
-    return () => {
-        window.removeEventListener('mousemove', updateMousePosition);
-    };
-});
+          return () => {
+              window.removeEventListener('mousemove', updateMousePosition);
+          };
+      })
+    : readable({ x: 0, y: 0 });
