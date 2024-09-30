a2e78aa8552f1758e8f80a839497f90eb1e239a0
Jesse Winton <jesse@appwrite.io>
2024-09-30 17:12:19 -0400

    update the map

--- src/routes/globe/(components)/Map.svelte
+++ src/routes/globe/(components)/Map.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
     import DottedMap, { type DottedMapLib } from 'dotted-map';
-    import { mousePosition } from '../mouse-position';
     import { fly } from 'svelte/transition';
+    import MobileMap from './MobileMap.svelte';
+    import Germany from '../(assets)/germany.png';
 
     let mapContainer: SVGSVGElement;
 
     let tooltip: {
+        x: number;
+        y: number;
         isActive: boolean;
         country: null | string;
         city: null | string;
         available: null | boolean;
     } = {
+        x: 0,
+        y: 0,
         isActive: false,
         country: null,
         city: null,
@@ -20,14 +25,18 @@
     let showTooltip: boolean = false;
 
     const handleTooltip = ({
+        event,
         activeCity,
         activeCountry,
         isAvailable
     }: {
+        event: MouseEvent;
         activeCity: string;
         activeCountry: string;
         isAvailable: boolean;
     }) => {
+        tooltip.x = event.clientX;
+        tooltip.y = event.clientY;
         showTooltip = true;
         tooltip.city = activeCity;
         tooltip.country = activeCountry;
@@ -39,14 +48,24 @@
         grid: 'diagonal'
     });
 
-    const pinsData: DottedMapLib.Pin[] = [
+    type Pin = DottedMapLib.Pin & {
+        data: {
+            city: string;
+            country: string;
+            available: boolean;
+            flag: string;
+        };
+    };
+
+    const pinsData: Pin[] = [
         {
             lat: 50.1109,
             lng: 8.6821,
             data: {
                 city: 'Frankfurt',
-                country: 'Germany',
-                available: true
+                country: 'GER',
+                available: true,
+                flag: Germany
             }
         },
         {
@@ -54,8 +73,9 @@
             lng: 2.3514,
             data: {
                 city: 'Paris',
-                country: 'France',
-                available: false
+                country: 'FRA',
+                available: false,
+                flag: Germany
             }
         },
         {
@@ -63,8 +83,9 @@
             lng: 77.5946,
             data: {
                 city: 'Bangalore',
-                country: 'India',
-                available: false
+                country: 'IND',
+                available: false,
+                flag: Germany
             }
         },
         {
@@ -72,8 +93,9 @@
             lng: 103.8198,
             data: {
                 city: 'Singapore',
-                country: 'Singapore',
-                available: false
+                country: 'SGP',
+                available: false,
+                flag: Germany
             }
         },
         {
@@ -81,8 +103,9 @@
             lng: 151.2093,
             data: {
                 city: 'Sydney',
-                country: 'Australia',
-                available: false
+                country: 'AUS',
+                available: false,
+                flag: Germany
             }
         },
         {
@@ -91,7 +114,8 @@
             data: {
                 city: 'New York, NY',
                 country: 'USA',
-                available: false
+                available: false,
+                flag: Germany
             }
         },
         {
@@ -100,7 +124,8 @@
             data: {
                 city: 'San Francisco, CA',
                 country: 'USA',
-                available: false
+                available: false,
+                flag: Germany
             }
         }
     ];
@@ -109,11 +134,7 @@
         map.addPin({
             lat: pin.lat,
             lng: pin.lng,
-            data: {
-                city: pin.data.city,
-                country: pin.data.country,
-                available: pin.data.available
-            }
+            data: pin.data
         });
     });
 
@@ -121,27 +142,64 @@
     const pins: DottedMapLib.Point[] = points.filter((point) => point.data);
 </script>
 
-<div class="bg-white">
+<div class="light overflow-hidden bg-[#EDEDF0]">
     <div
         class="container relative mx-auto flex items-center justify-center perspective-distant transform-3d"
     >
+        <MobileMap {pins} />
+        {#if showTooltip}
+            <div
+                in:fly={{ y: 10, duration: 200 }}
+                out:fly={{ y: -10, duration: 200 }}
+                class="pointer-events-none absolute z-100 block flex w-[190px] flex-col gap-2 rounded-[10px] border border-white bg-gradient-to-br from-white/64 to-white/32 p-2 backdrop-blur-sm"
+                style:left="{tooltip.x - 100}px"
+                style:top="{tooltip.y - 100}px"
+            >
+                <span class="text-primary text-caption w-fit"
+                    >{tooltip.city}
+                    ({tooltip.country})</span
+                >
+                {#if tooltip.available}
+                    <div
+                        class="text-caption flex h-5 items-center justify-center place-self-start rounded-[6px] bg-[#10B981]/16 p-1 text-center text-[#0A714F]"
+                    >
+                        <span class="text-micro -tracking-tight">Available now</span>
+                    </div>
+                {:else}
+                    <div
+                        class="text-caption bg-greyscale-200/32 text-primary flex h-5 items-center justify-center place-self-start rounded-[6px] p-1 text-center"
+                    >
+                        <span class="text-micro -tracking-tight">Available soon</span>
+                    </div>
+                {/if}
+            </div>
+        {/if}
         <svg
             viewBox="0 0 105 50"
-            class="relative z-10 h-full w-full rotate-x-45"
+            class="relative z-10 h-full w-[250%] rotate-x-45 md:w-full"
             bind:this={mapContainer}
         >
             {#each points as point}
                 <circle cx={point.x} cy={point.y} r="0.25" class="fill-black/[.08]" />
             {/each}
             {#each pins as pin}
-                <circle
+                <defs>
+                    <clipPath id="icon">
+                        <circle cx={pin.x} cy={pin.y} r="0.25" fill="#000" role="presentation" />
+                    </clipPath>
+                </defs>
+
+                <image
+                    width="100"
+                    height="100"
+                    role="presentation"
+                    xlink:href={pin.data.flag}
+                    clip-path="url(#icon)"
                     cx={pin.x}
                     cy={pin.y}
-                    r="0.35"
-                    class="fill-black"
-                    role="presentation"
-                    on:mouseenter={() => {
+                    on:mouseenter={(event) => {
                         handleTooltip({
+                            event,
                             activeCity: pin.data.city,
                             activeCountry: pin.data.country,
                             isAvailable: pin.data.available
@@ -151,18 +209,5 @@
                 />
             {/each}
         </svg>
-
-        {#if showTooltip}
-            <div
-                in:fly={{ y: 10, duration: 200 }}
-                out:fly={{ y: -10, duration: 200 }}
-                class="pointer-events-none absolute z-100 -translate-1/2 rounded-2xl bg-black p-3 text-white"
-                style:left="{$mousePosition.x}px"
-                style:top="{$mousePosition.y}px"
-            >
-                {tooltip.city}
-                {tooltip.country}
-            </div>
-        {/if}
     </div>
 </div>
---
+++ src/routes/globe/(components)/MobileMap.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { type DottedMapLib } from 'dotted-map';
+
+    export let pins: DottedMapLib.Point[];
+</script>
+
+<div class="absolute inset-0 md:hidden">
+    {#each pins as pin}
+        <div>
+            {pin.data.city}
+        </div>
+    {/each}
+</div>
