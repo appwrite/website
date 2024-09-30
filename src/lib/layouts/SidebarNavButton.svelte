af59d6064d10765eaeed5a17b0ccbdeb9900c519
ItzNotABug <itznotabug@gmail.com>
2024-09-30 15:10:27 +0530

    add: new tag to `roles` feature.

--- src/lib/layouts/SidebarNavButton.svelte
+++ src/lib/layouts/SidebarNavButton.svelte
@@ -13,14 +13,26 @@
     {#if groupItem.icon}
         <span class="icon {groupItem.icon}" aria-hidden="true" />
     {/if}
-    <span class="text-caption">{groupItem.label} </span>
+    <span class="flex gap-2 text-caption">
+        <span>{groupItem.label}</span>
+
+        {#if groupItem.new}
+            <span class="web-inline-tag is-pink" style="">New</span>
+        {/if}
+    </span>
     {#if groupItem.isParent}
         <span class="icon-cheveron-right ml-auto" aria-hidden="true" />
     {/if}
 </a>
 
 <style lang="scss">
-    [class*='icon'] {
-        font-size: 1rem;
-    }
+  [class*='icon'] {
+    font-size: 1rem;
+  }
+
+  .web-inline-tag {
+    font-size: 0.75rem;
+    padding-block: 2px;
+    margin-block-start: 1px;
+  }
 </style>
