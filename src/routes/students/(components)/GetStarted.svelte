5029dbedf20de0c0dbc98648e41de2c1056adaca
Jesse Winton <jesse@appwrite.io>
2024-10-02 10:44:44 -0400

    fix cta

--- src/routes/students/(components)/GetStarted.svelte
+++ src/routes/students/(components)/GetStarted.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
     import Badge from '$lib/components/ui/Badge.svelte';
-    import Button from '$lib/components/ui/Button.svelte';
     import { classNames } from '$lib/utils/classnames';
 
     const items = [
         {
             label: 'Enroll to the GitHub Student Developer Pack',
             description: 'Sign up for the Student Developer pack and explore the benefits.',
-            cta: { url: 'https://github.com', label: 'Enroll on GitHub Education' }
+            cta: { url: 'https://github.com', label: 'Enroll on GitHub Education', icon: 'github' }
         },
         {
             label: 'Redeem your $250 credits',
@@ -26,16 +25,16 @@
 
 <div
     class={classNames(
-        'light relative mt-10 min-h-80',
-        'before:absolute before:top-0 before:left-0 before:z-0 before:block before:h-80 before:w-full before:bg-[radial-gradient(at_25%_0%,_hsla(343,_98%,_60%,_0.4)_0px,_transparent_75%)] md:before:w-1/2',
-        'after:absolute after:top-0 after:right-0 after:z-0 after:block after:hidden after:h-80 after:w-1/2 after:bg-[radial-gradient(at_100%_0%,_hsla(177,_53%,_69%,_0.6)_0px,_transparent_75%)] md:after:block'
+        'light border-smooth relative mt-10 min-h-80 border-t',
+        'before:absolute before:top-0 before:left-0 before:z-0 before:block before:h-80 before:w-full before:bg-[radial-gradient(at_25%_0%,_hsla(343,_98%,_60%,_0.4)_0px,_transparent_73%,_transparent_100%)] md:before:w-1/2',
+        'after:absolute after:top-0 after:right-0 after:z-0 after:block after:hidden after:h-80 after:w-1/2 after:bg-[radial-gradient(at_100%_0%,_hsla(177,_53%,_69%,_0.6)_0px,_transparent_73%,_transparent_100%)] md:after:block'
     )}
 >
     <div class="container relative z-10 flex flex-col justify-center py-24">
         <h2 class="text-primary font-aeonik-pro text-4xl">Get started today</h2>
 
         <div
-            class="mt-12 grid place-items-center gap-8 divide-[#19191C]/4 rounded-2xl bg-white p-5 shadow-[-6px_8px_24px_rgba(0,_0,_0,_0.06),_0px_0px_0px_rgba(0,_0,_0,_0.06)] max-sm:divide-y md:grid-cols-3 md:divide-x"
+            class="divide-smooth mt-12 grid place-items-center gap-8 rounded-2xl bg-white py-10 shadow-[-6px_8px_24px_rgba(0,_0,_0,_0.06),_0px_0px_0px_rgba(0,_0,_0,_0.06)] max-sm:divide-y md:grid-cols-3 md:divide-x"
         >
             {#each items as item, i}
                 <div
@@ -48,7 +47,12 @@
                     </div>
                     <a
                         class="web-button is-secondary text-accent mt-auto mb-0 !w-full md:!w-fit"
-                        href={item.cta.url}>{item.cta.label}</a
+                        href={item.cta.url}
+                    >
+                        {#if item.cta.icon}
+                            <span class="web-icon-{item.cta.icon}" />
+                        {/if}
+                        {item.cta.label}</a
                     >
                 </div>
             {/each}
