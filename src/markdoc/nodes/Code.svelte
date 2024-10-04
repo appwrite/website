940d2760f02b8b6f95dd1ccce626322505ed9338
Jesse Winton <jesse@appwrite.io>
2024-10-04 15:35:21 -0400

    rm unneeded changes

--- src/markdoc/nodes/Code.svelte
+++ src/markdoc/nodes/Code.svelte
@@ -2,7 +2,4 @@
     export let content: string;
 </script>
 
-<span
-    class="web-inline-code web-code text-caption inline-flex break-words rounded py-[.0625rem] px-1 font-mono"
-    >{content}</span
->
+<span class="web-inline-code web-code">{content}</span>
