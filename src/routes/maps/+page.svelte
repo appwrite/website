75eacd6f3661dc97a24bb7aaeae3eed93f3af417
Jesse Winton <jesse@appwrite.io>
2024-10-07 16:18:00 -0400

    update

---
+++ src/routes/maps/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import MapWithMarkers from '$lib/components/regions/MapWithMarkers.svelte';
+    import Main from '$lib/layouts/Main.svelte';
+</script>
+
+<Main>
+    <MapWithMarkers />
+</Main>
