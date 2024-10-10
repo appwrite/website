38205d08625244d2bbf5585aa9c740af2f03f746
ernstmul <ernst.mulders@gmail.com>
2024-10-10 14:36:22 +0200

    Add self hosted support form

---
+++ src/routes/contact-us/index.ts
@@ -0,0 +1,20 @@
+import { PUBLIC_GROWTH_ENDPOINT } from '$env/static/public';
+
+export async function sendSalesEmail(body: {
+    email: string;
+    subject: string;
+    cloudEmail: string | undefined;
+    companyName: string;
+    companySize: string | null;
+    companyWebsite: string;
+    firstName: string;
+    message: string;
+}) {
+    return await fetch(`${PUBLIC_GROWTH_ENDPOINT}/feedback/sales`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+}
