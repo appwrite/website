434978d3ec05385d936dfea00184c40e2274a23d
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-10-14 15:21:03 +0200

    feat: cdn

--- src/lib/utils/image.ts
+++ src/lib/utils/image.ts
@@ -2,7 +2,7 @@ import type { ImageProps } from '@unpic/svelte';
 import { Avatars, Client } from '@appwrite.io/console';
 
 const client = new Client();
-client.setProject('console').setEndpoint('https://migration.appwrite.org/v1')
+client.setProject('console').setEndpoint('https://appwrite-test-cdn.global.ssl.fastly.net/v1')
 const avatars = new Avatars(client);
 
 export const transformer: ImageProps['transformer'] = (options) => {
