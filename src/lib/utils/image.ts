6aac55f237db215bc6172618800483b4a131c185
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-10-14 15:17:39 +0200

    chore: fix style

--- src/lib/utils/image.ts
+++ src/lib/utils/image.ts
@@ -6,8 +6,7 @@ client.setProject('console').setEndpoint('https://migration.appwrite.org/v1')
 const avatars = new Avatars(client);
 
 export const transformer: ImageProps['transformer'] = (options) => {
-	const { url, height, width } = options;
-    // if (dev) return url;
+    const { url, height, width } = options;
     const src = new URL(url, 'https://appwrite.io');
     return avatars.getImage(src.toString(), width, height);
 };
