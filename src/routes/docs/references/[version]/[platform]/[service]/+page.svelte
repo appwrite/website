3f5df1bdbca44cf390d244da9f76160f485fb3d1
Christy Jacob <christyjacob4@gmail.com>
2024-11-08 12:03:08 +0100

    feat: update base url

--- src/routes/docs/references/[version]/[platform]/[service]/+page.svelte
+++ src/routes/docs/references/[version]/[platform]/[service]/+page.svelte
@@ -166,7 +166,7 @@
                     <Fence
                         language="text"
                         badge="Base URL"
-                        content="https://cloud.appwrite.io/v1"
+                        content="https://<REGION>.cloud.appwrite.io/v1"
                         process
                         withLineNumbers={false}
                     />
