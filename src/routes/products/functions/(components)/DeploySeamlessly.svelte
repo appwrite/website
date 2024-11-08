45a949c1341ebe6c36ff8ba62274097bb89fb014
Jesse Winton <jesse@appwrite.io>
2024-11-08 17:16:18 -0500

    Update DeploySeamlessly.svelte

--- src/routes/products/functions/(components)/DeploySeamlessly.svelte
+++ src/routes/products/functions/(components)/DeploySeamlessly.svelte
@@ -1,140 +1,5 @@
 <script lang="ts">
-    import { trackEvent } from '$lib/actions/analytics';
-    import { Tooltip } from '$lib/components';
     import { classNames } from '$lib/utils/classnames';
-    import { Framework, Platform } from '$lib/utils/references';
-    import MultiFrameworkCode from './MultiFrameworkCode.svelte';
-
-    const codeSnippets = [
-        {
-            language: Platform.ClientWeb,
-            platform: Framework.NextJs,
-            content: `import { Client, Account } from 'node-appwrite'
-import { cookies } from 'next/headers'
-
-async function getLoggedInUser() {
-  const session = cookies().get("custom-session-cookie");  
-  if (!session) return
-
-    const client = new Client()
-    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
-    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID)
-
-    client.setSession(session.value)
-    const account = new Account(client);
-
-    return await account.get()
-}
-  `
-        },
-        {
-            language: Platform.ClientWeb,
-            platform: Framework.SvelteKit,
-            content: `import { Client, Account } from 'node-appwrite'
-
-async function getLoggedInUser() {
-  const session = cookies().get("custom-session-cookie");  
-  if (!session) return
-
-  const client = new Client()
-    .setEndpoint(process.env.PUBLIC_APPWRITE_ENDPOINT)    
-    .setProject(process.env.PUBLIC_APPWRITE_PROJECT_ID)
-
-      client.setSession(session.value)
-      const account = new Account(client);
-
-    return await account.get()
-  }
-`
-        },
-        {
-            language: Platform.ClientWeb,
-            platform: Framework.Astro,
-            content: `import { Client, Account } from 'node-appwrite'
-
-async function getLoggedInUser(context) {
-  const session = cookies().get("custom-session-cookie");  
-  if (!session) return
-
-  const client = new Client()
-    .setEndpoint(import.meta.env.PUBLIC_APPWRITE_ENDPOINT)    
-    .setProject(import.meta.env.PUBLIC_APPWRITE_PROJECT_ID)
-
-      client.setSession(session.value)
-      const account = new Account(client);
-
-    return await account.get()
-  }
-`
-        },
-        {
-            language: Platform.ClientWeb,
-            platform: Framework.Nuxt3,
-            content: `import { Client, Account } from 'node-appwrite'
-import { H3Event, getCookie } from 'h3'
-
-async function getLoggedInUser(event) {
-  const session = getCookie(event, "custom-session-cookie");  
-  if (!session) return
-
-  const client = new Client()
-    .setEndpoint(process.env.PUBLIC_APPWRITE_ENDPOINT)    
-    .setProject(process.env.PUBLIC_APPWRITE_PROJECT_ID)
-
-      client.setSession(session.value)
-      const account = new Account(client);
-
-    return await account.get()
-  }`
-        },
-        {
-            language: Platform.ClientWeb,
-            platform: Framework.Remix,
-            content: `import { Client, Account } from 'node-appwrite'
-import { createCookie } from '@remix-run/node'
-
-export const customSessionCookie = createCookie("custom-session-cookie", {
-  maxAge: 604800,
-})
-
-async function getLoggedInUser(request) {
-  const cookies = request.headers.get("Cookie")  
-  const session = await customSessionCookie.parse(cookies)  
-  if (!session) return
-
-  const client = new Client()
-    .setEndpoint(process.env.PUBLIC_APPWRITE_ENDPOINT)    
-    .setProject(process.env.PUBLIC_APPWRITE_PROJECT_ID)
-
-      client.setSession(session.value)
-      const account = new Account(client);
-
-    return await account.get()
-  }`
-        }
-    ];
-
-    const platforms = [
-        {
-            name: 'Next',
-            href: '/docs/quick-starts/nextjs',
-            image: `/images/platforms/light/nextjs.svg`
-        },
-        {
-            name: 'Svelte',
-            href: '/docs/quick-starts/sveltekit',
-            image: `/images/platforms/light/svelte.svg`
-        },
-        {
-            name: 'Nuxt',
-            href: '/docs/quick-starts/nuxt',
-            image: `/images/platforms/light/nuxt.svg`
-        }
-    ] as Array<{
-        name: string;
-        href: string;
-        image: string;
-    }>;
 </script>
 
 <section class="py-4 md:py-20">
@@ -318,12 +183,17 @@ async function getLoggedInUser(request) {
                     >
                         {#each Array.from({ length: 22 }) as _, i}
                             {@const lineNumber = i + 37}
+                            {@const highlightedNumbers = [47, 48, 49, 50]}
                             <li
                                 class={classNames('pointer-events-none pl-2 text-[10px]', {
                                     'rounded bg-[#FF453A]/8 text-white': lineNumber === 41,
                                     'bg-[#7ADF82]/8 text-white': [47, 48, 49, 50].includes(
                                         lineNumber
-                                    )
+                                    ),
+                                    'rounded-t': lineNumber === highlightedNumbers[0],
+                                    'rounded-b':
+                                        lineNumber ===
+                                        highlightedNumbers[highlightedNumbers.length - 1]
                                 })}
                             >
                                 {lineNumber}
