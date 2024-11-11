dd73eac30b6bb01f57f8f7822242b17e034cd66e
ItzNotABug <itznotabug@gmail.com>
2024-11-11 13:36:32 +0530

    improve: animations, misc.

--- src/lib/components/TocNav.svelte
+++ src/lib/components/TocNav.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-    import { getTocCtx } from './TocRoot.svelte';
     import TocTree from './TocTree.svelte';
+    import { cubicOut } from 'svelte/easing';
+    import { getTocCtx } from './TocRoot.svelte';
 
     export let showToc = true;
 
@@ -12,6 +13,30 @@
     } = getTocCtx();
 
     $: progress = Math.max(...$activeHeadingIdxs) / ($headingsTree.length - 1);
+
+    function slideFade(
+        node: HTMLElement,
+        {
+            delay = 0,
+            duration = 400,
+            easing = cubicOut
+        }: { delay?: number; duration?: number; easing?: (t: number) => number } = {}
+    ) {
+        const initialHeight = node.offsetHeight;
+
+        return {
+            delay,
+            duration,
+            easing,
+            css: (t: number) => {
+                return `
+                    opacity: ${t};
+                    height: ${t * initialHeight}px;
+                    overflow: hidden;
+                `;
+            }
+        };
+    }
 </script>
 
 <section class="web-mobile-header">
@@ -24,109 +49,115 @@
                 <span class="text-description">Table of contents</span>
                 <span
                     aria-hidden="true"
-                    class="{!showToc ? 'icon-menu-alt-4' : 'web-icon-close'} toggle-icon"
-                />
+                    class="toggle-icon {showToc ? 'web-icon-close' : 'icon-menu-alt-4'}"
+                ></span>
             </span>
         </button>
     </div>
 
-    <aside class="web-grid-120-1fr-auto-side" class:web-is-mobile-closed={!showToc}>
-        <div class="web-page-steps">
-            <div
-                class="web-page-steps-location web-is-not-mobile"
-                style="--location:{progress * 100}%;"
-            >
-                <span class="web-page-steps-location-button">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        viewBox="0 0 16 16"
-                        fill="none"
-                    >
-                        <g clip-path="url(#clip0_1684_10747)">
-                            <g filter="url(#filter0_b_1684_10747)">
-                                <circle
-                                    cx="8"
-                                    cy="8"
-                                    r="8"
-                                    fill="url(#paint0_linear_1684_10747)"
-                                    fill-opacity="0.32"
-                                />
-                                <circle
-                                    cx="8"
-                                    cy="8"
-                                    r="7.75"
-                                    stroke="url(#paint1_linear_1684_10747)"
-                                    stroke-width="0.5"
-                                />
+    {#if showToc}
+        <aside
+            class="web-grid-120-1fr-auto-side"
+            class:web-is-mobile-closed={!showToc}
+            transition:slideFade={{ duration: 300 }}
+        >
+            <div class="web-page-steps">
+                <div
+                    class="web-page-steps-location web-is-not-mobile"
+                    style="--location:{progress * 100}%;"
+                >
+                    <span class="web-page-steps-location-button">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            viewBox="0 0 16 16"
+                            fill="none"
+                        >
+                            <g clip-path="url(#clip0_1684_10747)">
+                                <g filter="url(#filter0_b_1684_10747)">
+                                    <circle
+                                        cx="8"
+                                        cy="8"
+                                        r="8"
+                                        fill="url(#paint0_linear_1684_10747)"
+                                        fill-opacity="0.32"
+                                    />
+                                    <circle
+                                        cx="8"
+                                        cy="8"
+                                        r="7.75"
+                                        stroke="url(#paint1_linear_1684_10747)"
+                                        stroke-width="0.5"
+                                    />
+                                </g>
+                                <circle cx="8" cy="7.99219" r="3" fill="white" />
                             </g>
-                            <circle cx="8" cy="7.99219" r="3" fill="white" />
-                        </g>
-                        <defs>
-                            <filter
-                                id="filter0_b_1684_10747"
-                                x="-200"
-                                y="-200"
-                                width="416"
-                                height="416"
-                                filterUnits="userSpaceOnUse"
-                                color-interpolation-filters="sRGB"
-                            >
-                                <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                                <feGaussianBlur in="BackgroundImageFix" stdDeviation="100" />
-                                <feComposite
-                                    in2="SourceAlpha"
-                                    operator="in"
-                                    result="effect1_backgroundBlur_1684_10747"
-                                />
-                                <feBlend
-                                    mode="normal"
-                                    in="SourceGraphic"
-                                    in2="effect1_backgroundBlur_1684_10747"
-                                    result="shape"
-                                />
-                            </filter>
-                            <linearGradient
-                                id="paint0_linear_1684_10747"
-                                x1="2.02105"
-                                y1="1.10843"
-                                x2="16.3872"
-                                y2="17.2901"
-                                gradientUnits="userSpaceOnUse"
-                            >
-                                <stop stop-color="white" stop-opacity="0.4" />
-                                <stop offset="1" stop-color="white" stop-opacity="0" />
-                            </linearGradient>
-                            <linearGradient
-                                id="paint1_linear_1684_10747"
-                                x1="7.45643"
-                                y1="-1.10615"
-                                x2="5.53812"
-                                y2="17.9973"
-                                gradientUnits="userSpaceOnUse"
-                            >
-                                <stop stop-color="white" stop-opacity="0.16" />
-                                <stop offset="1" stop-color="white" stop-opacity="0" />
-                            </linearGradient>
-                            <clipPath id="clip0_1684_10747">
-                                <rect width="16" height="16" fill="white" />
-                            </clipPath>
-                        </defs>
-                    </svg>
-                </span>
-            </div>
+                            <defs>
+                                <filter
+                                    id="filter0_b_1684_10747"
+                                    x="-200"
+                                    y="-200"
+                                    width="416"
+                                    height="416"
+                                    filterUnits="userSpaceOnUse"
+                                    color-interpolation-filters="sRGB"
+                                >
+                                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                    <feGaussianBlur in="BackgroundImageFix" stdDeviation="100" />
+                                    <feComposite
+                                        in2="SourceAlpha"
+                                        operator="in"
+                                        result="effect1_backgroundBlur_1684_10747"
+                                    />
+                                    <feBlend
+                                        mode="normal"
+                                        in="SourceGraphic"
+                                        in2="effect1_backgroundBlur_1684_10747"
+                                        result="shape"
+                                    />
+                                </filter>
+                                <linearGradient
+                                    id="paint0_linear_1684_10747"
+                                    x1="2.02105"
+                                    y1="1.10843"
+                                    x2="16.3872"
+                                    y2="17.2901"
+                                    gradientUnits="userSpaceOnUse"
+                                >
+                                    <stop stop-color="white" stop-opacity="0.4" />
+                                    <stop offset="1" stop-color="white" stop-opacity="0" />
+                                </linearGradient>
+                                <linearGradient
+                                    id="paint1_linear_1684_10747"
+                                    x1="7.45643"
+                                    y1="-1.10615"
+                                    x2="5.53812"
+                                    y2="17.9973"
+                                    gradientUnits="userSpaceOnUse"
+                                >
+                                    <stop stop-color="white" stop-opacity="0.16" />
+                                    <stop offset="1" stop-color="white" stop-opacity="0" />
+                                </linearGradient>
+                                <clipPath id="clip0_1684_10747">
+                                    <rect width="16" height="16" fill="white" />
+                                </clipPath>
+                            </defs>
+                        </svg>
+                    </span>
+                </div>
 
-            <div class="toc-tree-holder">
-                <TocTree
-                    tree={$headingsTree}
-                    activeHeadingIdxs={$activeHeadingIdxs}
-                    {item}
-                    bind:showToc
-                />
+                <div class="toc-tree-holder">
+                    <TocTree
+                        tree={$headingsTree}
+                        activeHeadingIdxs={$activeHeadingIdxs}
+                        {item}
+                        bind:showToc
+                    />
+                </div>
             </div>
-        </div>
-    </aside>
+        </aside>
+    {/if}
 </section>
 
 <style>
@@ -147,7 +178,8 @@
             position: sticky;
             padding: 1.375rem 0;
             align-content: center;
-            padding-block: 1.25rem;
+            /** 1.5rem covers main header completely so fragments of it are not shown during scroll */
+            padding-block: 1.5rem;
             padding-inline: 1.25rem;
             background: hsl(var(--p-body-bg-color));
             border-block-end: solid 1px var(--p-mobile-header-border-color);
@@ -159,8 +191,8 @@
             margin-left: 0.25rem;
         }
 
-        .toggle-icon {
-            max-width: 16px;
+        .web-icon-close {
+            max-width: 20px;
             max-height: 24px;
         }
     }
