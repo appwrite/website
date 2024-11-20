42d92d36869829d699d02bf6aed1836090b01626
Jesse Winton <jesse@appwrite.io>
2024-11-20 13:09:34 -0500

    Update product-cards.svelte

--- src/lib/components/product-pages/product-cards.svelte
+++ src/lib/components/product-pages/product-cards.svelte
@@ -5,47 +5,74 @@
             description: 'Use Appwrite messaging to send email, SMS, and push notifications.',
             icon: '/images/icons/illustrated/dark/messaging.png',
             url: '/products/messaging'
+        },
+        functions: {
+            title: 'Messaging',
+            description: 'Use Appwrite messaging to send email, SMS, and push notifications.',
+            icon: '/images/icons/illustrated/dark/messaging.png',
+            url: '/products/messaging'
+        },
+        databases: {
+            title: 'Messaging',
+            description: 'Use Appwrite messaging to send email, SMS, and push notifications.',
+            icon: '/images/icons/illustrated/dark/messaging.png',
+            url: '/products/messaging'
+        },
+        storage: {
+            title: 'Messaging',
+            description: 'Use Appwrite messaging to send email, SMS, and push notifications.',
+            icon: '/images/icons/illustrated/dark/messaging.png',
+            url: '/products/messaging'
+        },
+        auth: {
+            title: 'Messaging',
+            description: 'Use Appwrite messaging to send email, SMS, and push notifications.',
+            icon: '/images/icons/illustrated/dark/messaging.png',
+            url: '/products/messaging'
+        },
+        realtime: {
+            title: 'Messaging',
+            description: 'Use Appwrite messaging to send email, SMS, and push notifications.',
+            icon: '/images/icons/illustrated/dark/messaging.png',
+            url: '/products/messaging'
         }
-    };
+    } as const;
 
-    export let products = [
-        'messaging',
-        'functions',
-        'databases',
-        'storage',
-        'auth',
-        'realtime',
-        'sites'
-    ];
+    export let exclude:
+        | 'messaging'
+        | 'functions'
+        | 'databases'
+        | 'storage'
+        | 'auth'
+        | 'realtime'
+        | undefined = undefined;
 </script>
 
 <section class="border-smooth -mt-8 border-t py-40">
     <div class="container">
         <h4 class="text-label text-primary text-center">Keep exploring our products</h4>
         <div class="mt-8 flex gap-8">
-            {#each products as product}
-                {@const productInfo = allProducts[product]}
+            <!--            
                 <div>
                     <a
                         class="web-card is-normal"
-                        href={product.url}
+                        href={productInfo.url}
                         style="background: rgba(255, 255, 255, 0.04);"
                     >
                         <div
                             class="web-u-padding-inline-8 web-u-padding-block-end-8 flex flex-col gap-2"
                         >
                             <div class="flex items-center gap-2">
-                                <img src={product.icon} alt="auth" width="32" height="32" />
-                                <h4 class="text-body text-primary">{product.title}</h4>
+                                <img src={productInfo.icon} alt="auth" width="32" height="32" />
+                                <h4 class="text-body text-primary">{productInfo.title}</h4>
                                 <span class="web-icon-arrow-right ml-auto" aria-hidden="true" />
                             </div>
                             <p class="text-sub-body">
-                                {product.description}
+                                {productInfo.description}
                             </p>
                         </div>
                     </a>
-                </div>
-            {/each}
+                </div> -->
         </div>
     </div>
 </section>
