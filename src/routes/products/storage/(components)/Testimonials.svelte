7e534bf3851b754aee8c477a26ce7513caf875c3
Jesse Winton <jesse@appwrite.io>
2024-11-20 17:19:52 -0500

    Update Testimonials.svelte

--- src/routes/products/storage/(components)/Testimonials.svelte
+++ src/routes/products/storage/(components)/Testimonials.svelte
@@ -1,49 +1,97 @@
 <script lang="ts">
     const testimonials = [
         {
-            name: 'Kap.ts',
-            username: '@Kaperskyguru',
-            avatar: '/images/community/avatars/kap.png',
-            quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in ultrices lacus.'
+            name: 'Ryan O’Conner',
+            copy: `The switch to using Appwrite brought infinite value that I'm still discovering today, but a major impact that it made was the amount of time and stress that it saved me as it simply just works.`,
+            image: '/images/community/avatars/kap.png',
+            title: 'Founder',
+            company: 'K-Collect'
         },
         {
-            name: 'Kap.ts',
-            username: '@Kaperskyguru',
-            avatar: '/images/community/avatars/kap.png',
-            quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in ultrices lacus.'
+            name: 'David Forster',
+            copy: `We really loved working with Appwrite for launching our bootstrapped "Open Mind" App. I am still surprised how easy the implementation into Flutter was.`,
+            image: '/images/community/avatars/kap.png',
+            title: 'Founder',
+            company: 'Open Mind'
         },
         {
-            name: 'Kap.ts',
-            username: '@Kaperskyguru',
-            avatar: '/images/community/avatars/kap.png',
-            quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in ultrices lacus.'
+            name: 'Marius Bolik',
+            copy: `The integrated user authentication and the ease of creating data structures have undoubtedly saved us several weeks' worth of time.`,
+            image: '/images/community/avatars/kap.png',
+            title: 'CTO',
+            company: 'mySHOEFITTER'
+        },
+        {
+            name: 'Sergio Ponguta',
+            copy: `Just go for it, don’t think twice. Try Appwrite, and you will love it!`,
+            image: '/images/community/avatars/kap.png',
+            title: 'Founder',
+            company: 'Smart Bee'
+        },
+        {
+            name: 'Phil McClusky',
+            copy: 'Just like a Swiss Army Knife, you can choose and use the tools that you need with Appwrite.',
+            image: '/images/community/avatars/kap.png',
+            title: 'Developer',
+            company: 'Majik Kids'
         }
     ];
 </script>
 
-<div class="relative">
-    <div class="container">
-        <ul class="grid gap-8 md:grid-cols-3">
-            {#each testimonials as testimonial}
-                <li class="flex flex-col gap-5">
-                    <div class="web-card is-white flex flex-col gap-5">
-                        <q>{testimonial.quote}</q>
-                        <div class="web-user-box">
-                            <img
-                                class="web-user-box-image"
-                                src={testimonial.avatar}
-                                alt="Avatar of {testimonial.name}"
-                                height="48"
-                                width="48"
-                            />
-                            <div class="web-user-box-name web-sub-body-500">{testimonial.name}</div>
-                            <div class="web-user-box-username web-sub-body-400">
-                                {testimonial.username}
-                            </div>
-                        </div>
+<div class="group relative my-8 w-full overflow-clip">
+    <div
+        class="animate-marquee light flex w-max gap-4 pl-4 group-hover:[animation-play-state:paused]"
+    >
+        {#each testimonials as testimonial}
+            <div
+                class="flex h-fit w-lg flex-col justify-center rounded-2xl bg-white p-6 transition-all"
+            >
+                <p class="text-sub-body text-secondary flex-1 font-medium">
+                    {testimonial.copy}
+                </p>
+
+                <div class="mt-4 flex items-center gap-3">
+                    <img
+                        src={testimonial.image}
+                        class="size-12 rounded-full"
+                        alt="{testimonial.company} Logo"
+                    />
+                    <div>
+                        <span class="text-secondary text-sub-body block font-medium">
+                            {testimonial.name}
+                        </span>
+                        <span class="text-sub-body text-secondary block"
+                            >{testimonial.title} // {testimonial.company}</span
+                        >
                     </div>
-                </li>
-            {/each}
-        </ul>
+                </div>
+            </div>
+        {/each}
+        {#each testimonials as testimonial}
+            <div
+                aria-hidden="true"
+                class="flex h-fit w-lg flex-col justify-center rounded-2xl bg-white p-6 transition-all"
+            >
+                <p class="text-sub-body text-secondary flex-1 font-medium">
+                    {testimonial.copy}
+                </p>
+
+                <div class="mt-4 flex items-center gap-3">
+                    <img
+                        src={testimonial.image}
+                        class="size-12 rounded-full"
+                        alt="{testimonial.company} Logo"
+                    />
+                    <div>
+                        <span class="text-secondary text-sub-body block font-medium">
+                            {testimonial.name}
+                        </span>
+                        <span class="text-sub-body text-secondary block"
+                            >{testimonial.title} // {testimonial.company}</span
+                        >
+                    </div>
+                </div>
+            </div>
+        {/each}
     </div>
 </div>
