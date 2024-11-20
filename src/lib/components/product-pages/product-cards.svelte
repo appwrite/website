9dd5bf67889f33e12816db92400ed4e77df909d7
Jesse Winton <jesse@appwrite.io>
2024-11-20 13:15:41 -0500

    update

--- src/lib/components/product-pages/product-cards.svelte
+++ src/lib/components/product-pages/product-cards.svelte
@@ -7,34 +7,28 @@
             url: '/products/messaging'
         },
         functions: {
-            title: 'Messaging',
-            description: 'Use Appwrite messaging to send email, SMS, and push notifications.',
-            icon: '/images/icons/illustrated/dark/messaging.png',
-            url: '/products/messaging'
+            title: 'Functions',
+            description: ' Scale big and unlock limitless potential with Appwrite functions.',
+            icon: '/images/icons/illustrated/dark/functions.png',
+            url: '/products/functions'
         },
         databases: {
-            title: 'Messaging',
-            description: 'Use Appwrite messaging to send email, SMS, and push notifications.',
-            icon: '/images/icons/illustrated/dark/messaging.png',
-            url: '/products/messaging'
+            title: 'Databases',
+            description: 'Store and query structured data, ensuring scalable storage.',
+            icon: '/images/icons/illustrated/dark/databases.png',
+            url: '/products/databases'
         },
         storage: {
-            title: 'Messaging',
-            description: 'Use Appwrite messaging to send email, SMS, and push notifications.',
-            icon: '/images/icons/illustrated/dark/messaging.png',
-            url: '/products/messaging'
-        },
-        auth: {
-            title: 'Messaging',
-            description: 'Use Appwrite messaging to send email, SMS, and push notifications.',
-            icon: '/images/icons/illustrated/dark/messaging.png',
-            url: '/products/messaging'
+            title: 'Storage',
+            description: 'Manage your files project, using convenient APIs and utilities.',
+            icon: '/images/icons/illustrated/dark/storage.png',
+            url: '/products/storage'
         },
         realtime: {
-            title: 'Messaging',
-            description: 'Use Appwrite messaging to send email, SMS, and push notifications.',
-            icon: '/images/icons/illustrated/dark/messaging.png',
-            url: '/products/messaging'
+            title: 'Realtime',
+            description: 'Utilize realtime information from all Appwrite services.',
+            icon: '/images/icons/illustrated/dark/realtime.png',
+            url: '/products/realtime'
         }
     } as const;
 
@@ -46,33 +40,36 @@
         | 'auth'
         | 'realtime'
         | undefined = undefined;
+
+    const products = Object.entries(allProducts)
+        .filter(([key]) => key !== exclude)
+        .map(([_, value]) => value);
 </script>
 
 <section class="border-smooth -mt-8 border-t py-40">
     <div class="container">
         <h4 class="text-label text-primary text-center">Keep exploring our products</h4>
-        <div class="mt-8 flex gap-8">
-            <!--            
-                <div>
-                    <a
-                        class="web-card is-normal"
-                        href={productInfo.url}
-                        style="background: rgba(255, 255, 255, 0.04);"
+        <div class="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+            {#each products as product}
+                <a
+                    class="web-card is-normal"
+                    href={product.url}
+                    style="background: rgba(255, 255, 255, 0.04);"
+                >
+                    <div
+                        class="web-u-padding-inline-8 web-u-padding-block-end-8 flex flex-col gap-2"
                     >
-                        <div
-                            class="web-u-padding-inline-8 web-u-padding-block-end-8 flex flex-col gap-2"
-                        >
-                            <div class="flex items-center gap-2">
-                                <img src={productInfo.icon} alt="auth" width="32" height="32" />
-                                <h4 class="text-body text-primary">{productInfo.title}</h4>
-                                <span class="web-icon-arrow-right ml-auto" aria-hidden="true" />
-                            </div>
-                            <p class="text-sub-body">
-                                {productInfo.description}
-                            </p>
+                        <div class="flex items-center gap-2">
+                            <img src={product.icon} alt="auth" width="32" height="32" />
+                            <h4 class="text-body text-primary">{product.title}</h4>
+                            <span class="web-icon-arrow-right ml-auto" aria-hidden="true" />
                         </div>
-                    </a>
-                </div> -->
+                        <p class="text-sub-body">
+                            {product.description}
+                        </p>
+                    </div>
+                </a>
+            {/each}
         </div>
     </div>
 </section>
