95c892ba4400573b0ee16643970bb65a3692b4dd
Jesse Winton <jesse@appwrite.io>
2024-11-25 16:59:59 -0500

    create page route

---
+++ src/routes/cloud-ga/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { FooterNav, MainFooter } from '$lib/components';
+    import { Main } from '$lib/layouts';
+    import { buildOpenGraphImage } from '$lib/utils/metadata';
+
+    const title = 'Cloud GA - Appwrite';
+    const description = 'Appwrite Cloud is now generally available.';
+    const ogImage = buildOpenGraphImage('Cloud GA', description);
+</script>
+
+<svelte:head>
+    <!-- Titles -->
+    <title>{title}</title>
+    <meta property="og:title" content={title} />
+    <meta name="twitter:title" content={title} />
+    <!-- Description -->
+    <meta name="description" content={description} />
+    <meta property="og:description" content={description} />
+    <meta name="twitter:description" content={description} />
+    <!-- Image -->
+    <meta property="og:image" content={ogImage} />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta name="twitter:image" content={ogImage} />
+    <meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+
+<Main>
+    <div class="container">
+        <FooterNav />
+        <MainFooter />
+    </div>
+</Main>
