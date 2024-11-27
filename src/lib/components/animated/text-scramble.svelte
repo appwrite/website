d0c33076dfc0ce3fc7122a19834dfbfda38bad04
Jesse Winton <jesse@appwrite.io>
2024-11-27 13:37:27 -0500

    update

--- src/lib/components/animated/text-scramble.svelte
+++ src/lib/components/animated/text-scramble.svelte
@@ -1,59 +1,29 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-
-    const defaultChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    import { write } from '$lib/animations';
+    import { cn } from '$lib/utils/classnames';
+    import { onDestroy, onMount } from 'svelte';
 
     export let text: string = '';
-    export let duration: number = 0.8;
-    export let speed: number = 0.04;
-    export let characterSet: string = defaultChars;
+    export let delay: number = 0;
+    export let duration: number = 2500;
+
     export let trigger: boolean = true;
 
     let className: string = '';
     export { className as class };
 
-    let displayText: string = text;
-    let isAnimating: boolean = false;
-
-    const scramble = async () => {
-        if (isAnimating) return;
-        isAnimating = true;
-
-        const steps = duration / speed;
-        let step = 0;
-
-        const interval = setInterval(() => {
-            let scrambled = '';
-            const progress = step / steps;
-
-            for (let i = 0; i < text.length; i++) {
-                if (text[i] === ' ') {
-                    scrambled += ' ';
-                    continue;
-                }
-
-                if (progress * text.length > i) {
-                    scrambled += text[i];
-                } else {
-                    scrambled += characterSet[Math.floor(Math.random() * characterSet.length)];
-                }
-            }
-
-            displayText = scrambled;
-            step++;
-
-            if (step > steps) {
-                clearInterval(interval);
-                displayText = text;
-                isAnimating = false;
-            }
-        }, speed * 1000);
-    };
+    let displayText: string = '';
 
     onMount(() => {
         if (!trigger) return;
-        scramble();
+        const delayTimeout = setTimeout(() => {
+            write(text, (v) => (displayText = v), duration);
+        }, delay);
+
+        return () => clearTimeout(delayTimeout);
     });
 </script>
 
-<span class={className}>{displayText}</span>
+<span class={cn('inline-block w-full text-balance', className)}
+    >{displayText}<slot name="suffix" /></span
+>
