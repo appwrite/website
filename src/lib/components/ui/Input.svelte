87391302b9f3425f00366d955fce81d7af4771b6
Darshan <itznotabug@gmail.com>
2024-11-27 17:19:54 +0530

    ran: formatter.

--- src/lib/components/ui/Input.svelte
+++ src/lib/components/ui/Input.svelte
@@ -15,7 +15,7 @@
 {#if $$slots.icon}
     <label
         class={classNames(
-            'focus:border-greyscale-100 bg-greyscale-800 border-greyscale-700 flex items-center gap-1 rounded-lg border px-3 py-2 text-sm font-light transition-colors focus-within:border-white active:shadow-sm active:shadow-black/30',
+            'focus:border-greyscale-100 bg-greyscale-800 border-greyscale-700 flex items-center gap-1 rounded-lg border py-2 px-3 text-sm font-light transition-colors focus-within:border-white active:shadow-sm active:shadow-black/30',
             classes
         )}
     >
@@ -29,7 +29,7 @@
                 on:change
                 on:focus
                 on:blur
-                class="w-full border-0 outline-none ring-0"
+                class="w-full border-0 ring-0 outline-none"
                 {...props}
             />
         {/key}
@@ -50,7 +50,7 @@
             on:focus
             on:blur
             class={classNames(
-                'focus:border-greyscale-100 bg-greyscale-800 border-greyscale-700 mt-2 flex w-full items-center gap-1 rounded-lg border px-3 py-2 text-sm font-light transition-colors focus-within:border-white active:shadow-sm active:shadow-black/30',
+                'focus:border-greyscale-100 bg-greyscale-800 border-greyscale-700 mt-2 flex w-full items-center gap-1 rounded-lg border py-2 px-3 text-sm font-light transition-colors focus-within:border-white active:shadow-sm active:shadow-black/30',
                 classes
             )}
             {...props}
