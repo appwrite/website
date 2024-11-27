0adad1a67267b54aa360d430c7956735926cf341
Jesse Winton <jrandallwinton@gmail.com>
2024-11-27 09:55:20 -0500

    Revert "Functions product page"

--- src/lib/components/product-pages/testimonials.svelte
+++ src/lib/components/product-pages/testimonials.svelte
@@ -10,7 +10,9 @@
         {
             name: 'David Forster',
             copy: `We really loved working with Appwrite for launching our bootstrapped "Open Mind" App. I am still surprised how easy the implementation into Flutter was.`,
-            image: '/images/testimonials/david-forster.png'
+            image: '/images/testimonials/david-forster.png',
+            title: 'Founder',
+            company: 'Open Mind'
         },
         {
             name: 'Marius Bolik',
@@ -72,7 +74,7 @@
 </div> -->
 
 <div class="relative my-8 w-full max-w-[100vw] overflow-hidden">
-    <div class="light group flex w-fit gap-4">
+    <div class="group light flex w-fit gap-4">
         {#each Array.from({ length: 4 }) as _, i}
             <div
                 class="animate-scroll flex items-center gap-4 group-hover:[animation-play-state:paused;]"
@@ -80,7 +82,7 @@
             >
                 {#each testimonials as testimonial}
                     <div
-                        class="w-lg flex h-fit flex-col justify-center rounded-2xl bg-white p-6 transition-all"
+                        class="flex h-fit w-lg flex-col justify-center rounded-2xl bg-white p-6 transition-all"
                     >
                         <p class="text-sub-body text-secondary flex-1 font-medium">
                             {testimonial.copy}
