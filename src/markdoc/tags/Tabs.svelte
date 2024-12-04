6b9c5719ed99187876303c7210d043fd49fd1ee0
ernstmul <ernst.mulders@gmail.com>
2024-12-04 09:36:01 +0100

    Fix so the active state will be visible again

--- src/markdoc/tags/Tabs.svelte
+++ src/markdoc/tags/Tabs.svelte
@@ -32,13 +32,20 @@
     >
         <ul class="tabs-list flex items-center gap-4" {...$list} use:list>
             {#each $ctx.triggers.entries() as [id, title]}
-                <li class="tabs-item rounded-t-[0.625rem] hover:bg-white/4">
+                <li
+                    class="tabs-item rounded-t-[0.625rem] hover:bg-white/4"
+                    class:text-[var(--color-primary)]={$value === id}
+                >
                     <button
                         class="tabs-button cursor-pointer bg-clip-padding py-[0.625rem] px-1 font-light outline-none"
-                        class:is-selected={$value === id}
                         {...$trigger(id)}
                         use:trigger>{title}</button
                     >
+                    {#if $value === id}
+                        <div class="absolute mt-0.5 h-px overflow-hidden bg-[var(--color-primary)]">
+                            {title}
+                        </div>
+                    {/if}
                 </li>
             {/each}
         </ul>
