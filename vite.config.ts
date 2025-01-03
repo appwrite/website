a859dc5d6582af30b40f76b97aed5d4893743442
loks0n <22452787+loks0n@users.noreply.github.com>
2025-01-03 19:33:47 +0000

    fix: avif & webp double optimisations

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
             }
         }),
         ViteImageOptimizer({
+            exclude: ['**/*.avif', '**/*.webp'],
             cache: true,
             cacheLocation: '.cache'
         }),
