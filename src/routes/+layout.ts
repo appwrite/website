728b9733b9bd304e67596babfa2f50315fa6b3b8
Jesse Winton <jesse@appwrite.io>
2025-01-07 09:15:02 -0500

    tweaks

---
+++ src/routes/+layout.ts
@@ -0,0 +1,19 @@
+import posthog from 'posthog-js';
+import { browser } from '$app/environment';
+
+import { getAllChangelogEntries } from './changelog/utils';
+import { PUBLIC_POSTHOG_API_KEY } from '$env/static/public';
+export const prerender = true;
+export const trailingSlash = 'never';
+
+export const load = async () => {
+    if (browser) {
+        posthog.init(PUBLIC_POSTHOG_API_KEY, {
+            api_host: 'https://eu.i.posthog.com'
+        });
+    }
+
+    return {
+        changelogEntries: (await getAllChangelogEntries()).length
+    };
+};
