57c8b4abef7ba978008285e3c3958f4b40778c08
Jesse Winton <jesse@appwrite.io>
2025-01-07 15:45:56 -0500

    updates

---
+++ src/routes/partners/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import Main from '$lib/layouts/Main.svelte';
+    import { DEFAULT_DESCRIPTION, DEFAULT_HOST } from '$lib/utils/metadata';
+    import { TITLE_SUFFIX } from '$routes/titles';
+    import Hero from './(components)/hero.svelte';
+    import Puzzle from './(components)/puzzle.svelte';
+
+    const title = 'Partners' + TITLE_SUFFIX;
+    const description = DEFAULT_DESCRIPTION;
+    const ogImage = DEFAULT_HOST + '/images/open-graph/website.png';
+</script>
+
+<svelte:head>
+    <!-- Titles -->
+    <title>{title}</title>
+    <meta property="og:title" content={title} />
+    <meta name="twitter:title" content={title} />
+    <!-- Description -->
+    <meta name="description" content={description} />
+    <meta property="og:description" content={description} />
+    <meta name="twitter:description" content={description} />
+    <!-- Image -->
+    <meta property="og:image" content={ogImage} />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta name="twitter:image" content={ogImage} />
+    <meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+
+<Main>
+    <Hero />
+</Main>
