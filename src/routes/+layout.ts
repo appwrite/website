5a1f310a59c50acb06d8fb5cdeacb7fa66f27c29
Jesse Winton <jesse@appwrite.io>
2025-01-08 17:40:02 -0500

    identify all events

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -10,7 +10,7 @@ export const load = async () => {
     if (browser) {
         posthog.init(PUBLIC_POSTHOG_API_KEY, {
             api_host: 'https://eu.i.posthog.com',
-            person_profiles: 'identified_only',
+            person_profiles: 'always',
             persistence: 'memory'
         });
     }
