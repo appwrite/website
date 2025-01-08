f1ce430c3713735ba18950a5ec59f4b142589d64
Darshan <itznotabug@gmail.com>
2025-01-08 12:24:07 +0530

    fix: dates, ran: formatter.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -121,14 +121,14 @@
             <div class="my-12 lg:my-[7.5rem]">
                 <section class="container pb-0">
                     <a
-                        href="/blog/post/introducing-database-backups"
+                        href="/blog/post/introducing-new-compute-capabilities-appwrite-functions"
                         class="web-hero-banner-button mb-4"
                         on:click={() => trackEvent({ plausible: { name: 'Banner button click' } })}
                     >
                         <span class="web-icon-star shrink-0" aria-hidden="true" />
                         <span class="text-caption shrink-0 font-medium">New</span>
                         <div class="web-hero-banner-button-sep" />
-                        <span class="text-caption web-u-trim-1">Introducing Database Backups</span>
+                        <span class="text-caption web-u-trim-1">New compute options available</span>
                         <span class="web-icon-arrow-right shrink-0" aria-hidden="true" />
                     </a>
                     <Hero>
