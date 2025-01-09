dced1b8dc9cfc8247d1c546bc5d71dc03797ecc6
Andrei C <65463568+Necromancer94@users.noreply.github.com>
2025-01-09 12:05:46 +0100

    Update +page.svelte

--- src/routes/products/storage/+page.svelte
+++ src/routes/products/storage/+page.svelte
@@ -83,7 +83,7 @@
                             manage security, permissions, compression, and more.
                         </p>
                         <a
-                            href="https://builtwith.appwrite.io"
+                            href="/docs/products/storage/buckets"
                             target="_blank"
                             rel="noopener noreferrer"
                             class="web-button is-secondary mt-4 mx-auto !w-full md:!w-fit"
