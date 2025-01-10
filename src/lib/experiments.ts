4603b3159c841cfde97864c167263ab953036db9
Jesse Winton <jesse@appwrite.io>
2025-01-10 17:23:03 -0500

    updates

--- src/lib/experiments.ts
+++ src/lib/experiments.ts
@@ -1,4 +1,5 @@
 import { posthog } from 'posthog-js';
+import { writable } from 'svelte/store';
 
 export const experiments = {
     'sticky-navigation_ab-test': 'sticky-navigation_ab-test'
@@ -7,3 +8,8 @@ export const experiments = {
 export const getFeatureFlag = (key: keyof typeof experiments) => {
     return posthog.getFeatureFlag(key);
 };
+
+export const isHeaderExperiment = writable<{ isEnabled: boolean; isInitialized: boolean }>({
+    isEnabled: false,
+    isInitialized: false
+});
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,29 +2,43 @@ import { getAllChangelogEntries } from './changelog/utils';
 import { browser } from '$app/environment';
 import posthog from 'posthog-js';
 import { PUBLIC_POSTHOG_API_KEY } from '$env/static/public';
-//import fpPromise from '@fingerprintjs/fingerprintjs';
+import fpPromise from '@fingerprintjs/fingerprintjs';
+import { isHeaderExperiment } from '$lib/experiments';
+import { get } from 'svelte/store';
 
 export const prerender = true;
 export const trailingSlash = 'never';
 
 export const load = async () => {
     if (browser) {
-        //const fp = await fpPromise.load();
-        //const { visitorId: distinct_id } = await fp.get();
+        const fp = await fpPromise.load();
+        const { visitorId: distinct_id } = await fp.get();
 
         posthog.init(PUBLIC_POSTHOG_API_KEY, {
             api_host: 'https://eu.i.posthog.com',
-            persistence: 'memory'
+            persistence: 'memory',
+            bootstrap: {
+                distinctID: distinct_id,
+                featureFlags: {
+                    'sticky-navigation_ab-test': 'sticky-nav'
+                }
+            }
         });
-        posthog.identify('jesse@appwrite.io');
-
-        posthog.featureFlags.override({ 'sticky-navigation_ab-test': 'sticky-nav' });
+        posthog.identify(distinct_id);
 
         posthog.onFeatureFlags(() => {
             const stickyNavVariant = posthog.getFeatureFlag('sticky-navigation_ab-test');
-            if (stickyNavVariant !== 'sticky-nav') {
-                localStorage.setItem('variant', stickyNavVariant as string);
-                document.body.dataset.isSticky = '';
+
+            if (stickyNavVariant === 'sticky-nav') {
+                isHeaderExperiment.set({
+                    isEnabled: true,
+                    isInitialized: get(isHeaderExperiment).isInitialized
+                });
+            } else {
+                isHeaderExperiment.set({
+                    isEnabled: false,
+                    isInitialized: get(isHeaderExperiment).isInitialized
+                });
             }
         });
     }
