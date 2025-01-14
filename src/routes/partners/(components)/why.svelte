105c66a4598631bc4b69da8f3c9f9c8f64051737
Jesse Winton <jesse@appwrite.io>
2025-01-14 11:57:04 -0500

    fixes

--- src/routes/partners/(components)/why.svelte
+++ src/routes/partners/(components)/why.svelte
@@ -5,21 +5,21 @@
 
     let items: Array<{ number: number; label: string; suffix?: string }> = [
         {
-            number: 650,
+            number: 0,
             suffix: 'k+',
             label: 'Community members'
         },
         {
-            number: 45,
+            number: 0,
             suffix: 'k+',
             label: 'GitHub stars'
         },
         {
-            number: 900,
+            number: 0,
             suffix: '+',
             label: 'OSS Contributors'
         },
-        { number: 300, label: 'Top OSS GitHub projects' }
+        { number: 0, label: 'Top OSS GitHub projects' }
     ];
 
     const numbers = [650, 45, 900, 300];
@@ -30,13 +30,10 @@
         });
     };
 
-    let animate: boolean = false;
-
     const useInView = (node: HTMLElement) => {
         inView(
             node,
             () => {
-                animate = true;
                 updateNumbers();
             },
             { amount: 0.25 }
@@ -44,7 +41,7 @@
     };
 </script>
 
-<div class="border-smooth mb-0 border-y py-32" use:useInView>
+<div class="border-smooth mb-0 border-y py-32">
     <div class="container space-y-12">
         <div class="mx-auto flex max-w-2xl flex-col gap-4 text-center">
             <span class="font-aeonik-fono tracking-loose text-micro text-primary uppercase">
@@ -58,7 +55,7 @@
                 network as a Appwrite Partner.
             </p>
         </div>
-        <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+        <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4" use:useInView>
             {#each items as { label, number, suffix }}
                 <GradientBorderCard class="bg-greyscale-750 rounded-lg p-6">
                     <h3 class="text-title text-primary"><NumberFlow value={number} {suffix} /></h3>
