5145c76775d1113227c272cf0bc862e4a7daee48
Jesse Winton <jesse@appwrite.io>
2025-01-20 12:48:11 -0500

    add slugify

---
+++ src/lib/utils/slugify.ts
@@ -0,0 +1,10 @@
+export const slugify = (text: string) => {
+    return text
+        .toLowerCase()
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .replace(/[^\w\s-]/g, '')
+        .replace(/\s+/g, '-')
+        .replace(/-+/g, '-')
+        .replace(/^-+|-+$/g, '');
+};
