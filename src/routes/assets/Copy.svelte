faa6b6fa96119cb431618192b7e2c7599292fb82
Darshan <itznotabug@gmail.com>
2025-01-21 19:02:13 +0530

    fix: assets page copy button styling.

--- src/routes/assets/Copy.svelte
+++ src/routes/assets/Copy.svelte
@@ -2,6 +2,8 @@
     import { copy } from '$lib/utils/copy';
 
     export let toCopy: string;
+    export let variant: 'light' | 'dark' | 'pink' = 'light';
+
     let copied = false;
 
     async function copyToClipboard() {
@@ -11,7 +13,37 @@
     }
 </script>
 
-<button class="web-button is-secondary cursor-pointer" on:click={copyToClipboard}>
+<button class="copy-button cursor-pointer {variant}" on:click={copyToClipboard}>
     <span class="web-icon-{copied ? 'check' : 'copy'}" aria-label={copied ? 'Copied' : 'Copy'} />
     <span>Copy</span>
 </button>
+
+<style lang="scss">
+    .copy-button {
+        gap: 8px;
+        display: flex;
+        padding: 6px 12px;
+        border-radius: 8px;
+        align-items: center;
+        justify-content: center;
+        transition: opacity ease-in-out 0.3s;
+
+        &.light {
+            background-color: rgba(255, 255, 255, 0.64);
+        }
+
+        &.dark {
+            background-color: rgba(255, 255, 255, 0.04);
+            border: 1.002px solid var(--smooth, rgba(255, 255, 255, 0.06));
+        }
+
+        &.pink {
+            background-color: rgba(255, 255, 255, 0.125);
+            border: 1.002px solid var(--smooth, rgba(255, 255, 255, 0.06));
+        }
+
+        &:hover {
+            opacity: 0.8;
+        }
+    }
+</style>
