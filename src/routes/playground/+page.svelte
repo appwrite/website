aca5f314f4aeabe60235c008aeca70c9ca546e3e
Jesse Winton <jesse@appwrite.io>
2025-01-22 10:11:24 -0500

    Update +page.svelte

--- src/routes/playground/+page.svelte
+++ src/routes/playground/+page.svelte
@@ -88,4 +88,10 @@
         <FooterNav />
         <MainFooter />
     </div>
+    <div
+        class="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"
+    ></div>
 </Main>
+
+<style>
+</style>
