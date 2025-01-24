4b39612b4c2a9bcef736a64993f249c51fd84998
Bradley Schofield <ionicisere@gmail.com>
2025-01-24 13:14:22 +0900

    Update src/routes/integrations/technology-partner/+page.svelte
    Co-authored-by: Darshan <itznotabug@gmail.com>

--- src/routes/integrations/technology-partner/+page.svelte
+++ src/routes/integrations/technology-partner/+page.svelte
@@ -36,7 +36,7 @@
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                name: name,
+                name,
                 email,
                 companyName,
                 companySize,
