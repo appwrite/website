7dfd1170484a9981eef330fc0f06de78db68e5f9
Ebenezer Don <ebenezerdonu@gmail.com>
2025-01-27 21:14:16 +0000

    Update to "manage team invites"

--- src/routes/docs/products/auth/+layout.svelte
+++ src/routes/docs/products/auth/+layout.svelte
@@ -98,7 +98,7 @@
                     href: '/docs/products/auth/verify-user'
                 },
                 {
-                    label: 'Team invites',
+                    label: 'Manage team invites',
                     href: '/docs/products/auth/team-invites'
                 }
             ]
