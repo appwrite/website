0444461385be2fbfbd7ec334b8f18b6cc722a9db
Jesse Winton <jesse@appwrite.io>
2025-01-29 12:18:15 -0500

    more reliable fingerprint?

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,38 +1,30 @@
 import crypto from 'node:crypto';
 
-import { posthogServerClient } from '$lib/experiments';
 import { getAllChangelogEntries } from './changelog/utils';
 
 export const trailingSlash = 'never';
 
-const generateDistinctId = (fingerprintData: Record<string, string>) => {
-    return crypto.createHash('sha256').update(JSON.stringify(fingerprintData)).digest('hex');
+const generateDistinctId = (request: Request, clientAddress?: string) => {
+    const headers = Object.fromEntries(request.headers);
+
+    const identifiers = [
+        clientAddress || 'unknown-ip',
+        headers['user-agent'] || 'unknown-ua',
+
+        !clientAddress ? Math.random().toString() : '',
+        request.headers.get('cookie')?.match(/sessionid=([^;]+)/)?.[1] || ''
+    ].join('|');
+
+    return crypto.createHash('sha256').update(identifiers).digest('hex');
 };
 
 export const load = async ({ request, getClientAddress }) => {
-    const clientAddress = getClientAddress();
-    const headers = Object.fromEntries(request.headers);
-    const fingerprintData = {
-        ip: clientAddress,
-        userAgent: headers['user-agent'],
-        acceptLanguage: headers['accept-language'],
-        platform: headers['sec-ch-ua-platform'],
-        mobile: headers['sec-ch-ua-mobile'],
-        browserBrand: headers['sec-ch-ua']
-    };
+    const distinctId = generateDistinctId(request, getClientAddress());
+    // const ctaVariant = await posthogServerClient?.getFeatureFlag('cta-copy_ab-test', distinctId);
 
-    const distinctId = generateDistinctId(fingerprintData);
-    const ctaVariant = await posthogServerClient?.getFeatureFlag('cta-copy_ab-test', distinctId);
-
-    const ctaCopy =
-        ctaVariant === 'start-building_variant'
-            ? 'Start building'
-            : ctaVariant === 'start-for-free_variant'
-              ? 'Start for free'
-              : 'Get started';
+    console.log('distinctId', distinctId);
 
     return {
-        ctaCopy,
         distinctId,
         changelogEntries: (await getAllChangelogEntries()).length
     };
