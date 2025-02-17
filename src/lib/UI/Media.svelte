da60d235acae77828929b5466619ccb05fe6b5a6
Darshan <itznotabug@gmail.com>
2025-02-17 11:02:19 +0530

    update: use better logic for consistency.

--- src/lib/UI/Media.svelte
+++ src/lib/UI/Media.svelte
@@ -6,18 +6,15 @@
     let className = '';
     export { className as class };
 
-    export let height: number | undefined = undefined;
-    export let width: number | undefined = undefined;
-
     const videoExtensions = ['mp4', 'webm', 'ogg'] as const;
     $: isVideo = videoExtensions.some((ext) => src.endsWith(ext));
 </script>
 
 {#if isVideo}
     <!-- svelte-ignore a11y-media-has-caption-->
-    <video {src} class={className} {controls} {autoplay} {height} {width}>
+    <video {src} class={className} {controls} {autoplay}>
         <slot />
     </video>
 {:else}
-    <img loading="lazy" {src} {alt} class={className} {height} {width} data-active="" />
+    <img loading="lazy" {src} {alt} class={className} data-active="" />
 {/if}
