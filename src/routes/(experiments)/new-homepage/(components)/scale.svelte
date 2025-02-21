7201adf457ad2e047eb91a2d105d43f4ec7b92bb
Jesse Winton <jesse@appwrite.io>
2025-02-21 12:03:41 -0500

    update

--- src/routes/(experiments)/new-homepage/(components)/scale.svelte
+++ src/routes/(experiments)/new-homepage/(components)/scale.svelte
@@ -1,8 +1,41 @@
 <script lang="ts">
+    import NumberFlow from '@number-flow/svelte';
+    import { onMount } from 'svelte';
+
+    let stats = [
+        {
+            number: 0,
+            suffix: '+',
+            description: 'Regions served'
+        },
+        {
+            number: 0,
+            suffix: 'TB',
+            description: 'of data served'
+        },
+        {
+            number: 0,
+            suffix: 'million',
+            description: 'end users'
+        },
+        {
+            number: 0,
+            suffix: '+',
+            description: 'total compute time'
+        }
+    ];
+
+    const updatedNumbers = [12, 900, 1, 999];
+
+    onMount(() => {
+        stats.forEach((stat, i) => {
+            stat.number = updatedNumbers[i];
+        });
+    });
 </script>
 
 <div
-    class="light relative flex min-h-[70vh] w-full flex-col gap-4 overflow-hidden bg-[#EDEDF0] py-20"
+    class="light relative flex min-h-[65vh] w-full flex-col gap-4 overflow-hidden bg-[#EDEDF0] py-20"
 >
     <div class="container relative z-10 w-full">
         <div class="max-w-xl">
@@ -21,7 +54,24 @@
         </div>
     </div>
 
-    <div class="absolute inset-0 overflow-hidden">
+    <div class="wiper absolute inset-0 overflow-hidden">
+        <div class="container relative h-full">
+            <div class="absolute inset-0 grid grid-cols-4">
+                {#each stats as stat, i}
+                    <div
+                        class="mask h-full border-l border-dashed border-black/10"
+                        style:--mask-direction="bottom"
+                        style:--mask-height={`${(4 - i) * 25}%`}
+                    >
+                        <div class="relative top-40">
+                            <span class="text-description text-primary font-medium"
+                                ><NumberFlow value={stat.number} suffix={stat.suffix} /></span
+                            >
+                        </div>
+                    </div>
+                {/each}
+            </div>
+        </div>
         <div
             class="from-accent/15 absolute inset-0 bg-gradient-to-tr to-transparent [clip-path:polygon(0_100%,_100%_25%,_100%_100%,_0_100%)]"
         />
@@ -30,3 +80,27 @@
         />
     </div>
 </div>
+
+<style>
+    /* .wiper {
+        animation: wipe-in 5s ease-in-out;
+    } */
+
+    @keyframes mask-wipe {
+        0% {
+            transform: translateX(0);
+        }
+        100% {
+            transform: translateX(100%);
+        }
+    }
+
+    @keyframes wipe-in {
+        0% {
+            clip-path: polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%);
+        }
+        100% {
+            clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%);
+        }
+    }
+</style>
