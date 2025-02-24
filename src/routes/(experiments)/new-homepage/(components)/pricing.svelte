250ff5c9b083bbb1208d2dedd993f455e9f7a657
Jesse Winton <jesse@appwrite.io>
2025-02-24 15:33:38 -0500

    clean up pricing section

--- src/routes/(experiments)/new-homepage/(components)/pricing.svelte
+++ src/routes/(experiments)/new-homepage/(components)/pricing.svelte
@@ -41,13 +41,13 @@
         />
 
         <div
-            class="animate-fade-in relative flex w-full items-center justify-between [animation-delay:150ms] [animation-duration:1000ms]"
+            class="animate-fade-in relative flex w-full flex-col justify-between gap-8 [animation-delay:150ms] [animation-duration:1000ms] md:flex-row"
         >
             <h2 class="text-sub-display text-primary font-aeonik-pro max-w-xl text-pretty">
                 Start building like a team of hundreds today<span class="text-accent">_</span>
             </h2>
 
-            <div class="flex items-center gap-4">
+            <div class="flex gap-4">
                 <a href="/" class="web-button">Get started</a>
                 <a href="/" class="web-button is-secondary">View pricing plans</a>
             </div>
