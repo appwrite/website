3fc25866ddefba7b3d16528b044fd1e19a870777
Ebenezer Don <ebenezerdonu@gmail.com>
2025-02-25 16:07:43 +0000

    Use image component

--- src/markdoc/tags/Storage_Image.svelte
+++ src/markdoc/tags/Storage_Image.svelte
@@ -1,23 +1,25 @@
 <script lang="ts">
     import { storage } from '$lib/appwrite';
+    import Image from '../nodes/Image.svelte';
+
     export let file_id: string;
     export let bucket_id: string = 'images';
     export let alt: string = '';
     export let title: string = '';
-    export let width: number = 1800;
-    export let height: number = 1800;
+    export let width: number = 0;
+    export let height: number = 0;
     export let gravity: string = 'center';
     export let quality: number = 90;
-    export let border_width: number = 5;
+    export let border_width: number = 0;
     export let border_color: string = 'CDCA30';
     export let border_radius: number = 0;
     export let opacity: number = 1;
     export let rotation: number = 0;
-    export let background_color: string = 'FFFFFF';
+    export let background_color: string = '000000';
     export let output: string = 'webp';
 
     // Get the file preview URL from Appwrite Storage
-    const imageSrc = storage
+    const src = storage
         .getFilePreview(
             bucket_id,
             file_id,
@@ -36,4 +38,4 @@
         .toString();
 </script>
 
-<img src={imageSrc} {alt} {title} />
+<Image {src} {alt} {title} />
