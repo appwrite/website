8f38c21fee7d696ab8c2c3f2c4976a743f747d7c
Ebenezer Don <ebenezerdonu@gmail.com>
2025-02-25 11:55:10 +0000

    Use storage

--- src/lib/appwrite/index.ts
+++ src/lib/appwrite/index.ts
@@ -1,5 +1,5 @@
 import { PUBLIC_APPWRITE_ENDPOINT, PUBLIC_APPWRITE_PROJECT_ID } from '$env/static/public';
-import { Client, Databases, Functions } from '@appwrite.io/console';
+import { Client, Databases, Functions, Storage } from '@appwrite.io/console';
 
 export const client = new Client();
 
@@ -7,3 +7,4 @@ client.setEndpoint(PUBLIC_APPWRITE_ENDPOINT).setProject(PUBLIC_APPWRITE_PROJECT_
 
 export const databases = new Databases(client);
 export const functions = new Functions(client);
+export const storage = new Storage(client);
--- src/markdoc/tags/Storage_Image.svelte
+++ src/markdoc/tags/Storage_Image.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
-    export let src: string;
+    import { storage } from '$lib/appwrite';
+    export let file_id: string;
+    export let bucket_id: string = 'images';
     export let alt: string = '';
     export let title: string = '';
+
+    console.log('<><><>bucket_id<><><><><><>>>==>', bucket_id);
+    console.log('<><><>file_id<><><><><><>>>==>', file_id);
+
+    // Get the file preview URL from Appwrite Storage
+    const imageSrc = storage.getFilePreview(bucket_id, file_id).toString();
+    console.log('<><><>imageSrc<><><><><><>>>==>', imageSrc);
 </script>
 
-<img {src} {alt} {title} />
+<img src={imageSrc} {alt} {title} />
