e3ea690bcd931dd877682a01455ed85e157e4336
Jesse Winton <jesse@appwrite.io>
2025-03-06 10:14:13 -0500

    Update puzzle.svelte

--- src/routes/partners/(components)/puzzle.svelte
+++ src/routes/partners/(components)/puzzle.svelte
@@ -320,7 +320,7 @@
         }
 
         &.active {
-            animation: var(--ease-spring) right-piece 1.333s forwards;
+            animation: var(--transition-timing-function-spring) right-piece 1.333s forwards;
         }
     }
 
@@ -343,7 +343,7 @@
         }
 
         &.active {
-            animation: var(--ease-spring) left-piece 1.333s forwards;
+            animation: var(--transition-timing-function-spring) left-piece 1.333s forwards;
         }
     }
 
