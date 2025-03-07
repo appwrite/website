e0adf7b422bb3796a563b72468d9f700901a1f45
Jesse Winton <jesse@appwrite.io>
2025-03-07 15:43:54 -0500

    footer cta

---
+++ src/routes/partners/catalog/(components)/call-to-action.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { classNames } from '$lib/utils/classnames';
+</script>
+
+<div
+    class={classNames(
+        'border-smooth relative -mb-24 border-t py-32',
+        'bg-[url("/images/bgs/building-blocks.webp")] [background-size:50%] bg-right-bottom bg-no-repeat',
+        'before:absolute before:top-0 before:left-0 before:z-0 before:block before:h-80 before:w-full before:bg-[radial-gradient(at_25%_0%,_hsla(343,_98%,_60%,_0.05)_0px,_transparent_73%,_transparent_100%)] md:before:w-1/2',
+        'after:absolute after:top-0 after:right-0 after:z-0 after:hidden after:h-80 after:w-1/2 after:bg-[radial-gradient(at_100%_0%,_hsla(177,_53%,_69%,_0.1)_0px,_transparent_73%,_transparent_100%)] after:md:block md:after:block'
+    )}
+>
+    <div class="container relative grid grid-cols-1 place-items-center md:grid-cols-2">
+        <section class="flex flex-col gap-4">
+            <h2 class="text-display font-aeonik-pro text-primary max-w-[600px]">
+                Become a Technology Partner
+            </h2>
+            <p class="text-body font-medium">
+                Join our Technology Partners program to integrate your solutions with Appwrite’s
+                API, enhancing functionality and expanding your reach.
+            </p>
+            <a href="/integrations/technology-partner" class="web-button mt-4">
+                <span class="text">Get Started</span>
+            </a>
+        </section>
+    </div>
+</div>
---
+++ src/routes/partners/catalog/+page.ts
@@ -0,0 +1,91 @@
+import { base } from '$app/paths';
+import { groupBy } from 'remeda';
+import type { IntegrationCategory } from '$lib/constants';
+import { integrationCategoryDescriptions as categoryDescriptions } from '$lib/constants';
+
+export type Integration = {
+    title: string;
+    description: string;
+    featured?: boolean;
+    cover: string;
+    isNew?: boolean;
+    isPartner?: boolean;
+    platform: string[];
+    category: string;
+    product: {
+        avatar: string;
+        vendor: string;
+        description: string;
+    };
+    href: string;
+    images: string[];
+};
+
+export const load = () => {
+    const integrationsGlob = import.meta.glob('./**/*.markdoc', {
+        eager: true
+    });
+
+    const categories: IntegrationCategory[] = [];
+    const platforms: string[] = [];
+
+    const integrations = Object.entries(integrationsGlob).map(([filepath, integrationList]) => {
+        const { frontmatter } = integrationList as {
+            frontmatter: Integration;
+        };
+
+        const slug = filepath.replace('./', '').replace('/+page.markdoc', '');
+        const integrationName = slug.slice(slug.lastIndexOf('/') + 1);
+
+        frontmatter.platform.map((platform) => platforms.push(platform));
+        categories.push(
+            categoryDescriptions.find((i) => i.slug === frontmatter.category) ??
+                ({} as IntegrationCategory)
+        );
+
+        return {
+            ...frontmatter,
+            href: `${base}/integrations/${integrationName}`
+        };
+    });
+
+    const groupedIntegrations = groupBy(integrations, (i) => i.category);
+
+    const integrationsWithDescriptions = Object.entries(groupedIntegrations).map(
+        ([category, integrations]) => {
+            const integrationCategory = categoryDescriptions.find(
+                (key) => key.slug === category.toLowerCase()
+            );
+            return {
+                category,
+                heading: integrationCategory?.heading,
+                description: integrationCategory?.description,
+                integrations
+            };
+        }
+    );
+
+    const featuredIntegrations = integrations.filter((i) => i.featured);
+
+    const featuredIntegrationsWithCategoryHeadings = Object.entries(featuredIntegrations).map(
+        ([_, integration]) => {
+            const integrationCategory = categoryDescriptions.find(
+                (key) => key.slug === integration.category.toLowerCase()
+            );
+            return {
+                heading: integrationCategory?.heading,
+                integration
+            };
+        }
+    );
+
+    return {
+        integrations: integrationsWithDescriptions,
+        list: integrations,
+        categories: new Set(categories),
+        platforms: new Set(platforms),
+        featured: featuredIntegrationsWithCategoryHeadings
+    };
+};
+
+export const prerender = false;
