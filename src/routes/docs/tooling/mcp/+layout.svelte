542b980b86bb1794b28532d81137f378b21c75c1
Aditya Oberai <adityaoberai1@gmail.com>
2025-03-09 18:38:14 +0000

    Add mcp page

---
+++ src/routes/docs/tooling/mcp/+layout.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import Docs from '$lib/layouts/Docs.svelte';
+    import Sidebar, { type NavParent, type NavTree } from '$lib/layouts/Sidebar.svelte';
+
+    const parent: NavParent = {
+        href: '/docs',
+        label: 'MCP'
+    };
+
+    const navigation: NavTree = [
+        {
+            label: 'Getting Started',
+            items: [
+                {
+                    label: 'Overview',
+                    href: '/docs/tooling/mcp'
+                }
+            ]
+        },
+        {
+            label: 'Integrations',
+            items: [
+                {
+                    label: 'Claude Desktop',
+                    href: '/docs/tooling/mcp/claude'
+                },
+                {
+                    label: 'Cursor',
+                    href: '/docs/tooling/mcp/cursor'
+                }
+            ]
+        }
+    ];
+</script>
+
+<Docs variant="two-side-navs">
+    <Sidebar {navigation} {parent} />
+    <slot />
+</Docs>
