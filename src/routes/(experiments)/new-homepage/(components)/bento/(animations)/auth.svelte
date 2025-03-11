976752538c9a262ff82807d341bd079b891c3055
Jesse Winton <jesse@appwrite.io>
2025-03-11 16:20:57 -0400

    work on auth

--- src/routes/(experiments)/new-homepage/(components)/bento/(animations)/auth.svelte
+++ src/routes/(experiments)/new-homepage/(components)/bento/(animations)/auth.svelte
@@ -1,20 +1,26 @@
 <script lang="ts">
+    import Github from '../../../(assets)/logos/github.svg';
+    import Google from '../../../(assets)/logos/google.svg';
+    import Apple from '../../../(assets)/logos/apple.svg';
+    import Microsoft from '../../../(assets)/logos/microsoft.svg';
+    import Switch from '$lib/components/Switch.svelte';
+
     const platforms = [
         {
             label: 'GitHub',
-            icon: ''
+            icon: Github
         },
         {
             label: 'Google',
-            icon: ''
+            icon: Google
         },
         {
             label: 'Apple',
-            icon: ''
+            icon: Apple
         },
         {
             label: 'Microsoft',
-            icon: ''
+            icon: Microsoft
         }
     ];
 
@@ -54,23 +60,23 @@
     <div
         class="relative flex h-[26.25rem] items-center justify-between overflow-clip rounded-xl bg-black/24 px-8"
     >
-        <div class="mt-20 grid h-full w-full grid-cols-2 place-items-center">
-            <div class="w-full">
+        <div class="grid h-full w-full grid-cols-2 place-items-center">
+            <div
+                class="divide-smooth border-smooth flex aspect-square w-[200px] flex-col justify-center divide-y rounded-2xl border bg-[#232325]/90 p-2"
+            >
                 {#each platforms as platform}
-                    <button
-                        class="flex items-center gap-2 rounded-lg bg-[#19191C] py-2 px-3 text-white"
-                    >
-                        <img
-                            src="/images/icons/gray-colors/{platform.label.toLowerCase()}.svg"
-                            alt={platform.label}
-                            class="size-5"
-                        />
-                        <span class="text-micro">{platform.label}</span>
-                    </button>
+                    <div class="flex items-center justify-between p-2">
+                        <button class="text-caption flex items-center gap-3 text-white">
+                            <img src={platform.icon} alt={platform.label} class="size-7" />
+                            <span class="text-micro">{platform.label}</span>
+                        </button>
+
+                        <Switch />
+                    </div>
                 {/each}
             </div>
             <div
-                class="mask bg-greyscale-850 border-smooth flex h-full w-full flex-col rounded-t-[40px] border-t border-x"
+                class="mask border-smooth mt-20 flex h-full w-full flex-col rounded-t-[40px] border-t border-x bg-[#232325]/90"
                 style:--mask-height="150px"
             >
                 <div class="mx-2 mt-2 flex-1 rounded-t-4xl bg-[#19191C] pt-4 px-3">
