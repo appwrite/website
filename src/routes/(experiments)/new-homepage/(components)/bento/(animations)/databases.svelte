b61f0be04c88d8291af8e341b5fc47cd691068d4
Jesse Winton <jesse@appwrite.io>
2025-03-11 11:54:49 -0400

    update

--- src/routes/(experiments)/new-homepage/(components)/bento/(animations)/databases.svelte
+++ src/routes/(experiments)/new-homepage/(components)/bento/(animations)/databases.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { classNames } from '$lib/utils/classnames';
+
     const collection = [
         {
             category: '',
@@ -10,21 +12,25 @@
 
     const products = [
         {
-            id: 3397,
+            ID: 3397,
             name: 'Turtlenecks',
             barcode: 'TRTL'
         },
         {
-            id: 2224,
+            ID: 2224,
             name: 'Pullovers',
             barcode: 'PLOV'
         },
         {
-            id: 5689,
+            ID: 5689,
             name: 'Zip-up',
             barcode: 'ZPUP'
         }
     ];
+
+    const [productHeadings] = products.map((product) => Object.keys(product));
+
+    //         style:transform={`translateY(-${i * 10}px) translateX(-${i * 10}px)`}
 </script>
 
 <div
@@ -48,27 +54,61 @@
     <div
         class="flex h-[26.25rem] items-center justify-between overflow-clip rounded-xl bg-black/24 px-8"
     >
-        <div
-            class="border-smooth flex h-[190px] w-[260px] flex-col rounded-2xl border bg-[#232325]/90 backdrop-blur-md"
-        >
-            <h3 class="text-caption text-primary p-3">Sweaters</h3>
-            <table class="bg-greyscale-900 mx-1 mt-auto mb-1 table flex-1 rounded-xl p-2">
-                <thead>
-                    <tr class="text-primary text-micro w-full font-normal">
-                        <th class="pl-2 text-left">ID</th>
-                        <th class="pl-2 text-left">name</th>
-                        <th class="pl-2 text-left">barcode</th>
-                    </tr>
-                </thead>
-                <tbody class="text-micro">
-                    {#each products as product}<tr>
-                            <td class="text-primary pl-2 text-left">{product.id}</td>
-                            <td class="text-primary pl-2 text-left">{product.name}</td>
-                            <td class="text-primary pl-2 text-left">{product.barcode}</td>
-                        </tr>
-                    {/each}</tbody
-                >
-            </table>
+        <div class="relative">
+            <div
+                class="border-smooth absolute flex aspect-[3/2] min-w-[260px] flex-col rounded-2xl border bg-[#232325]/90 shadow-[4px_8px_20px_rgba(0,0,0,0.2)] backdrop-blur-md"
+            >
+                <h3 class="text-caption text-primary py-1 px-2.5">Sweaters</h3>
+                <div class="border-smooth mx-1 mt-auto mb-1 flex-1 rounded-xl border">
+                    <table class="table w-full p-2.5">
+                        <thead>
+                            <tr
+                                class="bg-greyscale-900 border-smooth text-primary text-micro w-full border-b font-normal"
+                            >
+                                {#each productHeadings as heading}
+                                    <th
+                                        class="p-2 text-left first-of-type:rounded-tl-xl last-of-type:rounded-tr-xl"
+                                    >
+                                        <span class="inline">{heading}</span>
+                                        <svg
+                                            width="16"
+                                            height="17"
+                                            viewBox="0 0 16 17"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            class="inline h-[1lh]"
+                                        >
+                                            <path
+                                                fill-rule="evenodd"
+                                                clip-rule="evenodd"
+                                                d="M8.00005 2.8999C8.21222 2.8999 8.41571 2.98419 8.56573 3.13422L10.9657 5.53422C11.2782 5.84664 11.2782 6.35317 10.9657 6.66559C10.6533 6.97801 10.1468 6.97801 9.83436 6.66559L8.00005 4.83127L6.16573 6.66559C5.85331 6.97801 5.34678 6.97801 5.03436 6.66559C4.72194 6.35317 4.72194 5.84664 5.03436 5.53422L7.43436 3.13422C7.58439 2.98419 7.78788 2.8999 8.00005 2.8999ZM5.03436 10.3342C5.34678 10.0218 5.85331 10.0218 6.16573 10.3342L8.00005 12.1685L9.83436 10.3342C10.1468 10.0218 10.6533 10.0218 10.9657 10.3342C11.2782 10.6466 11.2782 11.1532 10.9657 11.4656L8.56573 13.8656C8.25331 14.178 7.74678 14.178 7.43436 13.8656L5.03436 11.4656C4.72194 11.1532 4.72194 10.6466 5.03436 10.3342Z"
+                                                fill="#6C6C71"
+                                            />
+                                        </svg>
+                                    </th>
+                                {/each}
+                            </tr>
+                        </thead>
+                        <tbody class="text-micro divide-smooth divide-y">
+                            {#each products as product, p}
+                                <tr class="bg-[#1D1D21]">
+                                    {#each Object.values(product) as item, i}
+                                        <td
+                                            class={classNames('text-primary p-2.5 text-left', {
+                                                'rounded-bl-xl':
+                                                    p === products.length - 1 && i === 0,
+                                                'rounded-br-xl':
+                                                    p === products.length - 1 &&
+                                                    i === Object.values(product).length - 1
+                                            })}>{item}</td
+                                        >
+                                    {/each}
+                                </tr>
+                            {/each}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- src/routes/(experiments)/new-homepage/+page.svelte
+++ src/routes/(experiments)/new-homepage/+page.svelte
@@ -1,4 +1,11 @@
 <script>
+    import {
+        DEFAULT_DESCRIPTION,
+        DEFAULT_HOST,
+        getInlinedScriptTag,
+        softwareAppSchema,
+        organizationJsonSchema
+    } from '$lib/utils/metadata';
     import Bento from './(components)/bento/bento.svelte';
     import CaseStudies from './(components)/case-studies.svelte';
     import Features from './(components)/features.svelte';
@@ -8,7 +15,34 @@
     import Pricing from './(components)/pricing.svelte';
     import Scale from './(components)/scale.svelte';
     import Templates from './(components)/templates.svelte';
+
+    const title = 'Appwrite - Backend APIs, Frontend hosting';
+    const description = DEFAULT_DESCRIPTION;
+    const ogImage = `${DEFAULT_HOST}/images/open-graph/website.png`;
 </script>
+
+<svelte:head>
+    <!-- Titles -->
+    <title>{title}</title>
+    <meta property="og:title" content={title} />
+    <meta name="twitter:title" content={title} />
+    <!-- Description -->
+    <meta name="description" content={description} />
+    <meta property="og:description" content={description} />
+    <meta name="twitter:description" content={description} />
+    <!-- Image -->
+    <meta property="og:image" content={ogImage} />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta name="twitter:image" content={ogImage} />
+    <meta name="twitter:card" content="summary_large_image" />
+
+    <!-- eslint-disable-next-line svelte/no-at-html-tags-->
+    {@html getInlinedScriptTag(softwareAppSchema())}
+
+    <!-- eslint-disable-next-line svelte/no-at-html-tags-->
+    {@html getInlinedScriptTag(organizationJsonSchema())}
+</svelte:head>
 
 <Hero />
 <Platforms />
