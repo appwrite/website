20bcac69941256e2c0f94f1816540663bb4b519a
Jesse Winton <jesse@appwrite.io>
2025-03-11 16:34:33 -0400

    start messaging

--- src/routes/(experiments)/new-homepage/(components)/bento/(animations)/messaging.svelte
+++ src/routes/(experiments)/new-homepage/(components)/bento/(animations)/messaging.svelte
@@ -21,8 +21,26 @@
         </p>
     </div>
     <div
-        class="relative flex h-[26.25rem] items-center justify-between overflow-clip rounded-xl bg-black/24 px-8"
+        class="relative flex h-[26.25rem] items-center justify-center overflow-clip rounded-xl bg-black/24 px-8"
     >
+        <div
+            class="light absolute top-16 z-10 flex h-20 w-[340px] items-center justify-between gap-3 rounded-[20px] bg-white/80 py-1 px-3 shadow-[-8px_4px_32px_rgba(0,0,0,0.24)] backdrop-blur-xl"
+        >
+            <div class="flex size-10 shrink-0 items-center justify-center rounded-lg bg-black">
+                <div class="size-4 rounded-full bg-white" />
+            </div>
+            <div class="text-micro flex flex-col">
+                <h2 class="text-primary font-medium">New security measures implemented</h2>
+                <p>Check out our latest security updates to protect your account!</p>
+            </div>
+            <span class="text-micro block self-start">now</span>
+        </div>
+        <div
+            class="mask border-smooth mt-20 flex h-full w-[300px] flex-col rounded-t-[40px] border-t border-x bg-white/8 backdrop-blur-2xl"
+            style:--mask-height="250px"
+        >
+            <div class="mx-2 mt-2 flex-1 rounded-t-4xl bg-white pt-4 px-3"></div>
+        </div>
         <div
             class="absolute inset-0 -z-10 h-full w-full bg-transparent bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:60px_60px]"
         />
