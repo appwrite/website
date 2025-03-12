d9af3558f6a46f8a94159280fa9aca7db6e06deb
Jesse Winton <jesse@appwrite.io>
2025-03-12 14:34:34 -0400

    unused

---
+++ src/routes/products/sites/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import Main from '$lib/layouts/Main.svelte';
+    import { DEFAULT_HOST } from '$lib/utils/metadata';
+    import { TITLE_SUFFIX } from '$routes/titles';
+    import Integrate from './(components)/integrate.svelte';
+
+    const title = 'Sites' + TITLE_SUFFIX;
+    const description = 'Appwrite Sites is very cool';
+    const ogImage = DEFAULT_HOST + '/images/open-graph/website.png';
+</script>
+
+<svelte:head>
+    <!-- Titles -->
+    <title>{title}</title>
+    <meta property="og:title" content={title} />
+    <meta name="twitter:title" content={title} />
+    <!-- Description -->
+    <meta name="description" content={description} />
+    <meta property="og:description" content={description} />
+    <meta name="twitter:description" content={description} />
+    <!-- Image -->
+    <meta property="og:image" content={ogImage} />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta name="twitter:image" content={ogImage} />
+    <meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+
+<Main>
+    <Integrate />
+</Main>
