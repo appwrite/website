b65ca6f358e54f16af5a9fcf3e9d3bc3cfe86071
Christy Jacob <christyjacob4@gmail.com>
2025-03-13 15:53:32 +0000

    chore: remove leftovers

--- vite.config.ts
+++ vite.config.ts
@@ -49,7 +49,4 @@ export default defineConfig({
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}']
     },
-    server: {
-        allowedHosts: ['5173-appwrite-website-46vk5bvcfb8.ws-us118.gitpod.io']
-    }
 });
