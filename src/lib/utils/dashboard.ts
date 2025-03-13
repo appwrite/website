aa56b82c30d46e6e399a8d7c73238f5425a627c9
ernstmul <ernst.mulders@gmail.com>
2025-03-13 12:16:32 +0100

    Update utm and dashboard function

--- src/lib/utils/dashboard.ts
+++ src/lib/utils/dashboard.ts
@@ -3,7 +3,14 @@ import { getUtmSourceForLink } from '$lib/utils/utm';
 
 export function getAppwriteDashboardUrl(path = '') {
     const url = new URL(path, PUBLIC_APPWRITE_DASHBOARD);
-    return url.toString().includes('?')
-        ? `${url.toString()}&${getUtmSourceForLink()}`
-        : `${url.toString()}?${getUtmSourceForLink()}`;
+    const params = new URLSearchParams(url.search);
+
+    const utmParams = getUtmSourceForLink();
+    Object.entries(utmParams).forEach(([key, value]) => {
+        if (typeof value === 'string') {
+            params.append(key, value);
+        }
+    });
+    url.search = params.toString();
+    return url.toString();
 }
