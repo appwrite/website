851f3c7b5c673d4f8cf6d79b2ae0813b4792163a
Torsten Dittmann <torsten.dittmann@googlemail.com>
2025-03-25 17:04:26 +0100

    Update webserver command to use pnpm

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
     webServer: {
-        command: 'npm run dev',
+        command: 'pnpm run dev',
         port: 5173
     },
     fullyParallel: true,
