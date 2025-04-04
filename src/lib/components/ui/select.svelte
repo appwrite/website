e720877da805402a1d3256919b7d45a456cbf531
Jesse Winton <jesse@appwrite.io>
2025-04-04 09:18:57 -0400

    update

---
+++ src/lib/components/ui/select.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { Select } from 'melt/builders';
+
+    type Props = {
+        options: Array<{
+            label: string;
+            value: string;
+        }>;
+    };
+
+    const { options }: Props = $props();
+    type Option = (typeof options)[number];
+
+    const select = new Select<Option['value']>();
+</script>
+
+<button {...select.trigger}>
+    {select.value ?? 'Select an anime'}
+</button>
+
+<div {...select.content}>
+    {#each options as option}
+        <div {...select.getOption(option.value)}>
+            {option}
+        </div>
+    {/each}
+</div>
