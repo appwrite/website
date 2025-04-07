006fbdad959b8fe3a5a3da56f4f12446e73521cd
Jesse Winton <jesse@appwrite.io>
2025-04-07 13:47:56 -0400

    updates

--- src/lib/components/shared/index.ts
+++ src/lib/components/shared/index.ts
@@ -1,3 +1,2 @@
 export { default as GithubStats} from './github-stats.svelte';
 export { default as DiscordLink } from './discord-link.svelte';
-export { default as SocialStats} from './social-stats.svelte';
