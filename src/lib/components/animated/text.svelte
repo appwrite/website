87a208d7a5b8b1e44b3f5cd4f2d41e736270eed2
Jesse Winton <jesse@appwrite.io>
2025-04-09 13:21:22 -0400

    update animated text component

--- src/lib/components/animated/text.svelte
+++ src/lib/components/animated/text.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
-    export let text: string;
+    import { classNames } from '$lib/utils/classnames';
+    import type { SvelteHTMLElements } from 'svelte/elements';
 
-    const words = text.split(' ');
+    type Props = {
+        text: string;
+        splitBy?: 'words' | 'characters';
+        staggerBy?: number;
+    } & SvelteHTMLElements['span'];
+
+    const { text, splitBy = 'words', staggerBy = 75, class: className, ...rest }: Props = $props();
+
+    const words = splitBy === 'words' ? text.split(' ') : text.split('');
 </script>
 
 <span class="sr-only">{text}</span>
-<span class="relative">
+<span class={classNames('relative', className)} {...rest}>
     {#each words as word, i}
-        <span
-            class="animate-text mr-2 inline-block"
-            style:animation-delay="{i * 75}ms
-            ">{word}</span
+        <span class="animate-text mr-2 inline-block" style:animation-delay={`${i * staggerBy}ms`}
+            >{word}</span
         >
     {/each}
 </span>
