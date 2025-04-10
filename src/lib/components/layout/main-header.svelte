b12b454016c9f1d6c9013f6a007571847033f521
Jesse Winton <jesse@appwrite.io>
2025-04-10 15:30:48 -0400

    updated nav menu?

--- src/lib/components/layout/main-header.svelte
+++ src/lib/components/layout/main-header.svelte
@@ -1,16 +1,23 @@
+<script>
+    import MainNavigation from './main-navigation.svelte';
+</script>
+
 <header
     class="border-smooth sticky top-0 z-1000 flex h-[4.5rem] items-center border-b backdrop-blur-md"
 >
     <div class="container flex flex-1 items-center justify-between">
-        <a href="/">
-            <img
-                class="web-logo web-u-only-dark"
-                src="/images/logos/appwrite.svg"
-                alt="appwrite"
-                height="24"
-                width="130"
-            /></a
-        >
+        <div class="flex items-center gap-8">
+            <a href="/">
+                <img
+                    class="hidden dark:block"
+                    src="/images/logos/appwrite.svg"
+                    alt="appwrite"
+                    height="24"
+                    width="130"
+                /></a
+            >
+            <MainNavigation />
+        </div>
 
         <button class="web-button">Start building for free</button>
     </div>
---
+++ src/lib/components/layout/main-navigation.svelte
@@ -0,0 +1,119 @@
+<script lang="ts">
+    import { NavigationMenu } from 'bits-ui';
+    import { Icon } from '$lib/components/ui';
+    import { classNames } from '$lib/utils/classnames';
+
+    const products: { name: string; href: string; description: string }[] = [
+        {
+            name: 'Auth',
+            href: '/products/auth',
+            description: 'Secure login with multi-factor auth.'
+        },
+        {
+            name: 'Databases',
+            href: '/docs/products/databases',
+            description: 'Scalable and robust databases.'
+        },
+        {
+            name: 'Storage',
+            href: '/products/storage',
+            description: 'Advanced compression and encryption.'
+        },
+        {
+            name: 'Functions',
+            href: '/products/functions',
+            description: 'Deploy & scale serverless functions.'
+        },
+        {
+            name: 'Messaging',
+            href: '/products/messaging',
+            description: 'Set up a full-functioning messaging service.'
+        },
+        {
+            name: 'Realtime',
+            href: '/docs/apis/realtime',
+            description: 'Subscribe and react to any event.'
+        }
+    ];
+
+    type ListItemProps = {
+        className?: string;
+        name: string;
+        href: string;
+        content: string;
+    };
+</script>
+
+{#snippet ListItem({ className, name, content, href }: ListItemProps)}
+    <li>
+        <NavigationMenu.Link
+            class={classNames(
+                'hover:bg-muted hover:text-accent-foreground focus:bg-muted focus:text-accent-foreground block space-y-1 rounded-md p-3 leading-none no-underline outline-hidden transition-colors select-none',
+                className
+            )}
+            {href}
+        >
+            <div class="text-sm leading-none font-medium">{name}</div>
+            <p class="text-muted-foreground line-clamp-2 text-sm leading-snug">
+                {content}
+            </p>
+        </NavigationMenu.Link>
+    </li>
+{/snippet}
+
+<NavigationMenu.Root>
+    <NavigationMenu.List class="flex items-center gap-8">
+        <NavigationMenu.Item>
+            <NavigationMenu.Trigger class="flex items-center gap-3"
+                >Products <Icon
+                    name="chevron-down"
+                    class="relative size-4 transition-transform duration-200 group-data-[state=open]:rotate-90"
+                    aria-hidden="true"
+                /></NavigationMenu.Trigger
+            >
+            <NavigationMenu.Content
+                class="data-[motion=from-end]:animate-enter-from-right data-[motion=from-start]:animate-enter-from-left data-[motion=to-end]:animate-exit-to-right data-[motion=to-start]:animate-exit-to-left w-full sm:w-auto"
+            >
+                <ul
+                    class="grid gap-3 p-3 sm:w-[600px] sm:p-6 md:w-[800px] md:grid-cols-3 lg:w-[1000px]"
+                >
+                    {#each products as component (component.name)}
+                        {@render ListItem({
+                            href: component.href,
+                            name: component.name,
+                            content: component.description
+                        })}
+                    {/each}
+                </ul>
+            </NavigationMenu.Content>
+        </NavigationMenu.Item>
+        <NavigationMenu.Item>
+            <NavigationMenu.Link href="/docs">Docs</NavigationMenu.Link>
+        </NavigationMenu.Item>
+        <NavigationMenu.Item>
+            <NavigationMenu.Link href="/pricing">Pricing</NavigationMenu.Link>
+        </NavigationMenu.Item>
+        <NavigationMenu.Item>
+            <NavigationMenu.Link href="/contact/enterprise">Enterprise</NavigationMenu.Link>
+        </NavigationMenu.Item>
+        <NavigationMenu.Indicator
+            class={classNames(
+                'top-full z-10 flex h-2.5 items-end justify-center overflow-hidden opacity-100 transition-all duration-200',
+                'data-[state=hidden]:animate-fade-out data-[state=visible]:animate-fade-in data-[state=hidden]:opacity-0'
+            )}
+        >
+            <div
+                class="bg-primary relative top-[70%] size-2.5 rotate-[45deg] rounded-tl-[2px]"
+            ></div>
+        </NavigationMenu.Indicator>
+    </NavigationMenu.List>
+    <div class="absolute top-full left-0 flex w-full justify-center perspective-[2000px]">
+        <NavigationMenu.Viewport
+            class={classNames(
+                'bg-greyscale-850 border-gradient relative w-full origin-[top_center] overflow-hidden rounded-md shadow-lg transition-[width,_height,_opacity] duration-200 before:rounded-md after:rounded-md',
+                'data-[state=open]:animate-scale-in data-[state=closed]:hidden',
+                'h-(--bits-navigation-menu-viewport-height) sm:w-(--bits-navigation-menu-viewport-width)'
+            )}
+        />
+    </div>
+</NavigationMenu.Root>
