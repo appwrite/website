26b58dc19b632a03b4e37d6a5387b8cf2c7ba7c4
Jesse Winton <jesse@appwrite.io>
2025-04-18 12:01:46 -0400

    updates

--- src/routes/products/sites/(components)/create.svelte
+++ src/routes/products/sites/(components)/create.svelte
@@ -1,28 +1,5 @@
 <script lang="ts">
-    import Docs from '../(assets)/sites/docs.png';
-
-    const sites = [
-        {
-            image: Docs,
-            title: 'Docs',
-            frameworks: ['Astro', 'SvelteKit']
-        },
-        {
-            image: Docs,
-            title: 'Docs',
-            frameworks: ['Astro', 'SvelteKit']
-        },
-        {
-            image: Docs,
-            title: 'Docs',
-            frameworks: ['Astro', 'SvelteKit']
-        },
-        {
-            image: Docs,
-            title: 'Docs',
-            frameworks: ['Astro', 'SvelteKit']
-        }
-    ];
+    import Dashboard from '../(assets)/dashboards/create-dashboard.svg';
 </script>
 
 <div class="flex w-full flex-col gap-8 md:px-8">
@@ -34,36 +11,7 @@
         class="mask border-smooth flex aspect-video w-full flex-col rounded-t-3xl border bg-gradient-to-br from-white/10 via-white/5 to-white/10 px-2 pt-2 backdrop-blur-xl"
         style:--mask-height="450px"
     >
-        <div class="flex-1 rounded-t-2xl bg-[#19191C] px-8 pt-16 md:px-16">
-            <header class="flex flex-col justify-between gap-4 pb-10 md:flex-row md:items-center">
-                <h2 class="text-primary font-aeonik-pro text-label">Create Site</h2>
-
-                <div
-                    class="border-greyscale-800 flex min-w-[250px] items-center gap-1.5 rounded-lg border px-1.5 py-0.5"
-                >
-                    <span class="web-icon-search text-secondary"></span>
-                    <input
-                        class="text-secondary placeholder:text-secondary text-micro w-full appearance-none border-none"
-                        type="text"
-                        disabled
-                        placeholder="Search by framework or use case"
-                    />
-                </div>
-            </header>
-            <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-                {#each [...sites, ...sites, ...sites] as { image, title, frameworks }}
-                    <div
-                        class="border-smooth flex aspect-[2.5/1.8] flex-col rounded-xl border bg-white/5 p-2"
-                    >
-                        <img src={image} alt={title} class="w-full rounded-lg object-cover" />
-                        <div class="mt-auto mb-0">
-                            <h3 class="text-primary text-micro font-medium">{title}</h3>
-                            <span class="text-secondary text-x-micro">{frameworks.join(', ')}</span>
-                        </div>
-                    </div>
-                {/each}
-            </div>
-        </div>
+        <img src={Dashboard} alt="Create a site dashboard" class="h-full w-full rounded-t-2xl" />
     </div>
 
     <div class="relative z-10 mx-auto -mt-24 flex max-w-2xl flex-col gap-4 pl-12 text-center">
