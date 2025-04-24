beaeb5001ca84c03ececb2d54f27d5e22b094519
Jesse Winton <jesse@appwrite.io>
2025-04-24 14:20:57 -0400

    finish homepage details

--- src/routes/(marketing)/(components)/bento/bento.svelte
+++ src/routes/(marketing)/(components)/bento/bento.svelte
@@ -4,14 +4,21 @@
     import Functions from './(animations)/functions.svelte';
     import Messaging from './(animations)/messaging.svelte';
     import Realtime from './(animations)/realtime.svelte';
+    import Sites from './(animations)/sites.svelte';
     import Storage from './(animations)/storage.svelte';
 </script>
 
-<div class="container grid grid-cols-12 gap-4 py-20">
-    <Auth />
-    <Databases />
-    <Storage />
-    <Functions />
-    <Messaging />
-    <Realtime />
+<div class="container py-20">
+    <h2 class="text-primary font-aeonik-pro mx-auto mb-16 max-w-sm text-center text-5xl">
+        Backend APIs, frontend hosting
+    </h2>
+    <div class="grid grid-cols-12 gap-4">
+        <Auth />
+        <Databases />
+        <Storage />
+        <Functions />
+        <Messaging />
+        <Realtime />
+        <Sites />
+    </div>
 </div>
