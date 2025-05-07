5ac90921b219436f66de5de2f6fe262dd8773d0b
Jesse Winton <jesse@appwrite.io>
2025-05-07 09:13:00 -0400

    add margin

--- src/routes/(init)/init/tickets/customize/+page.svelte
+++ src/routes/(init)/init/tickets/customize/+page.svelte
@@ -23,7 +23,7 @@
     let originalTicketData = $state({
         name: data.ticket?.name ?? '',
         title: data.ticket?.title ?? '',
-        sticker: data.ticket.sticker
+        sticker: data.ticket?.sticker
     });
 
     let updatedTicketData = $derived({
@@ -63,105 +63,86 @@
     />
 </svelte:head>
 
-<Window class="container my-10">
-    {#snippet link()}
-        <a href="/init" class="group flex items-center gap-2 uppercase">
-            <Icon name="chevron-left" class="transition-transform group-hover:-translate-x-0.5" />
-            Back</a
-        >
-    {/snippet}
-    {#snippet title()}
-        <div>Init Ticket<span class="text-accent">_</span></div>
-    {/snippet}
-
-    <div class="grid grid-cols-1 p-0.5 md:grid-cols-12">
-        <div class="col-span-3 flex flex-col p-4">
-            <h3
-                class="text-primary font-aeonik-pro text-label border-offset border-b border-dashed pb-4"
+<div class="mx-4">
+    <Window class="container my-10">
+        {#snippet link()}
+            <a href="/init" class="group flex items-center gap-2 uppercase">
+                <Icon
+                    name="chevron-left"
+                    class="transition-transform group-hover:-translate-x-0.5"
+                />
+                Back</a
             >
-                Customize ticket
-            </h3>
+        {/snippet}
+        {#snippet title()}
+            <div>Init Ticket<span class="text-accent">_</span></div>
+        {/snippet}
 
-            <form
-                method="POST"
-                class="mt-4 flex flex-1 flex-col gap-4"
-                use:enhance={async () => {
-                    formState.saving = true;
-                    return async ({ result, update }) => {
-                        if (result.type === 'success') {
-                            originalTicketData = updatedTicketData;
-                            formState.saved = true;
-                            formState.saving = false;
-                        }
-                        // update({ reset: false });
-                    };
-                }}
-            >
-                <div class="flex flex-col gap-2">
-                    <label
-                        for="name"
-                        class="text-primary font-aeonik-fono text-x-micro tracking-loose uppercase"
-                        >First name</label
-                    >
-                    <input
-                        bind:value={originalTicketData.name}
-                        onfocus={() => (formState.editing = true)}
-                        onblur={() => (formState.editing = false)}
-                        type="text"
-                        name="name"
-                        class="bg-smooth border-offset w-full appearance-none rounded-lg border p-2"
-                        required
-                    />
-                </div>
-                <div class="flex flex-col gap-2">
-                    <label
-                        for="title"
-                        class="text-primary font-aeonik-fono text-x-micro tracking-loose uppercase"
-                        >Title</label
-                    >
-                    <input
-                        bind:value={originalTicketData.title}
-                        type="text"
-                        name="title"
-                        class="bg-smooth border-offset w-full appearance-none rounded-lg border p-2"
-                    />
-                </div>
+        <div class="grid grid-cols-1 p-0.5 md:grid-cols-12">
+            <div class="col-span-3 flex flex-col p-4">
+                <h3
+                    class="text-primary font-aeonik-pro text-label border-offset border-b border-dashed pb-4"
+                >
+                    Customize ticket
+                </h3>
 
-                <div class="flex h-full flex-1 flex-col gap-2">
-                    <span
-                        class="text-primary font-aeonik-fono text-x-micro tracking-loose uppercase"
-                        >Sticker Pack</span
-                    >
-                    <div
-                        class="bg-smooth border-offset grid h-full flex-1 grid-cols-2 place-items-center gap-4 overflow-y-scroll rounded-lg border p-4"
-                    >
-                        <div
-                            class={classNames(
-                                'relative flex aspect-square w-full items-center justify-center rounded-[2px] border-black bg-black outline-2 outline-[var(--color-offset)] outline-dashed',
-                                originalTicketData.sticker === null
-                                    ? 'outline-white'
-                                    : 'outline-[var(--color-offset)]'
-                            )}
+                <form
+                    method="POST"
+                    class="mt-4 flex flex-1 flex-col gap-4"
+                    use:enhance={async () => {
+                        formState.saving = true;
+                        return async ({ result, update }) => {
+                            if (result.type === 'success') {
+                                originalTicketData = updatedTicketData;
+                                formState.saved = true;
+                                formState.saving = false;
+                            }
+                            // update({ reset: false });
+                        };
+                    }}
+                >
+                    <div class="flex flex-col gap-2">
+                        <label
+                            for="name"
+                            class="text-primary font-aeonik-fono text-x-micro tracking-loose uppercase"
+                            >First name</label
                         >
-                            <input
-                                type="radio"
-                                class="absolute inset-0 appearance-none border-none"
-                                name="sticker"
-                                value=""
-                                onclick={() => (originalTicketData.sticker = null)}
-                            />
-                            <div
-                                class="text-tertiary font-aeonik-fono tracking-loose text-micro bg-smooth flex size-[calc(100%_-_6px)] items-center justify-center rounded-[1px] p-1 uppercase"
-                            >
-                                None
-                            </div>
-                        </div>
+                        <input
+                            bind:value={originalTicketData.name}
+                            onfocus={() => (formState.editing = true)}
+                            onblur={() => (formState.editing = false)}
+                            type="text"
+                            name="name"
+                            class="bg-smooth border-offset w-full appearance-none rounded-lg border p-2"
+                            required
+                        />
+                    </div>
+                    <div class="flex flex-col gap-2">
+                        <label
+                            for="title"
+                            class="text-primary font-aeonik-fono text-x-micro tracking-loose uppercase"
+                            >Title</label
+                        >
+                        <input
+                            bind:value={originalTicketData.title}
+                            type="text"
+                            name="title"
+                            class="bg-smooth border-offset w-full appearance-none rounded-lg border p-2"
+                        />
+                    </div>
 
-                        {#each stickerPack as s, i}
+                    <div class="flex h-full flex-1 flex-col gap-2">
+                        <span
+                            class="text-primary font-aeonik-fono text-x-micro tracking-loose uppercase"
+                            >Sticker Pack</span
+                        >
+                        <div
+                            class="bg-smooth border-offset grid h-full flex-1 grid-cols-2 place-items-center gap-4 overflow-y-scroll rounded-lg border p-4"
+                        >
                             <div
                                 class={classNames(
-                                    'relative flex aspect-square w-full items-center justify-center rounded-sm bg-black outline-2 [outline-offset:-1px] transition outline-dashed',
-                                    originalTicketData.sticker === i
+                                    'relative flex aspect-square w-full items-center justify-center rounded-[2px] border-black bg-black outline-2 outline-[var(--color-offset)] outline-dashed',
+                                    originalTicketData.sticker === null
                                         ? 'outline-white'
                                         : 'outline-[var(--color-offset)]'
                                 )}
@@ -170,60 +151,84 @@
                                     type="radio"
                                     class="absolute inset-0 appearance-none border-none"
                                     name="sticker"
-                                    value={i}
-                                    onclick={() => (originalTicketData.sticker = i)}
+                                    value=""
+                                    onclick={() => (originalTicketData.sticker = null)}
                                 />
                                 <div
-                                    class="bg-smooth flex size-[calc(100%_-_6px)] items-center justify-center rounded-[1px] p-1"
+                                    class="text-tertiary font-aeonik-fono tracking-loose text-micro bg-smooth flex size-[calc(100%_-_6px)] items-center justify-center rounded-[1px] p-1 uppercase"
                                 >
-                                    <img src={s} alt="Sticker" class="size-20" />
+                                    None
                                 </div>
                             </div>
-                        {/each}
+
+                            {#each stickerPack as s, i}
+                                <div
+                                    class={classNames(
+                                        'relative flex aspect-square w-full items-center justify-center rounded-sm bg-black outline-2 [outline-offset:-1px] transition outline-dashed',
+                                        originalTicketData.sticker === i
+                                            ? 'outline-white'
+                                            : 'outline-[var(--color-offset)]'
+                                    )}
+                                >
+                                    <input
+                                        type="radio"
+                                        class="absolute inset-0 appearance-none border-none"
+                                        name="sticker"
+                                        value={i}
+                                        onclick={() => (originalTicketData.sticker = i)}
+                                    />
+                                    <div
+                                        class="bg-smooth flex size-[calc(100%_-_6px)] items-center justify-center rounded-[1px] p-1"
+                                    >
+                                        <img src={s} alt="Sticker" class="size-20" />
+                                    </div>
+                                </div>
+                            {/each}
+                        </div>
                     </div>
+                    <Button type="submit" class="w-full!" variant="secondary">
+                        {#if formState.saving}
+                            Saving
+                        {:else if formState.saved}
+                            Saved
+                        {:else}
+                            Save
+                        {/if}
+                    </Button>
+                </form>
+            </div>
+            <div
+                class="bg-smooth relative flex w-full flex-col items-center justify-center gap-8 rounded-xl p-4 outline-2 [outline-offset:-2px] outline-[var(--color-offset)] outline-dashed md:col-span-9 md:flex-row"
+            >
+                <div class="flex flex-col items-center gap-4 uppercase">
+                    <TicketCard
+                        {...data.ticket!}
+                        name={originalTicketData.name}
+                        title={originalTicketData.title}
+                        sticker={originalTicketData.sticker}
+                        editing={formState.editing}
+                        disableEffects
+                    />
+                    <span
+                        class="font-aeonik-fono tracking-loose text-x-micro text-primary transition-opacity peer-hover:opacity-0"
+                        >Front</span
+                    >
                 </div>
-                <Button type="submit" class="w-full!" variant="secondary">
-                    {#if formState.saving}
-                        Saving
-                    {:else if formState.saved}
-                        Saved
-                    {:else}
-                        Save
-                    {/if}
-                </Button>
-            </form>
-        </div>
-        <div
-            class="bg-smooth relative flex w-full flex-col items-center justify-center gap-8 rounded-xl p-4 outline-2 [outline-offset:-2px] outline-[var(--color-offset)] outline-dashed md:col-span-9 md:flex-row"
-        >
-            <div class="flex flex-col items-center gap-4 uppercase">
-                <TicketCard
-                    {...data.ticket}
-                    name={originalTicketData.name}
-                    title={originalTicketData.title}
-                    sticker={originalTicketData.sticker}
-                    editing={formState.editing}
-                    disableEffects
-                />
-                <span
-                    class="font-aeonik-fono tracking-loose text-x-micro text-primary transition-opacity peer-hover:opacity-0"
-                    >Front</span
-                >
-            </div>
-            <div class="flex flex-col items-center gap-4 uppercase">
-                <TicketCard
-                    {...data.ticket}
-                    contributions={data.streamed.contributions}
-                    sticker={updatedTicketData.sticker}
-                    disableEffects
-                    flipped
-                    {stickerPack}
-                />
-                <span
-                    class="font-aeonik-fono tracking-loose text-x-micro text-primary transition duration-300 peer-hover:opacity-0 peer-hover:blur-sm"
-                    >Back</span
-                >
+                <div class="flex flex-col items-center gap-4 uppercase">
+                    <TicketCard
+                        {...data.ticket!}
+                        contributions={data.streamed.contributions}
+                        sticker={updatedTicketData.sticker}
+                        disableEffects
+                        flipped
+                        {stickerPack}
+                    />
+                    <span
+                        class="font-aeonik-fono tracking-loose text-x-micro text-primary transition duration-300 peer-hover:opacity-0 peer-hover:blur-sm"
+                        >Back</span
+                    >
+                </div>
             </div>
         </div>
-    </div>
-</Window>
+    </Window>
+</div>
