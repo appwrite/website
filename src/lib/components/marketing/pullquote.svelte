53b7eb97dc5d4915a1d6c6803652722f806ea942
Jesse Winton <jesse@appwrite.io>
2025-05-08 11:43:12 -0400

    updates

---
+++ src/lib/components/marketing/pullquote.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { classNames } from '$lib/utils/classnames';
+    import type { SvelteHTMLElements } from 'svelte/elements';
+
+    type BlockquoteElementProps = SvelteHTMLElements['blockquote'];
+    interface PullquoteProps extends BlockquoteElementProps {}
+
+    const { class: className, children }: PullquoteProps = $props();
+</script>
+
+<blockquote class={classNames(className)}>
+    {@render children?.()}
+</blockquote>
