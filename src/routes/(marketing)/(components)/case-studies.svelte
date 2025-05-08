7c13b4c9ed7bfb3c7546191c6235c917dbcbbd09
Jesse Winton <jesse@appwrite.io>
2025-05-08 13:26:42 -0400

    Update case-studies.svelte

--- src/routes/(marketing)/(components)/case-studies.svelte
+++ src/routes/(marketing)/(components)/case-studies.svelte
@@ -1,148 +1,73 @@
 <script lang="ts">
+    import { PUBLIC_APPWRITE_DASHBOARD } from '$env/static/public';
+    import { Button } from '$lib/components/ui';
     import { classNames } from '$lib/utils/classnames';
-    import ProductHunt from '../(assets)/logos/product-hunt.svg';
-    import Avatar from '../(assets)/images/avatar.png';
-    import { ToggleGroup } from 'bits-ui';
 
-    import Icon from '$lib/components/ui/icon';
-    import { animate, stagger } from 'motion';
-
-    const studies = [
+    const testimonials = [
         {
-            logo: ProductHunt,
-            headline:
-                'Product Hunt has leveraged Appwrite to transform logistics and supply chain management for circular businesses',
-            blurb: 'Been a huge supporter of Appwrite for over a year, championing it even at the companies I was working at.',
-            name: 'Terry Lennon',
-            title: 'Engineer at Product Hunt',
-            avatar: Avatar,
-            url: '/case-studies/product-hunt'
+            name: 'Ryan O’Conner',
+            copy: `The switch to using Appwrite brought infinite value that I'm still discovering today, but a major impact that it made was the amount of time and stress that it saved me as it simply just works.`,
+            image: '/images/testimonials/ryan-oconner.png',
+            title: 'Founder',
+            company: 'K-Collect'
         },
         {
-            logo: ProductHunt,
-            headline:
-                'Product Hunt has leveraged Appwrite to transform logistics and supply chain management for circular businesses',
-            blurb: 'Been a huge supporter of Appwrite for over a year, championing it even at the companies I was working at.',
-            name: 'Terry Lennon',
-            title: 'Engineer at Product Hunt',
-            avatar: Avatar,
-            url: '/case-studies/product-hunt'
+            name: 'David Forster',
+            copy: `We really loved working with Appwrite for launching our bootstrapped "Open Mind" App. I am still surprised how easy the implementation into Flutter was.`,
+            image: '/images/testimonials/david-forster.png',
+            title: 'Founder',
+            company: 'Open Mind'
         },
         {
-            logo: ProductHunt,
-            headline:
-                'Product Hunt has leveraged Appwrite to transform logistics and supply chain management for circular businesses',
-            blurb: 'Been a huge supporter of Appwrite for over a year, championing it even at the companies I was working at.',
-            name: 'Terry Lennon',
-            title: 'Engineer at Product Hunt',
-            avatar: Avatar,
-            url: '/case-studies/product-hunt'
+            name: 'Marius Bolik',
+            copy: `The integrated user authentication and the ease of creating data structures have undoubtedly saved us several weeks' worth of time.`,
+            image: '/images/testimonials/marius-bolik2.png',
+            title: 'CTO',
+            company: 'mySHOEFITTER'
+        },
+        {
+            name: 'Sergio Ponguta',
+            copy: `Just go for it, don’t think twice. Try Appwrite, and you will love it!`,
+            image: '/images/testimonials/smartbee.png',
+            title: 'Founder',
+            company: 'Smart Bee'
+        },
+        {
+            name: 'Phil McClusky',
+            copy: 'Just like a Swiss Army Knife, you can choose and use the tools that you need with Appwrite.',
+            image: '/images/testimonials/majik.png',
+            title: 'Developer',
+            company: 'Majik Kids'
         }
     ];
-
-    let value = $state<string>('0');
-
-    const getValue = () => {
-        return value;
-    };
-
-    const setValue = (newValue: string) => {
-        if (!newValue.length) return;
-        value = newValue;
-    };
-
-    $effect(() => {
-        animate(
-            '.active .word',
-            {
-                y: [36, 0]
-            },
-            {
-                duration: 0.25,
-                delay: stagger(0.025),
-                ease: 'circOut'
-            }
-        );
-    });
 </script>
 
 <div
     class={classNames(
-        'relative flex min-h-[60vh] items-center justify-center overflow-hidden',
+        'border-smooth relative -mb-4 flex items-center justify-center overflow-hidden border-t pt-20 pb-40',
         'from-0% before:absolute before:inset-0 before:top-0 before:left-0 before:-z-10 before:block before:h-full before:bg-radial-[circle_at_120%_-50%] before:from-purple-500/30 before:to-transparent before:to-40% before:blur-2xl',
         'after:from-accent/20 after:absolute after:inset-0 after:top-0 after:right-0 after:-z-10 after:mt-auto after:mb-0 after:block after:h-full after:bg-radial-[circle_at_-15%_125%] after:from-0% after:to-transparent after:to-40% after:blur-2xl'
     )}
 >
-    <ToggleGroup.Root
-        bind:value={getValue, setValue}
-        type="single"
-        class="container flex h-full flex-col items-center gap-4 md:flex-row"
-    >
-        {#each studies as study, i}
-            <ToggleGroup.Item
-                value={i.toString()}
-                class={classNames(
-                    'data-[state="on"]:border-smooth grid cursor-pointer place-content-center place-items-center overflow-hidden rounded-2xl border border-transparent px-12 py-6 backdrop-blur-3xl transition-all duration-400 ease-in-out [grid-template-areas:"stack"] max-sm:data-[state="off"]:h-[125px] max-sm:data-[state="on"]:h-[425px] md:h-[425px]',
-                    'group/card text-left! hover:bg-black/24 data-[state="off"]:bg-black/16 data-[state="on"]:[flex-basis:70%] data-[state="on"]:bg-black/24 md:data-[state="off"]:[flex-basis:15%]'
-                )}
+    <div class="container mx-auto">
+        <div class="flex w-full flex-col justify-between py-8 md:flex-row">
+            <h2
+                class="text-description text-secondary font-aeonik-pro max-w-[789px] text-[32px] leading-9 -tracking-[1px] text-pretty"
             >
-                <img
-                    src={study.logo}
-                    alt={study.headline}
-                    class={classNames(
-                        'visible opacity-100 transition-all [grid-area:stack]',
-                        'group-[&[data-state=on]]/card:invisible group-[&[data-state=on]]/card:opacity-0',
-                        'brightness-50'
-                    )}
-                />
-
-                <div
-                    class={classNames(
-                        'invisible space-y-4 opacity-0 blur-sm transition-opacity delay-400 [grid-area:stack]',
-                        'group-[&[data-state=on]]/card:visible group-[&[data-state=on]]/card:opacity-100 group-[&[data-state=on]]/card:blur-none'
-                    )}
-                >
-                    <img src={study.logo} alt={study.headline} />
-
-                    <span
-                        class="text-title font-aeonik-pro text-primary relative flex flex-wrap gap-2"
-                        class:active={value === i.toString()}
-                        >{#each study.headline.split(' ') as word, i}
-                            <span class="relative overflow-clip">
-                                <span class="word inline-flex">
-                                    {word}
-                                </span>
-                            </span>
-                        {/each}</span
-                    >
-
-                    <div
-                        class={classNames('border-smooth mt-8 border-t border-dashed pt-8', {
-                            'animate-fade-in [animation-delay:500ms]': value === i.toString()
-                        })}
-                    >
-                        <div class="text-primary text-sub-body font-medium md:max-w-[60%]">
-                            "{study.blurb}"
-                        </div>
-
-                        <div class="mt-4 flex flex-col justify-between md:flex-row">
-                            <div class="flex gap-2">
-                                <img src={study.avatar} alt={study.headline} class="size-6" />
-                                <span class="text-caption">{study.name}, {study.title}</span>
-                            </div>
-
-                            <a
-                                href={study.url}
-                                class="text-primary group mt-4 flex items-center gap-2 md:mt-0"
-                                >Read customer story <Icon
-                                    name="arrow-right"
-                                    class="transition-transform group-hover:translate-x-0.5"
-                                ></Icon></a
-                            >
-                        </div>
-                    </div>
+                <span class="text-primary">DevKind</span> reduced development time by 60%, and
+                lowered server costs by 40%. LangX handled millions of requests. K-collect reduced
+                infrastructure costs by <span class="text-primary">700%</span>.
+            </h2>
+            <Button href={PUBLIC_APPWRITE_DASHBOARD} class="self-end" variant="secondary"
+                >Start building for free</Button
+            >
+        </div>
+        <div class="mt-12 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+            {#each testimonials as testimonial}
+                <div class="border-smooth bg-card h-fit rounded-2xl border p-5">
+                    {testimonial.copy}
                 </div>
-            </ToggleGroup.Item>
-        {/each}
-    </ToggleGroup.Root>
+            {/each}
+        </div>
+    </div>
 </div>
