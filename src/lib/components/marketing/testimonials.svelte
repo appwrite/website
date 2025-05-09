4008a6179581da6dce08c4fb0b8ebdabf50f37f1
Jesse Winton <jesse@appwrite.io>
2025-05-09 12:58:07 -0400

    Update testimonials.svelte

--- src/lib/components/marketing/testimonials.svelte
+++ src/lib/components/marketing/testimonials.svelte
@@ -46,15 +46,15 @@
 </script>
 
 <div class={classNames('relative w-full max-w-[100vw] overflow-hidden', className)}>
-    <div class="group light flex w-fit gap-8">
-        {#each Array.from({ length: 4 }) as _, i}
+    <div class="group light flex w-fit flex-nowrap gap-8 overflow-clip md:overflow-visible">
+        {#each Array.from({ length: 2 }) as _, i}
             <div
-                class="animate-scroll group-hover:[animation-play-state:paused;] flex items-center gap-8"
-                aria-hidden={i !== 0}
+                class="animate-scroll-x flex items-center gap-8 group-hover:[animation-play-state:paused]"
             >
                 {#each testimonials as testimonial}
                     <div
                         class="flex h-fit w-[90vw] flex-col justify-center rounded-2xl bg-white p-6 transition-all md:w-lg"
+                        aria-hidden={i < testimonials.length - 1}
                     >
                         <p class="text-sub-body text-secondary flex-1 font-medium">
                             {testimonial.copy}
