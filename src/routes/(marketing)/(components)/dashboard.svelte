e4e0c75d8a4635714d3fae0c09017561ddbac430
Jesse Winton <jesse@appwrite.io>
2025-05-30 16:31:07 -0400

    Update dashboard.svelte

--- src/routes/(marketing)/(components)/dashboard.svelte
+++ src/routes/(marketing)/(components)/dashboard.svelte
@@ -21,7 +21,7 @@
 
 <div
     class={classNames(
-        'bg-smooth -mb-72 max-w-screen translate-x-5 overflow-hidden rounded-t-2xl border-x border-t border-white/10 px-2 pt-2 backdrop-blur-2xl md:mb-0 md:translate-x-1/4 ',
+        'bg-smooth -mb-104 max-w-[150vw] translate-x-8 -translate-y-32 scale-70 overflow-hidden rounded-t-2xl border-x border-t border-white/10 px-2 pt-2 backdrop-blur-2xl md:mb-0 md:translate-x-1/4 md:translate-y-0 md:scale-100 ',
         'mask-b-from-0% mask-b-to-70% md:mask-b-to-100%'
     )}
 >
