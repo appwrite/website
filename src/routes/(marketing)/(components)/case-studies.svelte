a73ab0fcaa9f7e36d141b7cdf7abb4575f76dae0
Jesse Winton <jesse@appwrite.io>
2025-06-05 15:26:12 -0400

    back to OG

--- src/routes/(marketing)/(components)/case-studies.svelte
+++ src/routes/(marketing)/(components)/case-studies.svelte
@@ -51,26 +51,23 @@
 
 <div
     class={classNames(
-        'relative -mb-4 flex min-h-[60vh] items-center justify-center overflow-y-hidden pt-30 pb-40',
-        'bg-gradient-to-br from-black/40 to-black/16',
-        'before:absolute before:inset-0 before:top-0 before:left-0 before:block before:h-full before:bg-radial-[circle_at_120%_-50%] before:from-purple-500/30 before:from-0% before:to-transparent before:to-40% before:blur-2xl',
-        'after:from-accent/20 after:absolute after:inset-0 after:top-0 after:right-0 after:mt-auto after:mb-0 after:block after:h-full after:bg-radial-[circle_at_-15%_125%] after:from-0% after:to-transparent after:to-40% after:blur-2xl'
+        'relative -mb-4 flex min-h-[60vh] items-center justify-center overflow-hidden pt-30 pb-40',
+        'from-0% before:absolute before:inset-0 before:top-0 before:left-0 before:-z-10 before:block before:h-full before:bg-radial-[circle_at_120%_-50%] before:from-purple-500/30 before:to-transparent before:to-40% before:blur-2xl',
+        'after:from-accent/20 after:absolute after:inset-0 after:top-0 after:right-0 after:-z-10 after:mt-auto after:mb-0 after:block after:h-full after:bg-radial-[circle_at_-15%_125%] after:from-0% after:to-transparent after:to-40% after:blur-2xl'
     )}
 >
     <ToggleGroup.Root
         bind:value={getValue, setValue}
         type="single"
-        class="relative z-20 container flex h-full flex-col items-center gap-4 md:flex-row"
+        class="container flex h-full flex-col items-center gap-4 md:flex-row"
     >
         {#each studies as study, i}
-            {@const isOpen = i.toString() === value.toString()}
             <ToggleGroup.Item
                 value={i.toString()}
                 class={classNames(
-                    'align-items-center group/card grid w-full place-items-center justify-items-center overflow-hidden rounded-2xl border border-transparent p-4 backdrop-blur-3xl transition-all duration-400 ease-in-out [grid-template-areas:"stack"] hover:bg-black/24',
-                    'data-[state=off]:basis-[15%] data-[state=off]:cursor-pointer data-[state=off]:bg-black/16 data-[state=off]:hover:shadow-[0px_0px_0px_4px_var(--color-offset)] data-[state=off]:focus:shadow-[0px_0px_0px_4px_var(--color-offset)]',
-                    'data-[state=on]:border-smooth data-[state=on]:basis-full data-[state=on]:bg-black/24 data-[state=on]:p-12',
-                    'outline-0 duration-300'
+                    'data-[state="on"]:border-smooth align-items-center grid cursor-pointer justify-items-center overflow-hidden rounded-2xl border border-transparent px-12 py-12 backdrop-blur-3xl transition-all duration-400 ease-in-out [grid-template-areas:"stack"] max-sm:data-[state="off"]:h-[125px] max-sm:data-[state="on"]:h-[425px] md:h-[425px] md:place-content-center md:place-items-center md:items-center md:justify-center',
+                    'group/card hover:bg-black/24 data-[state="off"]:bg-black/16 data-[state="on"]:basis-full data-[state="on"]:bg-black/24 md:data-[state="off"]:basis-[15%] md:data-[state="on"]:basis-[70%]',
+                    'outline-0 duration-300 hover:shadow-[0px_0px_0px_4px_var(--color-offset)] focus:shadow-[0px_0px_0px_4px_var(--color-offset)]!'
                 )}
             >
                 <img
@@ -102,7 +99,7 @@
                     />
 
                     <span
-                        class="text-title font-aeonik-pro text-primary relative flex flex-wrap gap-2 text-left group-[&[data-state=off]]/card:max-sm:text-nowrap"
+                        class="text-title font-aeonik-pro text-primary relative flex flex-wrap gap-2 text-left"
                         class:active={value === i.toString()}
                     >
                         {study.headline}
