8b8a93f8cc7bb299b8ba7e88a86d8da8823bf5f6
Jesse Winton <jesse@appwrite.io>
2025-06-05 13:51:23 -0400

    update features padding

--- src/routes/(marketing)/(components)/features.svelte
+++ src/routes/(marketing)/(components)/features.svelte
@@ -72,7 +72,7 @@
         <div class="container grid grid-cols-2 overflow-hidden lg:grid-cols-4">
             {#each features as box}
                 <a
-                    class="text-sub-body group relative border-b border-dashed border-black/8 px-4 py-8 font-medium last-of-type:border-0 nth-of-type-[4]:border-r-0 nth-of-type-[7]:border-b-0 max-lg:even:border-r-0 md:border-r lg:nth-of-type-[5]:border-b-0 lg:nth-of-type-[6]:border-b-0 lg:nth-of-type-[8]:border-b-0"
+                    class="text-sub-body group relative border-b border-dashed border-black/8 p-8 font-medium last-of-type:border-0 nth-of-type-[4]:border-r-0 nth-of-type-[7]:border-b-0 max-lg:even:border-r-0 md:border-r lg:nth-of-type-[5]:border-b-0 lg:nth-of-type-[6]:border-b-0 lg:nth-of-type-[8]:border-b-0"
                     href={box.href}
                     onclick={() => {
                         trackEvent(`feature-${box.label.toLowerCase().replace(' ', '-')}-click`);
