25bf2a27e0d1e03649a98e7025bab7af0f8ca739
Jesse Winton <jesse@appwrite.io>
2025-08-04 13:06:07 -0400

    Update numbers.svelte

--- src/routes/cloud-ga/(components)/numbers.svelte
+++ src/routes/cloud-ga/(components)/numbers.svelte
@@ -11,19 +11,17 @@
 
 <div class={classNames('relative h-full', className)}>
     <div class="mx-auto flex w-full max-w-6xl flex-col justify-center">
-        <div class="grid grid-cols-2 place-content-between gap-4 pt-8 md:grid-cols-4">
+        <div class="flex justify-between gap-8 pt-8">
             {#each numbers as { label, description }, i}
                 <div
                     class={classNames(
-                        'border-gradient relative h-full w-[calc(100%_-_16px)] rounded-2xl bg-[#232325]/90 p-4 before:rounded-2xl after:rounded-2xl'
+                        'border-gradient relative flex h-full flex-1 flex-col gap-1 rounded-2xl bg-[#232325]/90 p-5 before:rounded-2xl after:rounded-2xl'
                     )}
-                    style:grid-column-start={i + 1}
-                    style:grid-column-end={i + 2}
                     style:--border-gradient-before="linear-gradient(180deg,rgba(255,255,255,0.12),transparent)"
                     style:--border-gradient-after="linear-gradient(180deg,rgba(255,255,255,0),transparent)"
                 >
                     <h3
-                        class="md:tracking-squeezed font-aeonik-pro text-primary text-[1.25rem] text-pretty md:text-[2rem]"
+                        class="md:tracking-squeeze font-aeonik-pro text-primary -mt-2 text-[1.25rem] text-pretty md:text-[2rem]"
                     >
                         {label}
                     </h3>
