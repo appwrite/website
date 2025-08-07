52400cc1da265134ff091a600535d9b3ca454adb
Jesse Winton <jesse@appwrite.io>
2025-08-07 10:04:19 -0400

    updates to auth page

--- src/lib/components/product-pages/testimonials.svelte
+++ src/lib/components/product-pages/testimonials.svelte
@@ -42,7 +42,7 @@
     <div class="group light flex w-fit gap-4">
         {#each Array.from({ length: 4 }) as _, i}
             <div
-                class="animate-scroll-deprecategroup-hover:[animation-play-state:paused;] flex items-center gap-8"
+                class="animate-scroll-deprecate group-hover:[animation-play-state:paused;] flex items-center gap-4"
                 aria-hidden={i !== 0}
             >
                 {#each testimonials as testimonial}
