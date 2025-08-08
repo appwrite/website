1ff3e152310c0d1fdaa4e8a768a56bce4bb45dbd
Jesse Winton <jesse@appwrite.io>
2025-08-08 15:47:15 -0400

    Revert "update illustration"
    This reverts commit 54ab3c0b3b2d2d3294a90909cc89de46f739cdb3.

--- src/routes/startups/+page.svelte
+++ src/routes/startups/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import ProjectTimeline from './(assets)/project-timeline.webp';
+    import ProjectTimeline from './(assets)/project-timeline.avif';
     import UsageGraphs from './(assets)/usage-graphs.avif';
     import SecurityOptions from './(assets)/security-options.avif';
 
@@ -339,9 +339,17 @@
                                 </div>
 
                                 <div
-                                    class="border-smooth mt-20 -mr-[85vw] overflow-hidden rounded-lg border shadow-[-28.0524px_51.0044px_22.952px_rgba(0,0,0,0.01),_-16.1514px_28.9025px_19.5517px_rgba(0,_0,_0,_0.02),_-6.80058px_12.7511px_14.4512px_rgba(0,_0,_0,_0.03),_-1.70015px_3.40029px_7.65065px_rgba(0,_0,_0,_0.04),_0px_0px_0px_rgba(0,_0,_0,_0.04)] md:mr-0 md:rounded-2xl"
+                                    class="web-is-only-desktop"
+                                    style="margin-block-start:80px; margin-inline-start: -50px"
                                 >
-                                    <img alt="" src={ProjectTimeline} class="" width={1250} />
+                                    <img alt="" src={ProjectTimeline} width="1250" />
+                                </div>
+
+                                <div
+                                    class="web-is-not-desktop"
+                                    style="padding-block-start: 48px; margin-inline-start:-32px;"
+                                >
+                                    <img alt="" src={ProjectTimeline} />
                                 </div>
                             </li>
                             <li class="web-timeline-content-item">
