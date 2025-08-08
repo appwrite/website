057321a65e864db26c8a84c469f82469f7b3f665
Jesse Winton <jesse@appwrite.io>
2025-08-08 11:59:45 -0400

    update articles

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -2,32 +2,30 @@
     import Media from '$lib/UI/Media.svelte';
     import { formatDate } from '$lib/utils/date';
 
-    export let title: string;
-    export let cover: string;
-    export let href: string;
-    export let date: Date;
-    export let timeToRead: number;
-    export let author: string;
-    export let avatar: string;
+    interface ArticleProps {
+        title: string;
+        cover: string;
+        href: string;
+        date: Date;
+        timeToRead: number;
+        author: string;
+        avatar: string;
+    }
+
+    const { title, cover, href, date, timeToRead, author, avatar }: ArticleProps = $props();
 </script>
 
 <li>
     <a class="web-grid-articles-item is-transparent" {href}>
         <div class="web-grid-articles-item-image">
-            <Media
-                src={cover}
-                class="web-u-media-ratio-16-9"
-                alt={title}
-                autoplay
-                controls={false}
-            />
+            <Media src={cover} class="aspect-video" alt={title} autoplay controls={false} />
         </div>
         <div class="web-grid-articles-item-content">
             <h4 class="text-label text-primary">
                 {title}
             </h4>
             <div class="web-author">
-                <div class="flex items-center gap-2">
+                <div class="flex items-center justify-between gap-2">
                     <img
                         class="web-author-image"
                         loading="lazy"
@@ -36,9 +34,9 @@
                         height="24"
                         alt={author}
                     />
-                    <div class="web-author-info">
+                    <div class="web-author-info flex-1 justify-between">
                         <h4 class="text-sub-body text-primary">{author}</h4>
-                        <ul class="web-metadata text-caption web-is-not-mobile">
+                        <ul class="web-metadata text-caption web-is-not-mobile flex-1 self-end">
                             <li>
                                 {formatDate(date)}
                             </li>
