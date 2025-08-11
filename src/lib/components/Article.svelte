318ae35ae88a9534ce0540ef1e0b1d8db3e5b861
Jesse Winton <jesse@appwrite.io>
2025-08-11 12:11:32 -0400

    update article

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -16,37 +16,28 @@
 </script>
 
 <li>
-    <a class="web-grid-articles-item is-transparent" {href}>
+    <a class="bg-transparent" {href}>
         <div class="overflow-hidden rounded-lg">
             <Media src={cover} class="aspect-video" alt={title} autoplay controls={false} />
         </div>
-        <div class="flex flex-col gap-4 pt-6 pb-3">
-            <h4 class="text-label text-primary">
-                {title}
-            </h4>
+        <div class="flex flex-col gap-3 pt-6 pb-3">
             <div class="flex w-full">
-                <div class="flex w-full items-start justify-between gap-2">
-                    <div class="flex items-center gap-2">
-                        <img
-                            class="rounded-full"
-                            loading="lazy"
-                            src={avatar}
-                            width="24"
-                            height="24"
-                            alt={author}
-                        />
-                        <h4 class="text-sub-body text-primary">{author}</h4>
+                <div class="text-micro flex w-full flex-col items-center xl:flex-row xl:gap-2">
+                    <div class="flex items-center justify-center gap-2">
+                        <img class="size-5 rounded-full" loading="lazy" src={avatar} alt={author} />
+                        <h4 class="text-primary">{author}</h4>
                     </div>
-                    <div class="flex justify-between">
-                        <ul class="text-secondary text-caption hidden items-end self-end md:flex">
-                            <li>
-                                {formatDate(date)}
-                            </li>
-                            <li class="before:mx-1 xl:before:content-['•']">{timeToRead} min</li>
-                        </ul>
+                    <div class="text-secondary ml-7 flex xl:ml-0">
+                        <span>
+                            {formatDate(date)}
+                        </span>
+                        <span class="before:mx-1 xl:before:content-['-']">{timeToRead} min</span>
                     </div>
                 </div>
             </div>
+            <h4 class="text-label font-aeonik-pro text-primary line-clamp-2">
+                {title}
+            </h4>
         </div>
     </a>
 </li>
