1e90323667d6d61e10b277f9bb0b9219f168ac56
Jesse Winton <jesse@appwrite.io>
2025-08-12 11:07:52 -0400

    update

--- src/markdoc/nodes/Blockquote.svelte
+++ src/markdoc/nodes/Blockquote.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-    import { setContext, type Snippet } from 'svelte';
+    import { type Snippet } from 'svelte';
     interface BlockquoteProps {
         children: Snippet;
     }
 
     const { children }: BlockquoteProps = $props();
-
-    setContext('no-paragraph', true);
 </script>
 
 <blockquote>
