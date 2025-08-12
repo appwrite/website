195385192e507cfab08ef2c4d5b9970b2ef0b431
Jesse Winton <jesse@appwrite.io>
2025-08-12 11:05:15 -0400

    update

--- src/markdoc/tags/Arrow_Link.svelte
+++ src/markdoc/tags/Arrow_Link.svelte
@@ -1,40 +1,20 @@
 <script lang="ts">
-    import { isInDocs } from '$lib/layouts/Docs.svelte';
-    import { isInChangelog } from '$markdoc/layouts/Changelog.svelte';
+    import { Icon } from '$lib/components/ui';
     import type { Snippet } from 'svelte';
-    interface Props {
+
+    interface ArrowLinkProps {
         href: string;
         children: Snippet;
     }
 
-    const { href, children }: Props = $props();
+    const { href, children }: ArrowLinkProps = $props();
 
     const isExternal = ['http://', 'https://'].some((prefix) => href.startsWith(prefix));
     const target = isExternal ? '_blank' : undefined;
     const rel = isExternal ? 'noopener nofollow' : undefined;
-
-    const inChangelog = isInChangelog();
-    const inDocs = isInDocs();
-
-    let classes = $derived(
-        (() => {
-            if (inDocs) return 'web-link text-paragraph-md';
-            if (inChangelog) return 'web-link text-paragraph-lg';
-            return '';
-        })()
-    );
 </script>
 
-<a class={classes} data-in-changelog={inChangelog ? '' : undefined} {href} {target} {rel}
-    >{@render children()}<span class="icon-cheveron-right" style:font-size="16px"></span></a
->
-
-<style>
-    a {
-        display: flex;
-    }
-
-    [data-in-changelog]:last-child {
-        padding-block-start: 1rem;
-    }
-</style>
+<a class="flex" {href} {target} {rel}>
+    {@render children()}
+    <Icon name="chevron-right" />
+</a>
