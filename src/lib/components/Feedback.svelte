5e06f1f761f68c0223a2c81428baa95435f00212
Jesse Winton <jesse@appwrite.io>
2025-08-13 11:08:45 -0400

    fixe

--- src/lib/components/Feedback.svelte
+++ src/lib/components/Feedback.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
     import { onMount } from 'svelte';
     import { page } from '$app/state';
-    import { fade } from 'svelte/transition';
     import { loggedIn, user } from '$lib/utils/console';
     import { PUBLIC_GROWTH_ENDPOINT } from '$env/static/public';
     import { Button } from '$lib/components/ui';
     import Icon from './ui/icon';
     import { createForm } from '@tanstack/svelte-form';
+    import { z } from 'zod';
+    import { useHotkeys } from '$lib/utils/use-hotkeys.svelte';
+
+    interface FeedbackProps {
+        date?: string;
+    }
+
+    const { date }: FeedbackProps = $props();
 
     const form = createForm(() => ({
         defaultValues: {
@@ -22,10 +29,10 @@
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    email,
+                    email: value.email,
                     type: feedbackType,
                     route: page.route.id,
-                    comment,
+                    comment: value.comment,
                     metaFields: {
                         userId
                     }
@@ -34,60 +41,18 @@
         }
     }));
 
-    export let date: string | undefined = undefined;
-    let showFeedback = false;
-    let feedbackType = '';
-    let email = '';
-    let comment = '';
-    let error: string | undefined;
-    let submitted = false;
-    let submitting = false;
+    let showFeedback = $state<boolean>(false);
+    let feedbackType = $state<'negative' | 'positive'>('positive');
 
-    async function handleSubmit() {
-        submitting = true;
-        error = undefined;
-
-        const userId = loggedIn && $user?.$id ? $user.$id : undefined;
-
-        const response = await fetch(`${PUBLIC_GROWTH_ENDPOINT}/feedback/docs`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email,
-                type: feedbackType,
-                route: page.route.id,
-                comment,
-                metaFields: {
-                    userId
-                }
-            })
+    $effect(() => {
+        useHotkeys(['Escape'], () => {
+            showFeedback = false;
         });
-        submitting = false;
-        if (response.status >= 400) {
-            error = response.status >= 500 ? 'Server Error.' : 'Error submitting form.';
-            return;
-        }
-        comment = email = '';
-        submitted = true;
-        setTimeout(() => (showFeedback = false), 500);
-    }
-
-    function reset() {
-        comment = email = '';
-        feedbackType = '';
-        submitted = false;
-        error = undefined;
-    }
-
-    $: if (!showFeedback) {
-        reset();
-    }
+    });
 
     onMount(() => {
         if (loggedIn && $user?.email) {
-            email = $user?.email;
+            form.setFieldValue('email', $user?.email);
         }
     });
 </script>
@@ -109,7 +74,7 @@
                             feedbackType = 'positive';
                         }}
                     >
-                        <Icon class="icon-thumb-up"></Icon>
+                        {@render HappyFace()}
                     </button>
                     <button
                         class="web-radio-button"
@@ -119,8 +84,7 @@
                             feedbackType = 'negative';
                         }}
                     >
-                        <!-- TODO: fix the icon name on pink -->
-                        <span class="icon-thumb-dowm"></span>
+                        {@render SadFace()}
                     </button>
                 </div>
             </div>
@@ -160,7 +124,12 @@
                         What did you {feedbackType === 'negative' ? 'dislike' : 'like'}?
                     </span>
                 </label>
-                <form.Field name="comment">
+                <form.Field
+                    name="comment"
+                    validators={{
+                        onChange: z.string().min(10, 'Please provide more details.')
+                    }}
+                >
                     {#snippet children(field)}
                         <textarea
                             class="web-input-text"
@@ -176,7 +145,12 @@
                 <label for="message" class="mt-2">
                     <span class="text-primary">Email</span>
                 </label>
-                <form.Field name="email">
+                <form.Field
+                    name="email"
+                    validators={{
+                        onChange: z.string().email()
+                    }}
+                >
                     {#snippet children(field)}
                         <input
                             name={field.name}
@@ -191,12 +165,12 @@
                     {/snippet}
                 </form.Field>
             </div>
-            {#if submitted}
+            {#if form.state.isSubmitted}
                 <p class="text-primary mt-4">
                     Your message has been sent successfully. We appreciate your feedback.
                 </p>
             {/if}
-            {#if error}
+            {#if form.state.errors.length}
                 <p class="text-primary mt-4">
                     There was an error submitting your feedback. Please try again later.
                 </p>
@@ -204,8 +178,38 @@
 
             <div class="mt-4 flex justify-end gap-2">
                 <Button variant="text" onclick={() => (showFeedback = false)}>Cancel</Button>
-                <Button type="submit" disabled={submitting || !email}>Submit</Button>
+                <Button type="submit" disabled={form.state.isSubmitting || !form.state.isDirty}
+                    >Submit</Button
+                >
             </div>
         </form>
     {/if}
 </section>
+
+{#snippet HappyFace()}
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        fill="var(--color-primary)"
+        viewBox="0 0 256 256"
+        class="p-2"
+        ><path
+            d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24ZM92,96a12,12,0,1,1-12,12A12,12,0,0,1,92,96Zm82.92,60c-10.29,17.79-27.39,28-46.92,28s-36.63-10.2-46.92-28a8,8,0,1,1,13.84-8c7.47,12.91,19.21,20,33.08,20s25.61-7.1,33.08-20a8,8,0,1,1,13.84,8ZM164,120a12,12,0,1,1,12-12A12,12,0,0,1,164,120Z"
+        ></path></svg
+    >
+{/snippet}
+
+{#snippet SadFace()}
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        fill="var(--color-primary)"
+        viewBox="0 0 256 256"
+        class="p-2"
+        ><path
+            d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24ZM92,96a12,12,0,1,1-12,12A12,12,0,0,1,92,96Zm80,86.92A8,8,0,0,1,161.08,180c-7.47-12.91-19.21-20-33.08-20s-25.61,7.1-33.08,20a8,8,0,1,1-13.84-8c10.29-17.79,27.39-28,46.92-28s36.63,10.2,46.92,28A8,8,0,0,1,172,182.92ZM164,120a12,12,0,1,1,12-12A12,12,0,0,1,164,120Z"
+        ></path></svg
+    >
+{/snippet}
