c7f0081a2bd5a7a8ad4094a4cbab014e54da8856
Jesse Winton <jesse@appwrite.io>
2025-08-21 13:27:59 -0400

    update inline info design

--- src/markdoc/tags/Info.svelte
+++ src/markdoc/tags/Info.svelte
@@ -9,8 +9,8 @@
     const { title, children }: Props = $props();
 </script>
 
-<div class="web-inline-info not-prose my-12!">
-    <span class="icon-info" aria-hidden="true"></span>
-    <h5 class="text-sub-body text-primary font-medium">{title}</h5>
-    {@render children?.()}
+<div class="bg-card border-smooth not-prose my-12! flex flex-col gap-3 rounded-xl border p-4">
+    <span class="icon-info" style:font-size="1.5rem" aria-hidden="true"></span>
+    <h5 class="text-description text-primary font-medium">{title}</h5>
+    <p class="text-paragraph-lg text-secondary">{@render children?.()}</p>
 </div>
