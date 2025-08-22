49dc216130ef8f7802bc52393b2f01d8534e4d38
Jesse Winton <jesse@appwrite.io>
2025-08-22 10:10:33 -0400

    tweaks

--- src/markdoc/tags/Tabs.svelte
+++ src/markdoc/tags/Tabs.svelte
@@ -44,7 +44,7 @@
 </script>
 
 <div
-    class="dark:bg-greyscale-850/90 not-prose mt-4 mb-8 flex flex-col gap-1 rounded-2xl border border-black/8 bg-white/90 px-6 pt-4 pb-6 outline-0 dark:border-white/10"
+    class="dark:bg-greyscale-850/90 not-prose mt-4 mb-8 flex flex-col gap-1 rounded-2xl border border-black/8 bg-white/90 p-5 outline-0 dark:border-white/10"
 >
     <div
         class="flex items-center gap-4 overflow-scroll [-ms-overflow-style:none] [scrollbar-width:none]"
@@ -54,7 +54,7 @@
                 <button
                     class={cn(
                         'shrink-0 rounded-t-[0.625rem] text-center hover:bg-white/4',
-                        'relative cursor-pointer bg-clip-padding px-1 py-[0.625rem] font-light outline-none',
+                        'relative cursor-pointer bg-clip-padding px-1 pt-1 pb-[0.625rem] font-light outline-none',
                         'after:relative after:top-1 after:bottom-0 after:block after:h-px after:transition-all',
                         {
                             'after:bg-(--color-primary)': tabs.value === id
