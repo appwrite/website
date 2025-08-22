c6fd8188cd3f8cb21f963a987fd3ac333462fd7d
Jesse Winton <jesse@appwrite.io>
2025-08-22 10:06:57 -0400

    update accordion item

--- src/markdoc/tags/Info.svelte
+++ src/markdoc/tags/Info.svelte
@@ -9,7 +9,7 @@
     const { title, children }: Props = $props();
 </script>
 
-<div class="bg-card border-smooth not-prose my-12! flex flex-col gap-3 rounded-xl border p-4">
+<div class="bg-card border-smooth not-prose my-12! flex flex-col gap-3 rounded-xl border p-5">
     <span class="icon-info" style:font-size="1.5rem" aria-hidden="true"></span>
     <h5 class="text-description text-primary font-medium">{title}</h5>
     <p class="text-paragraph-lg text-secondary">{@render children?.()}</p>
