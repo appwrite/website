e911e8dd4dd674033815279d46d7dfd2b2175892
Jesse Winton <jesse@appwrite.io>
2025-08-25 17:31:54 -0400

    rm

--- src/lib/meilisearch/index.ts
+++
@@ -1,6 +0,0 @@
-import { MeiliSearch } from 'meilisearch';
-
-export const meilisearchClient = new MeiliSearch({
-    host: 'https://search.appwrite.org',
-    apiKey: '10a5fea149bfaff21ef4d7cbe7f8a09d4fab404d6c3510279a365e065f8955a7'
-});
