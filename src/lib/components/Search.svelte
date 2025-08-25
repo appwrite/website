df73ebd8cf4d88df7469b3d0736b24d218df467d
Jesse Winton <jesse@appwrite.io>
2025-08-25 14:22:45 -0400

    Update Search.svelte

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -14,9 +14,14 @@
     let container: HTMLDivElement;
 
     const client = new MeiliSearch({
-        host: 'https://search.appwrite.org',
-        apiKey: '10a5fea149bfaff21ef4d7cbe7f8a09d4fab404d6c3510279a365e065f8955a7'
+        host: 'https://ms-4f2b8bcd5490-29219.fra.meilisearch.io',
+        apiKey: 'b347cbb673ff7c143dfb2dca1dda55c2e849e585'
     });
+
+    // const client = new MeiliSearch({
+    //     host: 'https://search.appwrite.org',
+    //     apiKey: '10a5fea149bfaff21ef4d7cbe7f8a09d4fab404d6c3510279a365e065f8955a7'
+    // });
     const index = client.index<Props>('website');
 
     type Props = {
