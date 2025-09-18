d35b5c7ef378f290805ec3820fa425efb061a4da
Laura Du Ry <123562732+LauraDuRy@users.noreply.github.com>
2025-09-18 17:41:58 +0200

    Update +page.svelte

--- src/routes/products/sites/offer-300/+page.svelte
+++ src/routes/products/sites/offer-300/+page.svelte
@@ -55,7 +55,7 @@
         <Testimonials class="mb-20" />
         <Scale
             testimonial={{
-                name: 'Ryan O’Conner',
+                name: 'Ryan O’Connor',
                 title: 'Founder',
                 company: 'K-Collect',
                 image: '/images/testimonials/ryan-oconner-testimonial.png'
