c11c15a5cc705d29a4812c3de8f9a935e43f8919
Torsten Dittmann <torsten.dittmann@googlemail.com>
2025-10-27 12:26:42 +0400

    chore: clean up files

--- eslint.config.js
+++ eslint.config.js
@@ -22,18 +22,6 @@ export default defineConfig(
             globals: { ...globals.browser, ...globals.node }
         },
         rules: {
-            // TODO: remove them one by one
-            '@typescript-eslint/no-unused-vars': 'off',
-            '@typescript-eslint/no-duplicate-enum-values': 'off',
-            '@typescript-eslint/no-empty-object-type': 'off',
-            '@typescript-eslint/no-unused-expressions': 'off',
-            'svelte/infinite-reactive-loop': 'off',
-            'svelte/require-each-key': 'off',
-            'svelte/no-immutable-reactive-statements': 'off',
-            'svelte/no-at-html-tags': 'off',
-            'svelte/no-useless-mustaches': 'off',
-            'svelte/no-reactive-reassign': 'off',
-            'svelte/no-reactive-literals': 'off',
             // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
             // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
             'no-undef': 'off'
