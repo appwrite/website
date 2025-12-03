6625a69b1dc80a5f0e1b8daed48f8e461a9b885a
Harsh Mahajan <harshmahajan2345@gmail.com>
2025-12-03 18:12:43 +0000

    first draft

---
+++ src/markdoc/tags/Video_Card.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { VideoCard } from '$lib/components';
+
+    interface Props {
+        href: string;
+        title: string;
+    }
+
+    const { href, title }: Props = $props();
+</script>
+
+<VideoCard {href} {title} />
