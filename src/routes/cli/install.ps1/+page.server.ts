91600567ba98ae32bc492abd2bddf379d585b1de
Madhav Gupta <madhav.gupta7674@gmail.com>
2025-12-04 16:03:55 +0530

    fix: run pnpm format

--- src/routes/cli/install.ps1/+page.server.ts
+++ src/routes/cli/install.ps1/+page.server.ts
@@ -1,5 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 
 export function load() {
-    redirect(301, 'https://raw.githubusercontent.com/appwrite/sdk-for-cli/refs/heads/master/install.ps1');
+    redirect(
+        301,
+        'https://raw.githubusercontent.com/appwrite/sdk-for-cli/refs/heads/master/install.ps1'
+    );
 }
