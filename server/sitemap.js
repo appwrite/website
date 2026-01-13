e7442d53a745bc718ee21e1db757e35d145c512c
Harsh Mahajan <harshmahajan2345@gmail.com>
2026-01-13 20:15:30 +0000

    fix: align sitemap XML namespaces with official spec

--- server/sitemap.js
+++ server/sitemap.js
@@ -72,7 +72,7 @@ export async function sitemaps() {
 
     const sitemapIndex = `
     <?xml version="1.0" encoding="UTF-8"?>
-    <sitemapindex xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+    <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         ${sitemapIndexOrder
             .map(
                 (name) => `
@@ -124,12 +124,12 @@ function writeSitemap(filename, routes, dir) {
     const body = `
 <?xml version="1.0" encoding="UTF-8" ?>
 <urlset
-  xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-  xmlns:xhtml="https://www.w3.org/1999/xhtml"
-  xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
-  xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
-  xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
-  xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:xhtml="http://www.w3.org/1999/xhtml"
+  xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"
+  xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
+  xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+  xmlns:video="http://www.google.com/schemas/sitemap-video/1.1"
 >
 ${routes
     .map((route) => {
